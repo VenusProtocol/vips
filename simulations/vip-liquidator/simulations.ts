baf869dc32cde47914b9393e1ef24ddfcea9a365
aditya-routv <aditya@venus.io>
2023-04-06 13:16:22 +0530

    test: add liquidator VIP permissions test

--- simulations/vip-liquidator/simulations.ts
+++ simulations/vip-liquidator/simulations.ts
@@ -1,6 +1,74 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { initMainnetUser } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
 import { vipLiquidator } from "../../vips/vip-liquidator";
+import ACCESS_CONTROL_ABI from "./abi/accessControlmanager.json";
+
+const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
+const LIQUIDATOR = "0x0870793286aada55d39ce7f82fb2766e8004cf43";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
 
 forking(27083193, () => {
+  let accessControlManager: ethers.Contract;
+  const provider = ethers.provider;
+  let impersonatedLiquidator: any;
+
+  before(async () => {
+    accessControlManager = new ethers.Contract(ACM, ACCESS_CONTROL_ABI, provider);
+    impersonatedLiquidator = await initMainnetUser(LIQUIDATOR, ethers.utils.parseEther("1"));
+  });
+
   testVip("VIP-Liquidator Liquidator Update", vipLiquidator());
+
+  describe("Post-VIP behavior", async () => {
+    it("Permissions restrictLiquidation", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "restrictLiquidation(address)"),
+      ).equals(true);
+    });
+
+    it("Permissions addToAllowlist", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "addToAllowlist(address,address)"),
+      ).equals(true);
+    });
+
+    it("Permissions unrestrictLiquidation", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "unrestrictLiquidation(address)"),
+      ).equals(true);
+    });
+
+    it("Permissions addToAllowlist", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "addToAllowlist(address,address)"),
+      ).equals(true);
+    });
+
+    it("Permissions removeFromAllowlist", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "removeFromAllowlist(address,address)"),
+      ).equals(true);
+    });
+
+    it("Permissions setTreasuryPercent(uint256)", async () => {
+      expect(
+        await accessControlManager
+          .connect(impersonatedLiquidator)
+          .isAllowedToCall(NORMAL_TIMELOCK, "setTreasuryPercent(uint256)"),
+      ).equals(true);
+    });
+  });
 });
