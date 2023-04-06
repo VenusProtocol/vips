88adcba40f9599a1526c5d1b49fc390f1b6dc9bb
Narayan Prusty <arsenal.narayan@gmail.com>
2023-04-06 17:30:14 +0100

    wip - added simulation for new oracle

---
+++ simulations/vip-106/simulations.ts
@@ -0,0 +1,43 @@
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { expectEvents, setMaxStalePeriodInOracle } from "../../src/utils";
+import { forking, testVip } from "../../src/vip-framework";
+import { vip106 } from "../../vips/vip-106";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import CHAINLINK_ORACLE_ABI from "./abi/chainlinkOracle.json";
+import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
+import PRICE_ORACLE_ABI from "./abi/priceOracle.json";
+
+const COMPTROLLER = "0xfd36e2c2a6789db23113685031d7f16329158384";
+const CHAINLINK_ORACLE = "0x672Ba3b2f5d9c36F36309BA913D708C4a5a25eb0";
+const RESILIENT_ORACLE = "0xe40C7548bFB764C48f9A037753A9F08c5B3Fde15";
+const PRICE_ORACLE = "0x7FabdD617200C9CB4dcf3dd2C41273e60552068A";
+
+const vUSDC = "0xeca88125a5adbe82614ffc12d0db554e2e2867c8";
+
+forking(27116217, () => {
+  let comptroller: ethers.Contract;
+  let chainlinkOracle: ethers.Contract;
+  let resilientOracle: ethers.Contract;
+  let priceOracle: ethers.Contract;
+  const provider = ethers.provider;
+
+  before(async () => {
+    comptroller = new ethers.Contract(COMPTROLLER, COMPTROLLER_ABI, provider);
+    chainlinkOracle = new ethers.Contract(CHAINLINK_ORACLE, CHAINLINK_ORACLE_ABI, provider);
+    resilientOracle = new ethers.Contract(RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
+    priceOracle = new ethers.Contract(PRICE_ORACLE, PRICE_ORACLE_ABI, provider)
+    await setMaxStalePeriodInOracle(COMPTROLLER);
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("valid USDC price", async () => {
+      const price = (await priceOracle.getUnderlyingPrice(vUSDC));
+      expect(price).to.equal(parseUnits("0.999769", 18));
+    });
+  });
+
+  testVip("VIP-106 Change Oracle and Configure Resilient Oracle", vip106());
+});
---
+++ vips/vip-106.ts
@@ -0,0 +1,67 @@
+import { ProposalType } from "../src/types";
+import { makeProposal } from "../src/utils";
+
+const COMPTROLLER = "0xfd36e2c2a6789db23113685031d7f16329158384";
+const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
+const CHAINLINK_ORACLE = "0x672Ba3b2f5d9c36F36309BA913D708C4a5a25eb0";
+const RESILIENT_ORACLE = "0xe40C7548bFB764C48f9A037753A9F08c5B3Fde15";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+
+const USDC_CHAINLINK_FEED = "0x51597f405303C4377E36123cBc172b13269EA163"
+const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"
+
+const MAX_STALE_PERIOD = 60 * 60; // 1 hour
+
+export const vip106 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-106 Resilient Oracle",
+    description: `
+    Configure Price Feeds for Existing Markets
+    Configure Oracle Address in Comptroller
+    `,
+    forDescription: "I agree that Venus Protocol should proceed with this recommendation",
+    againstDescription: "I do not think that Venus Protocol should proceed with this recommendation",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with this recommendation or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [CHAINLINK_ORACLE, "setTokenConfig(TokenConfig)", NORMAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RESILIENT_ORACLE, "setTokenConfig(TokenConfig)", NORMAL_TIMELOCK],
+      },
+      {
+        target: CHAINLINK_ORACLE,
+        signature: "setTokenConfig(TokenConfig)",
+        params: [{
+          asset: USDC,
+          feed: USDC_CHAINLINK_FEED,
+          maxStalePeriod: MAX_STALE_PERIOD
+        }],
+      },
+      {
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig(TokenConfig)",
+        params: [{
+          asset: USDC,
+          oracles: [CHAINLINK_ORACLE, false, false],
+          enableFlagsForOracles: [true,false,false]
+        }],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "_setPriceOracle(address)",
+        params: [RESILIENT_ORACLE],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
