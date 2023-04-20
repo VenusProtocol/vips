af04db477527359b6958299502fe14f4332aa634
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-04-20 15:25:19 +0530

    feat: add support to extract data for bscscan to propose VIP

--- scripts/createProposal.ts
+++ scripts/createProposal.ts
@@ -3,7 +3,7 @@ import Ajv from "ajv";
 import fs from "fs/promises";
 
 import { loadProposal, proposeVIP } from "../src/transactions";
-import { proposalSchema } from "../src/utils";
+import { getCalldatas, proposalSchema } from "../src/utils";
 
 const readline = require("readline-sync");
 
@@ -16,7 +16,7 @@ let transactionType: string;
 function processInputs(): Promise<void> {
   return new Promise(resolve => {
     vipNumber = readline.question("Number of the VIP to propose => ");
-    transactionType = readline.question("Type of the proposal txBuilder/venusApp => ");
+    transactionType = readline.question("Type of the proposal txBuilder/venusApp/bsc => ");
     governorAddress = readline.question("Address of the governance contract (optional, press enter to skip) => ");
     if (!governorAddress) {
       governorAddress = null;
@@ -54,13 +54,29 @@ const processVenusAppProposal = async () => {
   }
 };
 
+const processBscProposal = async () => {
+  const proposal = await loadProposal(vipNumber);
+  const data = {
+    targets: proposal.targets,
+    signatures: proposal.signatures,
+    values: proposal.values,
+    calldata: getCalldatas(proposal),
+    description: processJson(proposal.meta),
+    type: proposal.type,
+  };
+
+  return processJson(data);
+};
+
 const createProposal = async () => {
   await processInputs();
   let result;
   if (transactionType === "txBuilder") {
     result = await processTxBuilder();
-  } else {
+  } else if (transactionType === "venusApp") {
     result = await processVenusAppProposal();
+  } else {
+    result = await processBscProposal();
   }
   console.log(result);
   await fs.writeFile(`${transactionType}.json`, result);
