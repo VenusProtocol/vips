bcdc54135bdf0faa88380c188dfdc1735bc25e8e
Kirill Kuvshinov <kkirka@gmail.com>
2023-05-12 21:11:14 +0300

    test: replace borrower to cover the debt for

--- simulations/vip-117/simulations.ts
+++ simulations/vip-117/simulations.ts
@@ -8,7 +8,7 @@ import { vip117 } from "../../vips/vip-117";
 import VBEP20_ABI from "./abi/VBep20Abi.json";
 
 const vETH = "0xf508fcd89b8bd15579dc79a6827cb4686a3592c8";
-const BORROWER = "0x24e77e5b74b30b026e9996e4bc3329c881e24968";
+const BORROWER = "0x7589dd3355dae848fdbf75044a3495351655cb1a";
 
 forking(28140349, () => {
   testVip("VIP-117 Repay ETH debt on behalf debt", vip117());
@@ -31,7 +31,7 @@ forking(28140349, () => {
     it("Should decrese Borrow Balance Stored", async () => {
       const currentBalance = await vToken.callStatic.borrowBalanceCurrent(BORROWER);
       const delta = prevBalance.sub(currentBalance);
-      expect(delta).to.be.closeTo(parseUnits("1437.5", 18), parseUnits("0.00005", 18));
+      expect(delta).to.be.closeTo(parseUnits("1437.5", 18), parseUnits("0.00007", 18));
     });
   });
 });
--- vips/vip-117.ts
+++ vips/vip-117.ts
@@ -7,7 +7,7 @@ const vETH = "0xf508fcd89b8bd15579dc79a6827cb4686a3592c8";
 const ETH = "0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
-const BORROWER = "0x24e77e5b74b30b026e9996e4bc3329c881e24968";
+const BORROWER = "0x7589dd3355dae848fdbf75044a3495351655cb1a";
 const repayAmount = parseUnits("1437.5", 18);
 
 export const vip117 = () => {
@@ -16,7 +16,7 @@ export const vip117 = () => {
     title: "VIP-117 Repay ETH debt on behalf debt",
     description: `
         transfer 1,437.5 ETH from the treasury to the Normal timelock contract
-        repay 1,437.5 ETH debt on behalf of the account 0x24e77e5b74b30b026e9996e4bc3329c881e24968 from the Normal timelock contract`,
+        repay 1,437.5 ETH debt on behalf of the account 0x7589dd3355dae848fdbf75044a3495351655cb1a from the Normal timelock contract`,
     forDescription: "I agree that Venus Protocol should proceed with the Repay ETH debt on behalf debt debt",
     againstDescription: "I do not think that Venus Protocol should proceed with the Repay ETH debt on behalf debt debt",
     abstainDescription:
