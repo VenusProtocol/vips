e72d3f90ae40a76f7c02d2448f47b64cf3ab5ff2
Narayan Prusty <arsenal.narayan@gmail.com>
2023-06-09 16:07:08 +0100

    feat: set max stale period seperate for each asset

--- vips/vip-124.ts
+++ vips/vip-124.ts
@@ -9,11 +9,13 @@ interface ChainlinkAssetConfig {
   name: string;
   address: string;
   feed: string;
+  stalePeriod: number;
 }
 
 interface BinanceAssetConfig {
   name: string;
   address: string;
+  stalePeriod: number;
 }
 
 const CHAINLINK_ASSETS: ChainlinkAssetConfig[] = [
@@ -21,36 +23,43 @@ const CHAINLINK_ASSETS: ChainlinkAssetConfig[] = [
     name: "ALPACA",
     address: "0x8f0528ce5ef7b51152a59745befdd91d97091d2f",
     feed: "0xe0073b60833249ffd1bb2af809112c2fbf221DF6",
+    stalePeriod: 60 * 60 * 24.5
   },
   {
     name: "BIFI",
     address: "0xCa3F508B8e4Dd382eE878A314789373D80A5190A",
     feed: "0xaB827b69daCd586A37E80A7d552a4395d576e645",
+    stalePeriod: 60 * 60 * 24.5
   },
   {
     name: "BNBx",
     address: "0x1bdd3cf7f79cfb8edbb955f20ad99211551ba275",
     feed: "0xc4429B539397a3166eF3ef132c29e34715a3ABb4",
+    stalePeriod: 60 * 25
   },
   {
     name: "BSW",
     address: "0x965f527d9159dce6288a2219db51fc6eef120dd1",
     feed: "0x08e70777b982a58d23d05e3d7714f44837c06a21",
+    stalePeriod: 60 * 25
   },
   {
     name: "WBNB",
     address: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
     feed: "0x0567f2323251f0aab15c8dfb1967e4e8a7d42aee",
+    stalePeriod: 60 * 5
   },
   {
     name: "WIN",
     address: "0xaeF0d72a118ce24feE3cD1d43d383897D05B4e99",
     feed: "0x9e7377e194e41d63795907c92c3eb351a2eb0233",
+    stalePeriod: 60 * 25
   },
   {
     name: "WOO",
     address: "0x4691937a7508860f876c9c0a2a617e7d9e945d4b",
     feed: "0x02bfe714e78e2ad1bb1c2bee93ec8dc5423b66d4",
+    stalePeriod: 60 * 60 * 24.5
   },
 ];
 
@@ -58,42 +67,49 @@ const BINANCE_ASSETS: BinanceAssetConfig[] = [
   {
     name: "ANKR",
     address: "0xf307910A4c7bbc79691fD374889b36d8531B08e3",
+    stalePeriod: 60 * 25
   },
   {
     name: "ankrBNB",
     address: "0x52F24a5e03aee338Da5fd9Df68D2b6FAe1178827",
+    stalePeriod: 60 * 25
   },
   {
     name: "BTT",
     address: "0x352Cb5E19b12FC216548a2677bD0fce83BaE434B",
+    stalePeriod: 60 * 25
   },
   {
     name: "FLOKI",
     address: "0xfb5B838b6cfEEdC2873aB27866079AC55363D37E",
+    stalePeriod: 60 * 25
   },
   {
     name: "HAY",
     address: "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+    stalePeriod: 60 * 25
   },
   {
     name: "NFT",
     address: "0x20eE7B720f4E4c4FFcB00C4065cdae55271aECCa",
+    stalePeriod: 60 * 25
   },
   {
     name: "RACA",
     address: "0x12BB890508c125661E03b09EC06E404bc9289040",
+    stalePeriod: 60 * 25
   },
   {
     name: "stkBNB",
     address: "0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16",
+    stalePeriod: 60 * 25
   },
   {
     name: "USDD",
     address: "0xd17479997F34dd9156Deef8F95A52D81D265be9c",
+    stalePeriod: 60 * 25
   },
 ];
-
-const MAX_STALE_PERIOD = 60 * 60 * 24; // 24 hours
 
 export const vip124 = () => {
   const meta = {
