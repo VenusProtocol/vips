4cf8fbc31de0a0a69e30782df10d7b70649a227e
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-06-16 18:53:42 +0200

    feat: add the final description of the VIP 128

--- vips/vip-128/vip-128.ts
+++ vips/vip-128/vip-128.ts
@@ -20,25 +20,58 @@ const MAX_STALE_PERIOD = 60 * 25;
 export const vip128 = (maxStalePeriod?: number) => {
   const meta = {
     version: "v2",
-    title: "VIP-128 Add WBETH Market",
+    title: "VIP-128 Add WBETH market to the Venus Core pool",
     description: `
-    VIP
-      Risk parameters suggested by Quants:
-      Supply cap: 300 (full tokens)
-      Borrow cap: 200 (full tokens)
-      Collateral factor: 50%
-      Reserve factor: 20%
-      Interest rate: Base0bps_Slope1000bps_Jump20000bps_Kink7500bps
-      Initial supply:
-      amount: 5499943000000000000 (already available in the VTreasury)
-      vToken receiver: 0x7d3217feb6f310f7e7b7c8ee130db59dcad1dd45
-    XVS rewards:
-      borrowers: 596440972222220
-      suppliers: 596440972222220
+As part ot the BETH token deprecation and rebranding to WBETH as previously addressed in [VIP-111](https://app.venus.io/governance/proposal/111), if passed this VIP will perform the following actions: 
+
+* Add a new market in the Core pool for the token WBETH (wrapped Binance ETH).
+* Upgrade the BinanceOracle contract to support the new market
+
+**Description**
+
+A new market for WBETH will be added to the Core pool. 
+
+The risk parameters of the new market are:
+
+* Collateral factor: 50%
+* Supply cap: 300 WBETH
+* Borrow cap: 200 WBETH
+* Reserver factor: 20%
+* XVS Distributions: 34.34/Day
+
+These parameters will be monitored and adjusted if needed in the future.
+
+Moreover, in order to support the new market, it was needed to upgrade the implementation of the BinanceOracle contract. This upgrade translates the wBETH symbol of the token to WBETH, as expected by the Binance Oracle system.
+
+**Security and additional considerations**
+
+We applied the following security procedures for this upgrade:
+
+* **Add WBETH in a simulation environment**: we added the new market to the Core pool, and we verified everything works as expected
+* **Deployment on testnet**: the same market for WBETH has been deployed to testnet, and used in the Venus Protocol testnet deployment
+* **Security review**: Peckshield and Certik reviewed the change done in the BinanceOracle contract
+
+5.499943 WBETH will be initially provided, as bootstrap liquidity for this market.This mitigates potential attacks on empty pools previously observed in other Lending protocols. The vTokens minted with this bootstrap liquidity are sent to Binance Simple Earn, the liquidity provider.
+
+**Deployed contracts on main net**
+
+* vWBETH (new market): [0x6CFdEc747f37DAf3b87a35a1D9c8AD3063A1A8A0](https://bscscan.com/address/0x6CFdEc747f37DAf3b87a35a1D9c8AD3063A1A8A0)
+* BinanceOracle (new implementation): [0xe38AbE42948ef249E84f4e935e4f56483C1EE3B9](https://bscscan.com/address/0xe38AbE42948ef249E84f4e935e4f56483C1EE3B9)
+
+**Fork tests (simulations)**
+
+* https://github.com/VenusProtocol/vips/pull/26 
+
+**References**
+
+* [Repository](https://github.com/VenusProtocol/venus-protocol)
+* [WBETH token in BNB chain](https://bscscan.com/address/0xa2E3356610840701BDf5611a53974510Ae27E2e1)
+* vWBETH (new market) on testnet: [0x35566ED3AF9E537Be487C98b1811cDf95ad0C32b](https://testnet.bscscan.com/address/0x35566ED3AF9E537Be487C98b1811cDf95ad0C32b)
+* [Binance Introduces Wrapped Beacon ETH (WBETH) on ETH Staking](https://www.binance.com/en/support/announcement/binance-introduces-wrapped-beacon-eth-wbeth-on-eth-staking-a1197f34d832445db41654ad01f56b4d)
     `,
-    forDescription: "I agree that Venus Protocol should proceed with the Add WBETH Market",
-    againstDescription: "I do not think that Venus Protocol should proceed with the Add WBETH Market",
-    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with the Add WBETH Market or not",
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
   };
 
   return makeProposal(
