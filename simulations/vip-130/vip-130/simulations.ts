aa467890d2809caf5c28496bfa7cc48666cfbac4
defcon022 <manmeetparmar22@gmail.com>
2023-06-22 16:40:26 +0530

    feat: acceptAdmin for xvs store

--- simulations/vip-130/vip-130/simulations.ts
+++ simulations/vip-130/vip-130/simulations.ts
@@ -3,22 +3,34 @@ import { ethers } from "hardhat";
 
 import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
-import { swapRouterCorePool, vip130 } from "../../../vips/vip-130/vip-130";
+import { XVSStore, swapRouterCorePool, vip130 } from "../../../vips/vip-130/vip-130";
+import XVS_STORE_ABI from "./abi/XVSStore.json";
 import SWAP_ROUTER_ABI from "./abi/swapRouter.json";
 
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
 
-forking(29284246, () => {
+forking(29321964, async () => {
   testVip("VIP-130 Swap router accept ownership", vip130(), {
     callbackAfterExecution: async (txResponse: any) => {
-      await expectEvents(txResponse, [SWAP_ROUTER_ABI], ["OwnershipTransferred"], [1]);
+      await expectEvents(
+        txResponse,
+        [SWAP_ROUTER_ABI, XVS_STORE_ABI],
+        ["OwnershipTransferred", "NewPendingAdmin", "AdminTransferred"],
+        [1, 1, 1],
+      );
     },
   });
 
   describe("Check owner address after accepting owership", () => {
-    it("Check address", async () => {
+    it("Check swap router owner address", async () => {
       const router = new ethers.Contract(swapRouterCorePool, SWAP_ROUTER_ABI, ethers.provider);
       const ownerAddress = await router.owner();
+      expect(ownerAddress).equals(NORMAL_TIMELOCK);
+    });
+
+    it("Check XVS store owner address", async () => {
+      const store = new ethers.Contract(XVSStore, XVS_STORE_ABI, ethers.provider);
+      const ownerAddress = await store.admin();
       expect(ownerAddress).equals(NORMAL_TIMELOCK);
     });
   });
--- vips/vip-130/vip-130.ts
+++ vips/vip-130/vip-130.ts
@@ -6,17 +6,20 @@ import { makeProposal } from "../../src/utils";
 
 // Addresses of all the swapRouters
 export const swapRouterCorePool = "0x8938E6dA30b59c1E27d5f70a94688A89F7c815a4";
+export const XVSStore = "0x1e25CF968f12850003Db17E0Dba32108509C4359";
 
 export const vip130 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-130 Accepting ownership for swap router",
+    title: "VIP-130 Accepting ownership for swap router and become admin of XVS store",
     description: `Swap router is deployed for core pool, and the ownership of these router is transferred to Timelock
-        and this VIP is to accept the ownership of the swap router.`,
-    forDescription: "I agree that Venus Protocol should proceed with accepting ownership for swap router",
-    againstDescription: "I do not think that Venus Protocol should proceed with accepting ownership for swap router",
+        and this VIP is to accept the ownership of the swap router and become admin of XVS Store.`,
+    forDescription:
+      "I agree that Venus Protocol should proceed with accepting ownership for swap router and become admin of XVS Store",
+    againstDescription:
+      "I do not think that Venus Protocol should proceed with accepting ownership for swap router and become admin of XVS Store",
     abstainDescription:
-      "I am indifferent to whether Venus Protocol proceeds with accepting ownership for swap router or not",
+      "I am indifferent to whether Venus Protocol proceeds with accepting ownership for swap router and become admin of XVS Store or not",
   };
 
   return makeProposal(
@@ -24,6 +27,11 @@ export const vip130 = () => {
       {
         target: swapRouterCorePool,
         signature: "acceptOwnership()",
+        params: [],
+      },
+      {
+        target: XVSStore,
+        signature: "acceptAdmin()",
         params: [],
       },
     ],
