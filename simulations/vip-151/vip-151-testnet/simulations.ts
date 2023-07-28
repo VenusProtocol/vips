74410edc29829c7a1f3a8b72c58c4b46af008030
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-07-28 12:08:46 +0530

    feat: add testnet liquidator vip

--- simulations/vip-151/vip-151-testnet/simulations.ts
+++ simulations/vip-151/vip-151-testnet/simulations.ts
@@ -3,18 +3,21 @@ import { ethers } from "hardhat";
 
 import { expectEvents, initMainnetUser } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
-import { vip151 } from "../../../vips/vip-151/vip-151";
+import { vip151Testnet } from "../../../vips/vip-151/vip-151-testnet";
 import ACCESS_CONTROL_ABI from "./abi/accessControlmanager.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
 import LIQUIDATOR_ABI from "./abi/liquidatorAbi.json";
 
-const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
-const LIQUIDATOR = "0x0870793286aada55d39ce7f82fb2766e8004cf43";
-const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
-const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
-const CRITICAL_TIMELOCK = "0x213c446ec11e45b15a6E29C1C1b402B8897f606d";
+const LIQUIDATOR = "0x55AEABa76ecf144031Ef64E222166eb28Cb4865F";
+const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
+const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
+const COMPTROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
 
-forking(30349950, () => {
+forking(31942719, () => {
   let accessControlManager: ethers.Contract;
+  let comptroller: ethers.Contract;
   let liquidator: ethers.Contract;
   const provider = ethers.provider;
   let impersonatedLiquidator: any;
@@ -23,15 +26,22 @@ forking(30349950, () => {
     accessControlManager = new ethers.Contract(ACM, ACCESS_CONTROL_ABI, provider);
     impersonatedLiquidator = await initMainnetUser(LIQUIDATOR, ethers.utils.parseEther("1"));
     liquidator = new ethers.Contract(LIQUIDATOR, LIQUIDATOR_ABI, provider);
+    comptroller = new ethers.Contract(COMPTROLLER, COMPTROLLER_ABI, provider);
   });
 
-  testVip("VIP-Liquidator Liquidator Update", vip151(), {
+  testVip("VIP-Liquidator Liquidator Update", vip151Testnet(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(
         txResponse,
-        [ACCESS_CONTROL_ABI, LIQUIDATOR_ABI],
-        ["PermissionGranted", "NewPendingRedeemChunkLength", "ForceVAILiquidationResumed"],
-        [17, 1, 1],
+        [ACCESS_CONTROL_ABI, LIQUIDATOR_ABI, COMPTROLLER_ABI],
+        [
+          "PermissionGranted",
+          "OwnershipTransferred",
+          "NewPendingRedeemChunkLength",
+          "ForceVAILiquidationResumed",
+          "NewLiquidatorContract",
+        ],
+        [17, 1, 1, 1, 1],
       );
     },
   });
@@ -159,6 +169,10 @@ forking(30349950, () => {
 
     it("Should set forceVAILiquidate = true", async () => {
       expect(await liquidator.forceVAILiquidate()).equals(true);
+    });
+
+    it("Should set liquidator contract", async () => {
+      expect(await comptroller.liquidatorContract()).equals(LIQUIDATOR);
     });
   });
 });
