d4f89539f13aeb91c79c7fdf8702590f3d0d86e4
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-08-02 12:59:23 +0530

    test: add tests for force vaid debt post vip

--- simulations/vip-151/vip-151/simulations.ts
+++ simulations/vip-151/vip-151/simulations.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-import { expectEvents, initMainnetUser } from "../../../src/utils";
+import { expectEvents, initMainnetUser, setMaxStaleCoreAssets } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip151 } from "../../../vips/vip-151/vip-151";
 import IERC20_ABI from "./abi/IERC20UpgradableAbi.json";
@@ -11,6 +11,7 @@ import ACCESS_CONTROL_ABI from "./abi/accessControlmanager.json";
 import CHAINLINK_ABI from "./abi/chainlinkOracle.json";
 import COMPTROLLER_ABI from "./abi/comptroller.json";
 import LIQUIDATOR_ABI from "./abi/liquidatorAbi.json";
+import VAI_CONTROLLER_ABI from "./abi/vaiControllerAbi.json";
 
 const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
 const LIQUIDATOR = "0x0870793286aada55d39ce7f82fb2766e8004cf43";
@@ -19,7 +20,7 @@ const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
 const CRITICAL_TIMELOCK = "0x213c446ec11e45b15a6E29C1C1b402B8897f606d";
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 
-forking(30488755, () => {
+forking(30478951, () => {
   let accessControlManager: ethers.Contract;
   let liquidator: ethers.Contract;
   const provider = ethers.provider;
@@ -36,12 +37,12 @@ forking(30488755, () => {
     liquidator = new ethers.Contract(LIQUIDATOR, LIQUIDATOR_ABI, provider);
   });
 
-  testVip("VIP-Liquidator Liquidator Update", vip151(createInitializeData()), {
+  testVip("VIP-Liquidator Update", vip151(createInitializeData()), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(
         txResponse,
         [ACCESS_CONTROL_ABI, LIQUIDATOR_ABI],
-        ["PermissionGranted", "NewPendingRedeemChunkLength", "ForceVAILiquidationResumed"],
+        ["RoleGranted", "NewPendingRedeemChunkLength", "ForceVAILiquidationResumed"],
         [17, 1, 1],
       );
     },
@@ -179,13 +180,15 @@ forking(30488755, () => {
     });
   });
 
-  describe("Liquidations of TUSD", async () => {
+  describe("Reduce Reserves and Force VAI Debt Tests", async () => {
     let tusd: ethers.Contract;
     let liquidatorSigner: SignerWithAddress;
     let oracle: ethers.Contract;
     let impersonatedTimelock: any;
     let comptroller: ethers.Contract;
     let usdc: ethers.Contract;
+    let vaiController: ethers.Contract;
+    let vai: ethers.Contract;
 
     const USER = "0xbdc8f6ad3a729d8c1abe908939668ce3f92886a0";
     const LIQUIDATOR_USER = "0xf977814e90da44bfa03b6295a0616a897441acec";
@@ -196,6 +199,11 @@ forking(30488755, () => {
     const UNITROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
     const VUSDC = "0xecA88125a5ADbe82614ffC12D0DB554E2e2867C8";
     const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
+    const VBNB = "0xA07c5b74C9B40447a954e1466938b865b6BBea36";
+    const BNB = "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB";
+    const VAI_CONTROLLER = "0x004065D34C6b18cE4370ced1CeBDE94865DbFAFE";
+    const VAI = "0x4BD17003473389A42DAF6a0a729f6Fdb328BbBd7";
+    const VAI_HOLDER = "0x29aa70f8f3f2aa241b0ba9eaa744c97808d032c9";
 
     before(async () => {
       impersonatedTimelock = await initMainnetUser(NORMAL_TIMELOCK, ethers.utils.parseEther("2"));
@@ -204,6 +212,9 @@ forking(30488755, () => {
       comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_ABI, provider);
       tusd = new ethers.Contract(TUSD, IERC20_ABI, provider);
       usdc = new ethers.Contract(USDC, IERC20_ABI, provider);
+      vaiController = new ethers.Contract(VAI_CONTROLLER, VAI_CONTROLLER_ABI, provider);
+      vai = new ethers.Contract(VAI, IERC20_ABI, provider);
+      await setMaxStaleCoreAssets(CHAINLINK, NORMAL_TIMELOCK);
     });
 
     it("Tusd Liquidation and reduce liquidation reserves", async () => {
@@ -227,6 +238,39 @@ forking(30488755, () => {
       // As redeem was paused
       expect(protocolBalAfter).equals(protocolBalBefore);
       expect(await liquidator.pendingRedeem(0)).equals(VUSDC);
+    });
+
+    it("Should not able to liquidate any token when VAI debt is greater than minLiquidatableVAI", async () => {
+      const borowedToken = "0x95c78222B3D6e262426483D42CfA53685A67Ab9D"; // VBUSD
+      const borrower = "0xcdc4757ff570dcd6933f8d384293789907db6791";
+      await expect(
+        liquidator.connect(liquidatorSigner).liquidateBorrow(borowedToken, borrower, 10000, VUSDC),
+      ).to.be.revertedWithCustomError(liquidator, "VAIDebtTooHigh");
+    });
+
+    it("Should be able to liquidate any token when VAI debt is less than minLiquidatableVAI", async () => {
+      const borrower = "0x6B7a803BB85C7D1F67470C50358d11902d3169e0";
+      await expect(
+        liquidator.connect(liquidatorSigner).liquidateBorrow(VBNB, borrower, 1000, VBNB, { value: 1000 }),
+      ).to.be.emit(liquidator, "LiquidateBorrowedTokens");
+    });
+
+    it("Should able to liquidate VAI if debt > minLiquidatableVAI", async () => {
+      const borrower = "0x016699fb47d0816d71ebed2f24473d57c762af51";
+      const vaiHolder = await initMainnetUser(VAI_HOLDER, ethers.utils.parseEther("2"));
+      await vai.connect(vaiHolder).approve(LIQUIDATOR, parseUnits("60", 18));
+
+      // Manipulate price to decrease liquidity and introduce shortfall
+      await oracle.connect(impersonatedTimelock).setDirectPrice(BNB, parseUnits("100", 18));
+
+      const minLiquidatableVAI = await liquidator.minLiquidatableVAI();
+      const vaiDebt = await vaiController.getVAIRepayAmount(borrower);
+      expect(vaiDebt).to.greaterThan(minLiquidatableVAI);
+      await expect(
+        liquidator.connect(vaiHolder).liquidateBorrow(VAI_CONTROLLER, borrower, parseUnits("60", 18), VBNB),
+      ).to.be.emit(liquidator, "LiquidateBorrowedTokens");
+      const vaiDebtAfter = await vaiController.getVAIRepayAmount(borrower);
+      expect(vaiDebtAfter).lessThan(vaiDebt);
     });
   });
 });
