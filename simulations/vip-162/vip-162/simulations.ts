6359200f5dc37676f68c7b8bce96297e792b977c
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-08-25 16:13:03 +0530

    feat: add TWT market to DeFi Pool

---
+++ simulations/vip-162/vip-162/simulations.ts
@@ -0,0 +1,301 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumberish } from "ethers";
+import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { expectEvents, initMainnetUser, setMaxStalePeriodInBinanceOracle } from "../../../src/utils";
+import { forking, testVip } from "../../../src/vip-framework";
+import { vip162 } from "../../../vips/vip-162/vip-162";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import ERC20_ABI from "./abi/erc20.json";
+import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
+import RATE_MODEL_ABI from "./abi/rateModel.json";
+import REWARD_DISTRIBUTOR_ABI from "./abi/rewardsDistributor.json";
+import TREASURY_ABI from "./abi/treasury.json";
+import VTOKEN_ABI from "./abi/vToken.json";
+
+const USDD = "0xd17479997F34dd9156Deef8F95A52D81D265be9c";
+const vUSDD_DeFi = "0xA615467caE6B9E0bb98BC04B4411d9296fd1dFa0";
+const COMPTROLLER_DeFi = "0x3344417c9360b963ca93A4e8305361AEde340Ab9";
+const BINANCE_ORACLE = "0x594810b741d136f1960141C0d8Fb4a91bE78A820";
+
+const TWT = "0x4b0f1812e5df2a09796481ff14017e6005508003";
+const POOL_REGISTRY = "0x9F7b01A536aFA00EF10310A162877fd792cD0666";
+const VTOKEN_RECEIVER_TWT = "0x1c6C2498854662FDeadbC4F14eA2f30ca305104b";
+const VTWT_DeFi = "";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+const USDT = "0x55d398326f99059ff775485246999027b3197955";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+
+forking(30066043, () => {
+  let poolRegistry: Contract;
+  let comptroller: Contract;
+  let vTWT: Contract;
+  let usdt: Contract;
+  let communityWalletBalanceBefore: BigNumberish;
+
+  before(async () => {
+    poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
+    comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER_DeFi);
+    usdt = await ethers.getContractAt(ERC20_ABI, USDT);
+    await setMaxStalePeriodInBinanceOracle(BINANCE_ORACLE, "TWT");
+    await setMaxStalePeriodInBinanceOracle(BINANCE_ORACLE, "USDD");
+    communityWalletBalanceBefore = await usdt.balanceOf(COMMUNITY_WALLET);
+  });
+
+  describe("Contracts setup", () => {
+    const checkVToken = (
+      vTokenAddress: string,
+      {
+        name,
+        symbol,
+        decimals,
+        underlying,
+        exchangeRate,
+      }: {
+        name: string;
+        symbol: string;
+        decimals: BigNumberish;
+        underlying: string;
+        exchangeRate: BigNumberish;
+      },
+    ) => {
+      describe(symbol, () => {
+        let vToken: Contract;
+
+        before(async () => {
+          vToken = await ethers.getContractAt(VTOKEN_ABI, vTokenAddress);
+        });
+
+        it(`should have name = "${name}"`, async () => {
+          expect(await vToken.name()).to.equal(name);
+        });
+
+        it(`should have symbol = "${symbol}"`, async () => {
+          expect(await vToken.symbol()).to.equal(symbol);
+        });
+
+        it(`should have ${decimals.toString()} decimals`, async () => {
+          expect(await vToken.decimals()).to.equal(decimals);
+        });
+
+        it(`should have underlying = "${underlying}"`, async () => {
+          expect(await vToken.underlying()).to.equal(underlying);
+        });
+
+        it(`should have initial exchange rate of ${exchangeRate.toString()}`, async () => {
+          expect(await vToken.exchangeRateStored()).to.equal(exchangeRate);
+        });
+
+        it("should have the correct Comptroller", async () => {
+          expect(await vToken.exchangeRateStored()).to.equal(exchangeRate);
+        });
+      });
+    };
+
+    checkVToken(VTWT_DeFi, {
+      name: "Venus TWT (DeFi)",
+      symbol: "vTWT_DeFi",
+      decimals: 8,
+      underlying: TWT,
+      exchangeRate: parseUnits("1", 28),
+    });
+  });
+
+  testVip("VIP-162 Add Markets", vip162(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(
+        txResponse,
+        [COMPTROLLER_ABI, POOL_REGISTRY_ABI, REWARD_DISTRIBUTOR_ABI, ERC20_ABI, TREASURY_ABI],
+        [
+          "WithdrawTreasuryBEP20",
+          "Approval",
+          "MarketAdded",
+          "RewardTokenSupplySpeedUpdated",
+          "RewardTokenBorrowSpeedUpdated",
+          "OwnershipTransferred",
+        ],
+        [2, 6, 1, 1, 1, 4],
+      );
+    },
+  });
+
+  describe("Post-VIP state", () => {
+    describe("Balance Checks", () => {
+      it("should transfer funds to community wallet from treasury", async () => {
+        const communityWalletBalanceAfter = await usdt.balanceOf(COMMUNITY_WALLET);
+        const differenceInBalance = communityWalletBalanceAfter.sub(communityWalletBalanceBefore);
+        expect(differenceInBalance).equals(parseUnits("6000", 18));
+      });
+    });
+    describe("PoolRegistry state", () => {
+      it("should register pool's vTokens in Comptroller", async () => {
+        const vTokens = await comptroller.getAllMarkets();
+        expect(vTokens).to.have.lengthOf(8);
+        expect(vTokens).to.include(VTWT_DeFi);
+      });
+
+      it("should register vTWT_DeFi in PoolRegistry", async () => {
+        const vToken = await poolRegistry.getVTokenForAsset(COMPTROLLER_DeFi, TWT);
+        expect(vToken).to.equal(VTWT_DeFi);
+      });
+    });
+
+    describe("Ownership", () => {
+      it("should transfer ownership of vTWT_DeFi to Timelock", async () => {
+        expect(await vTWT.owner()).to.equal(NORMAL_TIMELOCK);
+      });
+    });
+
+    describe("Initial supply", () => {
+      it(`should mint 5882350000000 vTWT to ${VTOKEN_RECEIVER_TWT}`, async () => {
+        expect(await vTWT.balanceOf(VTOKEN_RECEIVER_TWT)).to.equal(parseUnits("58823.5", 8));
+      });
+    });
+
+    describe("Market and risk parameters", () => {
+      const checkInterestRate = (
+        vTokenAddress: string,
+        symbol: string,
+        {
+          base,
+          multiplier,
+          jump,
+          kink,
+        }: {
+          base: string;
+          multiplier: string;
+          jump: string;
+          kink: string;
+        },
+      ) => {
+        describe(`${symbol} interest rate model`, () => {
+          const BLOCKS_PER_YEAR = 10512000;
+          let rateModel: Contract;
+
+          before(async () => {
+            const vToken = await ethers.getContractAt(VTOKEN_ABI, vTokenAddress);
+            rateModel = await ethers.getContractAt(RATE_MODEL_ABI, await vToken.interestRateModel());
+          });
+
+          it(`should have base = ${base}`, async () => {
+            const basePerBlock = parseUnits(base, 18).div(BLOCKS_PER_YEAR);
+            expect(await rateModel.baseRatePerBlock()).to.equal(basePerBlock);
+          });
+
+          it(`should have jump = ${jump}`, async () => {
+            const jumpPerBlock = parseUnits(jump, 18).div(BLOCKS_PER_YEAR);
+            expect(await rateModel.jumpMultiplierPerBlock()).to.equal(jumpPerBlock);
+          });
+
+          it(`should have multiplier = ${multiplier}`, async () => {
+            const multiplierPerBlock = parseUnits(multiplier, 18).div(BLOCKS_PER_YEAR);
+            expect(await rateModel.multiplierPerBlock()).to.equal(multiplierPerBlock);
+          });
+
+          it(`should have kink = ${kink}`, async () => {
+            expect(await rateModel.kink()).to.equal(parseUnits(kink, 18));
+          });
+        });
+      };
+
+      describe("Interest rate models", () => {
+        checkInterestRate(VTWT_DeFi, "vTWT_DeFi", {
+          base: "0.02",
+          multiplier: "0.2",
+          jump: "3",
+          kink: "0.5",
+        });
+
+        describe("RF, CF, LT, Protocol seize share", () => {
+          it("should set vTWT_DeFi reserve factor to 0.25", async () => {
+            expect(await vTWT.reserveFactorMantissa()).to.equal(parseUnits("0.25", 18));
+          });
+
+          it("should set vTWT_DeFi collateral factor to 50%", async () => {
+            const market = await comptroller.markets(VTWT_DeFi);
+            expect(market.collateralFactorMantissa).to.equal(parseUnits("0.5", 18));
+          });
+
+          it("should set vTWT_DeFi liquidation threshold to 60%", async () => {
+            const market = await comptroller.markets(VTWT_DeFi);
+            expect(market.liquidationThresholdMantissa).to.equal(parseUnits("0.6", 18));
+          });
+
+          it("should set vTWT_DeFi protocolSeizeShareMantissa to 5%", async () => {
+            const protocolSeizeShare = await vTWT.protocolSeizeShareMantissa();
+            expect(protocolSeizeShare).to.equal(parseUnits("0.05", 18));
+          });
+        });
+
+        describe("Caps", () => {
+          it("should set vTWT_DeFi supply cap to 1,000,000", async () => {
+            expect(await comptroller.supplyCaps(VTWT_DeFi)).to.equal(parseUnits("1000000", 18));
+          });
+
+          it("should set vTWT_DeFi borrow cap to 500,000", async () => {
+            expect(await comptroller.borrowCaps(VTWT_DeFi)).to.equal(parseUnits("500000", 18));
+          });
+        });
+      });
+    });
+
+    describe("Basic supply/borrow/repay/redeem scenario", () => {
+      let twt: Contract;
+      let usdd: Contract;
+      let vUSDD: Contract;
+      let user: SignerWithAddress;
+
+      before(async () => {
+        [user] = await ethers.getSigners();
+        twt = await ethers.getContractAt(ERC20_ABI, TWT);
+        const USDD_HOLDER = "0xf8ba3ec49212ca45325a2335a8ab1279770df6c0";
+        const TWT_HOLDER = "0xD1B189422012e023edEEc0eAA1270AF78B0a4d71";
+        usdd = await ethers.getContractAt(ERC20_ABI, USDD);
+        vUSDD = await ethers.getContractAt(VTOKEN_ABI, vUSDD_DeFi);
+        const usddHolder = await initMainnetUser(USDD_HOLDER, parseUnits("1", 18));
+        const TWTHolder = await initMainnetUser(TWT_HOLDER, parseUnits("1", 18));
+        await usdd.connect(usddHolder).transfer(user.address, parseUnits("100", 18));
+        await twt.connect(TWTHolder).transfer(user.address, parseUnits("100", 18));
+      });
+
+      it("should be possible to supply", async () => {
+        await twt.connect(user).approve(vTWT.address, parseUnits("100", 18));
+        await vTWT.connect(user).mint(parseUnits("100", 18));
+        expect(await vTWT.balanceOf(user.address)).to.equal(parseUnits("100", 8));
+      });
+
+      it("should be possible to enable as collateral", async () => {
+        await comptroller.connect(user).enterMarkets([VTWT_DeFi]);
+        expect(await comptroller.getAssetsIn(user.address)).to.deep.equal([VTWT_DeFi]);
+      });
+
+      it("should be possible to borrow", async () => {
+        await usdd.connect(user).approve(vUSDD.address, parseUnits("100", 18));
+        await vUSDD.connect(user).mint(parseUnits("100", 18));
+        expect(await vUSDD.balanceOf(user.address)).to.equal(parseUnits("100", 8));
+        await comptroller.connect(user).enterMarkets([vUSDD.address]);
+
+        await vTWT.connect(user).borrow(1000000);
+        expect(await vTWT.borrowBalanceStored(user.address)).to.equal(1000000);
+        expect(await twt.balanceOf(user.address)).to.equal(1000000);
+      });
+
+      it("should be possible to repay", async () => {
+        await twt.approve(vTWT.address, 1000000);
+        await vTWT.repayBorrow(1000000);
+        expect(await vTWT.borrowBalanceStored(user.address)).to.be.lessThan(parseUnits("0.01", 18));
+        expect(await twt.balanceOf(user.address)).to.equal(0);
+      });
+
+      it("should be possible to redeem a part of ankr", async () => {
+        await vTWT.redeemUnderlying(parseUnits("30", 18));
+        expect(await twt.balanceOf(user.address)).to.equal(parseUnits("30", 18));
+        expect(await vTWT.balanceOf(user.address)).to.equal(parseUnits("70", 8));
+      });
+    });
+  });
+});
---
+++ vips/vip-162/vip-162.ts
@@ -0,0 +1,90 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+
+const TWT = "0x4b0f1812e5df2a09796481ff14017e6005508003";
+const POOL_REGISTRY = "0x9F7b01A536aFA00EF10310A162877fd792cD0666";
+const VTOKEN_RECEIVER_TWT = "0x1c6C2498854662FDeadbC4F14eA2f30ca305104b"; // To be revised
+const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+const VTWT_DeFi = "";
+const USDT = "0x55d398326f99059ff775485246999027b3197955";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+const RESILIENT_ORACLE = "0x6592b5DE802159F3E74B2486b091D11a8256ab8A";
+const BINANCE_ORACLE = "0x594810b741d136f1960141C0d8Fb4a91bE78A820";
+const MAX_STALE_PERIOD = 60 * 25;
+
+export const vip162 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-162 Add TWT market to DeFi Pool",
+    description: ``,
+    forDescription: "I agree that Venus Protocol should proceed with add market",
+    againstDescription: "I do not think that Venus Protocol should proceed with add market",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with add market",
+  };
+
+  return makeProposal(
+    [
+      // Tranfer From Treasury to community wallet
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [USDT, parseUnits("6000", 18), COMMUNITY_WALLET],
+      },
+      {
+        target: BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["TWT", MAX_STALE_PERIOD],
+      },
+      {
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [
+            TWT,
+            [
+              BINANCE_ORACLE,
+              "0x0000000000000000000000000000000000000000",
+              "0x0000000000000000000000000000000000000000",
+            ],
+            [true, false, false],
+          ],
+        ],
+      },
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [TWT, parseUnits("58823.5", 18), NORMAL_TIMELOCK],
+      },
+      {
+        target: TWT,
+        signature: "approve(address,uint256)",
+        params: [POOL_REGISTRY, 0],
+      },
+      {
+        target: TWT,
+        signature: "approve(address,uint256)",
+        params: [POOL_REGISTRY, parseUnits("58823.5", 18)],
+      },
+      {
+        target: POOL_REGISTRY,
+        signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
+        params: [
+          [
+            VTWT_DeFi,
+            parseUnits("0.5", 18),
+            parseUnits("0.6", 18),
+            parseUnits("58823.5", 18), // To be revised
+            VTOKEN_RECEIVER_TWT, // To be revised
+            parseUnits("1000000", 18),
+            parseUnits("500000", 18),
+          ],
+        ],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
