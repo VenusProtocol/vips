c038a61b682c6cc16b1a45aa90f76eb19f2bf6a4
Kirill Kuvshinov <kkirka@gmail.com>
2023-09-12 18:40:22 +0300

    feat: ensure funds reach their destination

--- simulations/vip-160/vip-160-testnet/simulations.ts
+++ simulations/vip-160/vip-160-testnet/simulations.ts
@@ -1,11 +1,14 @@
+import { TransactionResponse } from "@ethersproject/providers";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
+import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip160Testnet } from "../../../vips/vip-160/vip-160-testnet";
 import COMPTROLLER_ABI from "./abi/comptroller.json";
+import ERC20_ABI from "./abi/erc20.json";
 import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
 import PROTOCOL_SHARE_RESERVE_ABI from "./abi/protocolShareReserve.json";
 import RISK_FUND_ABI from "./abi/riskFund.json";
@@ -22,28 +25,72 @@ const SHORTFALL = "0x503574a82fE2A9f968d355C8AAc1Ba0481859369";
 const PROTOCOL_SHARE_RESERVE = "0xc987a03ab6C2A5891Fc0919f021cc693B5E55278";
 const RISK_FUND = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
 
+const ALL_MARKETS = [
+  "0x170d3b2da05cc2124334240fB34ad1359e34C562",
+  "0x3338988d0beb4419Acb8fE624218754053362D06",
+  "0x899dDf81DfbbF5889a16D075c352F2b959Dd24A4",
+
+  "0xe507B30C41E9e375BCe05197c1e09fc9ee40c0f6",
+  "0x5e68913fbbfb91af30366ab1B21324410b49a308",
+  "0xb7caC5Ef82cb7f9197ee184779bdc52c5490C02a",
+  "0x80CC30811e362aC9aB857C3d7875CbcCc0b65750",
+  "0xa109DE0abaeefC521Ec29D89eA42E64F37A6882E",
+  "0xb677e080148368EeeE70fA3865d07E92c6500174",
+  "0x4C94e67d239aD585275Fdd3246Ab82c8a2668564",
+
+  "0x1958035231E125830bA5d17D168cEa07Bb42184a",
+  "0xef470AbC365F88e4582D8027172a392C473A5B53",
+  "0x0bFE4e0B8A2a096A27e5B18b078d25be57C08634",
+  "0xdeDf3B2bcF25d0023115fd71a0F8221C91C92B1a",
+
+  "0x57a664Dd7f1dE19545fEE9c86C949e3BF43d6D47",
+  "0x644A149853E5507AdF3e682218b8AC86cdD62951",
+  "0x75aa42c832a8911B77219DbeBABBB40040d16987",
+  "0x231dED0Dfc99634e52EE1a1329586bc970d773b3",
+  "0x2197d02cC9cd1ad51317A0a85A656a0c82383A7c",
+  "0xD5b20708d8f0FcA52cb609938D0594C4e32E5DaD",
+
+  "0x47793540757c6E6D84155B33cd8D9535CFdb9334",
+  "0xEe543D5de2Dbb5b07675Fc72831A2f1812428393",
+  "0x410286c43a525E1DCC7468a9B091C111C8324cd1",
+  "0x712774CBFFCBD60e9825871CcEFF2F917442b2c3",
+  "0xD804F74fe21290d213c46610ab171f7c2EeEBDE7",
+];
+
 interface VenusPool {
   comptroller: string;
 }
 
-const getAllMarkets = async (poolRegistry: Contract) => {
-  const pools = await poolRegistry.getAllPools();
-  const comptrollers = await Promise.all(
-    pools.map((pool: VenusPool) => ethers.getContractAt(COMPTROLLER_ABI, pool.comptroller)),
-  );
-  const marketAddresses = (
-    await Promise.all(comptrollers.map(async (comptroller: Contract) => comptroller.getAllMarkets()))
-  ).flat();
-  const markets = await Promise.all(
-    marketAddresses.map((address: string) => ethers.getContractAt(VTOKEN_ABI, address)),
-  );
-  return markets;
-};
-
 forking(33268000, () => {
   let poolRegistry: Contract;
 
-  testVip("Risk fund, shortfall, PSR, stage 1", vip160Testnet());
+  before(async () => {
+    poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
+  });
+
+  describe("Simulation correctness", () => {
+    it("tests all markets", async () => {
+      const pools = await poolRegistry.getAllPools();
+      const comptrollers = await Promise.all(
+        pools.map((pool: VenusPool) => ethers.getContractAt(COMPTROLLER_ABI, pool.comptroller)),
+      );
+      const marketAddresses = (
+        await Promise.all(comptrollers.map(async (comptroller: Contract) => comptroller.getAllMarkets()))
+      ).flat();
+      expect(marketAddresses).to.deep.equal(ALL_MARKETS);
+    });
+  });
+
+  testVip("Risk fund, shortfall, PSR, stage 1", vip160Testnet(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(
+        txResponse,
+        [VTOKEN_ABI],
+        ["NewShortfallContract", "NewProtocolShareReserve", "OwnershipTransferred"],
+        [25, 25, 3],
+      );
+    },
+  });
 
   describe("RiskFund configuration", () => {
     let riskFund: Contract;
@@ -137,23 +184,61 @@ forking(33268000, () => {
     });
   });
 
-  describe("Markets configuration", () => {
-    before(async () => {
-      poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
-    });
+  describe("Markets tests", () => {
+    for (const marketAddress of ALL_MARKETS) {
+      describe(`VToken at ${marketAddress}`, () => {
+        let market: Contract;
+        let protocolShareReserve: Contract;
+        let underlying: Contract;
+        let comptrollerAddress: string;
+        let reservesToDistribute: BigNumber;
 
-    it("sets ProtocolShareReserve address for all markets", async () => {
-      const markets = await getAllMarkets(poolRegistry);
-      for (const market of markets) {
-        expect(await market.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
-      }
-    });
+        before(async () => {
+          market = await ethers.getContractAt(VTOKEN_ABI, marketAddress);
+          protocolShareReserve = await ethers.getContractAt(PROTOCOL_SHARE_RESERVE_ABI, PROTOCOL_SHARE_RESERVE);
+          comptrollerAddress = await market.comptroller();
+          underlying = await ethers.getContractAt(ERC20_ABI, await market.underlying());
+          await market.accrueInterest();
+          reservesToDistribute = await market.totalReserves();
+        });
 
-    it("sets Shortfall contract address for all markets", async () => {
-      const markets = await getAllMarkets(poolRegistry);
-      for (const market of markets) {
-        expect(await market.shortfall()).to.equal(SHORTFALL);
-      }
-    });
+        it("sets ProtocolShareReserve address", async () => {
+          expect(await market.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
+        });
+
+        it("sets Shortfall contract address", async () => {
+          expect(await market.shortfall()).to.equal(SHORTFALL);
+        });
+
+        it("transfers reserves", async () => {
+          // We have 7 markets on bsctestnet with zero reserves, so tests for these
+          // markets are essentially a no-op. Still, we can't skip them at this point.
+          // expect(reservesToDistribute).to.be.gt(0);
+          const tx = await market.reduceReserves(reservesToDistribute);
+          await expect(tx).to.changeTokenBalance(underlying, PROTOCOL_SHARE_RESERVE, reservesToDistribute);
+        });
+
+        it("tracks reserves in PSR correctly", async () => {
+          expect(await protocolShareReserve.getPoolAssetReserve(comptrollerAddress, underlying.address)).to.equal(
+            reservesToDistribute,
+          );
+        });
+
+        it("distributes reserves to VTreasury (50%) and RiskFund (50%)", async () => {
+          const tx = await protocolShareReserve.releaseFunds(
+            comptrollerAddress,
+            underlying.address,
+            reservesToDistribute,
+          );
+          const treasuryShare = reservesToDistribute.mul(5).div(10);
+          const riskFundShare = reservesToDistribute.sub(treasuryShare);
+          await expect(tx).to.changeTokenBalances(
+            underlying,
+            [VENUS_TREASURY, RISK_FUND],
+            [treasuryShare, riskFundShare],
+          );
+        });
+      });
+    }
   });
 });
--- simulations/vip-160/vip-160/simulation.ts
+++ simulations/vip-160/vip-160/simulation.ts
@@ -1,11 +1,14 @@
+import { TransactionResponse } from "@ethersproject/providers";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
+import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip160 } from "../../../vips/vip-160/vip-160";
 import COMPTROLLER_ABI from "./abi/comptroller.json";
+import ERC20_ABI from "./abi/erc20.json";
 import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
 import PROTOCOL_SHARE_RESERVE_ABI from "./abi/protocolShareReserve.json";
 import RISK_FUND_ABI from "./abi/riskFund.json";
@@ -22,28 +25,66 @@ const SHORTFALL = "0xf37530A8a810Fcb501AA0Ecd0B0699388F0F2209";
 const PROTOCOL_SHARE_RESERVE = "0xfB5bE09a1FA6CFDA075aB1E69FE83ce8324682e4";
 const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 
+const ALL_MARKETS = [
+  "0xCa2D81AA7C09A1a025De797600A7081146dceEd9",
+  "0x5e3072305F9caE1c7A82F6Fe9E38811c74922c3B",
+  "0xc3a45ad8812189cAb659aD99E64B1376f6aCD035",
+  "0x53728FD51060a85ac41974C6C3Eb1DaE42776723",
+  "0x8f657dFD3a1354DEB4545765fE6840cc54AFd379",
+  "0x02c5Fb0F26761093D297165e902e96D08576D344",
+  "0x1D8bBDE12B6b34140604E18e9f9c6e14deC16854",
+  "0xA615467caE6B9E0bb98BC04B4411d9296fd1dFa0",
+  "0x19CE11C8817a1828D1d357DFBF62dCf5b0B2A362",
+  "0x736bf1D21A28b5DC19A1aC8cA71Fc2856C23c03F",
+  "0xE5FE5527A5b76C75eedE77FdFA6B80D52444A465",
+  "0xc353B7a1E13dDba393B5E120D4169Da7185aA2cb",
+  "0x4978591f17670A846137d9d613e333C38dc68A37",
+  "0x9f2FD23bd0A5E08C5f2b9DD6CF9C96Bfb5fA515C",
+  "0xBfe25459BA784e70E2D7a718Be99a1f3521cA17f",
+  "0x5E21bF67a6af41c74C1773E4b473ca5ce8fd3791",
+  "0xcc5D9e502574cda17215E70bC0B4546663785227",
+  "0xe10E80B7FD3a29fE46E16C30CC8F4dd938B742e2",
+  "0x49c26e12959345472E2Fd95E5f79F8381058d3Ee",
+  "0xb114cfA615c828D88021a41bFc524B800E64a9D5",
+  "0x836beb2cB723C498136e1119248436A645845F4E",
+  "0x281E5378f99A4bc55b295ABc0A3E7eD32Deba059",
+  "0xf1da185CCe5BeD1BeBbb3007Ef738Ea4224025F7",
+];
+
 interface VenusPool {
   comptroller: string;
 }
 
-const getAllMarkets = async (poolRegistry: Contract) => {
-  const pools = await poolRegistry.getAllPools();
-  const comptrollers = await Promise.all(
-    pools.map((pool: VenusPool) => ethers.getContractAt(COMPTROLLER_ABI, pool.comptroller)),
-  );
-  const marketAddresses = (
-    await Promise.all(comptrollers.map(async (comptroller: Contract) => comptroller.getAllMarkets()))
-  ).flat();
-  const markets = await Promise.all(
-    marketAddresses.map((address: string) => ethers.getContractAt(VTOKEN_ABI, address)),
-  );
-  return markets;
-};
-
 forking(31671700, () => {
   let poolRegistry: Contract;
 
-  testVip("Risk fund, shortfall, PSR, stage 1", vip160());
+  before(async () => {
+    poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
+  });
+
+  describe("Simulation correctness", () => {
+    it("tests all markets", async () => {
+      const pools = await poolRegistry.getAllPools();
+      const comptrollers = await Promise.all(
+        pools.map((pool: VenusPool) => ethers.getContractAt(COMPTROLLER_ABI, pool.comptroller)),
+      );
+      const marketAddresses = (
+        await Promise.all(comptrollers.map(async (comptroller: Contract) => comptroller.getAllMarkets()))
+      ).flat();
+      expect(marketAddresses).to.deep.equal(ALL_MARKETS);
+    });
+  });
+
+  testVip("Risk fund, shortfall, PSR, stage 1", vip160(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(
+        txResponse,
+        [VTOKEN_ABI],
+        ["NewShortfallContract", "NewProtocolShareReserve", "OwnershipTransferred"],
+        [23, 23, 3],
+      );
+    },
+  });
 
   describe("RiskFund configuration", () => {
     let riskFund: Contract;
@@ -137,23 +178,61 @@ forking(31671700, () => {
     });
   });
 
-  describe("Markets configuration", () => {
-    before(async () => {
-      poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
-    });
+  describe("Markets tests", () => {
+    for (const marketAddress of ALL_MARKETS) {
+      describe(`VToken at ${marketAddress}`, () => {
+        let market: Contract;
+        let protocolShareReserve: Contract;
+        let underlying: Contract;
+        let comptrollerAddress: string;
+        let reservesToDistribute: BigNumber;
 
-    it("sets ProtocolShareReserve address for all markets", async () => {
-      const markets = await getAllMarkets(poolRegistry);
-      for (const market of markets) {
-        expect(await market.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
-      }
-    });
+        before(async () => {
+          market = await ethers.getContractAt(VTOKEN_ABI, marketAddress);
+          protocolShareReserve = await ethers.getContractAt(PROTOCOL_SHARE_RESERVE_ABI, PROTOCOL_SHARE_RESERVE);
+          comptrollerAddress = await market.comptroller();
+          underlying = await ethers.getContractAt(ERC20_ABI, await market.underlying());
+          await market.accrueInterest();
+          reservesToDistribute = await market.totalReserves();
+        });
 
-    it("sets Shortfall contract address for all markets", async () => {
-      const markets = await getAllMarkets(poolRegistry);
-      for (const market of markets) {
-        expect(await market.shortfall()).to.equal(SHORTFALL);
-      }
-    });
+        it("sets ProtocolShareReserve address", async () => {
+          expect(await market.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
+        });
+
+        it("sets Shortfall contract address", async () => {
+          expect(await market.shortfall()).to.equal(SHORTFALL);
+        });
+
+        it("transfers reserves", async () => {
+          // We actually have four markets on bscmainnet with zero reserves, so tests for these
+          // markets are essentially a no-op. Still, we can't skip them at this point.
+          // expect(reservesToDistribute).to.be.gt(0);
+          const tx = await market.reduceReserves(reservesToDistribute);
+          await expect(tx).to.changeTokenBalance(underlying, PROTOCOL_SHARE_RESERVE, reservesToDistribute);
+        });
+
+        it("tracks reserves in PSR correctly", async () => {
+          expect(await protocolShareReserve.getPoolAssetReserve(comptrollerAddress, underlying.address)).to.equal(
+            reservesToDistribute,
+          );
+        });
+
+        it("distributes reserves to VTreasury (50%) and RiskFund (50%)", async () => {
+          const tx = await protocolShareReserve.releaseFunds(
+            comptrollerAddress,
+            underlying.address,
+            reservesToDistribute,
+          );
+          const treasuryShare = reservesToDistribute.mul(5).div(10);
+          const riskFundShare = reservesToDistribute.sub(treasuryShare);
+          await expect(tx).to.changeTokenBalances(
+            underlying,
+            [VENUS_TREASURY, RISK_FUND],
+            [treasuryShare, riskFundShare],
+          );
+        });
+      });
+    }
   });
 });
