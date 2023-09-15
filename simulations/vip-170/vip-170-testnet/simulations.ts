31eedf2bd70877eb52a16dd4be770ba49ba87911
Kirill Kuvshinov <kkirka@gmail.com>
2023-09-15 09:59:14 +0300

    chore: update VIP number for funds VIP

--- simulations/vip-170/vip-170-testnet/simulations.ts
+++ simulations/vip-170/vip-170-testnet/simulations.ts
@@ -6,7 +6,7 @@ import { ethers } from "hardhat";
 
 import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
-import { vip160 } from "../../../vips/vip-160/vip-160";
+import { vip170Testnet } from "../../../vips/vip-170/vip-170-testnet";
 import COMPTROLLER_ABI from "./abi/comptroller.json";
 import ERC20_ABI from "./abi/erc20.json";
 import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
@@ -15,47 +15,53 @@ import RISK_FUND_ABI from "./abi/riskFund.json";
 import SHORTFALL_ABI from "./abi/shortfall.json";
 import VTOKEN_ABI from "./abi/vToken.json";
 
-const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
-const POOL_REGISTRY = "0x9F7b01A536aFA00EF10310A162877fd792cD0666";
-const USDT = "0x55d398326f99059fF775485246999027B3197955";
-const SWAP_ROUTER_CORE_POOL = "0x8938E6dA30b59c1E27d5f70a94688A89F7c815a4";
-const VENUS_TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const POOL_REGISTRY = "0xC85491616Fa949E048F3aAc39fbf5b0703800667";
+const USDT = "0xA11c8D9DC9b66E209Ef60F0C8D969D3CD988782c";
+const VENUS_TREASURY = "0x8b293600C50D6fbdc6Ed4251cc75ECe29880276f";
+const SWAP_ROUTER_CORE_POOL = "0x83edf1deE1B730b7e8e13C00ba76027D63a51ac0";
 
-const SHORTFALL = "0xf37530A8a810Fcb501AA0Ecd0B0699388F0F2209";
-const PROTOCOL_SHARE_RESERVE = "0xfB5bE09a1FA6CFDA075aB1E69FE83ce8324682e4";
-const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
+const SHORTFALL = "0x503574a82fE2A9f968d355C8AAc1Ba0481859369";
+const PROTOCOL_SHARE_RESERVE = "0xc987a03ab6C2A5891Fc0919f021cc693B5E55278";
+const RISK_FUND = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
 
 const ALL_MARKETS = [
-  "0xCa2D81AA7C09A1a025De797600A7081146dceEd9",
-  "0x5e3072305F9caE1c7A82F6Fe9E38811c74922c3B",
-  "0xc3a45ad8812189cAb659aD99E64B1376f6aCD035",
-  "0x53728FD51060a85ac41974C6C3Eb1DaE42776723",
-  "0x8f657dFD3a1354DEB4545765fE6840cc54AFd379",
-  "0x02c5Fb0F26761093D297165e902e96D08576D344",
-  "0x1D8bBDE12B6b34140604E18e9f9c6e14deC16854",
-  "0xA615467caE6B9E0bb98BC04B4411d9296fd1dFa0",
-  "0x19CE11C8817a1828D1d357DFBF62dCf5b0B2A362",
-  "0x736bf1D21A28b5DC19A1aC8cA71Fc2856C23c03F",
-  "0xE5FE5527A5b76C75eedE77FdFA6B80D52444A465",
-  "0xc353B7a1E13dDba393B5E120D4169Da7185aA2cb",
-  "0x4978591f17670A846137d9d613e333C38dc68A37",
-  "0x9f2FD23bd0A5E08C5f2b9DD6CF9C96Bfb5fA515C",
-  "0xBfe25459BA784e70E2D7a718Be99a1f3521cA17f",
-  "0x5E21bF67a6af41c74C1773E4b473ca5ce8fd3791",
-  "0xcc5D9e502574cda17215E70bC0B4546663785227",
-  "0xe10E80B7FD3a29fE46E16C30CC8F4dd938B742e2",
-  "0x49c26e12959345472E2Fd95E5f79F8381058d3Ee",
-  "0xb114cfA615c828D88021a41bFc524B800E64a9D5",
-  "0x836beb2cB723C498136e1119248436A645845F4E",
-  "0x281E5378f99A4bc55b295ABc0A3E7eD32Deba059",
-  "0xf1da185CCe5BeD1BeBbb3007Ef738Ea4224025F7",
+  "0x170d3b2da05cc2124334240fB34ad1359e34C562",
+  "0x3338988d0beb4419Acb8fE624218754053362D06",
+  "0x899dDf81DfbbF5889a16D075c352F2b959Dd24A4",
+
+  "0xe507B30C41E9e375BCe05197c1e09fc9ee40c0f6",
+  "0x5e68913fbbfb91af30366ab1B21324410b49a308",
+  "0xb7caC5Ef82cb7f9197ee184779bdc52c5490C02a",
+  "0x80CC30811e362aC9aB857C3d7875CbcCc0b65750",
+  "0xa109DE0abaeefC521Ec29D89eA42E64F37A6882E",
+  "0xb677e080148368EeeE70fA3865d07E92c6500174",
+  "0x4C94e67d239aD585275Fdd3246Ab82c8a2668564",
+
+  "0x1958035231E125830bA5d17D168cEa07Bb42184a",
+  "0xef470AbC365F88e4582D8027172a392C473A5B53",
+  "0x0bFE4e0B8A2a096A27e5B18b078d25be57C08634",
+  "0xdeDf3B2bcF25d0023115fd71a0F8221C91C92B1a",
+
+  "0x57a664Dd7f1dE19545fEE9c86C949e3BF43d6D47",
+  "0x644A149853E5507AdF3e682218b8AC86cdD62951",
+  "0x75aa42c832a8911B77219DbeBABBB40040d16987",
+  "0x231dED0Dfc99634e52EE1a1329586bc970d773b3",
+  "0x2197d02cC9cd1ad51317A0a85A656a0c82383A7c",
+  "0xD5b20708d8f0FcA52cb609938D0594C4e32E5DaD",
+
+  "0x47793540757c6E6D84155B33cd8D9535CFdb9334",
+  "0xEe543D5de2Dbb5b07675Fc72831A2f1812428393",
+  "0x410286c43a525E1DCC7468a9B091C111C8324cd1",
+  "0x712774CBFFCBD60e9825871CcEFF2F917442b2c3",
+  "0xD804F74fe21290d213c46610ab171f7c2EeEBDE7",
 ];
 
 interface VenusPool {
   comptroller: string;
 }
 
-forking(31671700, () => {
+forking(33268000, () => {
   let poolRegistry: Contract;
 
   before(async () => {
@@ -75,13 +81,13 @@ forking(31671700, () => {
     });
   });
 
-  testVip("Risk fund, shortfall, PSR, stage 1", vip160(), {
+  testVip("Risk fund, shortfall, PSR, stage 1", vip170Testnet(), {
     callbackAfterExecution: async (txResponse: TransactionResponse) => {
       await expectEvents(
         txResponse,
         [VTOKEN_ABI],
         ["NewShortfallContract", "NewProtocolShareReserve", "OwnershipTransferred"],
-        [23, 23, 3],
+        [25, 25, 3],
       );
     },
   });
@@ -205,7 +211,7 @@ forking(31671700, () => {
         });
 
         it("transfers reserves", async () => {
-          // We actually have four markets on bscmainnet with zero reserves, so tests for these
+          // We have 7 markets on bsctestnet with zero reserves, so tests for these
           // markets are essentially a no-op. Still, we can't skip them at this point.
           // expect(reservesToDistribute).to.be.gt(0);
           const tx = await market.reduceReserves(reservesToDistribute);
