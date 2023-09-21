6b002dacf0ac5961cb0fba15d7b0fc9fbafeb72c
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-09-21 17:13:57 +0530

    test: risk parameters update chaos lab simulations

---
+++ vips/vip-173.ts
@@ -0,0 +1,47 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../src/types";
+import { makeProposal } from "../src/utils";
+
+export const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+export const VTUSD = "0xBf762cd5991cA1DCdDaC9ae5C638F5B5Dc3Bee6E";
+
+export const vip173 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-173 Risk Parameters Adjustments",
+    description: `**Summary**
+
+This VIP will perform the following Risk Parameters actions as per Chaos Labs latest recommendations in this Venus community forum publication: [Risk Parameter Updates 09/18/2023](https://community.venus.io/t/chaos-labs-risk-parameter-updates-09-18-2023/3808)
+
+* Increase **TUSD** supply cap to 5,000,000
+* Increase **TUSD** borrow cap to 4,000,000
+
+
+Complete analysis and details of these recommendations are available in the above publication.
+
+VIP simulation: [https://github.com/VenusProtocol/vips/pull/75](https://github.com/VenusProtocol/vips/pull/75)
+`,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: COMPTROLLER,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[VTUSD], [parseUnits("5000000", 18)]],
+      },
+
+      {
+        target: COMPTROLLER,
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [[VTUSD], [parseUnits("4000000", 18)]],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
