dee5a00fdfd476c5e9588d0ca79bad03d9f4e024
Narayan Prusty <arsenal.narayan@gmail.com>
2023-10-06 20:03:15 +0100

    fix: fixed mainnet vip

--- vips/vip-152.ts
+++ vips/vip-152.ts
@@ -4,12 +4,13 @@ import { makeProposal } from "../src/utils";
 const vBNB_ADDRESS = "0xA07c5b74C9B40447a954e1466938b865b6BBea36";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
 const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
-const VBNBAdmin_ADDRESS = "0xBC612ec01bB52349De615112F65A3DA66fb02648";
+const VBNBAdmin_ADDRESS = "0x2b11a94DA41a5cAcAa6e1E3F23139cED805808b5";
 const PSR = "0x4E5A49Ce81993504327a848167d76212b7a341E2";
 const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 const TREASURY = "0xf322942f644a996a617bd29c16bd7d231d9f35e9";
 const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
 const CRITICAL_TIMELOCK = "0x213c446ec11e45b15a6E29C1C1b402B8897f606d";
+const POOL_REGISTRY = "0x9F7b01A536aFA00EF10310A162877fd792cD0666";
 
 export const vip152 = () => {
   const meta = {
@@ -107,6 +108,11 @@ export const vip152 = () => {
           ],
         ],
       },
+      {
+        target: PSR,
+        signature: "setPoolRegistry(address)",
+        params: [POOL_REGISTRY],
+      }
     ],
     meta,
     ProposalType.REGULAR,
