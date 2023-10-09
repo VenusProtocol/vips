3b844fede7c23a7ea62747a68c98c78a34d2471f
Narayan Prusty <arsenal.narayan@gmail.com>
2023-10-09 11:36:23 +0100

    fix: added tests for reduce reserves

--- simulations/vip-152/vip-152/simulation.ts
+++ simulations/vip-152/vip-152/simulation.ts
@@ -6,6 +6,8 @@ import { vip152 } from "../../../vips/vip-152";
 import PSR_ABI from "./abi/PSR.json";
 import vBNB_ABI from "./abi/vBNB.json";
 import vBNBAdmin_ABI from "./abi/vBNBAdmin.json";
+import ERC20_ABI from "./abi/ERC20.json";
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 
 const vBNB_ADDRESS = "0xa07c5b74c9b40447a954e1466938b865b6bbea36";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
@@ -13,6 +15,7 @@ const VBNBAdmin_ADDRESS = "0x2b11a94DA41a5cAcAa6e1E3F23139cED805808b5";
 const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 const PSR = "0x4E5A49Ce81993504327a848167d76212b7a341E2";
+const WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 
 forking(32374397, () => {
   const provider = ethers.provider;
@@ -45,11 +48,16 @@ forking(32374397, () => {
     let vBNB: ethers.Contract;
     let psr: ethers.Contract;
     let vBNBAdmin: ethers.Contract;
+    let WBNB: ethers.Contract;
 
     before(async () => {
+      impersonateAccount(NORMAL_TIMELOCK);
+      const signer = await ethers.getSigner(NORMAL_TIMELOCK);
+
       vBNB = new ethers.Contract(vBNB_ADDRESS, vBNB_ABI, provider);
-      psr = new ethers.Contract(PSR, PSR_ABI, provider);
-      vBNBAdmin = new ethers.Contract(VBNBAdmin_ADDRESS, vBNBAdmin_ABI, provider);
+      psr = new ethers.Contract(PSR, PSR_ABI, signer);
+      vBNBAdmin = new ethers.Contract(VBNBAdmin_ADDRESS, vBNBAdmin_ABI, signer);
+      WBNB = new ethers.Contract(WBNB_ADDRESS, ERC20_ABI, provider);
     });
 
     it("validate admin", async () => {
@@ -89,6 +97,13 @@ forking(32374397, () => {
       expect(config.schema).to.be.equal(1);
       expect(config.percentage).to.be.equal(60);
       expect(config.destination).to.be.equal(TREASURY);
+    });
+
+    it("reduce reserves", async () => {
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(ethers.utils.parseEther("0"));
+      await vBNBAdmin.reduceReserves(ethers.utils.parseEther("1"));
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(ethers.utils.parseEther("1"));
+      // await psr.releaseFunds("0xfD36E2c2a6789Db23113685031d7F16329158384", [WBNB_ADDRESS])
     });
   });
 });
