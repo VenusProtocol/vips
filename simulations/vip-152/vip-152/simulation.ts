d1555b83bc5fca7e7c5eafe753cfd2f40737d9ca
Narayan Prusty <arsenal.narayan@gmail.com>
2023-10-09 11:48:40 +0100

    fix: added events

--- simulations/vip-152/vip-152/simulation.ts
+++ simulations/vip-152/vip-152/simulation.ts
@@ -1,13 +1,14 @@
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip152 } from "../../../vips/vip-152";
+import ERC20_ABI from "./abi/ERC20.json";
 import PSR_ABI from "./abi/PSR.json";
 import vBNB_ABI from "./abi/vBNB.json";
 import vBNBAdmin_ABI from "./abi/vBNBAdmin.json";
-import ERC20_ABI from "./abi/ERC20.json";
-import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 
 const vBNB_ADDRESS = "0xa07c5b74c9b40447a954e1466938b865b6bbea36";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
@@ -42,7 +43,15 @@ forking(32374397, () => {
     });
   });
 
-  testVip("VIP-152", vip152());
+  testVip("VIP-152", vip152(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [PSR_ABI], ["DistributionConfigAdded"], [4]);
+
+      await expectEvents(txResponse, [vBNBAdmin_ABI], ["ProtocolShareReserveUpdated"], [1]);
+
+      await expectEvents(txResponse, [PSR_ABI], ["PoolRegistryUpdated"], [1]);
+    },
+  });
 
   describe("Post-VIP behavior", async () => {
     let vBNB: ethers.Contract;
