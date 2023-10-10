8a726b279700ed02b9a46709e3d2104de8edfa08
Narayan Prusty <arsenal.narayan@gmail.com>
2023-10-10 16:38:39 +0100

    fix: wip - create new testnet vip

--- simulations/vip-152/vip-152-testnet/simulation.ts
+++ simulations/vip-152/vip-152-testnet/simulation.ts
@@ -1,40 +1,139 @@
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
-import { vip152Testnet } from "../../../vips/vip-152-testnet";
+import ERC20_ABI from "./abi/ERC20.json";
+import PSR_ABI from "./abi/PSR.json";
 import vBNB_ABI from "./abi/vBNB.json";
+import vBNBAdmin_ABI from "./abi/vBNBAdmin.json";
+import PROXY_ADMIN_ABI from "./abi/ProxyAdmin.json";
+import { vip152Testnet } from "../../../vips/vip-152-testnet";
 
+const VBNBAdmin_ADDRESS = "0x2b11a94DA41a5cAcAa6e1E3F23139cED805808b5";
+const WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const CORE_POOL_COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+const RISK_FUND = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
+const PROXY_ADMIN = "0x7877fFd62649b6A1557B55D4c20fcBaB17344C91";
+const PSR = "0x9B34c7aDCEa239b83Ef364627071Be7665bcb2E9";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const TREASURY = "0x8b293600c50d6fbdc6ed4251cc75ece29880276f";
 const vBNB_ADDRESS = "0x2E7222e51c0f6e98610A1543Aa3836E092CDe62c";
-const VBNBAdmin_ADDRESS = "0x7575D142AAb97229e5928f94c03da39b34Bb0E96";
-const CURRENT_ADMIN = "0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706";
 
-forking(32356229, () => {
+forking(32482549, () => {
   const provider = ethers.provider;
 
   describe("Pre-VIP behavior", async () => {
     let vBNB: ethers.Contract;
+    let psr: ethers.Contract;
 
     before(async () => {
       vBNB = new ethers.Contract(vBNB_ADDRESS, vBNB_ABI, provider);
+      psr = new ethers.Contract(PSR, PSR_ABI, provider);
     });
 
     it("validate admin", async () => {
-      expect(await vBNB.admin()).to.be.equal(CURRENT_ADMIN);
+      expect(await vBNB.admin()).to.be.equal(NORMAL_TIMELOCK);
+    });
+
+    it("config check", async () => {
+      expect(await psr.totalDistributions()).to.be.equal(0);
+    });
+
+    it("ownership check", async () => {
+      expect(await psr.pendingOwner()).to.be.equal(NORMAL_TIMELOCK);
     });
   });
 
-  testVip("VIP-152", vip152Testnet());
+  testVip("VIP-152", vip152Testnet(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [PSR_ABI], ["DistributionConfigAdded"], [4]);
+
+      await expectEvents(txResponse, [vBNBAdmin_ABI], ["ProtocolShareReserveUpdated"], [1]);
+
+      await expectEvents(txResponse, [PSR_ABI], ["PoolRegistryUpdated"], [1]);
+    },
+  });
 
   describe("Post-VIP behavior", async () => {
     let vBNB: ethers.Contract;
+    let psr: ethers.Contract;
+    let vBNBAdmin: ethers.Contract;
+    let WBNB: ethers.Contract;
+    let proxyAdmin: ethers.Contract;
 
     before(async () => {
+      impersonateAccount(NORMAL_TIMELOCK);
+      const signer = await ethers.getSigner(NORMAL_TIMELOCK);
+
       vBNB = new ethers.Contract(vBNB_ADDRESS, vBNB_ABI, provider);
+      psr = new ethers.Contract(PSR, PSR_ABI, signer);
+      vBNBAdmin = new ethers.Contract(VBNBAdmin_ADDRESS, vBNBAdmin_ABI, signer);
+      WBNB = new ethers.Contract(WBNB_ADDRESS, ERC20_ABI, provider);
+      proxyAdmin = new ethers.Contract(PROXY_ADMIN, PROXY_ADMIN_ABI, provider);
     });
 
     it("validate admin", async () => {
       expect(await vBNB.admin()).to.be.equal(VBNBAdmin_ADDRESS);
     });
+
+    it("validate PSR", async () => {
+      expect(await vBNBAdmin.protocolShareReserve()).to.be.equal(PSR);
+    });
+
+    it("config check", async () => {
+      expect(await psr.totalDistributions()).to.be.equal(4);
+    });
+
+    it("ownership check", async () => {
+      expect(await psr.pendingOwner()).to.be.equal("0x0000000000000000000000000000000000000000");
+      expect(await psr.owner()).to.be.equal(NORMAL_TIMELOCK);
+    });
+
+    it("configurations", async () => {
+      let config = await psr.distributionTargets(0);
+      expect(config.schema).to.be.equal(0);
+      expect(config.percentage).to.be.equal(50);
+      expect(config.destination).to.be.equal(RISK_FUND);
+
+      config = await psr.distributionTargets(1);
+      expect(config.schema).to.be.equal(0);
+      expect(config.percentage).to.be.equal(50);
+      expect(config.destination).to.be.equal(TREASURY);
+
+      config = await psr.distributionTargets(2);
+      expect(config.schema).to.be.equal(1);
+      expect(config.percentage).to.be.equal(40);
+      expect(config.destination).to.be.equal(RISK_FUND);
+
+      config = await psr.distributionTargets(3);
+      expect(config.schema).to.be.equal(1);
+      expect(config.percentage).to.be.equal(60);
+      expect(config.destination).to.be.equal(TREASURY);
+    });
+
+    it("reduce reserves", async () => {
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(ethers.utils.parseEther("0"));
+      await vBNBAdmin.reduceReserves(ethers.utils.parseEther("1"));
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(ethers.utils.parseEther("1"));
+      await psr.releaseFunds("0xfD36E2c2a6789Db23113685031d7F16329158384", [WBNB_ADDRESS]);
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(ethers.utils.parseEther("0"));
+      expect(await WBNB.balanceOf(RISK_FUND)).to.be.equal("1093600150889113267");
+      expect(await WBNB.balanceOf(TREASURY)).to.be.equal("1093600150889113267");
+      await vBNBAdmin.reduceReserves("100");
+      expect(await WBNB.balanceOf(PSR)).to.be.equal(100);
+      await psr.releaseFunds(CORE_POOL_COMPTROLLER, [WBNB_ADDRESS]);
+      expect(await WBNB.balanceOf(RISK_FUND)).to.be.equal("1093600150889113317");
+      expect(await WBNB.balanceOf(TREASURY)).to.be.equal("1093600150889113317");
+    });
+
+    it("validate proxy admin", async () => {
+      expect(await proxyAdmin.getProxyAdmin(VBNBAdmin_ADDRESS)).to.be.equal(PROXY_ADMIN);
+      expect(await proxyAdmin.getProxyAdmin(PSR)).to.be.equal(PROXY_ADMIN);
+      expect(await proxyAdmin.getProxyAdmin(RISK_FUND)).to.be.equal(PROXY_ADMIN);
+
+      expect(await proxyAdmin.owner()).to.be.equal(NORMAL_TIMELOCK);
+    })
   });
 });
--- vips/vip-152-testnet.ts
+++ vips/vip-152-testnet.ts
@@ -1,43 +1,136 @@
 import { ProposalType } from "../src/types";
 import { makeProposal } from "../src/utils";
 
+const RISK_FUND = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
+const NEW_RISK_FUND_IMPLEMENTATION = "0x1E7DEC93C77740c2bB46daf87ef42056E388dA14";
+const PROXY_ADMIN = "0x7877fFd62649b6A1557B55D4c20fcBaB17344C91";
+const PSR = "0x9B34c7aDCEa239b83Ef364627071Be7665bcb2E9";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const ACM = "0x45f8a08f534f34a97187626e05d4b6648eeaa9aa";
+const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
+const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
+const POOL_REGISTRY = "0xC85491616Fa949E048F3aAc39fbf5b0703800667";
+const TREASURY = "0x8b293600c50d6fbdc6ed4251cc75ece29880276f";
+const VBNBAdmin = "0x78459C0a0Fe91d382322D09FF4F86A10dbAF78a4";
+const PSR_PROXY_ADMIN = "0xF77810085c7F6323bB19dddaE2d62793E5AE2e3b";
+const VBNBAdmin_PROXY_ADMIN = "0x7877fFd62649b6A1557B55D4c20fcBaB17344C91";
 const vBNB_ADDRESS = "0x2E7222e51c0f6e98610A1543Aa3836E092CDe62c";
-const VBNBAdmin_ADDRESS = "0x7575D142AAb97229e5928f94c03da39b34Bb0E96";
 
 export const vip152Testnet = () => {
   const meta = {
     version: "v2",
-    title: "Change vBNB admin to vBNBAdmin",
+    title: "Change vBNB admin to vBNBAdmin and Setup Protocol Share Reserve",
     description: ``,
-    forDescription: "I agree that Venus Protocol should proceed with setting the vBNB admin",
-    againstDescription: "I do not think that Venus Protocol should proceed with setting the vBNB admin",
-    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with setting the vBNB admin",
+    forDescription: "I agree that Venus Protocol should proceed with setting the vBNB admin and setting up PSR",
+    againstDescription:
+      "I do not think that Venus Protocol should proceed with setting the vBNB admin and setting up PSR",
+    abstainDescription:
+      "I am indifferent to whether Venus Protocol proceeds with setting the vBNB admin and setting up PSR",
   };
 
   return makeProposal(
     [
-      // this step is only required for testnet because vBNB admin in testnet is an EOA instead of normal timelock
       {
-        target: vBNB_ADDRESS,
-        signature: "_acceptAdmin()",
-        params: [],
+        target: PROXY_ADMIN,
+        signature: "upgrade(address,address)",
+        params: [RISK_FUND, NEW_RISK_FUND_IMPLEMENTATION],
       },
-      // this step is to make timelock the admin of vBNBAdmin contract. For this step to work, we must make sure we have
-      // invoked vBNBAdmin.transferOwnership(timelockAddress) after deploying the vBNBAdmin contract
       {
-        target: VBNBAdmin_ADDRESS,
+        target: VBNBAdmin,
         signature: "acceptOwnership()",
         params: [],
       },
       {
         target: vBNB_ADDRESS,
         signature: "_setPendingAdmin(address)",
-        params: [VBNBAdmin_ADDRESS],
+        params: [VBNBAdmin],
       },
       {
-        target: VBNBAdmin_ADDRESS,
+        target: VBNBAdmin,
         signature: "_acceptAdmin()",
         params: [],
+      },
+      {
+        target: PSR,
+        signature: "acceptOwnership()",
+        params: [],
+      },
+      {
+        target: VBNBAdmin_PROXY_ADMIN,
+        signature: "changeProxyAdmin(address,address)",
+        params: [VBNBAdmin, PROXY_ADMIN],
+      },
+      {
+        target: VBNBAdmin,
+        signature: "setProtocolShareReserve(address)",
+        params: [PSR],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrUpdateDistributionConfigs(DistributionConfig[])", NORMAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrUpdateDistributionConfigs(DistributionConfig[])", FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrUpdateDistributionConfigs(DistributionConfig[])", CRITICAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "removeDistributionConfig(Schema,address)", CRITICAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "removeDistributionConfig(Schema,address)", FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "removeDistributionConfig(Schema,address)", NORMAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrRemoveAssetFromPrime(address,bool)", CRITICAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrRemoveAssetFromPrime(address,bool)", FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [PSR, "addOrRemoveAssetFromPrime(address,bool)", NORMAL_TIMELOCK],
+      },
+      {
+        target: PSR_PROXY_ADMIN,
+        signature: "changeProxyAdmin(address,address)",
+        params: [PSR, PROXY_ADMIN],
+      },
+      {
+        target: PSR,
+        signature: "addOrUpdateDistributionConfigs((uint8,uint8,address)[])",
+        params: [
+          [
+            [0, 50, RISK_FUND],
+            [0, 50, TREASURY],
+            [1, 40, RISK_FUND],
+            [1, 60, TREASURY],
+          ],
+        ],
+      },
+      {
+        target: PSR,
+        signature: "setPoolRegistry(address)",
+        params: [POOL_REGISTRY],
       },
     ],
     meta,
