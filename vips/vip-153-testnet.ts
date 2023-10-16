db88e2da09f5097b1fd1115fe403abd12755ec0f
Narayan Prusty <arsenal.narayan@gmail.com>
2023-10-16 18:48:39 +0100

    fix: added testnet vip

--- vips/vip-153-testnet.ts
+++ vips/vip-153-testnet.ts
@@ -1,15 +1,16 @@
 import { ProposalType } from "../src/types";
 import { makeProposal } from "../src/utils";
 
-const ACCOUNT = "0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706";
-const ACM = "0x45f8a08f534f34a97187626e05d4b6648eeaa9aa";
-const PRIME = "0x4e446d6Fe16c4e95eEAd7f963C9312Cf4c280270"
-const PLP = "0x2B8e226a462138250df2551bb499ad71218c4353";
+const VBNBAdmin_ADDRESS = "0x04109575c1dbB4ac2e59e60c783800ea10441BBe";
+const RISK_FUND = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
+const PSR = "0x25c7c7D6Bf710949fD7f03364E9BA19a1b3c10E3";
+const TREASURY = "0x8b293600C50D6fbdc6Ed4251cc75ECe29880276f";
+const POOL_REGISTRY = "0xC85491616Fa949E048F3aAc39fbf5b0703800667";
 
 export const vip153Testnet = () => {
   const meta = {
     version: "v2",
-    title: "Allow Shared Account access to Prime and PLP",
+    title: "Use new PSR and Add New Config",
     description: ``,
     forDescription: "I agree that Venus Protocol should proceed",
     againstDescription:
@@ -21,54 +22,26 @@ export const vip153Testnet = () => {
   return makeProposal(
     [
       {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "updateAlpha(uint128,uint128)", ACCOUNT],
+        target: VBNBAdmin_ADDRESS,
+        signature: "setProtocolShareReserve(address)",
+        params: [PSR],
       },
       {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "updateMultipliers(address,uint256,uint256)", ACCOUNT],
+        target: PSR,
+        signature: "addOrUpdateDistributionConfigs((uint8,uint8,address)[])",
+        params: [
+          [
+            [0, 40, RISK_FUND],
+            [0, 60, TREASURY],
+            [1, 50, RISK_FUND],
+            [1, 50, TREASURY],
+          ],
+        ],
       },
       {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "addMarket(address,uint256,uint256)", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "setLimit(uint256,uint256)", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "issue(bool,address[])", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "burn(address)", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PRIME, "togglePause()", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PLP, "pauseFundsTransfer()", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PLP, "resumeFundsTransfer()", ACCOUNT],
-      },
-      {
-        target: ACM,
-        signature: "giveCallPermission(address,string,address)",
-        params: [PLP, "setTokensDistributionSpeed(address[],uint256[])", ACCOUNT],
+        target: PSR,
+        signature: "setPoolRegistry(address)",
+        params: [POOL_REGISTRY],
       },
     ],
     meta,
