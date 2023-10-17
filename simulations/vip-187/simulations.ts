bb4ef275bb0d19965c281602d995685494eed508
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-10-17 15:57:08 +0200

    feat: add pre-vip checks and final description for VIP 187

--- simulations/vip-187/simulations.ts
+++ simulations/vip-187/simulations.ts
@@ -1,7 +1,7 @@
 import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
 import { checkInterestRate } from "../../src/vip-framework/checks/interestRateModel";
-import { vip186 } from "../../vips/vip-186";
+import { vip187 } from "../../vips/vip-187";
 import VTOKEN_CORE_ABI from "./abi/VToken_core.json";
 import VTOKEN_IL_ABI from "./abi/VToken_il.json";
 
@@ -9,7 +9,25 @@ const vETH = "0xf508fcd89b8bd15579dc79a6827cb4686a3592c8";
 const vUSDT_Stablecoins = "0x5e3072305F9caE1c7A82F6Fe9E38811c74922c3B";
 
 forking(32681348, () => {
-  testVip("VIP-186 Risk Parameters Update", vip186(), {
+  describe("Pre-VIP behavior", async () => {
+    it("Check current interest rate model", async () => {
+      await checkInterestRate(vETH, "vETH", {
+        base: "0",
+        multiplier: "0.09",
+        jump: "2",
+        kink: "0.75",
+      });
+
+      await checkInterestRate(vUSDT_Stablecoins, "vUSDT_Stablecoins", {
+        base: "0.02",
+        multiplier: "0.1",
+        jump: "2.5",
+        kink: "0.8",
+      });
+    });
+  });
+
+  testVip("VIP-187 Risk Parameters Update", vip187(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(
         txResponse,
