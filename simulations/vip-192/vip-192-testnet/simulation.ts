c93e2779cfe0922058d9b24929659fbef78fcfb4
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-10-26 19:32:09 +0200

    test: accept stale prices for a long time

--- simulations/vip-192/vip-192-testnet/simulation.ts
+++ simulations/vip-192/vip-192-testnet/simulation.ts
@@ -5,7 +5,7 @@ import { vip192Testnet } from "../../../vips/vip-192/vip-192-tetsnet";
 import SETTER_FACET_ABI from "./abis/SetterFacet.json";
 import PRIME_ABI from "./abis/Prime.json";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
-import { expectEvents } from "../../../src/utils";
+import { expectEvents, setMaxStalePeriodInChainlinkOracle } from "../../../src/utils";
 import { expect } from "chai";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { TransactionResponse } from "@ethersproject/providers";
@@ -13,6 +13,39 @@ import { TransactionResponse } from "@ethersproject/providers";
 const PRIME_LIQUIDITY_PROVIDER = "0xce20cACeF98DC03b2e30cD63b7B56B018d171E9c";
 const PRIME = "0x3BE88819113535177af7E87203bC48416098b160";
 const STAKED_USER = "0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706";
+const CHAINLINK_ORACLE = "0xCeA29f1266e880A1482c06eD656cD08C148BaA32";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+
+interface vTokenConfig {
+  name: string;
+  address: string;
+  price: string;
+  assetAddress: string;
+  feed: string;
+}
+
+const vTokens: vTokenConfig[] = [
+  {
+    name: "vUSDC",
+    assetAddress: "0x16227D60f7a0e586C66B005219dfc887D13C9531",
+    feed: "0x90c069C4538adAc136E051052E14c1cD799C41B7",
+  },
+  {
+    name: "vUSDT",
+    assetAddress: "0xA11c8D9DC9b66E209Ef60F0C8D969D3CD988782c",
+    feed: "0xEca2605f0BCF2BA5966372C99837b1F182d3D620",
+  },
+  {
+    name: "vETH",
+    assetAddress: "0x98f7A83361F7Ac8765CcEBAB1425da6b341958a7",
+    feed: "0x143db3CEEfbdfe5631aDD3E50f7614B6ba708BA7",
+  },
+  {
+    name: "vBTC",
+    assetAddress: "0xA808e341e8e723DC6BA0Bb5204Bafc2330d7B8e4",
+    feed: "0x5741306c21795FdCBb9b265Ea0255F499DFe515C",
+  },
+];
 
 forking(34544154, () => {
   describe("Pre-VIP behavior", () => {
@@ -54,6 +87,11 @@ forking(34544154, () => {
       const signer = await ethers.getSigner(STAKED_USER);
       prime = await ethers.getContractAt(PRIME_ABI, PRIME, signer);
       primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+
+      for (let i = 0; i < vTokens.length; i++) {
+        const vToken = vTokens[i];
+        await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, vToken.assetAddress, vToken.feed, NORMAL_TIMELOCK);
+      }
     });
 
     it("prime markets", async () => {
