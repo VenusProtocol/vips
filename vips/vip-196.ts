7c790c1119798457b59181c4e0de16badc21d37d
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-11-02 16:57:49 +0530

    feat: Chaos labs recommendations for the week October 31st, 2023

---
+++ vips/vip-196.ts
@@ -0,0 +1,38 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../src/types";
+import { makeProposal } from "../src/utils";
+
+const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+const VCAKE = "0x86aC3974e2BD0d60825230fa6F355fF11409df5c";
+const VWBETH = "0x6CFdEc747f37DAf3b87a35a1D9c8AD3063A1A8A0";
+
+export const vip196 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-196 Risk Parameters Update",
+    description: ``,
+
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: COMPTROLLER,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[VCAKE], [parseUnits("14000000", 18)]],
+      },
+
+      {
+        target: COMPTROLLER,
+        signature: "_setCollateralFactor(address,uint256)",
+        params: [VWBETH, parseUnits(".80", 18)],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
