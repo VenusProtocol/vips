09fc03458aeb9a2c9851c16ff1cfe717bf11d0a2
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-11-02 19:57:55 +0100

    feat: add description for the VIP adjusting risk parameters

--- vips/vip-196.ts
+++ vips/vip-196.ts
@@ -10,8 +10,19 @@ const VWBETH = "0x6CFdEc747f37DAf3b87a35a1D9c8AD3063A1A8A0";
 export const vip196 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-196 Risk Parameters Update",
-    description: ``,
+    title: "VIP-197 Risk Parameters Adjustments (CAKE and WBETH)",
+    description: `#### Description
+
+This VIP will perform the following Risk Parameters actions as per Chaos Labs latest recommendations in this Venus community forum publication: [Risk Parameter Updates 10/31/2023](https://community.venus.io/t/chaos-labs-risk-parameter-updates-10-31-2023/3874).
+
+- **CAKE** (Core pool)
+    - Increase supply cap to 14M (from 7M)
+- **WBETH** (Core pool)
+    - Increase Collateral Factor to 0.8 (from 0.7)
+
+Complete analysis and details of these recommendations are available in the above publication.
+
+VIP simulation: https://github.com/VenusProtocol/vips/pull/101`,
 
     forDescription: "I agree that Venus Protocol should proceed with this proposal",
     againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
