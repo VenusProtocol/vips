96400aa5ea1c87e7f7b8d55ded995ff765a2d009
narayanprusty <arsenal.narayan@gmail.com>
2023-11-04 00:33:12 +0000

    fix: check interest accrued

--- simulations/vip-194/vip-194-testnet/simulation.ts
+++ simulations/vip-194/vip-194-testnet/simulation.ts
@@ -5,13 +5,17 @@ import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip194Testnet } from "../../../vips/vip-194/vip-194-testnet";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
 
 const PRIME_LIQUIDITY_PROVIDER = "0xce20cACeF98DC03b2e30cD63b7B56B018d171E9c";
+const PRIME = "0xb13Ea8C39594449B2AB5555769580BDB23f5E2Cf";
+const STAKED_USER = "0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706";
 
 const ETH = "0x98f7A83361F7Ac8765CcEBAB1425da6b341958a7";
 const BTC = "0xA808e341e8e723DC6BA0Bb5204Bafc2330d7B8e4";
 const USDC = "0x16227D60f7a0e586C66B005219dfc887D13C9531";
 const USDT = "0xA11c8D9DC9b66E209Ef60F0C8D969D3CD988782c";
+const vETH = "0x162D005F0Fff510E54958Cfc5CF32A3180A84aab";
 
 forking(34696892, () => {
   describe("Pre-VIP behavior", () => {
@@ -67,6 +71,12 @@ forking(34696892, () => {
     it("paused", async () => {
       const paused = await primeLiquidityProvider.paused();
       expect(paused).to.be.equal(true);
+    });
+
+    it("rewards", async () => {
+      await mine(1000)
+      let rewards = await prime.callStatic.getInterestAccrued(vETH, STAKED_USER);
+      expect(rewards).to.be.not.equal("0");
     });
   });
 });
--- simulations/vip-194/vip-194/simulation.ts
+++ simulations/vip-194/vip-194/simulation.ts
@@ -5,13 +5,18 @@ import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip194 } from "../../../vips/vip-194/vip-194";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
+import PRIME_ABI from "./abis/Prime.json";
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
 
 const PRIME_LIQUIDITY_PROVIDER = "0x103Af40c4C30A564A2158D7Db6c57a0802b9217A";
+const PRIME = "0x78d8dD5b0003723826E1FDb2031e9466000469Fe";
+const STAKED_USER = "0x2e7a15e186cc81f7efc4bf7df12dbd5e3db4fefb";
 
 const ETH = "0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
 const BTC = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
 const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
 const USDT = "0x55d398326f99059fF775485246999027B3197955";
+const vETH = "0xf508fCD89b8bd15579dc79A6827cB4686A3592c8";
 
 forking(33118003, () => {
   describe("Pre-VIP behavior", () => {
@@ -45,9 +50,11 @@ forking(33118003, () => {
 
   describe("Post-VIP behavior", async () => {
     let primeLiquidityProvider: Contract;
+    let prime: Contract;
 
     before(async () => {
       primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+      prime = await ethers.getContractAt(PRIME_ABI, PRIME);
     });
 
     it("speeds", async () => {
@@ -67,6 +74,12 @@ forking(33118003, () => {
     it("paused", async () => {
       const paused = await primeLiquidityProvider.paused();
       expect(paused).to.be.equal(true);
+    });
+
+    it("rewards", async () => {
+      await mine(1000)
+      let rewards = await prime.callStatic.getInterestAccrued(vETH, STAKED_USER);
+      expect(rewards).to.be.not.equal("0");
     });
   });
 });
