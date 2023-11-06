27b35b9fa677ced36ad377ae63f8fc2d58286300
Narayan Prusty <arsenal.narayan@gmail.com>
2023-11-06 16:39:34 +0000

    fix: check if prime is paused in testnet

--- simulations/vip-195/vip-195-testnet/simulation.ts
+++ simulations/vip-195/vip-195-testnet/simulation.ts
@@ -5,6 +5,7 @@ import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip195Testnet } from "../../../vips/vip-195/vip-195-testnet";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
+import PRIME_ABI from "./abis/Prime.json";
 const PRIME = "0xb13Ea8C39594449B2AB5555769580BDB23f5E2Cf";
 
 const PRIME_LIQUIDITY_PROVIDER = "0xce20cACeF98DC03b2e30cD63b7B56B018d171E9c";
@@ -28,14 +29,19 @@ forking(34696892, () => {
 
   describe("Post-VIP behavior", async () => {
     let primeLiquidityProvider: Contract;
+    let prime: Contract;
 
     before(async () => {
       primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+      prime = await ethers.getContractAt(PRIME_ABI, PRIME);
     });
 
     it("paused", async () => {
       const paused = await primeLiquidityProvider.paused();
       expect(paused).to.be.equal(false);
+
+      const primePaused = await prime.paused();
+      expect(primePaused).to.be.equal(false);
     });
   });
 });
