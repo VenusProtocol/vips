d786f2f5fd83c8477f8c7894f85eed2fb1a644f8
Narayan Prusty <arsenal.narayan@gmail.com>
2023-11-06 17:04:37 +0000

    fix: added tests for claimInterest

--- simulations/vip-195/vip-195/simulation.ts
+++ simulations/vip-195/vip-195/simulation.ts
@@ -6,9 +6,54 @@ import { forking, testVip } from "../../../src/vip-framework";
 import { vip195 } from "../../../vips/vip-195/vip-195";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
 import PRIME_ABI from "./abis/Prime.json";
+import ERC20_ABI from "./abis/ERC20.json";
+import { impersonateAccount, mine } from "@nomicfoundation/hardhat-network-helpers";
+import { setMaxStalePeriodInChainlinkOracle } from "../../../src/utils";
 
 const PRIME_LIQUIDITY_PROVIDER = "0x103Af40c4C30A564A2158D7Db6c57a0802b9217A";
 const PRIME = "0x78d8dD5b0003723826E1FDb2031e9466000469Fe";
+const STAKED_USER = "0x07cf6eb791b038ecc157a81738b865154579c911";
+const CHAINLINK_ORACLE = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+
+const ETH = "0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
+const BTC = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
+const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
+const USDT = "0x55d398326f99059fF775485246999027B3197955";
+const vETH = "0xf508fCD89b8bd15579dc79A6827cB4686A3592c8";
+
+const vTokens: vTokenConfig[] = [
+  {
+    name: "vUSDC",
+    assetAddress: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+    feed: "0x51597f405303c4377e36123cbc172b13269ea163",
+    marketAddress: "0xecA88125a5ADbe82614ffC12D0DB554E2e2867C8"
+  },
+  {
+    name: "vUSDT",
+    assetAddress: "0x55d398326f99059fF775485246999027B3197955",
+    feed: "0xb97ad0e74fa7d920791e90258a6e2085088b4320",
+    marketAddress: "0xfD5840Cd36d94D7229439859C0112a4185BC0255"
+  },
+  {
+    name: "vETH",
+    assetAddress: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    feed: "0x9ef1b8c0e4f7dc8bf5719ea496883dc6401d5b2e",
+    marketAddress: "0xf508fCD89b8bd15579dc79A6827cB4686A3592c8"
+  },
+  {
+    name: "vBTC",
+    assetAddress: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+    feed: "0x264990fbd0a4796a3e3d8e37c4d5f87a3aca5ebf",
+    marketAddress: "0x882C173bC7Ff3b7786CA16dfeD3DFFfb9Ee7847B"
+  },
+  {
+    name: "vXVS",
+    assetAddress: "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63",
+    feed: "0xbf63f430a79d4036a5900c19818aff1fa710f206",
+    marketAddress: "0x151B1e2635A717bcDc836ECd6FbB62B674FE3E1D"
+  },
+];
 
 forking(33118003, () => {
   describe("Pre-VIP behavior", () => {
@@ -35,10 +80,20 @@ forking(33118003, () => {
   describe("Post-VIP behavior", async () => {
     let primeLiquidityProvider: Contract;
     let prime: Contract;
+    let eth: Contract;
 
     before(async () => {
+      impersonateAccount(STAKED_USER);
+      const signer = await ethers.getSigner(STAKED_USER);
+
       primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
-      prime = await ethers.getContractAt(PRIME_ABI, PRIME);
+      prime = await ethers.getContractAt(PRIME_ABI, PRIME, signer);
+      eth = await ethers.getContractAt(ERC20_ABI, ETH);
+
+      for (let i = 0; i < vTokens.length; i++) {
+        const vToken = vTokens[i];
+        await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, vToken.assetAddress, vToken.feed, NORMAL_TIMELOCK);
+      }
     });
 
     it("paused", async () => {
@@ -47,6 +102,15 @@ forking(33118003, () => {
 
       const primePaused = await prime.paused();
       expect(primePaused).to.be.equal(false);
+    });
+
+    it("rewards", async () => {
+      await prime.claim();
+      await mine(1000)
+
+      expect(await eth.balanceOf(STAKED_USER)).to.be.equal("0");
+      await prime["claimInterest(address)"](vETH);
+      expect(await eth.balanceOf(STAKED_USER)).to.be.equal("24463096064814388");
     });
   });
 });
