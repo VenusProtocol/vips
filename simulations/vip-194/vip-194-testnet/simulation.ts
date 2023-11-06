7905f91d0e08b6f1db88897a053847407f89aa80
Narayan Prusty <arsenal.narayan@gmail.com>
2023-11-06 16:29:02 +0000

    fix: claim and check rewards in testnet

--- simulations/vip-194/vip-194-testnet/simulation.ts
+++ simulations/vip-194/vip-194-testnet/simulation.ts
@@ -5,17 +5,21 @@ import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip194Testnet } from "../../../vips/vip-194/vip-194-testnet";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
-import { mine } from "@nomicfoundation/hardhat-network-helpers";
+import { impersonateAccount, mine } from "@nomicfoundation/hardhat-network-helpers";
+import PRIME_ABI from "./abis/Prime.json";
+import { setMaxStalePeriodInChainlinkOracle } from "../../../src/utils";
 
 const PRIME_LIQUIDITY_PROVIDER = "0xce20cACeF98DC03b2e30cD63b7B56B018d171E9c";
 const PRIME = "0xb13Ea8C39594449B2AB5555769580BDB23f5E2Cf";
 const STAKED_USER = "0x2Ce1d0ffD7E869D9DF33e28552b12DdDed326706";
+const CHAINLINK_ORACLE = "0xCeA29f1266e880A1482c06eD656cD08C148BaA32";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
 
 const ETH = "0x98f7A83361F7Ac8765CcEBAB1425da6b341958a7";
 const BTC = "0xA808e341e8e723DC6BA0Bb5204Bafc2330d7B8e4";
 const USDC = "0x16227D60f7a0e586C66B005219dfc887D13C9531";
 const USDT = "0xA11c8D9DC9b66E209Ef60F0C8D969D3CD988782c";
-const vETH = "0x162D005F0Fff510E54958Cfc5CF32A3180A84aab";
+const vBTC = "0xb6e9322C49FD75a367Fcb17B0Fcd62C5070EbCBe"
 
 const vTokens: vTokenConfig[] = [
   {
@@ -76,9 +80,18 @@ forking(34696892, () => {
 
   describe("Post-VIP behavior", async () => {
     let primeLiquidityProvider: Contract;
+    let prime: Contract;
 
     before(async () => {
+      impersonateAccount(STAKED_USER);
+      const signer = await ethers.getSigner(STAKED_USER);
+      prime = await ethers.getContractAt(PRIME_ABI, PRIME, signer);
       primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+
+      for (let i = 0; i < vTokens.length; i++) {
+        const vToken = vTokens[i];
+        await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, vToken.assetAddress, vToken.feed, NORMAL_TIMELOCK);
+      }
     });
 
     it("speeds", async () => {
@@ -101,9 +114,10 @@ forking(34696892, () => {
     });
 
     it("rewards", async () => {
+      await prime.claim();
       await mine(1000)
-      let rewards = await prime.callStatic.getInterestAccrued(vETH, STAKED_USER);
-      expect(rewards).to.be.not.equal("0");
+      let rewards = await prime.callStatic.getInterestAccrued(vBTC, STAKED_USER);
+      expect(rewards).to.be.equal("1261574074073998");
     });
   });
 });
