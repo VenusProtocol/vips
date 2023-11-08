dfe9d786318c0fbba5e57726b6cc260c874e68ab
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-11-08 15:41:50 +0100

    refactor: use fresh values in the simulation of VIP-199

--- simulations/vip-199/simulations.ts
+++ simulations/vip-199/simulations.ts
@@ -1,11 +1,15 @@
 import { expect } from "chai";
 import { BigNumber } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import { BNB_AMOUNT, RECEIVER, vip199 } from "../../vips/vip-199";
+import { vip199 } from "../../vips/vip-199";
 import TREASURY_ABI from "./abi/VTreasury.json";
+
+const RECEIVER = "0x6657911f7411765979da0794840d671be55ba273";
+const BNB_AMOUNT = parseUnits("26438.868831443265017372", 18);
 
 forking(33276563, () => {
   let preBalance: BigNumber;
