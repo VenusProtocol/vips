e83f4378857037ff05e944a2bd6aa2b01df0bed0
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-11-10 17:36:42 +0100

    feat: update the description of VIP 200 adding links

--- vips/vip-200/vip-200-testnet.ts
+++ vips/vip-200/vip-200-testnet.ts
@@ -11,17 +11,17 @@ export const vip200Testnet = () => {
   const meta = {
     version: "v2",
     title: "VIP-200 Isolated Lending Pools: Configure Liquidity Mining (PLANET)",
-    description: `#### Summary 
+    description: `#### Summary
 
 If passed, this VIP will set the last block with rewards for the PLANET market in the DeFi pool.
 
-#### Description 
+#### Description
 
 This VIP sets the last block with rewards for the PLANET market in the DeFi pool.
 
-In vPLANET_DeFi, the last block with rewards will be 34150223. That means around 28 days (806,400 blocks) after being enabled.
+In [vPLANET_DeFi](https://app.venus.io/#/isolated-pools/pool/0x3344417c9360b963ca93A4e8305361AEde340Ab9/market/0xFf1112ba7f88a53D4D23ED4e14A117A2aE17C6be), the last block with rewards will be 34150223. That means around 28 days (806,400 blocks) after [being enabled](https://app.venus.io/#/governance/proposal/198).
 
-Simulation of the VIP: https://github.com/VenusProtocol/vips/pull/106`,
+Simulation of the VIP: [https://github.com/VenusProtocol/vips/pull/106](https://github.com/VenusProtocol/vips/pull/106)`,
     forDescription: "Process to configure Liquidity Mining",
     againstDescription: "Defer configuration of Liquidity Mining",
     abstainDescription: "No opinion on the matter",
