14f5b7c9d084784174c8d1e0dbb9557e688b94cc
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-11-10 18:10:58 +0530

    feat: add multisig proposal for remote xvs bridge configurations

---
+++ multisig/proposals/vip-003/vip-003-sepolia.ts
@@ -0,0 +1,143 @@
+import { makeProposal } from "../../../src/utils";
+import { ADDRESSES } from "../../helpers/config";
+
+const { sepoliaContracts } = ADDRESSES;
+
+export const vip003 = () => {
+  return makeProposal([
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setSendVersion(uint16)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setReceiveVersion(uint16)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "forceResumeReceive(uint16,bytes)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setOracle(address)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        sepoliaContracts.XVS_BRIDGE_ADMIN,
+        "setMaxSingleTransactionLimit(uint16,uint256)",
+        sepoliaContracts.TIMELOCK,
+      ],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setMaxDailyLimit(uint16,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        sepoliaContracts.XVS_BRIDGE_ADMIN,
+        "setMaxSingleReceiveTransactionLimit(uint16,uint256)",
+        sepoliaContracts.TIMELOCK,
+      ],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setMaxDailyReceiveLimit(uint16,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "pause()", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "unpause()", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "removeTrustedRemote(uint16)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "dropFailedMessage(uint16,bytes)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "fallbackWithdraw(address,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setPrecrime(address)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setMinDstGas(uint16,uint16,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setPayloadSizeLimit(uint16,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setWhitelist(address,bool)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setConfig(uint16,uint16,uint256,bytes)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS, "mint(address,uint256)", sepoliaContracts.XVS_PROXY_OFT_DEST],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS, "burn(address,uint256)", sepoliaContracts.XVS_PROXY_OFT_DEST],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS, "setMintCap(address,uint256)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "setTrustedRemoteAddress(uint16,bytes)", sepoliaContracts.TIMELOCK],
+    },
+    {
+      target: sepoliaContracts.ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [sepoliaContracts.XVS_BRIDGE_ADMIN, "transferBridgeOwnership(address)", sepoliaContracts.TIMELOCK],
+    },
+    { target: sepoliaContracts.XVS_BRIDGE_ADMIN, signature: "acceptOwnership()", params: [] },
+    {
+      target: sepoliaContracts.XVS_BRIDGE_ADMIN,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [10102, "0x0000000000000000000000000000000000000001"],
+    },
+    {
+      target: sepoliaContracts.XVS,
+      signature: "setMintCap(address,uint256)",
+      params: [sepoliaContracts.XVS_PROXY_OFT_DEST, "100000000000000000000"],
+    },
+  ]);
+};
---
+++ multisig/simulations/vip-003/vip-003-sepolia/simulations.ts
@@ -0,0 +1,89 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
+import { ADDRESSES } from "../../../helpers/config";
+import { vip003 } from "../../../proposals/vip-003/vip-003-sepolia";
+import XVS_ABI from "./abi/xvs.json";
+import XVS_BRIDGE_ADMIN_ABI from "./abi/xvsBridgeAdmin.json";
+import XVS_BRIDGE_ABI from "./abi/xvsProxyOFTDest.json";
+
+const { sepoliaContracts } = ADDRESSES;
+const XVS = sepoliaContracts.XVS;
+const XVS_BRIDGE_ADMIN = sepoliaContracts.XVS_BRIDGE_ADMIN;
+const XVS_BRIDGE = sepoliaContracts.XVS_PROXY_OFT_DEST;
+const SEPOLIA_MULTISIG = sepoliaContracts.TIMELOCK;
+
+forking(4665307, () => {
+  let xvs: Contract;
+  let xvsBridgeAdmin: Contract;
+  let xvsBridge: Contract;
+
+  before(async () => {
+    xvs = await ethers.getContractAt(XVS_ABI, XVS);
+    xvsBridgeAdmin = await ethers.getContractAt(XVS_BRIDGE_ADMIN_ABI, XVS_BRIDGE_ADMIN);
+    xvsBridge = await ethers.getContractAt(XVS_BRIDGE_ABI, XVS_BRIDGE);
+  });
+
+  describe("Pre-Execution state", () => {
+    it("Bridge Owner != sepolia multisig", async () => {
+      const owner = await xvsBridgeAdmin.owner();
+      expect(owner).not.equal(SEPOLIA_MULTISIG);
+    });
+
+    it("Trusted remote should not exist", async () => {
+      await expect(xvsBridge.getTrustedRemoteAddress(10102)).to.be.revertedWith("LzApp: no trusted path record");
+    });
+
+    it("Mint limit = 0", async () => {
+      const cap = await xvs.minterToCap(XVS_BRIDGE);
+      expect(cap).equals(0);
+    });
+  });
+
+  describe("Post-Execution state", () => {
+    before(async () => {
+      await pretendExecutingVip(vip003());
+    });
+    it("Should set bridge owner to multisig", async () => {
+      const owner = await xvsBridgeAdmin.owner();
+      expect(owner).equals(SEPOLIA_MULTISIG);
+    });
+
+    it("Should set trusted remote address in bridge", async () => {
+      const trustedRemote = await xvsBridge.getTrustedRemoteAddress(10102);
+      expect(trustedRemote).equals("0x0000000000000000000000000000000000000001");
+    });
+
+    it("Should set minting limit in XVS token", async () => {
+      const cap = await xvs.minterToCap(XVS_BRIDGE);
+      expect(cap).equals("100000000000000000000");
+    });
+
+    it("Should set correct token address in bridge", async () => {
+      const token = await xvsBridge.token();
+      expect(token).equals(XVS);
+    });
+
+    it("Should set correct max daily limit", async () => {
+      const token = await xvsBridge.chainIdToMaxDailyLimit(10102);
+      expect(token).equals("500000000000000000000");
+    });
+
+    it("Should set correct max single limit", async () => {
+      const token = await xvsBridge.chainIdToMaxSingleTransactionLimit(10102);
+      expect(token).equals("10000000000000000000");
+    });
+
+    it("Should set correct max daily receive limit", async () => {
+      const token = await xvsBridge.chainIdToMaxDailyReceiveLimit(10102);
+      expect(token).equals("50000000000000000000");
+    });
+
+    it("Should set correct max single receive limit", async () => {
+      const token = await xvsBridge.chainIdToMaxSingleReceiveTransactionLimit(10102);
+      expect(token).equals("10000000000000000000");
+    });
+  });
+});
