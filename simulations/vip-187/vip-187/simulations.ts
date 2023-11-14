44acb6036d48f9849cff8316384013cecdbc3618
0xlucian <0xluciandev@gmail.com>
2023-11-14 18:03:33 +0300

    refactor: extend the simulation and add assertions for the RedStone oracle config

--- simulations/vip-187/vip-187/simulations.ts
+++ simulations/vip-187/vip-187/simulations.ts
@@ -7,6 +7,7 @@ import { expectEvents, setMaxStalePeriodInChainlinkOracle } from "../../../src/u
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip187 } from "../../../vips/vip-187/vip-187";
 import BOUND_VALIDATOR_ABI from "./abi/boundValidator.json";
+import CHAINLINK_ORACLE_ABI from "./abi/chainlinkOracle.json";
 import PROXY_ADMIN_ABI from "./abi/proxyAdmin.json";
 import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
 
@@ -21,13 +22,20 @@ const VBNB_UNDERLYING = "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB";
 const BNB_PRICE_FEED = "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE";
 const VAI_ADDRESS = "0x4BD17003473389A42DAF6a0a729f6Fdb328BbBd7";
 const TRX = "0xCE7de646e7208a4Ef112cb6ed5038FA6cC6b12e3";
-const TRX_PRICE_FEED = "0xF4C5e535756D11994fCBB12Ba8adD0192D9b88be";
+const TRX_OLD = "0x85EAC5Ac2F758618dFa09bDbe0cf174e7d574D5B";
+const CHAINLINK_TRX_FEED = "0xF4C5e535756D11994fCBB12Ba8adD0192D9b88be";
+const REDSTONE_TRX_FEED = "0xa17362dd9ad6d0af646d7c8f8578fddbfc90b916";
 const CHAINLINK_ORACLE = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
 const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+const REDSTONE_ORACLE = "0x8455EFA4D7Ff63b8BFD96AdD889483Ea7d39B70a";
 
-forking(32739800, () => {
+const PRICE_LOWER_BOUND = parseUnits("0.99", 18);
+const PRICE_UPPER_BOUND = parseUnits("1.01", 18);
+
+forking(33486800, () => {
   const provider = ethers.provider;
   let resilientOracle: ethers.Contract;
+  let redStoneOracle: ethers.Contract;
   let defaultProxyAdmin: ethers.Contract;
   let boundValidator: ethers.Contract;
 
@@ -36,9 +44,10 @@ forking(32739800, () => {
     const timelock = await ethers.getSigner(NORMAL_TIMELOCK);
 
     resilientOracle = new ethers.Contract(RESILIENT_ORACLE_PROXY, RESILIENT_ORACLE_ABI, timelock);
+    redStoneOracle = new ethers.Contract(REDSTONE_ORACLE, CHAINLINK_ORACLE_ABI, provider);
     boundValidator = new ethers.Contract(BOUND_VALIDATOR_PROXY, BOUND_VALIDATOR_ABI, timelock);
     defaultProxyAdmin = new ethers.Contract(DEFAULT_PROXY_ADMIN, PROXY_ADMIN_ABI, provider);
-    await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, TRX, TRX_PRICE_FEED, NORMAL_TIMELOCK);
+    await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, TRX, CHAINLINK_TRX_FEED, NORMAL_TIMELOCK);
     await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, VBNB_UNDERLYING, BNB_PRICE_FEED, NORMAL_TIMELOCK);
   });
 
@@ -47,15 +56,40 @@ forking(32739800, () => {
       const implementation = await defaultProxyAdmin.getProxyImplementation(RESILIENT_ORACLE_PROXY);
       expect(implementation).to.equal(OLD_RESILIENT_ORACLE_IMPLEMENTATION);
     });
+    it("Validate pivot oracle is not set for TRX_OLD", async () => {
+      const tokenConfig = await resilientOracle.getTokenConfig(TRX_OLD);
+      expect(tokenConfig.oracles[1]).to.equal(ethers.constants.AddressZero);
+      expect(tokenConfig.enableFlagsForOracles[1]).to.equal(false);
+    });
+    it("Validate pivot oracle is not set for TRX", async () => {
+      const tokenConfig = await resilientOracle.getTokenConfig(TRX);
+      expect(tokenConfig.oracles[1]).to.equal(ethers.constants.AddressZero);
+      expect(tokenConfig.enableFlagsForOracles[1]).to.equal(false);
+    });
+    it("Pending owner of RedStone oracle is Normal Timelock ", async () => {
+      const pendingOwner = await redStoneOracle.pendingOwner();
+      expect(pendingOwner).to.equal(NORMAL_TIMELOCK);
+    });
   });
 
-  testVip("vip187Testnet", vip187(), {
+  testVip("vip187", vip187(), {
+    proposer: "0xc444949e0054a23c44fc45789738bdf64aed2391",
+    supporter: "0x55A9f5374Af30E3045FB491f1da3C2E8a74d168D",
     callbackAfterExecution: async txResponse => {
       await expectEvents(txResponse, [RESILIENT_ORACLE_ABI], ["Upgraded"], [1]);
+      await expectEvents(txResponse, [CHAINLINK_ORACLE_ABI], ["TokenConfigAdded"], [2]);
+      await expectEvents(txResponse, [BOUND_VALIDATOR_ABI], ["ValidateConfigAdded"], [2]);
+      await expectEvents(txResponse, [RESILIENT_ORACLE_ABI], ["OracleSet"], [2]);
+      await expectEvents(txResponse, [RESILIENT_ORACLE_ABI], ["OracleEnabled"], [2]);
     },
   });
 
   describe("Post-VIP behavior", () => {
+    before(async () => {
+      //NEED TO SET STALE PRICE AFTER VIP EXECUTION BECAUSE WE ARE ACCEPTION OWNERSHIP THERE FROM TIMELOCK
+      await setMaxStalePeriodInChainlinkOracle(REDSTONE_ORACLE, TRX, REDSTONE_TRX_FEED, NORMAL_TIMELOCK);
+    });
+
     it("ResilientOracle proxy should have new implementation", async () => {
       const implementation = await defaultProxyAdmin.getProxyImplementation(RESILIENT_ORACLE_PROXY);
       expect(implementation).to.equal(NEW_RESILIENT_ORACLE_IMPLEMENTATION);
@@ -66,6 +100,31 @@ forking(32739800, () => {
       expect(await resilientOracle.vBnb()).to.equal(VBNB_ADDRESS);
       expect(await resilientOracle.vai()).to.equal(VAI_ADDRESS);
       expect(await resilientOracle.accessControlManager()).to.equal(ACM);
+    });
+
+    it("Owner of RedStone oracle is Normal Timelock ", async () => {
+      const owner = await redStoneOracle.owner();
+      expect(owner).to.equal(NORMAL_TIMELOCK);
+    });
+    it("Validate pivot oracle is set for TRX_OLD", async () => {
+      const tokenConfig = await resilientOracle.getTokenConfig(TRX_OLD);
+      expect(tokenConfig.oracles[1]).to.equal(REDSTONE_ORACLE);
+      expect(tokenConfig.enableFlagsForOracles[1]).to.equal(true);
+    });
+    it("Validate pivot oracle is set for TRX", async () => {
+      const tokenConfig = await resilientOracle.getTokenConfig(TRX);
+      expect(tokenConfig.oracles[1]).to.equal(REDSTONE_ORACLE);
+      expect(tokenConfig.enableFlagsForOracles[1]).to.equal(true);
+    });
+    it("Validate upper and lower bounds for TRX", async () => {
+      const validateConfig = await boundValidator.validateConfigs(TRX);
+      expect(validateConfig.upperBoundRatio).to.equal(PRICE_UPPER_BOUND);
+      expect(validateConfig.lowerBoundRatio).to.equal(PRICE_LOWER_BOUND);
+    });
+    it("Validate upper and lower bounds for TRX_OLD", async () => {
+      const validateConfig = await boundValidator.validateConfigs(TRX_OLD);
+      expect(validateConfig.upperBoundRatio).to.equal(PRICE_UPPER_BOUND);
+      expect(validateConfig.lowerBoundRatio).to.equal(PRICE_LOWER_BOUND);
     });
 
     it("Should return a valid price", async () => {
