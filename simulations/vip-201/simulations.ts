b4fb2c1cf430e45093e3630a947ad58d36abe662
0xlucian <0xluciandev@gmail.com>
2023-11-15 14:40:09 +0300

    chore: add simulation for BUSD repay on behalf of user VIP

---
+++ simulations/vip-201/simulations.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { expectEvents } from "../../src/utils";
+import { forking, testVip } from "../../src/vip-framework";
+import { vip201 } from "../../vips/vip-201";
+import VBEP20_ABI from "./abi/VBep20Abi.json";
+import VTREASURY_ABI from "./abi/VTreasury.json";
+
+const vBUSD = "0x95c78222b3d6e262426483d42cfa53685a67ab9d";
+const BORROWER = "0x8d655AAAA0ec224b17972df385e25325b9103332";
+
+forking(33508800, () => {
+  let vToken: ethers.Contract;
+  let prevBalance: BigNumber;
+
+  before(async () => {
+    vToken = new ethers.Contract(vBUSD, VBEP20_ABI, ethers.provider);
+    prevBalance = await vToken.callStatic.borrowBalanceCurrent(BORROWER);
+  });
+
+  testVip("VIP-118 Repay ETH debt on behalf debt", vip201(), {
+    proposer: "0x97a32d4506f6a35de68e0680859cdf41d077a9a9",
+    supporter: "0x55A9f5374Af30E3045FB491f1da3C2E8a74d168D",
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [VTREASURY_ABI, VBEP20_ABI], ["WithdrawTreasuryBEP20", "RepayBorrow"], [1, 1]);
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("Should decrese Borrow Balance Stored", async () => {
+      const currentBalance = await vToken.callStatic.borrowBalanceCurrent(BORROWER);
+      const delta = prevBalance.sub(currentBalance);
+      expect(delta).to.equal(parseUnits("6400762.2181", 18));
+    });
+  });
+});
