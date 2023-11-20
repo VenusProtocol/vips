2edfb883886adc310c64c7c7d8d4cc67ea52c6c3
0xlucian <0xluciandev@gmail.com>
2023-11-20 14:33:38 +0300

    refactor: add the rest of the assets in the VIP

--- multisig/proposals/vip-003/vip-003-sepolia.ts
+++ multisig/proposals/vip-003/vip-003-sepolia.ts
@@ -4,6 +4,9 @@ import { makeProposal } from "../../../src/utils";
 const { sepolia } = NETWORK_ADDRESSES;
 
 const REDSTONE_XVS_FEED = "0x0d7697a15bce933cE8671Ba3D60ab062dA216C60";
+const CHAINLINK_BTC_FEED = "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43";
+const CHAINLINK_ETH_FEED = "0x694AA1769357215DE4FAC081bf1f309aDC325306";
+const CHAINLINK_USDC_FEED = "0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E";
 
 // VIP: Configures the new oracle with the ACL and configures the initial price feeds on this oracle
 export const oracleAssetConfig2 = () => {
@@ -62,6 +65,86 @@ export const oracleAssetConfig2 = () => {
     { target: sepolia.CHAINLINK_ORACLE, signature: "acceptOwnership()", params: [] },
     { target: sepolia.BOUND_VALIDATOR, signature: "acceptOwnership()", params: [] },
     { target: sepolia.REDSTONE_ORACLE, signature: "acceptOwnership()", params: [] },
+    {
+      target: sepolia.CHAINLINK_ORACLE,
+      signature: "setTokenConfig((address,address,uint256))",
+      params: [[sepolia.MOCK_WBTC, CHAINLINK_BTC_FEED, 86400]],
+    },
+    {
+      target: sepolia.RESILIENT_ORACLE,
+      signature: "setTokenConfig((address,address[3],bool[3]))",
+      params: [
+        [
+          sepolia.MOCK_WBTC,
+          [
+            sepolia.CHAINLINK_ORACLE,
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+          ],
+          [true, false, false],
+        ],
+      ],
+    },
+    {
+      target: sepolia.CHAINLINK_ORACLE,
+      signature: "setTokenConfig((address,address,uint256))",
+      params: [[sepolia.MOCK_WETH, CHAINLINK_ETH_FEED, 86400]],
+    },
+    {
+      target: sepolia.RESILIENT_ORACLE,
+      signature: "setTokenConfig((address,address[3],bool[3]))",
+      params: [
+        [
+          sepolia.MOCK_WETH,
+          [
+            sepolia.CHAINLINK_ORACLE,
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+          ],
+          [true, false, false],
+        ],
+      ],
+    },
+    {
+      target: sepolia.CHAINLINK_ORACLE,
+      signature: "setTokenConfig((address,address,uint256))",
+      params: [[sepolia.MOCK_USDC, CHAINLINK_USDC_FEED, 86400]],
+    },
+    {
+      target: sepolia.RESILIENT_ORACLE,
+      signature: "setTokenConfig((address,address[3],bool[3]))",
+      params: [
+        [
+          sepolia.MOCK_USDC,
+          [
+            sepolia.CHAINLINK_ORACLE,
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+          ],
+          [true, false, false],
+        ],
+      ],
+    },
+    {
+      target: sepolia.CHAINLINK_ORACLE,
+      signature: "setDirectPrice(address,uint256)",
+      params: [sepolia.MOCK_USDT, "1000000000000000000"],
+    },
+    {
+      target: sepolia.RESILIENT_ORACLE,
+      signature: "setTokenConfig((address,address[3],bool[3]))",
+      params: [
+        [
+          sepolia.MOCK_USDT,
+          [
+            sepolia.CHAINLINK_ORACLE,
+            "0x0000000000000000000000000000000000000000",
+            "0x0000000000000000000000000000000000000000",
+          ],
+          [true, false, false],
+        ],
+      ],
+    },
     {
       target: sepolia.REDSTONE_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
