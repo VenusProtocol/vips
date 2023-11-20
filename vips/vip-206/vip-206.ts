bcee9ad5f0cdbdf0716179613a81af982b9a2ce0
Narayan Prusty <arsenal.narayan@gmail.com>
2023-11-20 17:26:00 +0000

    fix: added vip-206

---
+++ vips/vip-206/vip-206.ts
@@ -0,0 +1,15 @@
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+import staked from "./staked-users";
+
+export const vip206 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-206",
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal([staked], meta, ProposalType.REGULAR);
+};
