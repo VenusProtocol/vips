35ffac04781283071b9109aee9ada9570e56026c
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-11-30 15:35:57 +0100

    fixup! feat: add xvsVault generic check

--- multisig/simulations/vip-004/vip-004-sepolia/simulations.ts
+++ multisig/simulations/vip-004/vip-004-sepolia/simulations.ts
@@ -17,19 +17,17 @@ const { sepolia } = NETWORK_ADDRESSES;
 forking(4795108, () => {
   let xvsVault: Contract;
   let xvsStore: Contract;
-  let xvs: Contract;
 
   before(async () => {
     xvsVault = await ethers.getContractAt(XVS_VAULT_ABI, sepolia.XVS_VAULT_PROXY);
     xvsStore = await ethers.getContractAt(XVS_STORE_ABI, sepolia.XVS_STORE);
-    xvs = await ethers.getContractAt(XVS_ABI, sepolia.XVS);
     await pretendExecutingVip(vip004());
   });
 
   describe("Post tx checks", () => {
     describe("Generic checks", async () => {
       before(async () => {
-        xvs = await ethers.getContractAt(XVS_ABI, sepolia.XVS);
+        const xvs: Contract = await ethers.getContractAt(XVS_ABI, sepolia.XVS);
         const admin = await initMainnetUser(sepolia.NORMAL_TIMELOCK, ethers.utils.parseEther("1"));
         const xvsHolder = await initMainnetUser(sepolia.GENERIC_TEST_USER_ACCOUNT, ethers.utils.parseEther("1"));
         await xvsVault.connect(admin).setRewardAmountPerBlock(sepolia.XVS, "61805555555555555");
