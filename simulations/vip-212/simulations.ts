b04c3984e0b92b5668b70d7f5b647efd8d78c928
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-12-01 13:45:26 +0530

    feat: refund to community wallet & transferto BEP20 address

--- simulations/vip-212/simulations.ts
+++ simulations/vip-212/simulations.ts
@@ -3,34 +3,55 @@ import { BigNumber } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
+import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
 import { vip212 } from "../../vips/vip-212";
 import IERC20_ABI from "./abi/IERC20UpgradableAbi.json";
+import VTREASURY_ABI from "./abi/VTreasuryAbi.json";
 
 const USDT = "0x55d398326f99059ff775485246999027b3197955";
 const CERTIK_RECEIVER = "0x4cf605b238e9c3c72d0faed64d12426e4a54ee12";
+const BEP20_RECEIVER = "0x9c492e6c087b50f9191e671b6781be81579942ab";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
 
-const AMOUNT = parseUnits("19000", 18);
+const CERTIK_AMOUNT = parseUnits("19000", 18);
+const BINANCE_AMOUNT = parseUnits("36000", 18);
+const COMMUNITY_AMOUNT = parseUnits("15000", 18);
 
-forking(33940509, () => {
+forking(33968239, () => {
   let usdt: ethers.Contract;
   let prevBalanceCertik: BigNumber;
+  let prevBalancecommunity: any;
+  let prevBalanceBinance: any;
 
   before(async () => {
     usdt = new ethers.Contract(USDT, IERC20_ABI, ethers.provider);
     prevBalanceCertik = await usdt.balanceOf(CERTIK_RECEIVER);
+    prevBalancecommunity = await usdt.balanceOf(COMMUNITY_WALLET);
+    prevBalanceBinance = await usdt.balanceOf(BEP20_RECEIVER);
   });
 
   testVip("VIP-212 Payments for auditors", vip212(), {
-    proposer: "0xc444949e0054a23c44fc45789738bdf64aed2391",
-    supporter: "0x55A9f5374Af30E3045FB491f1da3C2E8a74d168D",
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [VTREASURY_ABI], ["WithdrawTreasuryBEP20", "Failure"], [3, 0]);
+    },
   });
 
   describe("Post-VIP behavior", async () => {
     it("Should increase balances of Certik receiver", async () => {
       const currentBalance = await usdt.balanceOf(CERTIK_RECEIVER);
       const delta = currentBalance.sub(prevBalanceCertik);
-      expect(delta).equals(AMOUNT);
+      expect(delta).equals(CERTIK_AMOUNT);
+    });
+    it("Should increase balances of Community wallet", async () => {
+      const currentBalance = await usdt.balanceOf(COMMUNITY_WALLET);
+      const delta = currentBalance.sub(prevBalancecommunity);
+      expect(delta).equals(COMMUNITY_AMOUNT);
+    });
+    it("Should increase balances of Binance receiver", async () => {
+      const currentBalance = await usdt.balanceOf(BEP20_RECEIVER);
+      const delta = currentBalance.sub(prevBalanceBinance);
+      expect(delta).equals(BINANCE_AMOUNT);
     });
   });
 });
--- vips/vip-212.ts
+++ vips/vip-212.ts
@@ -6,6 +6,8 @@ import { makeProposal } from "../src/utils";
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 const USDT = "0x55d398326f99059ff775485246999027b3197955";
 const CERTIK_RECEIVER = "0x4cf605b238e9c3c72d0faed64d12426e4a54ee12";
+const BEP20_RECEIVER = "0x9c492e6c087b50f9191e671b6781be81579942ab";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
 
 export const vip212 = () => {
   const meta = {
@@ -13,7 +15,9 @@ export const vip212 = () => {
     title: "VIP-212 Payments Issuance for audits",
     description: `
     If passed this VIP will perform the following actions:
-        - Transfer 19,000 USDT to Certik for the retainer program
+        - Transfer 19,000 USDT to Certik for the retainer program.
+        - Transfer 36,000 USDT to Binance oracle.
+        - Refund 15,000 USDT to community wallet BEP20 address.
 
     #### Details
     Certik - retainer program
@@ -31,6 +35,16 @@ export const vip212 = () => {
         target: TREASURY,
         signature: "withdrawTreasuryBEP20(address,uint256,address)",
         params: [USDT, parseUnits("19000", 18), CERTIK_RECEIVER],
+      },
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [USDT, parseUnits("15000", 18), COMMUNITY_WALLET],
+      },
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [USDT, parseUnits("36000", 18), BEP20_RECEIVER],
       },
     ],
     meta,
