fe1f02153a01ab12340f10a3e4b641d802fe3e8d
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-12-07 18:27:23 +0100

    feat: update final description for VIP-213

--- vips/vip-213.ts
+++ vips/vip-213.ts
@@ -12,19 +12,17 @@ export const vip213 = () => {
   const meta = {
     version: "v2",
     title: "VIP-213 Risk Parameters Adjustments (MATIC and PLANET)",
-    description: `#### Description
+    description: `This VIP will perform the following Risk Parameter actions as per Chaos Labs’ latest recommendations in this Venus community forum publication: [Risk Parameter Updates 12/06/2023](https://community.venus.io/t/chaos-labs-risk-parameter-updates-12-06-23/3958).
 
-This VIP will perform the following Risk Parameters actions as per Chaos Labs latest recommendations.
-
-- **MATIC** (Core pool)
-    - Increase supply cap to 16,000,000 (from 8,000,000)
-- **PLANET** (Defi pool)
-    - Increase supply cap to 4,000,000,000 (from 2,000,000,000)
-    - Increase borrow cap to 1,500,000,000 (from 1,000,000,000)
+- [MATIC (Core pool)](https://bscscan.com/address/0x5c9476FcD6a4F9a3654139721c949c2233bBbBc8)
+    - Increase supply cap to 16,000,000 MATIC (from 8,000,000 MATIC)
+- [PLANET (DeFi pool)](https://bscscan.com/address/0xFf1112ba7f88a53D4D23ED4e14A117A2aE17C6be)
+    - Increase supply cap to 4,000,000,000 PLANET (from 2,000,000,000 PLANET)
+    - Increase borrow cap to 1,500,000,000 PLANET (from 1,000,000,000 PLANET)
 
 Complete analysis and details of these recommendations are available in the above publication.
 
-VIP simulation: https://github.com/VenusProtocol/vips/pull/122`,
+VIP simulation: [https://github.com/VenusProtocol/vips/pull/122](https://github.com/VenusProtocol/vips/pull/122)`,
 
     forDescription: "I agree that Venus Protocol should proceed with this proposal",
     againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
