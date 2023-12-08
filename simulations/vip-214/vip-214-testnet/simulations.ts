3f481733b806e9aa00ffcaf667533f34d95c79a2
narayanprusty <arsenal.narayan@gmail.com>
2023-12-08 20:19:08 +0530

    fix: added event

--- simulations/vip-214/vip-214-testnet/simulations.ts
+++ simulations/vip-214/vip-214-testnet/simulations.ts
@@ -6,11 +6,13 @@ import { ethers } from "hardhat";
 import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
 import { vip214 } from "../../../vips/vip-214/vip-214-testnet";
+
 import PRIME_ABI from "./abi/prime.json";
 import PRIME_LIQUIDITY_PROVIDER_ABI from "./abi/primeLiquidityProvider.json";
 import VAI_CONTROLLER_ABI from "./abi/vaiController.json";
 import PROXY_ADMIN_ABI from "./abi/proxyAdmin.json";
 import VAI_CONTROLLER_PROXY_ABI from "./abi/vaiControllerProxy.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
 
 const PRIME_PROXY = "0xe840F8EC2Dc50E7D22e5e2991975b9F6e34b62Ad";
 const VAI_CONTROLLER_PROXY = "0xf70C3C6b749BbAb89C081737334E74C9aFD4BE16";
@@ -23,6 +25,8 @@ const NEW_PRIME_IMPLEMENTATION = "0xFa32e28b54B489CB72cF4BF956600A0910CCDb81";
 const NEW_VAI_CONTROLLER_IMPLEMENTATION = "0x8B13b4c2c634731be34cbF1874dC0b36F86b9b48";
 const NEW_PLP_IMPLEMENTATION = "0x29406DD113B5E90f56Fa7E1E1Ca148DB8B4E6E7F";
 const OLD_PLP_IMPLEMENTATION = "0x98d73B2E246a3506686CBA62d2118D2127dfD20E";
+const COMPTROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
+
 forking(35776436, () => {
   const provider = ethers.provider;
   let prime: ethers.Contract;
@@ -58,6 +62,7 @@ forking(35776436, () => {
   testVip("vip214Testnet", vip214(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(txResponse, [VAI_CONTROLLER_ABI], ["NewPrime", "MintOnlyForPrimeHolder", "NewVAIBaseRate", "NewVAIMintCap"], [1, 1, 1, 1]);
+      expectEvents(txResponse, [COMPTROLLER_ABI], ["NewVAIMintRate"], [1])
     },
   });
 
