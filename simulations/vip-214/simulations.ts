a05786fb70e1ffe89b3438cfede0c39d4441981f
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-12-10 21:45:39 +0100

    feat: simulation to send USDT to the community wallet

---
+++ simulations/vip-214/simulations.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { expectEvents } from "../../src/utils";
+import { forking, testVip } from "../../src/vip-framework";
+import { vip214 } from "../../vips/vip-214";
+import IERC20_ABI from "./abi/IERC20UpgradableAbi.json";
+import VTREASURY_ABI from "./abi/VTreasuryAbi.json";
+
+const USDT = "0x55d398326f99059ff775485246999027b3197955";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+
+const COMMUNITY_AMOUNT = parseUnits("275000", 18);
+
+forking(34241740, () => {
+  let usdt: ethers.Contract;
+  let prevBalanceCommunity: any;
+
+  before(async () => {
+    usdt = new ethers.Contract(USDT, IERC20_ABI, ethers.provider);
+    prevBalanceCommunity = await usdt.balanceOf(COMMUNITY_WALLET);
+  });
+
+  testVip("VIP-214", vip214(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [VTREASURY_ABI], ["WithdrawTreasuryBEP20", "Failure"], [1, 0]);
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("Should increase balance of Community wallet", async () => {
+      const currentBalance = await usdt.balanceOf(COMMUNITY_WALLET);
+      const delta = currentBalance.sub(prevBalanceCommunity);
+      expect(delta).equals(COMMUNITY_AMOUNT);
+    });
+  });
+});
---
+++ vips/vip-214.ts
@@ -0,0 +1,32 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../src/types";
+import { makeProposal } from "../src/utils";
+
+const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
+const USDT = "0x55d398326f99059ff775485246999027b3197955";
+const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+const USDT_AMOUNT = parseUnits("275000", 18);
+
+export const vip214 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-214",
+    description: `.`,
+
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with this proposal",
+  };
+  return makeProposal(
+    [
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [USDT, USDT_AMOUNT, COMMUNITY_WALLET],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
