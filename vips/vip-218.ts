685ad07b1d28518c5ca7235fb1b9ce442885e247
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-12-14 17:07:49 +0100

    feat: update description of VIP-218

--- vips/vip-218.ts
+++ vips/vip-218.ts
@@ -12,14 +12,14 @@ export const vip218 = () => {
     title: "VIP-218 Risk Parameters Adjustments (CAKE)",
     description: `#### Description
 
-This VIP will perform the following Risk Parameters actions as per Chaos Labs latest recommendations in this Venus community forum publication: [Risk Parameter Updates - 12/13/23](https://community.venus.io/t/chaos-labs-risk-parameter-updates-12-13-23/3969).
+This VIP will perform the following Risk Parameter actions as per Chaos Labs’ latest recommendations in this Venus community forum publication: [Risk Parameter Updates 12/13/2023](https://community.venus.io/t/chaos-labs-risk-parameter-updates-12-13-23/3969).
 
-- **CAKE** (Core pool)
-    - Increase supply cap to 21M (from 14M)
+- [CAKE (Core pool)](https://bscscan.com/address/0x86aC3974e2BD0d60825230fa6F355fF11409df5c)
+    - Increase supply cap from 14,000,000 CAKE to 21,000,000 CAKE
 
 Complete analysis and details of these recommendations are available in the above publication.
 
-VIP simulation: https://github.com/VenusProtocol/vips/pull/132`,
+VIP simulation: [https://github.com/VenusProtocol/vips/pull/132](https://github.com/VenusProtocol/vips/pull/132)`,
 
     forDescription: "I agree that Venus Protocol should proceed with this proposal",
     againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
