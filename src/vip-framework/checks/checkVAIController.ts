a8cea8a01b576790d638942af3f0353e06de34af
narayanprusty <arsenal.narayan@gmail.com>
2023-12-16 00:02:38 +0530

    fix: fixed vai address

--- src/vip-framework/checks/checkVAIController.ts
+++ src/vip-framework/checks/checkVAIController.ts
@@ -51,7 +51,11 @@ export const checkVAIController = () => {
           continue;
         }
 
-        await setMaxStalePeriodInChainlinkOracleWithoutFeed(CHAINLINK_ORACLE, await vToken.underlying(), NORMAL_TIMELOCK);
+        await setMaxStalePeriodInChainlinkOracleWithoutFeed(
+          CHAINLINK_ORACLE,
+          await vToken.underlying(),
+          NORMAL_TIMELOCK,
+        );
       }
     });
 
