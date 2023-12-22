9048f3d0c074524b3f561e2f86c70e59b8e2126d
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-12-22 22:31:56 +0530

    fix: remove unnecessary steps

--- multisig/proposals/vip-003/vip-003-opbnbtestnet.ts
+++ multisig/proposals/vip-003/vip-003-opbnbtestnet.ts
@@ -105,11 +105,6 @@ export const vip003 = () => {
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [XVS_BRIDGE_ADMIN, "setMinDstGas(uint16,uint16,uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
       params: [XVS, "mint(address,uint256)", XVS_BRIDGE],
     },
     {
@@ -153,11 +148,6 @@ export const vip003 = () => {
       target: XVS_BRIDGE_ADMIN,
       signature: "setTrustedRemoteAddress(uint16,bytes)",
       params: [10102, "0x0e132cd94fd70298b747d2b4d977db8d086e5fd0"],
-    },
-    {
-      target: XVS_BRIDGE_ADMIN,
-      signature: "setMinDstGas(uint16,uint16,uint256)",
-      params: [10102, 0, "300000"],
     },
     {
       target: XVS_BRIDGE_ADMIN,
