dd457ed346ba9a5e28e9666a90e410548b5a0324
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-12-27 10:22:12 +0530

    feat: configure rewards on sepolia

--- multisig/simulations/vip-006/vip-006-sepolia/simulations.ts
+++ multisig/simulations/vip-006/vip-006-sepolia/simulations.ts
@@ -8,21 +8,20 @@ import {
 } from "../../../../src/vip-framework/checks/rewardsDistributor";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework/index";
 import {
+  COMPTROLLER_CORE,
   REWARD_DISTRIBUTOR_CORE_0,
   REWARD_DISTRIBUTOR_CORE_1,
   REWARD_DISTRIBUTOR_CORE_2,
   REWARD_DISTRIBUTOR_CORE_3,
+  VUSDC_CORE,
+  VUSDT_CORE,
+  VWBTC_CORE,
+  VWETH_CORE,
+  XVS,
   vip006,
 } from "../../../proposals/vip-006/vip-006-sepolia";
 
-const COMPTROLLER_CORE = "0x7Aa39ab4BcA897F403425C9C6FDbd0f882Be0D70";
-const XVS = "0xDb633C11D3F9E6B8D17aC2c972C9e3B05DA59bF9";
-const VUSDC_CORE = "0xF87bceab8DD37489015B426bA931e08A4D787616";
-const VUSDT_CORE = "0x19252AFD0B2F539C400aEab7d460CBFbf74c17ff";
-const VWBTC_CORE = "0x74E708A7F5486ed73CCCAe54B63e71B1988F1383";
-const VWETH_CORE = "0xc2931B1fEa69b6D6dA65a50363A8D75d285e4da9";
-
-forking(4837900, () => {
+forking(4965382, () => {
   describe("Generic checks", async () => {
     before(async () => {
       await pretendExecutingVip(vip006());
