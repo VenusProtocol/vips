f87f3f5a5ceefe2f5b5a9308a5f8bf8ae7f31d8b
narayanprusty <arsenal.narayan@gmail.com>
2024-01-02 13:26:19 +0530

    fix: wip - fixing vip commands

--- multisig/proposals/vip-007/vip-007-sepolia.ts
+++ multisig/proposals/vip-007/vip-007-sepolia.ts
@@ -2,16 +2,15 @@ import { ProposalType } from "../../../src/types";
 import { makeProposal } from "../../../src/utils";
 import { ethers } from "hardhat";
 
-const NORMAL_TIMELOCK = "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb";
 const ACM = "0xbf705C00578d43B6147ab4eaE04DBBEd1ccCdc96";
-const FAST_TRACK_TIMELOCK = "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb";
-const CRITICAL_TIMELOCK = "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb";
 const XVS_VAULT_PROXY = "0x1129f882eAa912aE6D4f6D445b2E2b1eCbA99fd5";
 const PRIME_LIQUIDITY_PROVIDER = "0xF30312DF854742CAAf9E37D789B0F2617CE15239";
 const PRIME = "0x1c4B6D86712639b5d9EFaa938457f7a3dEa0de98";
 const GUARDIAN = "0x94fa6078b6b8a26F0B6EDFFBE6501B22A10470fB";
 const XVS = "0x66ebd019E86e0af5f228a0439EBB33f045CBe63E";
 const COMPTROLLER = "0x7Aa39ab4BcA897F403425C9C6FDbd0f882Be0D70";
+const COMPTROLLER_BEACON = "0x6cE54143a88CC22500D49D744fb6535D66a8294F";
+const VTOKEN_BEACON = "0x0463a7E5221EAE1990cEddB51A5821a68cdA6008";
 
 const ETH = "0x700868CAbb60e90d77B6588ce072d9859ec8E281";
 const BTC = "0x92A2928f5634BEa89A195e7BeCF0f0FEEDAB885b";
@@ -23,19 +22,12 @@ const vBTC = "0x74E708A7F5486ed73CCCAe54B63e71B1988F1383";
 const vUSDC = "0xF87bceab8DD37489015B426bA931e08A4D787616";
 const vUSDT = "0x19252AFD0B2F539C400aEab7d460CBFbf74c17ff";
 
+const NEW_COMPTROLLER_IML = "0x0bA3dBDb53a367e9132587c7Fc985153A2E25f08";
+const NEW_VTOKEN_IML = "0xa4Fd54cACdA379FB7CaA783B83Cc846f8ac0Faa6";
+
 const PRIME_POOL_ID = 0;
 
-
 export const vip007 = () => {
-  const meta = {
-    version: "v2",
-    title: "Prime Program Setup",
-    description: ``,
-    forDescription: "I agree that Venus Protocol should proceed with setting the prime program",
-    againstDescription: "I do not think that Venus Protocol should proceed with setting the prime program",
-    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with setting the prime program",
-  };
-
   return makeProposal([
     {
       target: PRIME_LIQUIDITY_PROVIDER,
@@ -50,22 +42,17 @@ export const vip007 = () => {
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setTokensDistributionSpeed(address[],uint256[])", NORMAL_TIMELOCK],
+      params: [PRIME_LIQUIDITY_PROVIDER, "setTokensDistributionSpeed(address[],uint256[])", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxTokensDistributionSpeed(address[],uint256[])", NORMAL_TIMELOCK],
+      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxTokensDistributionSpeed(address[],uint256[])", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxLoopsLimit(uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "pauseFundsTransfer()", NORMAL_TIMELOCK],
+      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxLoopsLimit(uint256)", GUARDIAN],
     },
     {
       target: ACM,
@@ -75,107 +62,47 @@ export const vip007 = () => {
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "resumeFundsTransfer()", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
       params: [PRIME_LIQUIDITY_PROVIDER, "resumeFundsTransfer()", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setTokensDistributionSpeed(address[],uint256[])", FAST_TRACK_TIMELOCK],
+      params: [PRIME, "updateAlpha(uint128,uint128)", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxTokensDistributionSpeed(address[],uint256[])", FAST_TRACK_TIMELOCK],
+      params: [PRIME, "updateMultipliers(address,uint256,uint256)", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxLoopsLimit(uint256)", FAST_TRACK_TIMELOCK],
+      params: [PRIME, "setStakedAt(address[],uint256[])", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "pauseFundsTransfer()", FAST_TRACK_TIMELOCK],
+      params: [PRIME, "addMarket(address,uint256,uint256)", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "resumeFundsTransfer()", FAST_TRACK_TIMELOCK],
+      params: [PRIME, "setLimit(uint256,uint256)", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setTokensDistributionSpeed(address[],uint256[])", CRITICAL_TIMELOCK],
+      params: [PRIME, "setMaxLoopsLimit(uint256)", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxTokensDistributionSpeed(address[],uint256[])", CRITICAL_TIMELOCK],
+      params: [PRIME, "issue(bool,address[])", GUARDIAN],
     },
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxLoopsLimit(uint256)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "pauseFundsTransfer()", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME_LIQUIDITY_PROVIDER, "resumeFundsTransfer()", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateAlpha(uint128,uint128)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateMultipliers(address,uint256,uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setStakedAt(address[],uint256[])", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "addMarket(address,uint256,uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setLimit(uint256,uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setMaxLoopsLimit(uint256)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "issue(bool,address[])", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "burn(address)", NORMAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "togglePause()", NORMAL_TIMELOCK],
+      params: [PRIME, "burn(address)", GUARDIAN],
     },
     {
       target: ACM,
@@ -183,103 +110,23 @@ export const vip007 = () => {
       params: [PRIME, "togglePause()", GUARDIAN],
     },
     {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateAlpha(uint128,uint128)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateMultipliers(address,uint256,uint256)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setStakedAt(address[],uint256[])", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "addMarket(address,uint256,uint256)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setLimit(uint256,uint256)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setMaxLoopsLimit(uint256)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "issue(bool,address[])", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "burn(address)", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "togglePause()", FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateAlpha(uint128,uint128)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "updateMultipliers(address,uint256,uint256)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setStakedAt(address[],uint256[])", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "addMarket(address,uint256,uint256)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setLimit(uint256,uint256)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "setMaxLoopsLimit(uint256)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "issue(bool,address[])", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "burn(address)", CRITICAL_TIMELOCK],
-    },
-    {
-      target: ACM,
-      signature: "giveCallPermission(address,string,address)",
-      params: [PRIME, "togglePause()", CRITICAL_TIMELOCK],
-    },
-    {
       target: PRIME_LIQUIDITY_PROVIDER,
       signature: "setPrimeToken(address)",
       params: [PRIME],
     },
     {
+      target: COMPTROLLER_BEACON,
+      signature: "upgradeTo(address)",
+      params: [NEW_COMPTROLLER_IML],
+    },
+    {
+      target: VTOKEN_BEACON,
+      signature: "upgradeTo(address)",
+      params: [NEW_VTOKEN_IML],
+    },
+    {
       target: COMPTROLLER,
-      signature: "_setPrimeToken(address)",
+      signature: "setPrimeToken(address)",
       params: [PRIME],
     },
     {
@@ -295,43 +142,43 @@ export const vip007 = () => {
         [0, 0, 0, 0],
       ],
     },
-    {
-      target: PRIME,
-      signature: "addMarket(address,uint256,uint256)",
-      params: [vETH, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
-    },
-    {
-      target: PRIME,
-      signature: "addMarket(address,uint256,uint256)",
-      params: [vBTC, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
-    },
-    {
-      target: PRIME,
-      signature: "addMarket(address,uint256,uint256)",
-      params: [vUSDC, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
-    },
-    {
-      target: PRIME,
-      signature: "addMarket(address,uint256,uint256)",
-      params: [vUSDT, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
-    },
-    {
-      target: PRIME,
-      signature: "setLimit(uint256,uint256)",
-      params: [
-        0, // irrevocable
-        500, // revocable
-      ],
-    },
-    {
-      target: PRIME_LIQUIDITY_PROVIDER,
-      signature: "pauseFundsTransfer()",
-      params: [],
-    },
-    {
-      target: XVS_VAULT_PROXY,
-      signature: "setPrimeToken(address,address,uint256)",
-      params: [PRIME, XVS, PRIME_POOL_ID],
-    },
-  ], meta, ProposalType.REGULAR);
+    // {
+    //   target: PRIME,
+    //   signature: "addMarket(address,uint256,uint256)",
+    //   params: [vETH, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
+    // },
+    // {
+    //   target: PRIME,
+    //   signature: "addMarket(address,uint256,uint256)",
+    //   params: [vBTC, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
+    // },
+    // {
+    //   target: PRIME,
+    //   signature: "addMarket(address,uint256,uint256)",
+    //   params: [vUSDC, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
+    // },
+    // {
+    //   target: PRIME,
+    //   signature: "addMarket(address,uint256,uint256)",
+    //   params: [vUSDT, ethers.utils.parseEther("2"), ethers.utils.parseEther("4")],
+    // },
+    // {
+    //   target: PRIME,
+    //   signature: "setLimit(uint256,uint256)",
+    //   params: [
+    //     0, // irrevocable
+    //     500, // revocable
+    //   ],
+    // },
+    // {
+    //   target: PRIME_LIQUIDITY_PROVIDER,
+    //   signature: "pauseFundsTransfer()",
+    //   params: [],
+    // },
+    // {
+    //   target: XVS_VAULT_PROXY,
+    //   signature: "setPrimeToken(address,address,uint256)",
+    //   params: [PRIME, XVS, PRIME_POOL_ID],
+    // },
+  ]);
 };
---
+++ multisig/simulations/vip-007/vip-007-sepolia/simulations.ts
@@ -0,0 +1,136 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { expectEvents, setMaxStalePeriodInChainlinkOracle } from "../../../../src/utils";
+// import { checkCorePoolComptroller } from "../../../src/vip-framework/checks/checkCorePoolComptroller";
+// import { checkXVSVault } from "../../../src/vip-framework/checks/checkXVSVault";
+import PRIME_ABI from "./abis/Prime.json";
+import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
+import SETTER_FACET_ABI from "./abis/SetterFacet.json";
+import XVS_VAULT_ABI from "./abis/XVSVault.json";
+import { vip007 } from "../../../proposals/vip-007/vip-007-sepolia";
+import { forking, pretendExecutingVip, testVip } from "../../../../src/vip-framework";
+
+const PRIME_LIQUIDITY_PROVIDER = "0xF30312DF854742CAAf9E37D789B0F2617CE15239";
+const PRIME = "0x1c4B6D86712639b5d9EFaa938457f7a3dEa0de98";
+const CHAINLINK_ORACLE = "0x102F0b714E5d321187A4b6E5993358448f7261cE";
+const GUARDIAN = "0x94fa6078b6b8a26F0B6EDFFBE6501B22A10470fB";
+
+interface vTokenConfig {
+  name: string;
+  assetAddress: string;
+  feed: string;
+  marketAddress: string;
+}
+
+const vTokens: vTokenConfig[] = [
+  {
+    name: "vUSDC",
+    assetAddress: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+    feed: "0x51597f405303c4377e36123cbc172b13269ea163",
+    marketAddress: "0xecA88125a5ADbe82614ffC12D0DB554E2e2867C8",
+  },
+  {
+    name: "vUSDT",
+    assetAddress: "0x55d398326f99059fF775485246999027B3197955",
+    feed: "0xb97ad0e74fa7d920791e90258a6e2085088b4320",
+    marketAddress: "0xfD5840Cd36d94D7229439859C0112a4185BC0255",
+  },
+  {
+    name: "vETH",
+    assetAddress: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    feed: "0x9ef1b8c0e4f7dc8bf5719ea496883dc6401d5b2e",
+    marketAddress: "0xf508fCD89b8bd15579dc79A6827cB4686A3592c8",
+  },
+  {
+    name: "vBTC",
+    assetAddress: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+    feed: "0x264990fbd0a4796a3e3d8e37c4d5f87a3aca5ebf",
+    marketAddress: "0x882C173bC7Ff3b7786CA16dfeD3DFFfb9Ee7847B",
+  },
+  {
+    name: "vXVS",
+    assetAddress: "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63",
+    feed: "0xbf63f430a79d4036a5900c19818aff1fa710f206",
+    marketAddress: "0x151B1e2635A717bcDc836ECd6FbB62B674FE3E1D",
+  },
+];
+
+forking(5005207, () => {
+  describe("Pre-VIP behavior", () => {
+    let prime: Contract;
+    let primeLiquidityProvider: Contract;
+
+    before(async () => {
+      prime = await ethers.getContractAt(PRIME_ABI, PRIME);
+      primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+    });
+
+    it("prime markets", async () => {
+      expect((await prime.getAllMarkets()).length).to.equal(0);
+    });
+
+    it("prime address", async () => {
+      expect(await primeLiquidityProvider.prime()).to.equal("0x0000000000000000000000000000000000000000");
+    });
+
+    it("claim prime token", async () => {
+      await expect(prime.claim()).to.be.reverted;
+    });
+
+    it("is paused", async () => {
+      expect(await prime.paused()).to.be.equal(true);
+    });
+
+    describe("generic tests", async () => {
+      // checkCorePoolComptroller();
+    });
+  });
+
+  // testVip("VIP-007 Prime Program", vip007(), {
+  //   callbackAfterExecution: async (txResponse: TransactionResponse) => {
+  //     await expectEvents(txResponse, [SETTER_FACET_ABI], ["NewPrimeToken"], [1]);
+  //     await expectEvents(txResponse, [PRIME_LIQUIDITY_PROVIDER_ABI], ["PrimeTokenUpdated"], [1]);
+  //     await expectEvents(txResponse, [PRIME_ABI], ["MarketAdded"], [4]);
+  //     await expectEvents(txResponse, [PRIME_ABI], ["MintLimitsUpdated"], [1]);
+  //     await expectEvents(txResponse, [PRIME_LIQUIDITY_PROVIDER_ABI], ["Paused"], [1]);
+  //     await expectEvents(txResponse, [XVS_VAULT_ABI], ["NewPrimeToken"], [1]);
+  //   },
+  // });
+
+  describe("Post-VIP behavior", async () => {
+    let prime: Contract;
+    let primeLiquidityProvider: Contract;
+
+    before(async () => {
+      await pretendExecutingVip(vip007());
+
+      // prime = await ethers.getContractAt(PRIME_ABI, PRIME);
+      // primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+
+      // for (let i = 0; i < vTokens.length; i++) {
+      //   const vToken = vTokens[i];
+      //   await setMaxStalePeriodInChainlinkOracle(CHAINLINK_ORACLE, vToken.assetAddress, vToken.feed, GUARDIAN);
+      // }
+    });
+
+    it("prime markets", async () => {
+      // expect((await prime.getAllMarkets()).length).to.equal(4);
+    });
+
+    // it("prime address", async () => {
+    //   expect(await primeLiquidityProvider.prime()).to.equal(PRIME);
+    // });
+
+    // it("is paused", async () => {
+    //   expect(await prime.paused()).to.be.equal(true);
+    // });
+
+    describe("generic tests", async () => {
+      // checkCorePoolComptroller();
+      // checkXVSVault();
+    });
+  });
+});
