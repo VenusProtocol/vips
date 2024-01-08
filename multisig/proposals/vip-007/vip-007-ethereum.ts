e81f23467d97b157aacdf0578d7c3f678e89c315
narayanprusty <arsenal.narayan@gmail.com>
2024-01-08 21:29:45 +0530

    fix: renamed 005 to 007

--- multisig/proposals/vip-007/vip-007-ethereum.ts
+++ multisig/proposals/vip-007/vip-007-ethereum.ts
@@ -3,7 +3,7 @@ import { makeProposal } from "../../../src/utils";
 const XVS_BRIDGE_ADMIN = "0x9C6C95632A8FB3A74f2fB4B7FfC50B003c992b96";
 const TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
 
-export const vip005 = () => {
+export const vip007 = () => {
   return makeProposal([
     {
       target: XVS_BRIDGE_ADMIN,
--- multisig/simulations/vip-007/vip-007-ethereum/simulations.ts
+++ multisig/simulations/vip-007/vip-007-ethereum/simulations.ts
@@ -3,18 +3,18 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework/index";
-import { vip005 } from "../../../proposals/vip-005/vip-005-sepolia";
+import { vip007 } from "../../../proposals/vip-007/vip-007-ethereum";
 import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTDest.json";
 
-const TREASURY = "0x4116CA92960dF77756aAAc3aFd91361dB657fbF8";
-const XVS_BRIDGE = "0xc340b7d3406502F43dC11a988E4EC5bbE536E642";
+const XVS_BRIDGE = "0x888E317606b4c590BBAD88653863e8B345702633";
+const TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
 
-forking(4965126, () => {
+forking(18918859, () => {
   let xvsBridge: Contract;
 
   before(async () => {
     xvsBridge = await ethers.getContractAt(XVS_BRIDGE_ABI, XVS_BRIDGE);
-    await pretendExecutingVip(vip005());
+    await pretendExecutingVip(vip007());
   });
 
   describe("Post tx checks", () => {
