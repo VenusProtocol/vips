d112f2708e7a97013aa5d4b57e3b2cabc87161e8
narayanprusty <arsenal.narayan@gmail.com>
2024-01-09 17:46:42 +0530

    fix: use pretentExecutingVip

--- simulations/vip-234/simulations.ts
+++ simulations/vip-234/simulations.ts
@@ -1,19 +1,22 @@
-import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-import { expectEvents, setMaxStalePeriodInChainlinkOracle } from "../../src/utils";
-import { forking, testVip } from "../../src/vip-framework";
+import { forking, pretendExecutingVip } from "../../src/vip-framework";
+import { ADDRESSES, PRIME, vip234 } from "../../vips/vip-234/vip-234";
 import PRIME_ABI from "./abis/Prime.json";
-import { vip234, ADDRESSES, PRIME } from "../../vips/vip-234/vip-234";
 
 forking(35091518, () => {
-  testVip("VIP-234 Prime Program", vip234(), {
-    callbackAfterExecution: async (txResponse: TransactionResponse) => {
-      expectEvents(txResponse, [PRIME_ABI], ["MintLimitsUpdated"], [1]);
-      // await expectEvents(txResponse, [PRIME_ABI], ["Mint"], [20]);
-    },
+  before(async () => {
+    await pretendExecutingVip(vip234());
   });
+
+  // testVip("VIP-234 Prime Program", vip234(), {
+  //   callbackAfterExecution: async (txResponse: TransactionResponse) => {
+  //     expectEvents(txResponse, [PRIME_ABI], ["MintLimitsUpdated"], [1]);
+  //     await expectEvents(txResponse, [PRIME_ABI], ["Mint"], [20]);
+  //   },
+  // });
 
   describe("Post-VIP behavior", async () => {
     let prime: Contract;
@@ -27,7 +30,7 @@ forking(35091518, () => {
         const address = ADDRESSES[i];
         it(`should have minted token for ${address}`, async () => {
           const data = await prime.tokens(address);
-          // expect(data.exists).to.be.equal(true);
+          expect(data.exists).to.be.equal(true);
         });
       }
     });
--- simulations/vip-235/simulations.ts
+++ simulations/vip-235/simulations.ts
@@ -1,22 +1,26 @@
-import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-import { expectEvents, setMaxStalePeriodInChainlinkOracle } from "../../src/utils";
-import { forking, testVip } from "../../src/vip-framework";
+import { forking, pretendExecutingVip } from "../../src/vip-framework";
+import { ADDRESSES, vip234 } from "../../vips/vip-234/vip-234";
+import { PRIME, vip235 } from "../../vips/vip-235/vip-235";
 import PRIME_ABI from "./abis/Prime.json";
-import { vip235, ADDRESSES, PRIME } from "../../vips/vip-235/vip-235";
-import { vip234 } from "../../vips/vip-234/vip-234";
 
 forking(35091518, () => {
-  testVip("VIP-234 Prime Program", vip234());
-
-  testVip("VIP-235 Prime Program", vip235(), {
-    callbackAfterExecution: async (txResponse: TransactionResponse) => {
-      expectEvents(txResponse, [PRIME_ABI], ["MintLimitsUpdated"], [1]);
-      // await expectEvents(txResponse, [PRIME_ABI], ["Burn"], [20]);
-    },
+  before(async () => {
+    await pretendExecutingVip(vip234());
+    await pretendExecutingVip(vip235());
   });
+
+  // testVip("VIP-234 Prime Program", vip234());
+
+  // testVip("VIP-235 Prime Program", vip235(), {
+  //   callbackAfterExecution: async (txResponse: TransactionResponse) => {
+  //     expectEvents(txResponse, [PRIME_ABI], ["MintLimitsUpdated"], [1]);
+  //     await expectEvents(txResponse, [PRIME_ABI], ["Burn"], [20]);
+  //   },
+  // });
 
   describe("Post-VIP behavior", async () => {
     let prime: Contract;
@@ -28,9 +32,9 @@ forking(35091518, () => {
     describe("check if token minted", async () => {
       for (let i = 0; i < ADDRESSES.length; i++) {
         const address = ADDRESSES[i];
-        it(`should have minted token for ${address}`, async () => {
+        it(`should burn minted token for ${address}`, async () => {
           const data = await prime.tokens(address);
-          // expect(data.exists).to.be.equal(false);
+          expect(data.exists).to.be.equal(false);
         });
       }
     });
