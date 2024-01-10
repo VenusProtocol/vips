45cd0c68d22bca25fb36e291a88dfc3ae68a5a08
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-01-10 13:13:09 +0530

    fix: edit description and removed unused variables

--- vips/vip-234.ts
+++ vips/vip-234.ts
@@ -4,14 +4,11 @@ import { parseUnits } from "ethers/lib/utils";
 import { ProposalType } from "../src/types";
 import { makeProposal } from "../src/utils";
 
-export const XVS_BRIDGE_ADMIN = "0x70d644877b7b73800E9073BCFCE981eAaB6Dbc21";
 export const XVS_BRIDGE_SRC = "0xf8F46791E3dB29a029Ec6c9d946226f3c613e854";
 export const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
 export const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const ETHEREUM_TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
 export const XVS = "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63";
-export const BNB_TREASURY = "0xf322942f644a996a617bd29c16bd7d231d9f35e9";
-export const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
 
 export const RECEIVER_ADDRESS = ethers.utils.defaultAbiCoder.encode(["address"], [ETHEREUM_TREASURY]);
 export const ADAPTER_PARAMS = ethers.utils.solidityPack(["uint16", "uint256"], [1, 300000]);
@@ -21,7 +18,7 @@ export const DEST_CHAIN_ID = 101;
 export const vip234 = () => {
   const meta = {
     version: "v2",
-    title: "VIP to send needed liquidity of 171,500 to Ethereum",
+    title: "VIP to send needed liquidity of 171,500 XVS to Ethereum",
     description: ``,
 
     forDescription: "I agree that Venus Protocol should proceed with this transfer for XVS",
