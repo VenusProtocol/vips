c73cc4f5d3ff69d6de9919230dc2e7957574730c
narayanprusty <arsenal.narayan@gmail.com>
2024-01-10 13:36:38 +0530

    fix: remove bootstrap liquidity

--- simulations/vip-213/vip-213-testnet/simulations.ts
+++ simulations/vip-213/vip-213-testnet/simulations.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 
 import { expectEvents } from "../../../src/utils";
 import { forking, testVip } from "../../../src/vip-framework";
-import { XVS, XVS_AMOUNT, XVS_BRIDGE_SRC, BTC, ETH, USDC, USDT, BTC_AMOUNT, USDC_AMOUNT, USDT_AMOUNT, ETH_AMOUNT, COMMUNITY_WALLET, BNB_TREASURY, vip213Testnet } from "../../../vips/vip-213/vip-213-testnet";
+import { XVS, XVS_AMOUNT, XVS_BRIDGE_SRC, vip213Testnet } from "../../../vips/vip-213/vip-213-testnet";
 import COMPTROLLER_ABI from "./abi/Comptroller.json";
 import ERC20_ABI from "./abi/ERC20.json";
 import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTSrc.json";
@@ -12,18 +12,6 @@ import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTSrc.json";
 forking(36355864, () => {
   let xvsBridge: ethers.Contract;
   let xvs: ethers.Contract;
-  let btc: ethers.Contract;
-  let eth: ethers.Contract;
-  let usdc: ethers.Contract;
-  let usdt: ethers.Contract;
-  let oldBTCBal: BigNumber;
-  let oldETHBal: BigNumber;
-  let oldUSDCBal: BigNumber;
-  let oldUSDTBal: BigNumber;
-  let oldBTCBalTreasury: BigNumber;
-  let oldETHBalTreasury: BigNumber;
-  let oldUSDCBalTreasury: BigNumber;
-  let oldUSDTBalTreasury: BigNumber;
   let oldCirculatingSupply: BigNumber;
   let oldXVSBal: BigNumber;
 
@@ -32,18 +20,6 @@ forking(36355864, () => {
     xvs = new ethers.Contract(XVS, ERC20_ABI, ethers.provider);
     oldCirculatingSupply = await xvsBridge.circulatingSupply();
     oldXVSBal = await xvs.balanceOf(XVS_BRIDGE_SRC);
-    btc = new ethers.Contract(BTC, ERC20_ABI, ethers.provider);
-    eth = new ethers.Contract(ETH, ERC20_ABI, ethers.provider);
-    usdc = new ethers.Contract(USDC, ERC20_ABI, ethers.provider);
-    usdt = new ethers.Contract(USDT, ERC20_ABI, ethers.provider);
-    oldBTCBal = await btc.balanceOf(COMMUNITY_WALLET);
-    oldETHBal = await eth.balanceOf(COMMUNITY_WALLET);
-    oldUSDCBal = await usdc.balanceOf(COMMUNITY_WALLET);
-    oldUSDTBal = await usdt.balanceOf(COMMUNITY_WALLET);
-    oldBTCBalTreasury = await btc.balanceOf(BNB_TREASURY);
-    oldETHBalTreasury = await eth.balanceOf(BNB_TREASURY);
-    oldUSDCBalTreasury = await usdc.balanceOf(BNB_TREASURY);
-    oldUSDTBalTreasury = await usdt.balanceOf(BNB_TREASURY);
   });
 
   testVip("VIP-213 Send XVS to Dest Chain", vip213Testnet(), {
@@ -61,28 +37,6 @@ forking(36355864, () => {
     it("Should increase number of locked tokens on bridge", async () => {
       const currXVSBal = await xvs.balanceOf(XVS_BRIDGE_SRC);
       expect(currXVSBal.sub(oldXVSBal)).equals(XVS_AMOUNT);
-    });
-
-    it("Should transfer BTC, ETH, USDC and USDT", async () => {
-      const currBTCBal = await btc.balanceOf(COMMUNITY_WALLET);
-      const currETHBal = await eth.balanceOf(COMMUNITY_WALLET);
-      const currUSDCBal = await usdc.balanceOf(COMMUNITY_WALLET);
-      const currUSDTBal = await usdt.balanceOf(COMMUNITY_WALLET);
-
-      expect(currBTCBal.sub(oldBTCBal)).equals(BTC_AMOUNT);
-      expect(currETHBal.sub(oldETHBal)).equals(ETH_AMOUNT);
-      expect(currUSDCBal.sub(oldUSDCBal)).equals(USDC_AMOUNT);
-      expect(currUSDTBal.sub(oldUSDTBal)).equals(USDT_AMOUNT);
-
-      const currBTCBalTreasury = await btc.balanceOf(BNB_TREASURY);
-      const currETHBalTreasury = await eth.balanceOf(BNB_TREASURY);
-      const currUSDCBalTreasury = await usdc.balanceOf(BNB_TREASURY);
-      const currUSDTBalTreasury = await usdt.balanceOf(BNB_TREASURY);
-
-      expect(currBTCBalTreasury.add(BTC_AMOUNT)).equals(oldBTCBalTreasury);
-      expect(currETHBalTreasury.add(ETH_AMOUNT)).equals(oldETHBalTreasury);
-      expect(currUSDCBalTreasury.add(USDC_AMOUNT)).equals(oldUSDCBalTreasury);
-      expect(currUSDTBalTreasury.add(USDT_AMOUNT)).equals(oldUSDTBalTreasury);
     });
   });
 });
--- vips/vip-213/vip-213.ts
+++ vips/vip-213/vip-213.ts
@@ -10,23 +10,11 @@ export const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
 export const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const ETHEREUM_TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
 export const XVS = "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63";
-export const BNB_TREASURY = "0xf322942f644a996a617bd29c16bd7d231d9f35e9";
-export const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
 
 export const RECEIVER_ADDRESS = ethers.utils.defaultAbiCoder.encode(["address"], [ETHEREUM_TREASURY]);
 export const ADAPTER_PARAMS = ethers.utils.solidityPack(["uint16", "uint256"], [1, 300000]);
 export const XVS_AMOUNT = parseUnits("75750", 18);
 export const DEST_CHAIN_ID = 101;
-
-export const BTC = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
-export const ETH = "0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
-export const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
-export const USDT = "0x55d398326f99059fF775485246999027B3197955";
-
-export const BTC_AMOUNT = parseUnits("0.3", 18);
-export const ETH_AMOUNT = parseUnits("5", 18);
-export const USDC_AMOUNT = parseUnits("20000", 18);
-export const USDT_AMOUNT = parseUnits("20000", 18);
 
 export const vip213 = () => {
   const meta = {
@@ -67,26 +55,6 @@ export const vip213 = () => {
           [NORMAL_TIMELOCK, ethers.constants.AddressZero, ADAPTER_PARAMS],
         ],
         value: "300000000000000000",
-      },
-      {
-        target: BNB_TREASURY,
-        signature: "withdrawTreasuryBEP20(address,uint256,address)",
-        params: [BTC, BTC_AMOUNT, COMMUNITY_WALLET],
-      },
-      {
-        target: BNB_TREASURY,
-        signature: "withdrawTreasuryBEP20(address,uint256,address)",
-        params: [ETH, ETH_AMOUNT, COMMUNITY_WALLET],
-      },
-      {
-        target: BNB_TREASURY,
-        signature: "withdrawTreasuryBEP20(address,uint256,address)",
-        params: [USDC, USDC_AMOUNT, COMMUNITY_WALLET],
-      },
-      {
-        target: BNB_TREASURY,
-        signature: "withdrawTreasuryBEP20(address,uint256,address)",
-        params: [USDT, USDT_AMOUNT, COMMUNITY_WALLET],
       },
     ],
     meta,
