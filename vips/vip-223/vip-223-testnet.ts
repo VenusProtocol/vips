ca0ef6f6679e78229b7f8859cb73feea5bfafa17
narayanprusty <arsenal.narayan@gmail.com>
2024-01-12 18:23:31 +0530

    fix: configured permissions

--- vips/vip-223/vip-223-testnet.ts
+++ vips/vip-223/vip-223-testnet.ts
@@ -3,6 +3,10 @@ import { makeProposal } from "../../src/utils";
 
 const XVS_VAULT_PROXY = "0x9aB56bAD2D7631B2A857ccf36d998232A8b82280";
 const XVS_VAULT = "0x9801565BE8fe2c9fD12C17992ecDeb510BaF677A";
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
+const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
+const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
 
 export const vip223Testnet = () => {
   const meta = {
@@ -30,6 +34,36 @@ export const vip223Testnet = () => {
         target: XVS_VAULT_PROXY,
         signature: "initializeTimeManager(bool,uint256)",
         params: [false, 10512000],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", CRITICAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", NORMAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlock(address,uint256)", FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlock(address,uint256)", CRITICAL_TIMELOCK],
+      },
+      {
+        target: ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [XVS_VAULT_PROXY, "setRewardAmountPerBlock(address,uint256)", NORMAL_TIMELOCK],
       },
     ],
     meta,
