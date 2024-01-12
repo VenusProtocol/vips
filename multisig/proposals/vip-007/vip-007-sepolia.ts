ab2a37472fc65e3f8f9a1a530dea580b24dae478
Narayan Prusty <7037606+narayanprusty@users.noreply.github.com>
2024-01-12 18:01:52 +0530

    fix: change to 5 minutes
    Co-authored-by: Jesus Lanchas <jesus.lanchas@daureos.com>

--- multisig/proposals/vip-007/vip-007-sepolia.ts
+++ multisig/proposals/vip-007/vip-007-sepolia.ts
@@ -3,7 +3,7 @@ import { makeProposal } from "../../../src/utils";
 export const XVS_VAULT_PROXY = "0x1129f882eAa912aE6D4f6D445b2E2b1eCbA99fd5";
 export const XVS = "0x66ebd019E86e0af5f228a0439EBB33f045CBe63E";
 export const POOL_ID = 0;
-export const LOCK_PERIOD = 360;
+export const LOCK_PERIOD = 300;
 
 export const vip007 = () => {
   return makeProposal([
