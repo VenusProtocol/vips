f68c882c5f52ec59203905bc694a56285510cd29
Kirill Kuvshinov <kkirka@gmail.com>
2024-01-17 16:25:06 +0300

    feat: swap directly on Uniswap

--- multisig/simulations/vip-002/vip-002-ethereum-bootstrap-liquidity/simulations.ts
+++ multisig/simulations/vip-002/vip-002-ethereum-bootstrap-liquidity/simulations.ts
@@ -1,25 +1,33 @@
 import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
+import { NETWORK_ADDRESSES } from "../../../../src/networkAddresses";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
 import { vip002 } from "../../../proposals/vip-002/vip-002-ethereum-bootstrap-liquidity";
 import ERC20_ABI from "./abis/ERC20.json";
 
 const VTREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
-const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+const NORMAL_TIMELOCK = NETWORK_ADDRESSES.ethereum.NORMAL_TIMELOCK;
+const UNI_V3_ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 const BBTC = "0x9BE89D2a4cd102D8Fecc6BF9dA793be995C22541";
+const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 
-const BBTC_AMOUNT = "30000000";
+const BBTC_AMOUNT = parseUnits("0.3", 8);
+const WBTC_AMOUNT = parseUnits("0.29818818", 8);
 
-forking(19020650, () => {
-  let bbtc: ethers.Contract;
-  let oldBBTCBal: BigNumber;
-  let oldBBTCBalTreasury: BigNumber;
+forking(19032429, () => {
+  let wbtc: Contract;
+  let bbtc: Contract;
+  let oldWBTCBalance: BigNumber;
+  let oldBBTCBalance: BigNumber;
 
   before(async () => {
+    wbtc = new ethers.Contract(WBTC, ERC20_ABI, ethers.provider);
     bbtc = new ethers.Contract(BBTC, ERC20_ABI, ethers.provider);
-    oldBBTCBal = await bbtc.balanceOf(COMMUNITY_WALLET);
-    oldBBTCBalTreasury = await bbtc.balanceOf(VTREASURY);
+    oldWBTCBalance = await wbtc.balanceOf(VTREASURY);
+    oldBBTCBalance = await bbtc.balanceOf(VTREASURY);
   });
 
   describe("Post-VIP behavior", async () => {
@@ -27,11 +35,26 @@ forking(19020650, () => {
       await pretendExecutingVip(vip002());
     });
 
-    it("Should transfer BBTC", async () => {
-      const currBBTCBal = await bbtc.balanceOf(COMMUNITY_WALLET);
-      expect(currBBTCBal.sub(oldBBTCBal)).equals(BBTC_AMOUNT);
-      const currBBTCBalTreasury = await bbtc.balanceOf(VTREASURY);
-      expect(currBBTCBalTreasury.add(BBTC_AMOUNT)).equals(oldBBTCBalTreasury);
+    it("should transfer BBTC out of treasury", async () => {
+      const currentBBTCBalance = await bbtc.balanceOf(VTREASURY);
+      expect(oldBBTCBalance.sub(currentBBTCBalance)).equals(BBTC_AMOUNT);
+    });
+
+    it("should transfer WBTC to treasury", async () => {
+      const currentWBTCBalance = await wbtc.balanceOf(VTREASURY);
+      expect(currentWBTCBalance.sub(oldWBTCBalance)).equals(WBTC_AMOUNT);
+    });
+
+    it("should not keep BBTC in the multisig", async () => {
+      expect(await bbtc.balanceOf(NORMAL_TIMELOCK)).equals(0);
+    });
+
+    it("should not keep WBTC in the multisig", async () => {
+      expect(await wbtc.balanceOf(NORMAL_TIMELOCK)).equals(0);
+    });
+
+    it("should not have any remaining approvals", async () => {
+      expect(await bbtc.allowance(NORMAL_TIMELOCK, UNI_V3_ROUTER)).equals(0);
     });
   });
 });
