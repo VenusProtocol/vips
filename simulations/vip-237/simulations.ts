0f2a6e8b999ebd51eb37e30962ad9c723c64a7ac
narayanprusty <arsenal.narayan@gmail.com>
2024-01-17 11:56:43 +0530

    fix: added payment to chaos labs

--- simulations/vip-237/simulations.ts
+++ simulations/vip-237/simulations.ts
@@ -11,20 +11,24 @@ import VTreasurey_ABI from "./abi/VTreasury.json";
 
 const USDT = "0x55d398326f99059ff775485246999027b3197955";
 const CERTIK_RECEIVER = "0x4cf605b238e9c3c72d0faed64d12426e4a54ee12";
+const CHAOS_LABS_RECEIVER = "0xfb1912af5b9d3fb678f801bf764e98f1c217ef35";
 const CERTIK_AMOUNT = parseUnits("19000", 18);
+const CHAOS_LABS_AMOUNT = parseUnits("130000", 18);
 
 forking(35259895, () => {
   let usdt: ethers.Contract;
   let prevBalanceCertik: BigNumber;
+  let prevBalanceChaosLabs: BigNumber;
 
   before(async () => {
     usdt = new ethers.Contract(USDT, IERC20_ABI, ethers.provider);
     prevBalanceCertik = await usdt.balanceOf(CERTIK_RECEIVER);
+    prevBalanceChaosLabs = await usdt.balanceOf(CHAOS_LABS_RECEIVER);
   });
 
   testVip("VIP-237 Payments for auditors", vip237(), {
     callbackAfterExecution: async txResponse => {
-      await expectEvents(txResponse, [VTreasurey_ABI], ["WithdrawTreasuryBEP20"], [1]);
+      await expectEvents(txResponse, [VTreasurey_ABI], ["WithdrawTreasuryBEP20"], [2]);
     },
   });
 
@@ -33,6 +37,12 @@ forking(35259895, () => {
       const currentBalance = await usdt.balanceOf(CERTIK_RECEIVER);
       const delta = currentBalance.sub(prevBalanceCertik);
       expect(delta).equals(CERTIK_AMOUNT);
+    });
+
+    it("Should increase balances of Chaos Labs receiver", async () => {
+      const currentBalance = await usdt.balanceOf(CHAOS_LABS_RECEIVER);
+      const delta = currentBalance.sub(prevBalanceChaosLabs);
+      expect(delta).equals(CHAOS_LABS_AMOUNT);
     });
   });
 });
--- vips/vip-237.ts
+++ vips/vip-237.ts
@@ -6,6 +6,7 @@ import { makeProposal } from "../src/utils";
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 const USDT = "0x55d398326f99059ff775485246999027b3197955";
 const CERTIK_RECEIVER = "0x4cf605b238e9c3c72d0faed64d12426e4a54ee12";
+const CHAOS_LABS_RECEIVER = "0xfb1912af5b9d3fb678f801bf764e98f1c217ef35";
 
 export const vip237 = () => {
   const meta = {
@@ -26,6 +27,11 @@ export const vip237 = () => {
         target: TREASURY,
         signature: "withdrawTreasuryBEP20(address,uint256,address)",
         params: [USDT, parseUnits("19000", 18), CERTIK_RECEIVER],
+      },
+      {
+        target: TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [USDT, parseUnits("130000", 18), CHAOS_LABS_RECEIVER],
       },
     ],
     meta,
