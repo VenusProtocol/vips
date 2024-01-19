da228cc2b9679d5b5c85ae6bec25e058118c6036
Corey Rice <coreyarice@gmail.com>
2024-01-19 11:31:38 -0300

    feat: add vip for steakhouse vai recomendations

---
+++ vips/vip-246/bscmainnet.ts
@@ -0,0 +1,52 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+
+export const VAI_CONTROLLER_PROXY = "0x004065D34C6b18cE4370ced1CeBDE94865DbFAFE";
+export const OLD_BASE_RATE_MANTISSA = parseUnits("0.07", 18); // Old Base Rate 7%
+export const NEW_BASE_RATE_MANTISSA = parseUnits("0.03", 18); // New Base Rate 3%
+
+export const OLD_FLOAT_RATE_MANTISSA = parseUnits("40", 18); // Old Float Rate 4000%
+export const NEW_FLOAT_RATE_MANTISSA = parseUnits("50", 18); // New Float Rate 5000%
+
+export const vip246 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-246 Update VAI Float and Base Rates",
+    description: `#### Summary
+
+This VIP will perform the following updates as per Steakhouse latest recommendations in this Venus community forum publication: [[VIP] Adjust VAI Base and Float Rate](https://community.venus.io/t/vip-adjust-vai-base-and-float-rate/4056)
+
+- Set VAI Base Rate: 3% (from 7%)
+- Set Float Rate: 5,000% (from 4,000%)
+
+Complete analysis and details of these recommendations are available in the above publication.
+
+#### References
+* [VIP Simulation](https://github.com/VenusProtocol/vips/pull/180)
+  `,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: VAI_CONTROLLER_PROXY,
+        signature: "setBaseRate(uint256)",
+        params: [NEW_BASE_RATE_MANTISSA],
+      },
+      {
+        target: VAI_CONTROLLER_PROXY,
+        signature: "setFloatRate(uint256)",
+        params: [NEW_FLOAT_RATE_MANTISSA],
+      },
+    ],
+    meta,
+    ProposalType.FAST_TRACK,
+  );
+};
+
+export default vip246;
