6b1757af0f422064a632955c5cb873774d58d23d
narayanprusty <arsenal.narayan@gmail.com>
2024-01-23 17:39:28 +0530

    fix: wip unlist market

--- simulations/vip-244/vip-244-testnet/simulation.ts
+++ simulations/vip-244/vip-244-testnet/simulation.ts
@@ -1,14 +1,28 @@
+import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
-import { vip244 } from "../../../vips/vip-244/vip-244-testnet";
+import { vip244, ACM, NORMAL_TIMELOCK, UNITROLLER } from "../../../vips/vip-244/vip-244-testnet";
 
+import ACM_ABI from "./abi/ACM.json";
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
 
 forking(37099393, () => {
+  let acm: ethers.Contract
 
   before(async () => {
+    impersonateAccount(UNITROLLER);
+    acm = new ethers.Contract(ACM, ACM_ABI, await ethers.getSigner(UNITROLLER));
   });
 
   testVip("VIP-244 Unlist Market", vip244(), {
     callbackAfterExecution: async txResponse => {
     },
   });
+
+  describe("ACM", () => {
+    it("should have the correct call permissions", async () => {
+      const callPermissions = await acm.isAllowedToCall(NORMAL_TIMELOCK, "unlistMarket(address)");
+      expect(callPermissions).to.be.true;
+    });
+  })
 });
--- vips/vip-244/vip-244-testnet.ts
+++ vips/vip-244/vip-244-testnet.ts
@@ -2,14 +2,14 @@ import { cutParams as params } from "../../simulations/vip-244/vip-244-testnet/u
 import { ProposalType } from "../../src/types";
 import { makeProposal } from "../../src/utils";
 
-const UNITROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
-const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
-const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
-const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
-const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
-const vLUNA = "0x9C3015191d39cF1930F92EB7e7BCbd020bCA286a";
-const vUST = "0xF206af85BC2761c4F876d27Bd474681CfB335EfA";
-const cutParams = params;
+export const UNITROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
+export const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
+export const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+export const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
+export const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
+export const vLUNA = "0x9C3015191d39cF1930F92EB7e7BCbd020bCA286a";
+export const vUST = "0xF206af85BC2761c4F876d27Bd474681CfB335EfA";
+export const cutParams = params;
 
 export const vip244 = () => {
   const meta = {
