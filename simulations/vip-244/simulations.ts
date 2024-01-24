33356c93c36525614b6ba4813edecc3e9c6f502e
narayanprusty <arsenal.narayan@gmail.com>
2024-01-24 17:14:25 +0530

    fix: added simulation and fixed calculations

--- simulations/vip-244/simulations.ts
+++ simulations/vip-244/simulations.ts
@@ -4,11 +4,19 @@ import { ethers } from "hardhat";
 
 import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import { UNITROLLER, vip244, VAI_VAULT_RATE, XVSSpeed, Speeds, vTokens, supplySpeeds, borrowSpeeds } from "../../vips/vip-244";
+import {
+  Speeds,
+  UNITROLLER,
+  VAI_VAULT_RATE,
+  blocksPerDay,
+  borrowSpeeds,
+  supplySpeeds,
+  vTokens,
+  vip244,
+} from "../../vips/vip-244";
 import COMPTROLLER_ABI from "./abi/Comptroller.json";
-import SETTER_FACET_ABI from "./abi/SetterFacet.json";
 
-const OLD_VAI_VAULT_RATE = parseUnits("0.004340277777777780", 18);
+const OLD_VAI_VAULT_RATE = parseUnits("125.00", 18).div(blocksPerDay);
 
 export const oldSupplySpeeds = Speeds.map(speed => speed.oldBorrowSpeed);
 export const oldBorrowSpeeds = Speeds.map(speed => speed.oldBorrowSpeed);
@@ -20,33 +28,48 @@ forking(35518397, () => {
     comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_ABI, ethers.provider);
   });
 
-  describe("Post-VIP behavior", async () => {
+  describe("Pre-VIP behavior", async () => {
     it("check XVS reward rate for VAI vault", async () => {
       const rate = await comptroller.venusVAIVaultRate();
-      expect(rate).to.be.equal(OLD_VAI_VAULT_RATE)
+      expect(rate).to.be.closeTo(OLD_VAI_VAULT_RATE, parseUnits("0.00000000000000001", 18).toString());
     });
+
+    for (let i = 0; i < vTokens.length; i++) {
+      it(`check XVS reward rate for ${Speeds[i].name}`, async () => {
+        const supplyRate = await comptroller.venusSupplySpeeds(Speeds[i].vToken);
+        expect(supplyRate).to.be.closeTo(oldSupplySpeeds[i], parseUnits("0.0001", 18).toString());
+
+        const borrowRate = await comptroller.venusBorrowSpeeds(Speeds[i].vToken);
+        expect(borrowRate).to.be.closeTo(oldSupplySpeeds[i], parseUnits("0.0001", 18).toString());
+      });
+    }
   });
 
   testVip("VIP-244", vip244(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(txResponse, [COMPTROLLER_ABI], ["NewVenusVAIVaultRate"], [1]);
-      await expectEvents(txResponse, [COMPTROLLER_ABI], ["VenusSupplySpeedUpdated", "VenusBorrowSpeedUpdated"], [21, 21]);
+      await expectEvents(
+        txResponse,
+        [COMPTROLLER_ABI],
+        ["VenusSupplySpeedUpdated", "VenusBorrowSpeedUpdated"],
+        [21, 21],
+      );
     },
   });
 
   describe("Post-VIP behavior", async () => {
     it("check XVS reward rate for VAI vault", async () => {
       const rate = await comptroller.venusVAIVaultRate();
-      expect(rate).to.be.equal(VAI_VAULT_RATE)
+      expect(rate).to.be.equal(VAI_VAULT_RATE);
     });
 
     for (let i = 0; i < vTokens.length; i++) {
       it(`check XVS reward rate for ${Speeds[i].name}`, async () => {
         const supplyRate = await comptroller.venusSupplySpeeds(Speeds[i].vToken);
-        expect(supplyRate).to.be.equal(supplySpeeds[i])
+        expect(supplyRate).to.be.equal(supplySpeeds[i]);
 
         const borrowRate = await comptroller.venusBorrowSpeeds(Speeds[i].vToken);
-        expect(borrowRate).to.be.equal(borrowSpeeds[i])
+        expect(borrowRate).to.be.equal(borrowSpeeds[i]);
       });
     }
   });
