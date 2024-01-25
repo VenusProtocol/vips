bac1d305fc144546894309ce7e99764d139dc8dd
narayanprusty <arsenal.narayan@gmail.com>
2024-01-25 22:50:49 +0530

    fix: minor changes

--- simulations/vip-244/vip-244-testnet/simulation.ts
+++ simulations/vip-244/vip-244-testnet/simulation.ts
@@ -1,9 +1,9 @@
 import { ethers } from "hardhat";
 import { forking, testVip } from "../../../src/vip-framework";
-import { vip244, ACM, NORMAL_TIMELOCK, UNITROLLER, vUST } from "../../../vips/vip-244/vip-244-testnet";
+import { vip244, ACM, NORMAL_TIMELOCK, UNITROLLER } from "../../../vips/vip-244/bsctestnet";
 
 import ACM_ABI from "./abi/ACM.json";
-import SETTER_FACET_ABI from "./abi/SetterFacet.json";
+import COMPTROLLER_FACET_ABI from "./abi/comptroller.json";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { expectEvents } from "../../../src/utils";
@@ -17,7 +17,7 @@ forking(37157398, () => {
     impersonateAccount(NORMAL_TIMELOCK);
 
     acm = new ethers.Contract(ACM, ACM_ABI, await ethers.getSigner(UNITROLLER));
-    comptroller = new ethers.Contract(UNITROLLER, SETTER_FACET_ABI, await ethers.getSigner(NORMAL_TIMELOCK));
+    comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_FACET_ABI, await ethers.getSigner(NORMAL_TIMELOCK));
   });
 
   describe("Pre-VIP", () => {
@@ -30,6 +30,12 @@ forking(37157398, () => {
   testVip("VIP-244 Unlist Market", vip244(), {
     callbackAfterExecution: async txResponse => {
       expectEvents(txResponse, [ACM_ABI], ["RoleGranted"], [3])
+      expectEvents(
+        txResponse, 
+        [COMPTROLLER_FACET_ABI], 
+        ["NewSupplyCap", "NewBorrowCap", "ActionPausedMarket", "NewCollateralFactor", "MarketUnlisted"], 
+        [2, 2, 12, 2, 2]
+      )
     },
   });
 
