e03f7cb9e3e582cc8bfc86f4dcddb74cb3410238
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-01-25 16:03:11 +0530

    feat: psr configuration for ethereum

---
+++ multisig/simulations/vip-005/vip-005-ethereum/simulations.ts
@@ -0,0 +1,79 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { initMainnetUser } from "../../../../src/utils";
+import { forking, pretendExecutingVip } from "../../../../src/vip-framework/index";
+import {
+  ACM,
+  ETHEREUM_MULTISIG,
+  MARKETS,
+  POOL_REGISTRY,
+  PROTOCOL_SHARE_RESERVE,
+  VTREASURY,
+  vip005,
+} from "../../../proposals/vip-005/vip-005-ethereum";
+import ACM_ABI from "./abi/accessControlManager.json";
+import PSR_ABI from "./abi/protocolShareReserve.json";
+import VTOKEN_ABI from "./abi/vToken.json";
+
+forking(19081818, () => {
+  let protocolShareReserve: Contract;
+  let accessControlManager: Contract;
+  let psrSigner: SignerWithAddress;
+
+  before(async () => {
+    protocolShareReserve = await ethers.getContractAt(PSR_ABI, PROTOCOL_SHARE_RESERVE);
+    accessControlManager = await ethers.getContractAt(ACM_ABI, ACM);
+
+    psrSigner = await initMainnetUser(PROTOCOL_SHARE_RESERVE, ethers.utils.parseEther("1"));
+    await pretendExecutingVip(vip005());
+  });
+
+  describe("Post tx checks", () => {
+    it("PSR owner should be multisig", async () => {
+      const owner = await protocolShareReserve.owner();
+      expect(owner).equals(ETHEREUM_MULTISIG);
+    });
+    it("PSR should have correct ACM reference", async () => {
+      const acm = await protocolShareReserve.accessControlManager();
+      expect(acm).equals(ACM);
+    });
+    it("PSR should have correct PoolRegistry reference", async () => {
+      expect(await protocolShareReserve.poolRegistry()).to.equal(POOL_REGISTRY);
+    });
+    it("Verify Multisig permissions for PSR", async () => {
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(ETHEREUM_MULTISIG, "addOrUpdateDistributionConfigs(DistributionConfig[])"),
+      ).to.be.true;
+
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(ETHEREUM_MULTISIG, "removeDistributionConfig(Schema,address)"),
+      ).to.be.true;
+    });
+    it("Validate PSR distribution config", async () => {
+      expect(await protocolShareReserve.totalDistributions()).to.equal(2);
+      expect(await protocolShareReserve.getPercentageDistribution(VTREASURY, 0)).to.equal(10000);
+      expect(await protocolShareReserve.getPercentageDistribution(VTREASURY, 1)).to.equal(10000);
+    });
+
+    MARKETS.forEach(market => {
+      describe(`${market.name}`, () => {
+        let vToken: Contract;
+
+        before(async () => {
+          vToken = await ethers.getContractAt(VTOKEN_ABI, market.address);
+        });
+
+        it(`Validate PSR reference in ${market.name} market `, async () => {
+          expect(await vToken.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
+        });
+      });
+    });
+  });
+});
