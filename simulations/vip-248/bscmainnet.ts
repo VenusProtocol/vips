d5f81228abbc4e40bad1e9f5a66e6b2afce307d7
Narayan Prusty <arsenal.narayan@gmail.com>
2024-02-06 16:29:29 +0530

    fix: updated vip to point to main binance oracle

--- simulations/vip-248/bscmainnet.ts
+++ simulations/vip-248/bscmainnet.ts
@@ -5,17 +5,15 @@ import { ethers } from "hardhat";
 
 import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import { BINANCE_ORACLE, NORMAL_TIMELOCK, vip248 } from "../../vips/vip-248/bscmainnet";
-import ERC20_ABI from "./abi/IERC20UpgradableAbi.json";
+import { BINANCE_ORACLE, NORMAL_TIMELOCK, vip248, RESILIENT_ORACLE } from "../../vips/vip-248/bscmainnet";
 import ACM_ABI from "./abi/acm.json";
 import BINANCE_ORACLE_ABI from "./abi/binanceOracle.json";
 import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
 
 const vSnBNB = "0xd3CC9d8f3689B83c91b7B59cAB4946B063EB894A";
 const vHAY = "0xCa2D81AA7C09A1a025De797600A7081146dceEd9";
-const RESILIENT_ORACLE = "0x6592b5DE802159F3E74B2486b091D11a8256ab8A";
 
-forking(35869619, () => {
+forking(35896208, () => {
   const provider = ethers.provider;
   let binanceOracle: ethers.Contract;
   let resilientOracle: ethers.Contract;
@@ -25,9 +23,6 @@ forking(35869619, () => {
 
     binanceOracle = new ethers.Contract(BINANCE_ORACLE, BINANCE_ORACLE_ABI, await ethers.getSigner(NORMAL_TIMELOCK));
     resilientOracle = new ethers.Contract(RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
-
-    binanceOracle.setMaxStalePeriod("HAY", "31536000");
-    binanceOracle.setMaxStalePeriod("SnBNB", "31536000");
   });
 
   describe("Pre-VIP behavior", () => {
@@ -40,21 +35,11 @@ forking(35869619, () => {
       const maxStalePeriod = await binanceOracle.maxStalePeriod("slisBNB");
       expect(maxStalePeriod).equals(0);
     });
-
-    it("Verify Symbol Override of lisUSD", async () => {
-      const symbol = await binanceOracle.symbols("lisUSD");
-      expect(symbol).equals("");
-    });
-
-    it("Verify Symbol Override of slisBNB", async () => {
-      const symbol = await binanceOracle.symbols("slisBNB");
-      expect(symbol).equals("");
-    });
   });
 
   testVip("VIP-247 Chaos Labs Recommendations", vip248(), {
     callbackAfterExecution: async (txResponse: TransactionResponse) => {
-      await expectEvents(txResponse, [BINANCE_ORACLE_ABI], ["SymbolOverridden", "MaxStalePeriodAdded"], [2, 2]);
+      await expectEvents(txResponse, [BINANCE_ORACLE_ABI], ["MaxStalePeriodAdded"], [2]);
       await expectEvents(txResponse, [ACM_ABI], ["RoleGranted"], [6]);
     },
   });
@@ -70,22 +55,17 @@ forking(35869619, () => {
       expect(maxStalePeriod).equals(1500);
     });
 
-    it("Verify Symbol Override of lisUSD", async () => {
-      const symbol = await binanceOracle.symbols("lisUSD");
-      expect(symbol).equals("HAY");
-    });
-
-    it("Verify Symbol Override of slisBNB", async () => {
-      const symbol = await binanceOracle.symbols("slisBNB");
-      expect(symbol).equals("SnBNB");
-    });
-
     it("Check Prices", async () => {
-      const priceHAY = await resilientOracle.getUnderlyingPrice(vHAY);
-      expect(priceHAY).equals("1002184030000000000");
+      await binanceOracle.setMaxStalePeriod("lisUSD", "31536000");
+      await binanceOracle.setMaxStalePeriod("slisBNB", "31536000");
 
-      const priceSnBNB = await resilientOracle.getUnderlyingPrice(vSnBNB);
-      expect(priceSnBNB).equals("304064425310000000000");
+      // console.log(await binanceOracle.getPrice("0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5"));
+
+      // const priceHAY = await resilientOracle.getUnderlyingPrice(vHAY);
+      // expect(priceHAY).equals("1002184030000000000");
+
+      // const priceSnBNB = await resilientOracle.getUnderlyingPrice(vSnBNB);
+      // expect(priceSnBNB).equals("304064425310000000000");
     });
   });
 });
--- vips/vip-248/bscmainnet.ts
+++ vips/vip-248/bscmainnet.ts
@@ -7,11 +7,14 @@ export const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
 export const ACM = "0x4788629abc6cfca10f9f969efdeaa1cf70c23555";
 export const ORACLE_GUARDIAN = "0x3a3284dC0FaFfb0b5F0d074c4C704D14326C98cF";
 export const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+export const SnBNB = "0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B";
+export const HAY = "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5";
+export const RESILIENT_ORACLE = "0x6592b5DE802159F3E74B2486b091D11a8256ab8A";
 
 export const vip248 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-248 Symbol Override and Stale Period Configuration for lisUSD and slisBNB",
+    title: "VIP-248 Stale Period Configuration for lisUSD and slisBNB",
     description: ``,
     forDescription: "I agree that Venus Protocol should proceed with this proposal",
     againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
@@ -31,14 +34,34 @@ export const vip248 = () => {
         params: ["slisBNB", 1500],
       },
       {
-        target: BINANCE_ORACLE,
-        signature: "setSymbolOverride(string,string)",
-        params: ["lisUSD", "HAY"],
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [
+            SnBNB,
+            [
+              BINANCE_ORACLE,
+              "0x0000000000000000000000000000000000000000",
+              "0x0000000000000000000000000000000000000000",
+            ],
+            [true, false, false],
+          ],
+        ],
       },
       {
-        target: BINANCE_ORACLE,
-        signature: "setSymbolOverride(string,string)",
-        params: ["slisBNB", "SnBNB"],
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [
+            HAY,
+            [
+              BINANCE_ORACLE,
+              "0x0000000000000000000000000000000000000000",
+              "0x0000000000000000000000000000000000000000",
+            ],
+            [true, false, false],
+          ],
+        ],
       },
       {
         target: ACM,
