b8d9c92763592fac9d22cf2cc0e382986405c19a
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-02-08 20:07:32 +0100

    feat: add description for VIP redeeming vTokens

--- vips/vip-256/bscmainnet.ts
+++ vips/vip-256/bscmainnet.ts
@@ -285,11 +285,149 @@ const BEP20TransferCommands = BEP20Transfers.map((bep20Transfer: BEP20Transfer) 
 export const vip256 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-256",
-    description: ``,
-    forDescription: "I agree that Venus Protocol should proceed with this proposal",
-    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
-    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with this proposal",
+    title: "VIP-253 Residual Shortfall Repayments",
+    description: `Following [VIP-244](https://app.venus.io/#/governance/proposal/244?chainId=56), if passed, this VIP will execute steps 1, 2, 3 and 4 of the execution plan to further repay the remaining shortfall in the BNB Bridge Exploiter account by reducing the USDC debt. This repayment will make use of the fees from the liquidation of this account and the protocol risk fund balance. The expected repayment amount is: $7,205,911
+
+The following are the relevant balances for the repayment operation:
+
+*USD values consider token prices for Feb 05, blocknumber: 35735239*
+
+BNB Bridge Exploiter Shortfall Balance:
+
+- Total Shortfall balance: $14,680,936
+- USDC Debt: 39,865,397
+
+Liquidation fees:
+
+- 15,426.60 BNB ($4,627,995)
+
+vBNB Reserves:
+
+- 14,663 BNB ($4,399,200)
+
+vTreasury vToken Balance (underlying tokens with positions larger than $1,000):
+
+- BNB: 15,846.91 ($4,765,166)
+- BTCB: 7.0058 ($298,554.20)
+- ETH: 34.742 ($79,664.51)
+- USDT: 59,016.0718 ($58,952.04)
+- LTC: 560.7673 ($37,805.07)
+- USDC: 28,907.0181 ($28,907.02)
+- XVS: 2,644.112 ($28,067.79)
+- Cake: 6,794.3173 ($16,408.28)
+- UNI: 2,504.5077 ($15,013.60)
+- FDUSD: 10,523.3085 ($10,511.16)
+- DOT: 1,236.6557 ($8,412.67)
+- ADA(Binance-Peg): 13,755.53 ($6,808.90)
+- LINK: 283.3125 ($5,463.20)
+- XRP: 10,855.8946 ($5,456.05)
+- TUSD(old): 4,882.8203 ($4,817.39)
+- MATIC: 5,323.2409 ($4,157.54)
+- BETH: 1.4393 ($3,388.09)
+- DOGE: 39,455.8297 ($3,100.74)
+- FIL: 532.2574 ($2,637.79)
+- TUSD: 2,654.6795 ($2,612.56)
+- wBETH: 0.8946 ($2,114.03)
+- Others: $607
+
+ProtocolShareReserve (PSR) Token Balance (tokens with positions larger than $1,000):
+
+- USDT: 563,748.4017 ($563,074.72)
+- USDC: 266,877.8406 ($266,877.84)
+- BTCB: 2.6778 ($113,881.01)
+- ETH: 38.1224 ($87,338.70)
+- WBNB: 88.3266 ($26,524.49)
+- DAI: 10,908.977 ($10,907.34)
+- TUSD: 10,108.571 ($9,948.18)
+- DOT: 1,359.6979 ($9,209.49)
+- FLOKI: 233,081,491.374 ($6,423.67)
+- Cake: 2,278.5927 ($5,477.74)
+- LINK: 192.3052 ($3,695.27)
+- TRX: 29,104.4352 ($3,455.94)
+- FDUSD: 2,645.3861 ($2,642.33)
+- ADA(Binance-Peg): 4,129.2605 ($2,040.56)
+- DOGE: 20,599.2848 ($1,618.85)
+- SXP: 11,475.6309 ($1,163.44)
+- MATIC: 1,330.6035 ($1,037.84)
+- Others: $4,171
+
+#### Execution Plan
+
+The protocol's tokenomics uses the same distribution proportion for treasury allocation and risk fund for protocol reserves (40%). These allocations will now be executed automatically, following the latest [Automatic Income Allocator deployment](https://app.venus.io/#/governance/proposal/248?chainId=56).
+
+The risk fund allocation is set to go through a converter to swap and store assets in USDT. The treasury allocation has no converter yet and will receive the allocations in underlying tokens.
+
+The current protocol shortfall primarily lies in the BNB Bridge Exploiter account, where the total debts are in USDC and USDT, meaning that repaying either USDT or USDC will have the same effect in insolvency reduction.
+
+The expense of borrowings for this account increases the protocol's insolvency, as the borrow amount rises over time. With this, due to current market conditions, the proposal is to repay USDC since the average borrow expense for USDC is four times larger than for USDT.
+
+The optimal procedure for repaying USDC will involve using the allocations transferred to the treasury, as this will prevent a double conversion from underlying tokens to USDT, then to USDC. Given that the treasury and risk fund share the same allocation proportion for reserve funds, the proposal suggests converting these assets from the treasury directly to USDC using Binance, and then reimbursing this expense with the USDT converted in the risk fund, allowing for a more cost-efficient process.
+
+To settle accounting, a future VIP will be proposed to send an equal amount of USDT to the treasury to cover the shortfall repayment. The end result will be a net neutral amount for the vTreasury balance, with 40% of the treasury allocation converted to USDT and a reduction in the risk fund balance for the repayments of the shortfall.
+
+To reference the liquidation fees, all revenues are stored in the vTreasury in the form of vTokens. These will be claimed, and 50% of their value will also be sent to Binance for conversion to USDC. The BNB market has an exception, as it includes liquidation fees generated through the various liquidations for the BNB Bridge Exploiter. These fees will be used entirely to cover the shortfall.
+
+It is important to consider that tokens with a balance of less than $1,000 will not be included in the repayment operation, as their impact is negligible and excluding them will reduce the complexity of the operation.
+
+**The process for the repayment will follow these steps:**
+
+1. Redeem all vTokens from the vTreasury.
+2. Send the Risk Fund allocation amount from the resulting redemption total to Binance to be converted to USDC.
+    1. 50% of the resulting token amounts for all tokens except vBNB.
+    2. vBNB will consider the total liquidation fees from the BNB Bridge exploiter in addition to the 50% allocation from regular liquidations.
+    3. USDC amounts in the vTreasury will not be sent to Binance as there is no need for conversion.
+3. Send 40% of all tokens sent from the PSR contract to the vTreasury to Binance for conversion to USDC.
+    1. 40% of the total USDC will not be sent as there are no conversion needs.
+4. Once the USDC is transferred to the vTreasury, use it to partially repay the USDC debt in the BNB Bridge Exploiter account. This step will be executed in another VIP.
+
+#### Amounts to transfer to Binance for conversion
+
+**Tokens from the vTreasury after redemption of vTokens**
+
+- BNB: 15,636.74 ($4,691,023)
+- BTCB: 3.5029 ($149,277.10)
+- ETH: 17.371 ($39,832.26)
+- USDT: 29,508.0359 ($29,476.02)
+- LTC: 280.38365 ($18,902.54)
+- XVS: 1,322.056 ($14,033.90)
+- Cake: 3,397.15865 ($8,204.14)
+- UNI: 1,252.25385 ($7,506.80)
+- FDUSD: 5,261.65425 ($5,255.58)
+- DOT: 618.32785 ($4,206.34)
+- ADA(Binance-Peg): 6,877.765 ($3,404.45)
+- XRP: 5,427.9473 ($2,728.03)
+- LINK: 141.65625 ($2,731.60)
+- MATIC: 2,661.62045 ($2,078.77)
+- BETH: 0.71965 ($1,694.05)
+- DOGE: 19,727.91485 ($1,550.37)
+- FIL: 266.1287 ($1,318.90)
+- TUSD: 1,327.33975 ($1,306.28)
+- wBETH: 0.4473 ($1,057.02)
+
+**Tokens from the vTreasury after allocation transfer from the PSR:**
+
+- BNB: 5,865.60 ($1,759,680.00)
+- USDT: 219,816.5002 ($219,718.68)
+- BTCB: 1.06876 ($45,966.30)
+- ETH: 14.55908 ($33,473.37)
+- DAI: 4,292.3044 ($4,291.88)
+- TUSD: 3,883.51888 ($3,823.56)
+- DOT: 529.97468 ($3,666.94)
+- Cake: 891.64848 ($2,180.97)
+- LINK: 75.03636 ($1,344.74)
+- TRX: 11,124.31032 ($1,292.25)
+- FDUSD: 1,031.64844 ($1,030.59)
+
+**Total Amount to send to Binance:**
+
+- $7,082,298.38
+
+**Estimated USDC Repayment Amount:**
+
+- $7,205,911.72`,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
   };
 
   return makeProposal(
