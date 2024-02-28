3e0ddf6c8a465fe127262aa759948df56bc69de7
Narayan Prusty <arsenal.narayan@gmail.com>
2024-02-28 17:50:02 +0530

    fix: added commands to bridge xvs

--- simulations/vip-263/simulations.ts
+++ simulations/vip-263/simulations.ts
@@ -17,14 +17,16 @@ import {
   USDT,
   XVS,
   XVS_AMOUNT,
+  XVS_BRIDGE,
   XVS_RECEIVER,
   vip263,
 } from "../../vips/vip-263/bscmainnet";
 import IERC20_ABI from "./abi/IERC20UpgradableAbi.json";
 import REWARD_FACET_ABI from "./abi/RewardFacet.json";
 import VTreasurey_ABI from "./abi/VTreasury.json";
+import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTSrc.json";
 
-forking(36524619, () => {
+forking(36530274, () => {
   let usdc: ethers.Contract;
   let usdt: ethers.Contract;
   let xvs: ethers.Contract;
@@ -34,11 +36,15 @@ forking(36524619, () => {
   let prevUSDTBalanceOfCommunity: BigNumber;
   let prevXVSBalanceOfXVSReceiver: BigNumber;
   let prevBNBBalanceOfCommunity: BigNumber;
+  let xvsBridge: ethers.Contract;
+  let oldCirculatingSupply: BigNumber;
+  let oldXVSBalance: BigNumber;
 
   before(async () => {
     usdc = new ethers.Contract(USDC, IERC20_ABI, ethers.provider);
     usdt = new ethers.Contract(USDT, IERC20_ABI, ethers.provider);
     xvs = new ethers.Contract(XVS, IERC20_ABI, ethers.provider);
+    xvsBridge = new ethers.Contract(XVS_BRIDGE, XVS_BRIDGE_ABI, ethers.provider);
 
     prevUSDTBalanceOfCertik = await usdt.balanceOf(CERTIK_RECEIVER);
     prevUSDCBalanceOfQuantstamp = await usdc.balanceOf(QUANTSTAMP_RECEIVER);
@@ -46,14 +52,19 @@ forking(36524619, () => {
     prevUSDTBalanceOfCommunity = await usdt.balanceOf(COMMUNITY_RECEIVER);
     prevXVSBalanceOfXVSReceiver = await xvs.balanceOf(XVS_RECEIVER);
     prevBNBBalanceOfCommunity = await ethers.provider.getBalance(COMMUNITY_RECEIVER);
+
+    oldCirculatingSupply = await xvsBridge.circulatingSupply();
+    oldXVSBalance = await xvs.balanceOf(XVS_BRIDGE);
   });
 
   testVip("VIP-263", vip263(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(txResponse, [VTreasurey_ABI], ["WithdrawTreasuryBEP20"], [3]);
       await expectEvents(txResponse, [VTreasurey_ABI], ["WithdrawTreasuryBNB"], [1]);
-      await expectEvents(txResponse, [REWARD_FACET_ABI], ["VenusGranted"], [1]);
+      await expectEvents(txResponse, [REWARD_FACET_ABI], ["VenusGranted"], [2]);
+      await expectEvents(txResponse, [XVS_BRIDGE_ABI], ["SendToChain"], [1]);
     },
+    proposer: "0x55A9f5374Af30E3045FB491f1da3C2E8a74d168D",
   });
 
   describe("Post-VIP behavior", async () => {
@@ -71,6 +82,16 @@ forking(36524619, () => {
       expect(newBNBBalanceOfCommunity).to.be.gte(prevBNBBalanceOfCommunity.add(COMMUNITY_BNB_AMOUNT));
       expect(newUSDTBalanceOfCommunity).to.be.eq(prevUSDTBalanceOfCommunity.add(COMMUNITY_USDT_AMOUNT));
       expect(newXVSBalanceOfXVSReceiver).to.be.eq(prevXVSBalanceOfXVSReceiver.add(XVS_AMOUNT));
+    });
+
+    it("Should decrease circulating supply", async () => {
+      const currCirculatingSupply = await xvsBridge.circulatingSupply();
+      expect(oldCirculatingSupply.sub(currCirculatingSupply)).equals(XVS_AMOUNT);
+    });
+
+    it("Should increase number of locked tokens on bridge", async () => {
+      const currXVSBal = await xvs.balanceOf(XVS_BRIDGE);
+      expect(currXVSBal.sub(oldXVSBalance)).equals(XVS_AMOUNT);
     });
   });
 });
