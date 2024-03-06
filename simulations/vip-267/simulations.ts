967673704843a00e669cb8af07f1eb9f0e48cc18
Kirill Kuvshinov <kkirka@gmail.com>
2024-03-06 15:40:07 +0300

    feat: support sweepTokens in the helper contract

--- simulations/vip-267/simulations.ts
+++ simulations/vip-267/simulations.ts
@@ -1,3 +1,4 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { formatUnits, parseEther, parseUnits } from "ethers/lib/utils";
@@ -8,14 +9,16 @@ import { NORMAL_TIMELOCK, forking, testVip } from "../../src/vip-framework";
 import vip267 from "../../vips/vip-267/bscmainnet";
 import COMPTROLLER_ABI from "./abi/Comptroller.json";
 import IERC20_ABI from "./abi/IERC20Abi.json";
+import LIQUIDATE_AND_REDEEM_ABI from "./abi/LiquidateAndRedeem.json";
 import VTOKEN_ABI from "./abi/VBep20DelegateAbi.json";
+import TREASURY_ABI from "./abi/VTreasury.json";
 
 const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
 const VUSDC = "0xecA88125a5ADbe82614ffC12D0DB554E2e2867C8";
 const CHAINLINK_ORACLE = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
 
 const TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
-const LIQUIDATE_AND_REDEEM_HELPER = "0xA08301b7C5f4BccD654De95E8C9BD4388CC54Ec1";
+const LIQUIDATE_AND_REDEEM_HELPER = "0x05D90a7Ca62E8f20AAb0D3B569Cf6BDd1C70AB8b";
 const VBNB = "0xA07c5b74C9B40447a954e1466938b865b6BBea36";
 const LIQUIDATOR = "0x0870793286aaDA55D39CE7f82fb2766e8004cF43";
 const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
@@ -23,20 +26,25 @@ const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 const EXPLOITER_WALLET = "0x489A8756C18C0b8B24EC2a2b9FF3D4d447F79BEc";
 const BINANCE_WALLET = "0x6657911F7411765979Da0794840D671Be55bA273";
 const VUSDC_AMOUNT = parseUnits("326081635.9401868", 8);
-const REPAY_AMOUNT = parseUnits("7605011.44891787996502290", 18);
-const EXPECTED_INTEREST_ACCUMULATED = parseUnits("8.018348270861963412", 18);
-const REPAY_AMOUNT_WITH_INTEREST = REPAY_AMOUNT.add(EXPECTED_INTEREST_ACCUMULATED);
-const EXPECTED_BNB_AMOUNT = parseUnits("20456.631368134499749989", 18);
+const REPAY_AMOUNT_WITH_INTEREST = parseUnits("7606059.637666350902865002", 18);
+const EXPECTED_BNB_AMOUNT = parseUnits("19740.154659961833876246", 18);
 
 // Interest rate model with no interest, for testing purposes
 const ZERO_RATE_MODEL = "0x93FBc248e83bc8931141ffC7f457EC882595135A";
 
-forking(36726026, () => {
+forking(36731756, () => {
   const usdc = new ethers.Contract(USDC, IERC20_ABI, ethers.provider);
   const vUSDC = new ethers.Contract(VUSDC, VTOKEN_ABI, ethers.provider);
   const vBNB = new ethers.Contract(VBNB, VTOKEN_ABI, ethers.provider);
   const comptroller = new ethers.Contract(COMPTROLLER, COMPTROLLER_ABI, ethers.provider);
+  const treasury = new ethers.Contract(TREASURY, TREASURY_ABI, ethers.provider);
+  const liquidateAndRedeemHelper = new ethers.Contract(
+    LIQUIDATE_AND_REDEEM_HELPER,
+    LIQUIDATE_AND_REDEEM_ABI,
+    ethers.provider,
+  );
 
+  let timelock: SignerWithAddress;
   let treasuryVTokenBalanceBefore: BigNumber;
   let treasuryBalanceBefore: BigNumber;
   let exploiterDebtBefore: BigNumber;
@@ -46,13 +54,56 @@ forking(36726026, () => {
     await setMaxStaleCoreAssets(CHAINLINK_ORACLE, NORMAL_TIMELOCK);
 
     // Set zero rate models to prevent interest accruals, so that borrow balance diffs are exact
-    const timelock = await initMainnetUser(NORMAL_TIMELOCK, parseEther("1"));
+    timelock = await initMainnetUser(NORMAL_TIMELOCK, parseEther("1"));
     vUSDC.connect(timelock)._setInterestRateModel(ZERO_RATE_MODEL);
 
     treasuryBalanceBefore = await usdc.balanceOf(TREASURY);
     treasuryVTokenBalanceBefore = await vUSDC.balanceOf(TREASURY);
     exploiterDebtBefore = await vUSDC.callStatic.borrowBalanceCurrent(EXPLOITER_WALLET);
     binanceWalletBalanceBefore = await ethers.provider.getBalance(BINANCE_WALLET);
+  });
+
+  describe("Liquidate and redeem helper", () => {
+    const USDC_SWEEP_AMOUNT = parseUnits("10", 18);
+    const NATIVE_SWEEP_AMOUNT = parseUnits("0.01", 18);
+
+    before(async () => {
+      await treasury.connect(timelock).withdrawTreasuryBEP20(USDC, USDC_SWEEP_AMOUNT, LIQUIDATE_AND_REDEEM_HELPER);
+      await treasury.connect(timelock).withdrawTreasuryBNB(NATIVE_SWEEP_AMOUNT, LIQUIDATE_AND_REDEEM_HELPER);
+    });
+
+    it("is owned by timelock", async () => {
+      const liquidateAndRedeemHelper = new ethers.Contract(
+        LIQUIDATE_AND_REDEEM_HELPER,
+        LIQUIDATE_AND_REDEEM_ABI,
+        ethers.provider,
+      );
+      expect(await liquidateAndRedeemHelper.owner()).to.equal(NORMAL_TIMELOCK);
+    });
+
+    it("does not allow a non-owner to sweep tokens", async () => {
+      const [someone] = await ethers.getSigners();
+      const tx = liquidateAndRedeemHelper.connect(someone).sweepTokens(USDC, someone.address);
+      await expect(tx).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("does not allow a non-owner to sweep native", async () => {
+      const [someone] = await ethers.getSigners();
+      const tx = liquidateAndRedeemHelper.connect(someone).sweepNative(someone.address);
+      await expect(tx).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("allows timelock to sweep tokens", async () => {
+      const tx = await liquidateAndRedeemHelper.connect(timelock).sweepTokens(USDC, TREASURY);
+      await expect(tx).to.changeTokenBalance(usdc, TREASURY, USDC_SWEEP_AMOUNT);
+      expect(await usdc.balanceOf(LIQUIDATE_AND_REDEEM_HELPER)).to.equal(0);
+    });
+
+    it("allows timelock to sweep native", async () => {
+      const tx = await liquidateAndRedeemHelper.connect(timelock).sweepNative(TREASURY);
+      await expect(tx).to.changeEtherBalance(TREASURY, NATIVE_SWEEP_AMOUNT);
+      expect(await ethers.provider.getBalance(LIQUIDATE_AND_REDEEM_HELPER)).to.equal(0);
+    });
   });
 
   testVip("VIP-267", vip267());
