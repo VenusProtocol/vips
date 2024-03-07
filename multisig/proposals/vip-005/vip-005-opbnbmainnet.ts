4d3cf4b9816127cc4774718b98c790e7a882b011
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-03-07 15:47:44 +0100

    fix: remove empty command

--- multisig/proposals/vip-005/vip-005-opbnbmainnet.ts
+++ multisig/proposals/vip-005/vip-005-opbnbmainnet.ts
@@ -44,7 +44,6 @@ export const vip005 = () => {
       signature: "acceptOwnership()",
       params: [],
     },
-    {},
     {
       target: ACM,
       signature: "giveCallPermission(address,string,address)",
