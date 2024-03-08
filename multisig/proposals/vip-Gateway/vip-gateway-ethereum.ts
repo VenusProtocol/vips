7979b00016812ca59517c987dcaae42c80c65bfa
Debugger022 <debugger022@gmail.com>
2024-03-08 09:41:39 +0530

    feat: update addresses for ethereum vip

--- multisig/proposals/vip-Gateway/vip-gateway-ethereum.ts
+++ multisig/proposals/vip-Gateway/vip-gateway-ethereum.ts
@@ -2,9 +2,10 @@ import { makeProposal } from "../../../src/utils";
 
 export const COMPTROLLER_BEACON = "0xAE2C3F21896c02510aA187BdA0791cDA77083708";
 export const VTOKEN_BEACON = "0xfc08aADC7a1A93857f6296C3fb78aBA1d286533a";
-export const NEW_COMPTROLLER_IMPLEMENTATION = "";
-export const NEW_VTOKEN_IMPLEMENTATION = "";
-export const NATIVE_TOKEN_GATEWAY = "";
+export const NEW_COMPTROLLER_IMPLEMENTATION = "0x13B3f65C0e2C64528F678B3C78ccac7341a2A66C";
+export const NEW_VTOKEN_IMPLEMENTATION = "0xE5A008B6A0bAB405343B3ABe8895966EAaFb5790";
+export const NATIVE_TOKEN_GATEWAY_VWETH_CORE = "0x062c7E0FB3E893d0fbB635F6911CDe7C2fB7E346";
+export const NATIVE_TOKEN_GATEWAY_VWETH_LST = "0x664488124Df6D48670fE38930F0e4Ce2a4faEdE1";
 
 export const vipGateway = () => {
   return makeProposal([
@@ -19,7 +20,12 @@ export const vipGateway = () => {
       params: [NEW_VTOKEN_IMPLEMENTATION],
     },
     {
-      target: NATIVE_TOKEN_GATEWAY,
+      target: NATIVE_TOKEN_GATEWAY_VWETH_CORE,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: NATIVE_TOKEN_GATEWAY_VWETH_LST,
       signature: "acceptOwnership()",
       params: [],
     },
--- multisig/simulations/vip-Gateway/vip-gateway-ethereum/simulations.ts
+++ multisig/simulations/vip-Gateway/vip-gateway-ethereum/simulations.ts
@@ -1,14 +1,13 @@
 import { expect } from "chai";
-import { BigNumber, Contract, Signer } from "ethers";
-import { parseUnits } from "ethers/lib/utils";
+import { BigNumber, Contract } from "ethers";
 import { ethers } from "hardhat";
 
-import { initMainnetUser } from "../../../../src/utils";
 import { NORMAL_TIMELOCK, forking, pretendExecutingVip } from "../../../../src/vip-framework";
 import { checkIsolatedPoolsComptrollers } from "../../../../src/vip-framework/checks/checkIsolatedPoolsComptrollers";
 import {
   COMPTROLLER_BEACON,
-  NATIVE_TOKEN_GATEWAY,
+  NATIVE_TOKEN_GATEWAY_VWETH_CORE,
+  NATIVE_TOKEN_GATEWAY_VWETH_LST,
   NEW_COMPTROLLER_IMPLEMENTATION,
   NEW_VTOKEN_IMPLEMENTATION,
   VTOKEN_BEACON,
@@ -16,35 +15,24 @@ import {
 } from "../../../proposals/vip-Gateway/vip-gateway-ethereum";
 import BEACON_ABI from "../abi/Beacon.json";
 import COMPTROLLER_ABI from "../abi/Comptroller.json";
-import ERC20_ABI from "../abi/ERC20.json";
 import NATIVE_TOKEN_GATEWAY_ABI from "../abi/NativeTokenGateway.json";
-import VTOKEN_ABI from "../abi/VToken.json";
 
-const OLD_COMPTROLLER_IMPLEMENTATION = "0x4e9d41D91Abb8590Dba0dcb37C682D312a245fC4 ";
+const OLD_COMPTROLLER_IMPLEMENTATION = "0x4e9d41D91Abb8590Dba0dcb37C682D312a245fC4";
 const OLD_VTOKEN_IMPLEMENTATION = "0x48Df4806e6734B5881f59AE7237988D3dA50891e";
 
-const VWETH_CORE = "0x7c8ff7d2A1372433726f879BD945fFb250B94c65";
 const VCRV_USD_CORE = "0x672208C10aaAA2F9A6719F449C4C8227bc0BC202";
 const CORE_POOL = "0x687a01ecF6d3907658f7A7c714749fAC32336D1B";
-
-const CRV_USD = "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E";
-const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
 const USER_1 = "0x9026A229b535ecF0162Dfe48fDeb3c75f7b2A7AE";
 const USER_2 = "0x7041bB74553fD011268Da863496dA3CBE4Ab8787";
 
-forking(19161237, () => {
+forking(19387790, () => {
   const provider = ethers.provider;
-  let user1: Signer;
-  let user2: Signer;
   let comptroller: Contract;
-  let vWeth: Contract;
-  let vcrvUsd: Contract;
-  let crvUsd: Contract;
-  let weth: Contract;
   let comptrollerBeacon: Contract;
   let vtokenBeacon: Contract;
-  let nativeTokenGateway: Contract;
+  let nativeTokenGatewayCore: Contract;
+  let nativeTokenGatewayLst: Contract;
 
   let accessControlManager: string;
   let closeFactorMantissa: BigNumber;
@@ -60,20 +48,13 @@ forking(19161237, () => {
   let prime: string;
 
   before(async () => {
-    user1 = await initMainnetUser(USER_1, parseUnits("2"));
-    user2 = await initMainnetUser(USER_2, parseUnits("2"));
-
-    nativeTokenGateway = new ethers.Contract(NATIVE_TOKEN_GATEWAY, NATIVE_TOKEN_GATEWAY_ABI, provider);
+    nativeTokenGatewayCore = new ethers.Contract(NATIVE_TOKEN_GATEWAY_VWETH_CORE, NATIVE_TOKEN_GATEWAY_ABI, provider);
+    nativeTokenGatewayLst = new ethers.Contract(NATIVE_TOKEN_GATEWAY_VWETH_LST, NATIVE_TOKEN_GATEWAY_ABI, provider);
 
     comptrollerBeacon = new ethers.Contract(COMPTROLLER_BEACON, BEACON_ABI, provider);
     comptroller = new ethers.Contract(CORE_POOL, COMPTROLLER_ABI, provider);
 
     vtokenBeacon = new ethers.Contract(VTOKEN_BEACON, BEACON_ABI, provider);
-    vWeth = new ethers.Contract(VWETH_CORE, VTOKEN_ABI, provider);
-    vcrvUsd = new ethers.Contract(VCRV_USD_CORE, VTOKEN_ABI, provider);
-
-    crvUsd = new ethers.Contract(CRV_USD, ERC20_ABI, provider);
-    weth = new ethers.Contract(WETH, ERC20_ABI, provider);
 
     accessControlManager = await comptroller.accessControlManager();
     closeFactorMantissa = await comptroller.closeFactorMantissa();
@@ -90,13 +71,13 @@ forking(19161237, () => {
   });
 
   describe("Pre-VIP behaviour", async () => {
-    it("comptroller should have old implementations", async () => {
+    it("comptroller should have old implementation", async () => {
       expect((await comptrollerBeacon.implementation()).toLowerCase()).to.equal(
         OLD_COMPTROLLER_IMPLEMENTATION.toLowerCase(),
       );
     });
 
-    it("vToken should have old implementations", async () => {
+    it("vToken should have old implementation", async () => {
       expect((await vtokenBeacon.implementation()).toLowerCase()).to.equal(OLD_VTOKEN_IMPLEMENTATION.toLowerCase());
     });
   });
@@ -106,18 +87,22 @@ forking(19161237, () => {
       await pretendExecutingVip(vipGateway());
     });
 
-    it("comptroller should have new implementations", async () => {
+    it("comptroller should have new implementation", async () => {
       expect((await comptrollerBeacon.implementation()).toLowerCase()).to.equal(
         NEW_COMPTROLLER_IMPLEMENTATION.toLowerCase(),
       );
     });
 
-    it("vToken should have new implementations", async () => {
+    it("vToken should have new implementation", async () => {
       expect((await vtokenBeacon.implementation()).toLowerCase()).to.equal(NEW_VTOKEN_IMPLEMENTATION.toLowerCase());
     });
 
-    it("timelock should be the owner of NativeTokenGateway contract", async () => {
-      expect(await nativeTokenGateway.owner()).to.equal(NORMAL_TIMELOCK);
+    it("timelock should be the owner of NativeTokenGateway contract for core pool market", async () => {
+      expect(await nativeTokenGatewayCore.owner()).to.equal(NORMAL_TIMELOCK);
+    });
+
+    it("timelock should be the owner of NativeTokenGateway contract for lst pool market", async () => {
+      expect(await nativeTokenGatewayLst.owner()).to.equal(NORMAL_TIMELOCK);
     });
 
     it("storage layout of comptroller should be consistent", async () => {
@@ -134,72 +119,6 @@ forking(19161237, () => {
       expect(await comptroller.poolRegistry()).to.equal(poolRegistry);
       expect(await comptroller.prime()).to.equal(prime);
       expect(await comptroller.approvedDelegates(USER_1, USER_2)).to.equal(false);
-    });
-
-    describe("onBehalfTests", () => {
-      beforeEach(async () => {
-        await comptroller.connect(user1).enterMarkets([VCRV_USD_CORE, VWETH_CORE]);
-        await comptroller.connect(user1).updateDelegate(USER_2, false);
-
-        await crvUsd.connect(user1).approve(vcrvUsd.address, parseUnits("1", 18));
-        await vcrvUsd.connect(user1).mint(parseUnits("1", 18));
-      });
-
-      describe("borrowBehalf", () => {
-        it("borrowBehalf should revert when approval is not given", async () => {
-          await expect(vWeth.connect(user2).borrowBehalf(USER_1, parseUnits("1", 8))).to.be.revertedWithCustomError(
-            vWeth,
-            "DelegateNotApproved",
-          );
-        });
-
-        it("borrowBehalf should work properly", async () => {
-          await comptroller.connect(user1).updateDelegate(USER_2, true);
-
-          const user2WbnbBalancePrevious = await weth.balanceOf(USER_2);
-          await vWeth.connect(user2).borrowBehalf(USER_1, parseUnits("1", 8));
-          const user2WbnbBalanceNew = await weth.balanceOf(USER_2);
-
-          expect(user2WbnbBalanceNew).to.greaterThan(user2WbnbBalancePrevious);
-        });
-      });
-
-      describe("redeemBehalf", () => {
-        it("redeemBehalf should revert when approval is not given", async () => {
-          await expect(vcrvUsd.connect(user2).redeemBehalf(USER_1, parseUnits("1", 6))).to.be.revertedWithCustomError(
-            vWeth,
-            "DelegateNotApproved",
-          );
-        });
-
-        it("redeemBehalf should work properly", async () => {
-          await comptroller.connect(user1).updateDelegate(USER_2, true);
-
-          const user2BnbxBalancePrevious = await crvUsd.balanceOf(USER_2);
-          await vcrvUsd.connect(user2).redeemBehalf(USER_1, parseUnits("1", 8));
-          const user2BnbxBalanceNew = await crvUsd.balanceOf(USER_2);
-
-          expect(user2BnbxBalanceNew).to.greaterThan(user2BnbxBalancePrevious);
-        });
-      });
-
-      describe("redeemUnderlyingBehalf", () => {
-        it("redeemUnderlyingBehalf should revert when approval is not given", async () => {
-          await expect(
-            vcrvUsd.connect(user2).redeemUnderlyingBehalf(USER_1, parseUnits("1", 6)),
-          ).to.be.revertedWithCustomError(vWeth, "DelegateNotApproved");
-        });
-
-        it("redeemUnderlyingBehalf should work properly", async () => {
-          await comptroller.connect(user1).updateDelegate(USER_2, true);
-
-          const user2BnbxBalancePrevious = await crvUsd.balanceOf(USER_2);
-          await vcrvUsd.connect(user2).redeemUnderlyingBehalf(USER_1, parseUnits("1", 18));
-          const user2BnbxBalanceNew = await crvUsd.balanceOf(USER_2);
-
-          expect(user2BnbxBalanceNew).to.greaterThan(user2BnbxBalancePrevious);
-        });
-      });
     });
 
     describe("generic tests", () => {
