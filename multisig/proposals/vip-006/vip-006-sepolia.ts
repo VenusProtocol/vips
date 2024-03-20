ac39fa771bacfc5eab203bf2a7e8bd9270bfffe5
0xlucian <0xluciandev@gmail.com>
2024-03-20 12:19:07 +0200

    refactor: align VIP and simulation with optimised RD configuration

--- multisig/proposals/vip-006/vip-006-sepolia.ts
+++ multisig/proposals/vip-006/vip-006-sepolia.ts
@@ -2,18 +2,14 @@ import { parseUnits } from "ethers/lib/utils";
 
 import { makeProposal } from "../../../src/utils";
 
-export const REWARD_DISTRIBUTOR_CORE_0 = "0xf32CF5C074279bB30165D1365D8EC2132C6Ae8f3";
-export const REWARD_DISTRIBUTOR_CORE_1 = "0xCE283856d0AD386991A48875294CDBD7B8D47663";
-export const REWARD_DISTRIBUTOR_CORE_2 = "0x1309ac8a414E29d91504edf57851AF546bB38197";
-export const REWARD_DISTRIBUTOR_CORE_3 = "0x247C3344cE23caab71D7E4Af606039bC2A02fBdE";
+export const REWARD_DISTRIBUTOR_CORE_0 = "0xB60666395bEFeE02a28938b75ea620c7191cA77a";
+export const REWARD_DISTRIBUTOR_CORE_1 = "0x341f52BfecC10115087e46eB94AA06E384b8925E";
 export const COMPTROLLER_CORE = "0x7Aa39ab4BcA897F403425C9C6FDbd0f882Be0D70";
-export const REWARD_DISTRIBUTOR_CURVE_0 = "0x46EF252f590F7f9BaAB39553811732e5357101Fa";
-export const REWARD_DISTRIBUTOR_CURVE_1 = "0x636f92eF497c96Fa53b70f076CD399984B29F9C6";
-export const REWARD_DISTRIBUTOR_CURVE_2 = "0xd51a858276f9A3dc0039653ead43D8c47F66a02e";
+export const REWARD_DISTRIBUTOR_CURVE_0 = "0x67dA6435b35d43081c7c27685fAbb2662b7f1290";
+export const REWARD_DISTRIBUTOR_CURVE_1 = "0xF6D57F8e37b1cb627470b5254fAb08dE07B49A0F";
 export const COMPTROLLER_CURVE = "0xD298182D3ACb43e98e32757FF09C91F203e9E67E";
-export const REWARD_DISTRIBUTOR_LST_0 = "0x97D9B4105ed0f1e3E9C20B717891623ADFBa2203";
-export const REWARD_DISTRIBUTOR_LST_1 = "0xbA88ad51b65c3c74d32a28D2dF869b7131f42B2D";
-export const REWARD_DISTRIBUTOR_LST_2 = "0xc93305A9448851E602535ee3533e3908c46af7a3";
+export const REWARD_DISTRIBUTOR_LST_0 = "0x4597B9287fE0DF3c5513D66886706E0719bD270f";
+export const REWARD_DISTRIBUTOR_LST_1 = "0xec594364d2B7eB3678f351Ac632cC71E718E0F89";
 export const COMPTROLLER_LST = "0xd79CeB8EF8188E44b7Eb899094e8A3A4d7A1e236";
 export const VUSDC_CORE = "0xF87bceab8DD37489015B426bA931e08A4D787616";
 export const VUSDT_CORE = "0x19252AFD0B2F539C400aEab7d460CBFbf74c17ff";
@@ -30,9 +26,9 @@ export const WSTETH = "0x9b87Ea90FDb55e1A0f17FBEdDcF7EB0ac4d50493";
 const TREASURY = "0x4116CA92960dF77756aAAc3aFd91361dB657fbF8";
 
 // REWARDS TO TRANSFER FOR EACH REWARD DISTRIBUTOR
-// CORE  - RD0: 45 XVS , RD1: 405 XVS , RD2: 60 XVS,  RD3: 1250 CRV
-// CURVE - RD0: 15 XVS , RD1: 60 XVS  , RD2: 1250 CRV
-// LST   - RD0: 155 XVS, RD1: 550 XVS , RD2: 0.154 WSTETH
+// CORE  - RD0: 510 XVS, RD1: 1250 CRV
+// CURVE - RD0: 75 XVS, RD1: 1250 CRV
+// LST   - RD0: 705 XVS, RD1: 0.154 WSTETH
 
 export const vip006 = () => {
   return makeProposal([
@@ -40,55 +36,35 @@ export const vip006 = () => {
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("45", 18), REWARD_DISTRIBUTOR_CORE_0],
+      params: [XVS, parseUnits("510", 18), REWARD_DISTRIBUTOR_CORE_0],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("405", 18), REWARD_DISTRIBUTOR_CORE_1],
-    },
-    {
-      target: TREASURY,
-      signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("60", 18), REWARD_DISTRIBUTOR_CORE_2],
-    },
-    {
-      target: TREASURY,
-      signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [CRV, parseUnits("1250", 18), REWARD_DISTRIBUTOR_CORE_3],
+      params: [CRV, parseUnits("1250", 18), REWARD_DISTRIBUTOR_CORE_1],
     },
     // CURVE POOL REWARDS
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("15", 18), REWARD_DISTRIBUTOR_CURVE_0],
+      params: [XVS, parseUnits("75", 18), REWARD_DISTRIBUTOR_CURVE_0],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("60", 18), REWARD_DISTRIBUTOR_CURVE_1],
-    },
-    {
-      target: TREASURY,
-      signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [CRV, parseUnits("1250", 18), REWARD_DISTRIBUTOR_CURVE_2],
+      params: [CRV, parseUnits("1250", 18), REWARD_DISTRIBUTOR_CURVE_1],
     },
 
     // LIQUID STAKED ETH POOL REWARDS
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("155", 18), REWARD_DISTRIBUTOR_LST_0],
+      params: [XVS, parseUnits("705", 18), REWARD_DISTRIBUTOR_LST_0],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [XVS, parseUnits("550", 18), REWARD_DISTRIBUTOR_LST_1],
-    },
-    {
-      target: TREASURY,
-      signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [WSTETH, parseUnits("0.154", 18), REWARD_DISTRIBUTOR_LST_2],
+      params: [WSTETH, parseUnits("0.154", 18), REWARD_DISTRIBUTOR_LST_1],
     },
     // REWARDS CONFIGURATION
     { target: REWARD_DISTRIBUTOR_CORE_0, signature: "acceptOwnership()", params: [] },
@@ -100,7 +76,11 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_CORE_0,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VWETH_CORE], ["27777777776000"], ["41666666664000"]],
+      params: [
+        [VWETH_CORE, VWBTC_CORE, VUSDT_CORE, VUSDC_CORE, VCRVUSD_CORE],
+        ["27777777777777", "83333333333333", "83333333333333", "83333333333333", "37037037037036"],
+        ["41666666666666", "125000000000000", "125000000000000", "125000000000000", "55555555555555"],
+      ],
     },
     { target: REWARD_DISTRIBUTOR_CORE_1, signature: "acceptOwnership()", params: [] },
     {
@@ -111,33 +91,7 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_CORE_1,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [
-        [VWBTC_CORE, VUSDT_CORE, VUSDC_CORE],
-        ["83333333320000", "83333333320000", "83333333320000"],
-        ["125000000000000", "125000000000000", "125000000000000"],
-      ],
-    },
-    { target: REWARD_DISTRIBUTOR_CORE_2, signature: "acceptOwnership()", params: [] },
-    {
-      target: COMPTROLLER_CORE,
-      signature: "addRewardsDistributor(address)",
-      params: [REWARD_DISTRIBUTOR_CORE_2],
-    },
-    {
-      target: REWARD_DISTRIBUTOR_CORE_2,
-      signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VCRVUSD_CORE], ["37037037036000"], ["55555555554000"]],
-    },
-    { target: REWARD_DISTRIBUTOR_CORE_3, signature: "acceptOwnership()", params: [] },
-    {
-      target: COMPTROLLER_CORE,
-      signature: "addRewardsDistributor(address)",
-      params: [REWARD_DISTRIBUTOR_CORE_3],
-    },
-    {
-      target: REWARD_DISTRIBUTOR_CORE_3,
-      signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VCRVUSD_CORE], ["771604938400000"], ["1157407408000000"]],
+      params: [[VCRVUSD_CORE], ["771604938271604"], ["1157407407407407"]],
     },
     { target: REWARD_DISTRIBUTOR_CURVE_0, signature: "acceptOwnership()", params: [] },
     {
@@ -148,7 +102,11 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_CURVE_0,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VCRV_CURVE], ["9259259260000"], ["13888888890000"]],
+      params: [
+        [VCRV_CURVE, VCRVUSD_CURVE],
+        ["9259259259259", "37037037037037"],
+        ["13888888888888", "55555555555555"],
+      ],
     },
     { target: REWARD_DISTRIBUTOR_CURVE_1, signature: "acceptOwnership()", params: [] },
     {
@@ -159,18 +117,7 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_CURVE_1,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VCRVUSD_CURVE], ["37037037036000"], ["55555555554000"]],
-    },
-    { target: REWARD_DISTRIBUTOR_CURVE_2, signature: "acceptOwnership()", params: [] },
-    {
-      target: COMPTROLLER_CURVE,
-      signature: "addRewardsDistributor(address)",
-      params: [REWARD_DISTRIBUTOR_CURVE_2],
-    },
-    {
-      target: REWARD_DISTRIBUTOR_CURVE_2,
-      signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VCRVUSD_CURVE], ["77160493840000000"], ["115740740800000000"]],
+      params: [[VCRVUSD_CURVE], ["771604938271604"], ["1157407407407407"]],
     },
     { target: REWARD_DISTRIBUTOR_LST_0, signature: "acceptOwnership()", params: [] },
     {
@@ -181,7 +128,11 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_LST_0,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VWSTETH_LST], ["222222222200000"], ["0"]],
+      params: [
+        [VWSTETH_LST, VWETH_LST],
+        ["222222222222222", "254629629629629"],
+        ["0", "594135802469135"],
+      ],
     },
     { target: REWARD_DISTRIBUTOR_LST_1, signature: "acceptOwnership()", params: [] },
     {
@@ -192,18 +143,7 @@ export const vip006 = () => {
     {
       target: REWARD_DISTRIBUTOR_LST_1,
       signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VWETH_LST], ["254629629600000"], ["594135802500000"]],
-    },
-    { target: REWARD_DISTRIBUTOR_LST_2, signature: "acceptOwnership()", params: [] },
-    {
-      target: COMPTROLLER_LST,
-      signature: "addRewardsDistributor(address)",
-      params: [REWARD_DISTRIBUTOR_LST_2],
-    },
-    {
-      target: REWARD_DISTRIBUTOR_LST_2,
-      signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
-      params: [[VWSTETH_LST], ["712962963000"], ["0"]],
+      params: [[VWSTETH_LST], ["712962962962"], ["0"]],
     },
     // These commands were previously executed. Just having them here for visibility
     // {
--- multisig/simulations/vip-006/vip-006-sepolia/simulations.ts
+++ multisig/simulations/vip-006/vip-006-sepolia/simulations.ts
@@ -14,14 +14,10 @@ import {
   CRV,
   REWARD_DISTRIBUTOR_CORE_0,
   REWARD_DISTRIBUTOR_CORE_1,
-  REWARD_DISTRIBUTOR_CORE_2,
-  REWARD_DISTRIBUTOR_CORE_3,
   REWARD_DISTRIBUTOR_CURVE_0,
   REWARD_DISTRIBUTOR_CURVE_1,
-  REWARD_DISTRIBUTOR_CURVE_2,
   REWARD_DISTRIBUTOR_LST_0,
   REWARD_DISTRIBUTOR_LST_1,
-  REWARD_DISTRIBUTOR_LST_2,
   VCRVUSD_CORE,
   VCRVUSD_CURVE,
   VCRV_CURVE,
@@ -36,116 +32,114 @@ import {
   vip006,
 } from "../../../proposals/vip-006/vip-006-sepolia";
 
-const BLOCKS_IN_90_DAYS = BigNumber.from(648_000); // assuming 12 sec block ==> 7200 blocks per day
-
-forking(5518286, () => {
+forking(5523471, () => {
   describe("Generic checks", async () => {
     before(async () => {
       await pretendExecutingVip(vip006());
     });
 
-    checkRewardsDistributorPool(COMPTROLLER_CORE, 4);
-    checkRewardsDistributorPool(COMPTROLLER_CURVE, 3);
-    checkRewardsDistributorPool(COMPTROLLER_LST, 3);
+    checkRewardsDistributorPool(COMPTROLLER_CORE, 2);
+    checkRewardsDistributorPool(COMPTROLLER_CURVE, 2);
+    checkRewardsDistributorPool(COMPTROLLER_LST, 2);
 
     const wethCoreRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
       address: REWARD_DISTRIBUTOR_CORE_0,
       token: XVS,
       vToken: VWETH_CORE,
-      borrowSpeed: "41666666664000",
-      supplySpeed: "27777777776000",
-      totalRewardsToDistribute: parseUnits("45", 18),
+      borrowSpeed: "41666666666666",
+      supplySpeed: "27777777777777",
+      totalRewardsToDistribute: parseUnits("510", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_0", wethCoreRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_0_WETH", wethCoreRewardsDistributorConfig);
 
     const wbtcRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
-      address: REWARD_DISTRIBUTOR_CORE_1,
+      address: REWARD_DISTRIBUTOR_CORE_0,
       token: XVS,
       vToken: VWBTC_CORE,
       borrowSpeed: "125000000000000",
-      supplySpeed: "83333333320000",
-      totalRewardsToDistribute: parseUnits("405", 18),
+      supplySpeed: "83333333333333",
+      totalRewardsToDistribute: parseUnits("510", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_1_VWBTC", wbtcRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_0_VWBTC", wbtcRewardsDistributorConfig);
 
     const usdtRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
-      address: REWARD_DISTRIBUTOR_CORE_1,
+      address: REWARD_DISTRIBUTOR_CORE_0,
       token: XVS,
       vToken: VUSDT_CORE,
       borrowSpeed: "125000000000000",
-      supplySpeed: "83333333320000",
-      totalRewardsToDistribute: parseUnits("405", 18),
+      supplySpeed: "83333333333333",
+      totalRewardsToDistribute: parseUnits("510", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_1_USDT", usdtRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_0_USDT", usdtRewardsDistributorConfig);
 
     const usdcRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
-      address: REWARD_DISTRIBUTOR_CORE_1,
+      address: REWARD_DISTRIBUTOR_CORE_0,
       token: XVS,
       vToken: VUSDC_CORE,
       borrowSpeed: "125000000000000",
-      supplySpeed: "83333333320000",
-      totalRewardsToDistribute: parseUnits("405", 18),
+      supplySpeed: "83333333333333",
+      totalRewardsToDistribute: parseUnits("510", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_1_USDT", usdcRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_0_USDC", usdcRewardsDistributorConfig);
 
     const vcrvUSDRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
-      address: REWARD_DISTRIBUTOR_CORE_2,
+      address: REWARD_DISTRIBUTOR_CORE_0,
       token: XVS,
       vToken: VCRVUSD_CORE,
-      borrowSpeed: "55555555554000",
-      supplySpeed: "37037037036000",
-      totalRewardsToDistribute: parseUnits("60", 18),
+      borrowSpeed: "55555555555555",
+      supplySpeed: "37037037037036",
+      totalRewardsToDistribute: parseUnits("510", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_2", vcrvUSDRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_0_CRVUSD", vcrvUSDRewardsDistributorConfig);
 
     const vcrvUSDCRVRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CORE,
-      address: REWARD_DISTRIBUTOR_CORE_3,
+      address: REWARD_DISTRIBUTOR_CORE_1,
       token: CRV,
       vToken: VCRVUSD_CORE,
-      borrowSpeed: "1157407408000000",
-      supplySpeed: "771604938400000",
+      borrowSpeed: "1157407407407407",
+      supplySpeed: "771604938271604",
       totalRewardsToDistribute: parseUnits("1250", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Core_3", vcrvUSDCRVRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Core_1", vcrvUSDCRVRewardsDistributorConfig);
 
     const crvRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CURVE,
       address: REWARD_DISTRIBUTOR_CURVE_0,
       token: XVS,
       vToken: VCRV_CURVE,
-      borrowSpeed: "13888888890000",
-      supplySpeed: "9259259260000",
-      totalRewardsToDistribute: parseUnits("15", 18),
+      borrowSpeed: "13888888888888",
+      supplySpeed: "9259259259259",
+      totalRewardsToDistribute: parseUnits("75", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Curve_0", crvRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Curve_0_CRV", crvRewardsDistributorConfig);
 
     const crvCurveRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CURVE,
-      address: REWARD_DISTRIBUTOR_CURVE_1,
+      address: REWARD_DISTRIBUTOR_CURVE_0,
       token: XVS,
       vToken: VCRVUSD_CURVE,
-      borrowSpeed: "55555555554000",
-      supplySpeed: "37037037036000",
-      totalRewardsToDistribute: parseUnits("60", 18),
+      borrowSpeed: "55555555555555",
+      supplySpeed: "37037037037037",
+      totalRewardsToDistribute: parseUnits("75", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Curve_1", crvCurveRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Curve_0_CRVUSD", crvCurveRewardsDistributorConfig);
 
     const crvUSDCurveRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_CURVE,
-      address: REWARD_DISTRIBUTOR_CURVE_2,
+      address: REWARD_DISTRIBUTOR_CURVE_1,
       token: CRV,
       vToken: VCRVUSD_CURVE,
-      borrowSpeed: "115740740800000000",
-      supplySpeed: "77160493840000000",
+      borrowSpeed: "1157407407407407",
+      supplySpeed: "771604938271604",
       totalRewardsToDistribute: parseUnits("1250", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_Curve_2", crvUSDCurveRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_Curve_1_CRVUSD", crvUSDCurveRewardsDistributorConfig);
 
     const wstETHXVSRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_LST,
@@ -153,31 +147,31 @@ forking(5518286, () => {
       token: XVS,
       vToken: VWSTETH_LST,
       borrowSpeed: "0",
-      supplySpeed: "222222222200000",
-      totalRewardsToDistribute: parseUnits("155", 18),
+      supplySpeed: "222222222222222",
+      totalRewardsToDistribute: parseUnits("705", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_LST_0", wstETHXVSRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_LST_0_WSTETH", wstETHXVSRewardsDistributorConfig);
 
     const wethLSTRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_LST,
-      address: REWARD_DISTRIBUTOR_LST_1,
+      address: REWARD_DISTRIBUTOR_LST_0,
       token: XVS,
       vToken: VWETH_LST,
-      borrowSpeed: "594135802500000",
-      supplySpeed: "254629629600000",
-      totalRewardsToDistribute: parseUnits("550", 18),
+      borrowSpeed: "594135802469135",
+      supplySpeed: "254629629629629",
+      totalRewardsToDistribute: parseUnits("705", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_LST_1", wethLSTRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_LST_0_WETH", wethLSTRewardsDistributorConfig);
 
     const wstETHLSTRewardsDistributorConfig: RewardsDistributorConfig = {
       pool: COMPTROLLER_LST,
-      address: REWARD_DISTRIBUTOR_LST_2,
+      address: REWARD_DISTRIBUTOR_LST_1,
       token: WSTETH,
       vToken: VWSTETH_LST,
       borrowSpeed: "0",
-      supplySpeed: "712962963000",
+      supplySpeed: "712962962962",
       totalRewardsToDistribute: parseUnits("0.154", 18),
     };
-    checkRewardsDistributor("RewardsDistributor_LST_2", wstETHLSTRewardsDistributorConfig);
+    checkRewardsDistributor("RewardsDistributor_LST_1_WSTETH", wstETHLSTRewardsDistributorConfig);
   });
 });
