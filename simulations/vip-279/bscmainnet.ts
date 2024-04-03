3c40ad002ccdd5c88ad3753e8a0473aebc40e229
Narayan Prusty <arsenal.narayan@gmail.com>
2024-04-03 16:14:53 +0530

    fix: added simulation

--- simulations/vip-279/bscmainnet.ts
+++ simulations/vip-279/bscmainnet.ts
@@ -1,30 +1,44 @@
 import { TransactionResponse } from "@ethersproject/providers";
 import { expect } from "chai";
 import { Contract } from "ethers";
-import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-import { expectEvents, setMaxStalePeriodInBinanceOracle } from "../../src/utils";
+import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import { vip279 } from "../../vips/vip-279/bscmainnet";
+import { XVS_VAULT_TREASURY, XVS_VAULT_TREASURY_NEW_IMPLEMENTATION, DEFAULT_PROXY_ADMIN, vip279 } from "../../vips/vip-279/bscmainnet";
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 
+import PROXY_ABI from "./abi/XVSVaultTreasuryProxy.json";
+import XVS_VAULT_TREASURY_ABI from "./abi/XVSVaultTreasury.json";
+import XVS_VAULT_ABI from "./abi/XVSVault.json";
 
-forking(37531985, () => {
+const XVS_VAULT_TREASURY_OLD_IMPLEMENTATION = "0xCA59D9e8889Bc6034CCD749c4Ddd09c865432bA8";
+
+forking(37533772, () => {
   const provider = ethers.provider;
+  let xvsVaultTreasuryProxy: Contract;
 
   before(async () => {
+    impersonateAccount(DEFAULT_PROXY_ADMIN);
+    xvsVaultTreasuryProxy = new ethers.Contract(XVS_VAULT_TREASURY, PROXY_ABI, ethers.provider.getSigner(DEFAULT_PROXY_ADMIN));
   });
 
-  describe("Pre-VIP behavior", () => {
-    
+  describe("Pre-VIP behavior", async  () => {
+    it("check implementation", async () => {
+      expect(await xvsVaultTreasuryProxy.callStatic.implementation()).to.be.equal(XVS_VAULT_TREASURY_OLD_IMPLEMENTATION);
+    })
   });
 
   testVip("VIP-279", vip279(), {
     callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(txResponse, [XVS_VAULT_TREASURY_ABI], ["SweepToken", "FundsTransferredToXVSStore"], [1, 1]);
+      await expectEvents(txResponse, [XVS_VAULT_ABI], ["RewardAmountUpdated"], [1]);
     },
   });
 
   describe("Post-VIP behavior", async () => {
-   
+    it("check implementation", async () => {
+      expect(await xvsVaultTreasuryProxy.callStatic.implementation()).to.be.equal(XVS_VAULT_TREASURY_NEW_IMPLEMENTATION);
+    })
   });
 });
--- vips/vip-279/bscmainnet.ts
+++ vips/vip-279/bscmainnet.ts
@@ -6,7 +6,8 @@ export const XVS_VAULT_TREASURY = "0x269ff7818DB317f60E386D2be0B259e1a324a40a";
 export const VTREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
 export const XVS = "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63";
 export const XVS_VAULT = "0x051100480289e704d20e9DB4804837068f3f9204";
-export const XVS_VAULT_TREASURY_NEW_IMPLEMENTATION = "0x3AD53bDAD07302259B72C34b111C37a0486f68C2";
+export const XVS_VAULT_TREASURY_NEW_IMPLEMENTATION = "0xA95a4F34337d8FaC283C3e3D2a605b95DA916cD6";
+export const DEFAULT_PROXY_ADMIN = "0x6beb6D2695B67FEb73ad4f172E8E2975497187e4";
 
 export const XVS_FOR_XVS_STORE = parseUnits("72905.06", 18);
 export const XVS_FOR_V_TREASURY = parseUnits("21789.17", 18);
@@ -35,15 +36,15 @@ export const vip279 = () => {
         params: [XVS, XVS_DISTRIBUTION_SPEED],
       },
       {
-        target: XVS_VAULT_TREASURY,
-        signature: "upgradeTo(address)",
-        params: [XVS_VAULT_TREASURY_NEW_IMPLEMENTATION],
+        target: DEFAULT_PROXY_ADMIN,
+        signature: "upgrade(address,address)",
+        params: [XVS_VAULT_TREASURY, XVS_VAULT_TREASURY_NEW_IMPLEMENTATION],
       },
-      // {
-      //   target: XVS_VAULT_TREASURY,
-      //   signature: "sweepToken(address,address,uint256)",
-      //   params: [XVS, VTREASURY, XVS_FOR_V_TREASURY],
-      // }
+      {
+        target: XVS_VAULT_TREASURY,
+        signature: "sweepToken(address,address,uint256)",
+        params: [XVS, VTREASURY, XVS_FOR_V_TREASURY],
+      }
     ],
     meta,
     ProposalType.REGULAR,
