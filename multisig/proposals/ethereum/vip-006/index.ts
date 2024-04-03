17cc1d36a37e44e9bbeba7098353ba1defd5889d
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-04-03 11:27:57 +0530

    chore: organize directory for vip-006

--- multisig/proposals/ethereum/vip-006/index.ts
+++ multisig/proposals/ethereum/vip-006/index.ts
@@ -1,6 +1,6 @@
 import { parseUnits } from "ethers/lib/utils";
 
-import { makeProposal } from "../../../src/utils";
+import { makeProposal } from "../../../../src/utils";
 
 export const XVS = "0xd3CC9d8f3689B83c91b7B59cAB4946B063EB894A";
 export const ETHEREUM_TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
@@ -159,3 +159,5 @@ export const vip006 = () => {
     },
   ]);
 };
+
+export default vip006;
