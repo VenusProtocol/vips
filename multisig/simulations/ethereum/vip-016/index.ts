f7f1eb5576c701b90dd6815d6c9608f59624f6f9
Narayan Prusty <arsenal.narayan@gmail.com>
2024-04-08 13:50:33 +0530

    fix: fixed simulation

--- multisig/simulations/ethereum/vip-016/index.ts
+++ multisig/simulations/ethereum/vip-016/index.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
@@ -7,7 +8,7 @@ import vip016 from "../../../proposals/ethereum/vip-016";
 import { XVS, XVS_BRIDGE_DEST, XVS_MINT_LIMIT } from "../../../proposals/ethereum/vip-016";
 import XVS_ABI from "./abi/xvs.json";
 
-forking(19609670, () => {
+forking(19609700, () => {
   let xvs: Contract;
 
   before(async () => {
@@ -15,9 +16,9 @@ forking(19609670, () => {
   });
 
   describe("Pre-Execution state", () => {
-    it("Mint limit = 0", async () => {
+    it("Mint limit = 500000", async () => {
       const cap = await xvs.minterToCap(XVS_BRIDGE_DEST);
-      expect(cap).equals(0);
+      expect(cap).equals(parseUnits("500000", 18));
     });
   });
 
