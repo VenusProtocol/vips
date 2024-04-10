41cc530b4dad501229740b81f23d8e4e800e2569
Debugger022 <debugger022@gmail.com>
2024-04-10 16:43:32 +0530

    refactor: remove unnecessary address from networkAddresses

--- multisig/proposals/arbitrumsepolia/vip-003/index.ts
+++ multisig/proposals/arbitrumsepolia/vip-003/index.ts
@@ -5,6 +5,8 @@ import { makeProposal } from "../../../../src/utils";
 
 const { arbitrumsepolia } = NETWORK_ADDRESSES;
 
+const ACM = "0xa36AD96441cB931D8dFEAAaC97D3FaB4B39E590F";
+
 const XVS_BRIDGE_ADMIN_PROXY = "0x0DC3d6717727FD5a023e41bA8134640ca18f3E48";
 const XVS = "0x47fA6E9F717c9eB081c4734FfB5a1EcD70508891";
 const XVS_BRIDGE_DEST = "0xE9B66800E63888DE29c4c9131faadbDbDCfae917";
@@ -12,37 +14,37 @@ const XVS_BRIDGE_DEST = "0xE9B66800E63888DE29c4c9131faadbDbDCfae917";
 const vip003 = () => {
   return makeProposal([
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setSendVersion(uint16)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setReceiveVersion(uint16)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "forceResumeReceive(uint16,bytes)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setOracle(address)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxSingleTransactionLimit(uint16,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxDailyLimit(uint16,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [
         XVS_BRIDGE_ADMIN_PROXY,
@@ -51,107 +53,107 @@ const vip003 = () => {
       ],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxDailyReceiveLimit(uint16,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "pause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "unpause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "removeTrustedRemote(uint16)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "dropFailedMessage(uint16,bytes,uint64)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setPrecrime(address)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setMinDstGas(uint16,uint16,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setPayloadSizeLimit(uint16,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setWhitelist(address,bool)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setConfig(uint16,uint16,uint256,bytes)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "sweepToken(address,address,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "updateSendAndCallEnabled(bool)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "mint(address,uint256)", XVS_BRIDGE_DEST],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "burn(address,uint256)", XVS_BRIDGE_DEST],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "setTrustedRemoteAddress(uint16,bytes)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS_BRIDGE_ADMIN_PROXY, "transferBridgeOwnership(address)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "migrateMinterTokens(address,address)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "setMintCap(address,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "updateBlacklist(address,bool)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "pause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [XVS, "unpause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
