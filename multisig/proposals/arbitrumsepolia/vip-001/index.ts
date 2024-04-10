0a710c85af6815d4394d915886a5c57d22bddf6f
Debugger022 <debugger022@gmail.com>
2024-04-10 16:34:16 +0530

    refactor: update mock token address and remove unnecessary address from networkAddresses file

--- multisig/proposals/arbitrumsepolia/vip-001/index.ts
+++ multisig/proposals/arbitrumsepolia/vip-001/index.ts
@@ -9,47 +9,56 @@ const CHAINLINK_USDC_FEED = "0x0153002d20B96532C639313c2d54c3dA09109309";
 const CHAINLINK_USDT_FEED = "0x80EDee6f667eCc9f63a0a6f55578F870651f06A4";
 const CHAINLINK_ARB_FEED = "0xD1092a65338d049DB68D7Be6bD89d17a0929945e";
 
+const MOCK_WBTC = "0xFb8d93FD3Cf18386a5564bb5619cD1FdB130dF7D";
+const MOCK_WETH = "0x980B62Da83eFf3D4576C647993b0c1D7faf17c73";
+const MOCK_USDT = "0xf3118a17863996B9F2A073c9A66Faaa664355cf8";
+const MOCK_USDC = "0x86f096B1D970990091319835faF3Ee011708eAe8";
+const MOCK_ARB = "0x4371bb358aB5cC192E481543417D2F67b8781731";
+
+const ACM = "0xa36AD96441cB931D8dFEAAaC97D3FaB4B39E590F";
+export const BOUND_VALIDATOR = "0xfe6bc1545Cc14C131bacA97476D6035ffcC0b889";
+
 const vip001 = () => {
   return makeProposal([
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.RESILIENT_ORACLE, "pause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.RESILIENT_ORACLE, "unpause()", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.RESILIENT_ORACLE, "setOracle(address,address,uint8)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.RESILIENT_ORACLE, "enableOracle(address,uint8,bool)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.RESILIENT_ORACLE, "setTokenConfig(TokenConfig)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.CHAINLINK_ORACLE, "setTokenConfig(TokenConfig)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
       params: [arbitrumsepolia.CHAINLINK_ORACLE, "setDirectPrice(address,uint256)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
-      target: arbitrumsepolia.ACM,
+      target: ACM,
       signature: "giveCallPermission(address,string,address)",
-      params: [arbitrumsepolia.BOUND_VALIDATOR, "setValidateConfig(ValidateConfig)", arbitrumsepolia.NORMAL_TIMELOCK],
+      params: [BOUND_VALIDATOR, "setValidateConfig(ValidateConfig)", arbitrumsepolia.NORMAL_TIMELOCK],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
@@ -62,21 +71,21 @@ const vip001 = () => {
       params: [],
     },
     {
-      target: arbitrumsepolia.BOUND_VALIDATOR,
+      target: BOUND_VALIDATOR,
       signature: "acceptOwnership()",
       params: [],
     },
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
-      params: [[arbitrumsepolia.MOCK_WBTC, CHAINLINK_BTC_FEED, 144000]],
+      params: [[MOCK_WBTC, CHAINLINK_BTC_FEED, 144000]],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
       signature: "setTokenConfig((address,address[3],bool[3]))",
       params: [
         [
-          arbitrumsepolia.MOCK_WBTC,
+          MOCK_WBTC,
           [
             arbitrumsepolia.CHAINLINK_ORACLE,
             "0x0000000000000000000000000000000000000000",
@@ -89,14 +98,14 @@ const vip001 = () => {
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
-      params: [[arbitrumsepolia.MOCK_WETH, CHAINLINK_ETH_FEED, 144000]],
+      params: [[MOCK_WETH, CHAINLINK_ETH_FEED, 144000]],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
       signature: "setTokenConfig((address,address[3],bool[3]))",
       params: [
         [
-          arbitrumsepolia.MOCK_WETH,
+          MOCK_WETH,
           [
             arbitrumsepolia.CHAINLINK_ORACLE,
             "0x0000000000000000000000000000000000000000",
@@ -109,14 +118,14 @@ const vip001 = () => {
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
-      params: [[arbitrumsepolia.MOCK_USDC, CHAINLINK_USDC_FEED, 144000]],
+      params: [[MOCK_USDC, CHAINLINK_USDC_FEED, 144000]],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
       signature: "setTokenConfig((address,address[3],bool[3]))",
       params: [
         [
-          arbitrumsepolia.MOCK_USDC,
+          MOCK_USDC,
           [
             arbitrumsepolia.CHAINLINK_ORACLE,
             "0x0000000000000000000000000000000000000000",
@@ -129,14 +138,14 @@ const vip001 = () => {
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
-      params: [[arbitrumsepolia.MOCK_USDT, CHAINLINK_USDT_FEED, 144000]],
+      params: [[MOCK_USDT, CHAINLINK_USDT_FEED, 144000]],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
       signature: "setTokenConfig((address,address[3],bool[3]))",
       params: [
         [
-          arbitrumsepolia.MOCK_USDT,
+          MOCK_USDT,
           [
             arbitrumsepolia.CHAINLINK_ORACLE,
             "0x0000000000000000000000000000000000000000",
@@ -149,14 +158,14 @@ const vip001 = () => {
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
       signature: "setTokenConfig((address,address,uint256))",
-      params: [[arbitrumsepolia.MOCK_ARB, CHAINLINK_ARB_FEED, 144000]],
+      params: [[MOCK_ARB, CHAINLINK_ARB_FEED, 144000]],
     },
     {
       target: arbitrumsepolia.RESILIENT_ORACLE,
       signature: "setTokenConfig((address,address[3],bool[3]))",
       params: [
         [
-          arbitrumsepolia.MOCK_ARB,
+          MOCK_ARB,
           [
             arbitrumsepolia.CHAINLINK_ORACLE,
             "0x0000000000000000000000000000000000000000",
--- multisig/simulations/arbitrumsepolia/vip-001/index.ts
+++ multisig/simulations/arbitrumsepolia/vip-001/index.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 
 import { NETWORK_ADDRESSES } from "../../../../src/networkAddresses";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
-import vip001 from "../../../proposals/arbitrumsepolia/vip-001";
+import vip001, { BOUND_VALIDATOR } from "../../../proposals/arbitrumsepolia/vip-001";
 import BOUND_VALIDATOR_ABI from "./abi/boundValidator.json";
 import CHAINLINK_ORACLE_ABI from "./abi/chainlinkOracle.json";
 import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
@@ -14,7 +14,6 @@ const { arbitrumsepolia } = NETWORK_ADDRESSES;
 const RESILIENT_ORACLE = arbitrumsepolia.RESILIENT_ORACLE;
 const CHAINLINK_ORACLE = arbitrumsepolia.CHAINLINK_ORACLE;
 const NORMAL_TIMELOCK = arbitrumsepolia.NORMAL_TIMELOCK;
-const BOUND_VALIDATOR = arbitrumsepolia.BOUND_VALIDATOR;
 
 interface AssetConfig {
   name: string;
@@ -27,42 +26,42 @@ interface AssetConfig {
 const assetConfigs: AssetConfig[] = [
   {
     name: "USDC",
-    address: "0xDb3EC247767d586a9B77F7472F3C7759b131bE69",
-    price: "1000039460000000000",
+    address: "0x86f096B1D970990091319835faF3Ee011708eAe8",
+    price: "1000100000000000000000000000000",
     feed: "0x0153002d20B96532C639313c2d54c3dA09109309",
     oracle: "chainlink",
   },
   {
     name: "WBTC",
-    address: "0xD47e7683a11d70E479bb948dc8E795778035C62D",
-    price: "66215606000000000000000",
+    address: "0xFb8d93FD3Cf18386a5564bb5619cD1FdB130dF7D",
+    price: "688190100000000000000000000000000",
     feed: "0x56a43EB56Da12C0dc1D972ACb089c06a5dEF8e69",
     oracle: "chainlink",
   },
   {
     name: "USDT",
-    address: "0xE1F20e585B57F82d57Da92ac1639B634f56D77CF",
-    price: "1000440000000000000",
+    address: "0xf3118a17863996B9F2A073c9A66Faaa664355cf8",
+    price: "999990000000000000000000000000",
     feed: "0x80EDee6f667eCc9f63a0a6f55578F870651f06A4",
     oracle: "chainlink",
   },
   {
     name: "ARB",
     address: "0x4371bb358aB5cC192E481543417D2F67b8781731",
-    price: "1478623200000000000",
+    price: "1468062750000000000",
     feed: "0xD1092a65338d049DB68D7Be6bD89d17a0929945e",
     oracle: "chainlink",
   },
   {
     name: "WETH",
     address: "0x980B62Da83eFf3D4576C647993b0c1D7faf17c73",
-    price: "3304928200000000000000",
+    price: "3511660000000000000000",
     feed: "0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165",
     oracle: "chainlink",
   },
 ];
 
-forking(29789348, () => {
+forking(32230384, () => {
   const provider = ethers.provider;
   let resilientOracle: Contract;
   let chainLinkOracle: Contract;
