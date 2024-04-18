25645bcc0e1a56ad7cd72400142d3285c6393fd2
Debugger022 <debugger022@gmail.com>
2024-04-18 15:53:59 +0530

    feat: add vip to enable bridge for bsctestnet to arbitrumsepolia

---
+++ vips/vip-291/bsctestnet.ts
@@ -0,0 +1,64 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+
+const XVS_BRIDGE_ADMIN = "0xB164Cb262328Ca44a806bA9e3d4094931E658513";
+
+export const MIN_DST_GAS = "300000";
+export const SINGLE_SEND_LIMIT = parseUnits("10000", 18);
+export const MAX_DAILY_SEND_LIMIT = parseUnits("50000", 18);
+export const SINGLE_RECEIVE_LIMIT = parseUnits("10000", 18);
+export const MAX_DAILY_RECEIVE_LIMIT = parseUnits("50000", 18);
+export const ARBITRUM_SEPOLIA_CHAIN_ID = 10231;
+const ARBITRUM_SEPOLIA_TRUSTED_REMOTE = "0xE9B66800E63888DE29c4c9131faadbDbDCfae917";
+
+const vip291 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-291 XVS bridge Bnb testnet -> Arbitrum Sepolia",
+    description: ``,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setTrustedRemoteAddress(uint16,bytes)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, ARBITRUM_SEPOLIA_TRUSTED_REMOTE],
+      },
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setMinDstGas(uint16,uint16,uint256)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, 0, MIN_DST_GAS],
+      },
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setMaxDailyLimit(uint16,uint256)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, MAX_DAILY_SEND_LIMIT],
+      },
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setMaxSingleTransactionLimit(uint16,uint256)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, SINGLE_SEND_LIMIT],
+      },
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setMaxDailyReceiveLimit(uint16,uint256)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, MAX_DAILY_RECEIVE_LIMIT],
+      },
+      {
+        target: XVS_BRIDGE_ADMIN,
+        signature: "setMaxSingleReceiveTransactionLimit(uint16,uint256)",
+        params: [ARBITRUM_SEPOLIA_CHAIN_ID, SINGLE_RECEIVE_LIMIT],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip291;
