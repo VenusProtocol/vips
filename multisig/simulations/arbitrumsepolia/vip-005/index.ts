3eb40ecc37a99342a0e559c4dafdf6406c0345fc
Debugger022 <debugger022@gmail.com>
2024-04-22 17:46:58 +0530

    refactor: update addresses

--- multisig/simulations/arbitrumsepolia/vip-005/index.ts
+++ multisig/simulations/arbitrumsepolia/vip-005/index.ts
@@ -10,12 +10,12 @@ import ACM_ABI from "./abi/accessControlManager.json";
 import PSR_ABI from "./abi/protocolShareReserve.json";
 
 const ACM = "0xa36AD96441cB931D8dFEAAaC97D3FaB4B39E590F";
-const PSR = "0x26C3dc654091D940CB5015591F40DAE85Eb47D4B";
+const PSR = "0x09267d30798B59c581ce54E861A084C6FC298666";
 const VTREASURY = "0x4e7ab1fD841E1387Df4c91813Ae03819C33D5bdB";
 const NORMAL_TIMELOCK = "0x1426A5Ae009c4443188DA8793751024E358A61C2";
-const POOL_REGISTRY = "0xEcFa4d4FcBB76A19EB90F947CCCd5c29375bD3Bd";
+const POOL_REGISTRY = "0x6866b2BDaaEf6648ddd5b678B3e9f3352bF3d2A5";
 
-forking(29622010, () => {
+forking(36331055, () => {
   let protocolShareReserve: Contract;
   let accessControlManager: Contract;
   let psrSigner: SignerWithAddress;
