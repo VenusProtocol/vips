50be8627353e461631fdb6321ae78b485d0889a5
Narayan Prusty <arsenal.narayan@gmail.com>
2024-04-23 20:13:51 +0530

    fix: added sepolia vip

--- multisig/proposals/ethereum/vip-021/index.ts
+++ multisig/proposals/ethereum/vip-021/index.ts
@@ -5,7 +5,7 @@ export const vweETH = "0xb4933AF59868986316Ed37fa865C829Eba2df0C7";
 
 // rewardStartBlock + (secondsIn30Days / secondsPerBlock)
 // rewardStartBlock can be found here https://etherscan.io/tx/0xab608fa2d5f8be982e0ed164cef2203ea6f7b6817dacee3b6644bdc70c0ec405
-export const LAST_REWARD_BLOCK = 19691005 + ((30*24*60*60) / 12);
+export const LAST_REWARD_BLOCK = 19691005 + (30 * 24 * 60 * 60) / 12;
 
 export const vip021 = () => {
   return makeProposal([
