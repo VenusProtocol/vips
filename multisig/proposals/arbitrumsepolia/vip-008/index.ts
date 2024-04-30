7b469583647053c7be0cb9e0fd913fde54367b5d
Debugger022 <debugger022@gmail.com>
2024-04-30 12:47:37 +0530

    feat: add vip to enable rewardsDistributor

---
+++ multisig/proposals/arbitrumsepolia/vip-008/index.ts
@@ -0,0 +1,65 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { NETWORK_ADDRESSES } from "../../../../src/networkAddresses";
+import { makeProposal } from "../../../../src/utils";
+
+const { arbitrumsepolia } = NETWORK_ADDRESSES;
+
+export const REWARD_DISTRIBUTOR_CORE_0 = "0x8E73FE3F7E29100Ad9d1C7F35fba2D2c823c8579";
+export const COMPTROLLER_CORE = "0xA198909e8432f262f6978F2C81B04b32c55eb063";
+export const VTREASURY = "0x4e7ab1fD841E1387Df4c91813Ae03819C33D5bdB";
+export const USDT = "0xf3118a17863996B9F2A073c9A66Faaa664355cf8";
+export const VUSDT_CORE = "0xFf3a9fD8d4f208Ed98f56A5F771AD470258866D7";
+export const ACM = "0xa36AD96441cB931D8dFEAAaC97D3FaB4B39E590F";
+
+const vip008 = () => {
+  return makeProposal([
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        ethers.constants.AddressZero,
+        "setLastRewardingBlocks(address[],uint32[],uint32[])",
+        arbitrumsepolia.NORMAL_TIMELOCK,
+      ],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        ethers.constants.AddressZero,
+        "setRewardTokenSpeeds(address[],uint256[],uint256[])",
+        arbitrumsepolia.NORMAL_TIMELOCK,
+      ],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        ethers.constants.AddressZero,
+        "setLastRewardingBlockTimestamps(address[],uint256[],uint256[])",
+        arbitrumsepolia.NORMAL_TIMELOCK,
+      ],
+    },
+    // CORE POOL REWARDS
+    {
+      target: VTREASURY,
+      signature: "withdrawTreasuryToken(address,uint256,address)",
+      params: [USDT, parseUnits("500", 6), REWARD_DISTRIBUTOR_CORE_0],
+    },
+    { target: REWARD_DISTRIBUTOR_CORE_0, signature: "acceptOwnership()", params: [] },
+    {
+      target: COMPTROLLER_CORE,
+      signature: "addRewardsDistributor(address)",
+      params: [REWARD_DISTRIBUTOR_CORE_0],
+    },
+    {
+      target: REWARD_DISTRIBUTOR_CORE_0,
+      signature: "setRewardTokenSpeeds(address[],uint256[],uint256[])",
+      params: [[VUSDT_CORE], ["2893"], ["2893"]],
+    },
+  ]);
+};
+
+export default vip008;
---
+++ multisig/simulations/arbitrumsepolia/vip-008/index.ts
@@ -0,0 +1,35 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import {
+  RewardsDistributorConfig,
+  checkRewardsDistributor,
+  checkRewardsDistributorPool,
+} from "../../../../src/vip-framework/checks/rewardsDistributor";
+import { forking, pretendExecutingVip } from "../../../../src/vip-framework/index";
+import vip008, {
+  COMPTROLLER_CORE,
+  REWARD_DISTRIBUTOR_CORE_0,
+  USDT,
+  VUSDT_CORE,
+} from "../../../proposals/arbitrumsepolia/vip-008";
+
+forking(39020341, () => {
+  describe("Generic checks", async () => {
+    before(async () => {
+      await pretendExecutingVip(vip008());
+    });
+
+    checkRewardsDistributorPool(COMPTROLLER_CORE, 1);
+
+    const usdtRewardsDistributorConfig: RewardsDistributorConfig = {
+      pool: COMPTROLLER_CORE,
+      address: REWARD_DISTRIBUTOR_CORE_0,
+      token: USDT,
+      vToken: VUSDT_CORE,
+      borrowSpeed: "2893",
+      supplySpeed: "2893",
+      totalRewardsToDistribute: parseUnits("500", 6),
+    };
+    checkRewardsDistributor("RewardsDistributor_Core_0_USDT", usdtRewardsDistributorConfig);
+  });
+});
