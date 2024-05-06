9689954ed7b56ad3820b98597863c102fd82ffe5
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-06 17:24:23 +0530

    fix: added generic tests

--- multisig/simulations/sepolia/vip-025/index.ts
+++ multisig/simulations/sepolia/vip-025/index.ts
@@ -1,9 +1,17 @@
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 import { NETWORK_ADDRESSES } from "../../../../src/networkAddresses";
+import { checkIsolatedPoolsComptrollers } from "../../../../src/vip-framework/checks/checkIsolatedPoolsComptrollers";
+import { checkVToken } from "../../../../src/vip-framework/checks/checkVToken";
+import { checkInterestRate } from "../../../../src/vip-framework/checks/interestRateModel";
+import {
+  checkRewardsDistributor,
+  checkRewardsDistributorPool,
+} from "../../../../src/vip-framework/checks/rewardsDistributor";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework/index";
 import {
   COMPTROLLER,
@@ -25,6 +33,61 @@ import ERC20_ABI from "./abi/erc20.json";
 import VTOKEN_ABI from "./abi/vToken.json";
 
 const { sepolia } = NETWORK_ADDRESSES;
+const BLOCKS_PER_YEAR = BigNumber.from(2628000); // assuming a block is mined every 12 seconds
+
+interface RiskParameters {
+  borrowCap: string;
+  supplyCap: string;
+  collateralFactor: string;
+  liquidationThreshold: string;
+  reserveFactor: string;
+  initialSupply: string;
+  vTokenReceiver: string;
+}
+
+const vFrax_riskParameters: RiskParameters = {
+  borrowCap: "8000000",
+  supplyCap: "10000000",
+  collateralFactor: "0.75",
+  liquidationThreshold: "0.80",
+  reserveFactor: "0.1",
+  initialSupply: "5000",
+  vTokenReceiver: sepolia.VTREASURY,
+};
+
+const vsFrax_riskParameters: RiskParameters = {
+  borrowCap: "1000000",
+  supplyCap: "10000000",
+  collateralFactor: "0.75",
+  liquidationThreshold: "0.80",
+  reserveFactor: "0.1",
+  initialSupply: "4800",
+  vTokenReceiver: sepolia.VTREASURY,
+};
+
+interface InterestRateModelSpec {
+  vToken: string;
+  kink: string;
+  base: string;
+  multiplier: string;
+  jump: string;
+}
+
+const vFrax_interestRateModel: InterestRateModelSpec = {
+  vToken: "vFRAX_Core",
+  kink: "0.8",
+  base: "0",
+  multiplier: "0.15",
+  jump: "2.5",
+};
+
+const vsFrax_interestRateModel: InterestRateModelSpec = {
+  vToken: "vsFRAX_Core",
+  kink: "0.8",
+  base: "0",
+  multiplier: "0.15",
+  jump: "2.5",
+};
 
 forking(5827248, () => {
   let resilientOracle: Contract;
@@ -34,8 +97,11 @@ forking(5827248, () => {
   let comptroller: Contract;
   let rewardDistributor: Contract;
   let xvs: Contract;
+  let frax: Contract;
 
   before(async () => {
+    await impersonateAccount(sepolia.NORMAL_TIMELOCK);
+
     resilientOracle = await ethers.getContractAt(RESILIENT_ORACLE_ABI, sepolia.RESILIENT_ORACLE);
     poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, sepolia.POOL_REGISTRY);
     vFRAXContract = await ethers.getContractAt(VTOKEN_ABI, vFRAX);
@@ -43,6 +109,7 @@ forking(5827248, () => {
     comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER);
     rewardDistributor = await ethers.getContractAt(REWARD_DISTRIBUTOR_ABI, REWARDS_DISTRIBUTOR_XVS);
     xvs = await ethers.getContractAt(ERC20_ABI, XVS);
+    frax = await ethers.getContractAt(ERC20_ABI, FRAX, await ethers.getSigner(sepolia.NORMAL_TIMELOCK));
   });
 
   describe("Pre-VIP behavior", () => {
@@ -107,6 +174,125 @@ forking(5827248, () => {
 
     it(`rewards distributor should have balance`, async () => {
       expect(await xvs.balanceOf(rewardDistributor.address)).to.gte(XVS_REWARD_TRANSFER);
+    });
+
+    describe(`check risk parameters`, () => {
+      it(`check reserve factor`, async () => {
+        expect(await vFRAXContract.reserveFactorMantissa()).to.equal(
+          parseUnits(vFrax_riskParameters.reserveFactor, 18),
+        );
+        expect(await vsFRAXContract.reserveFactorMantissa()).to.equal(
+          parseUnits(vsFrax_riskParameters.reserveFactor, 18),
+        );
+      });
+
+      it(`check CF`, async () => {
+        let market = await comptroller.markets(vFRAX);
+        expect(market.collateralFactorMantissa).to.equal(parseUnits(vFrax_riskParameters.collateralFactor, 18));
+
+        market = await comptroller.markets(vsFRAX);
+        expect(market.collateralFactorMantissa).to.equal(parseUnits(vsFrax_riskParameters.collateralFactor, 18));
+      });
+
+      it(`check liquidation threshold`, async () => {
+        let market = await comptroller.markets(vFRAX);
+        expect(market.liquidationThresholdMantissa).to.equal(parseUnits(vFrax_riskParameters.liquidationThreshold, 18));
+
+        market = await comptroller.markets(vsFRAX);
+        expect(market.liquidationThresholdMantissa).to.equal(
+          parseUnits(vsFrax_riskParameters.liquidationThreshold, 18),
+        );
+      });
+
+      it(`check protocol seize share`, async () => {
+        expect(await vFRAXContract.protocolSeizeShareMantissa()).to.equal(parseUnits("0.05", 18));
+        expect(await vsFRAXContract.protocolSeizeShareMantissa()).to.equal(parseUnits("0.05", 18));
+      });
+
+      it(`check supply cap`, async () => {
+        expect(await comptroller.supplyCaps(vFRAX)).to.equal(parseUnits(vFrax_riskParameters.supplyCap, 18));
+        expect(await comptroller.supplyCaps(vsFRAX)).to.equal(parseUnits(vsFrax_riskParameters.supplyCap, 18));
+      });
+
+      it(`check borrow cap`, async () => {
+        expect(await comptroller.borrowCaps(vFRAX)).to.equal(parseUnits(vFrax_riskParameters.borrowCap, 18));
+        expect(await comptroller.borrowCaps(vsFRAX)).to.equal(parseUnits(vsFrax_riskParameters.borrowCap, 18));
+      });
+
+      it("Interest rates", async () => {
+        checkInterestRate(
+          await vFRAXContract.interestRateModel(),
+          vFrax_interestRateModel.vToken,
+          {
+            base: vFrax_interestRateModel.base,
+            multiplier: vFrax_interestRateModel.multiplier,
+            jump: vFrax_interestRateModel.jump,
+            kink: vFrax_interestRateModel.kink,
+          },
+          BLOCKS_PER_YEAR,
+        );
+
+        checkInterestRate(
+          await vsFRAXContract.interestRateModel(),
+          vsFrax_interestRateModel.vToken,
+          {
+            base: vsFrax_interestRateModel.base,
+            multiplier: vsFrax_interestRateModel.multiplier,
+            jump: vsFrax_interestRateModel.jump,
+            kink: vsFrax_interestRateModel.kink,
+          },
+          BLOCKS_PER_YEAR,
+        );
+      });
+    });
+
+    it("generic IL tests", async () => {
+      await frax.faucet(parseUnits("10000", 18));
+      await checkIsolatedPoolsComptrollers({
+        [COMPTROLLER]: sepolia.NORMAL_TIMELOCK,
+      });
+
+      await checkVToken(vFRAX, {
+        name: "Venus Frax (Core)",
+        symbol: "vFRAX_Core",
+        decimals: 8,
+        underlying: FRAX,
+        exchangeRate: parseUnits("10000000000", 18),
+        comptroller: COMPTROLLER,
+      });
+
+      await checkVToken(vsFRAX, {
+        name: "Venus Staked FRAX (Core)",
+        symbol: "vsFRAX_Core",
+        decimals: 8,
+        underlying: sFRAX,
+        exchangeRate: parseUnits("10000000000", 18),
+        comptroller: COMPTROLLER,
+      });
+    });
+
+    it("generic reward tests", async () => {
+      await checkRewardsDistributor("RewardsDistributor_Core_0_FRAX", {
+        pool: COMPTROLLER,
+        address: REWARDS_DISTRIBUTOR_XVS,
+        token: XVS,
+        vToken: vFRAX,
+        borrowSpeed: "2222222222222222",
+        supplySpeed: "1481481481481481",
+        totalRewardsToDistribute: parseUnits("2400", 18),
+      });
+
+      await checkRewardsDistributor("RewardsDistributor_Core_0_SFRAX", {
+        pool: COMPTROLLER,
+        address: REWARDS_DISTRIBUTOR_XVS,
+        token: XVS,
+        vToken: vsFRAX,
+        borrowSpeed: "1481481481481481",
+        supplySpeed: "2222222222222222",
+        totalRewardsToDistribute: parseUnits("2400", 18),
+      });
+
+      await checkRewardsDistributorPool(COMPTROLLER, 2);
     });
   });
 });
