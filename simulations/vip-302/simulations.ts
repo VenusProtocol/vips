3345ca951e86fafc3929a0748703dfff165aa682
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-05-08 12:49:29 +0200

    feat: add description for VIP-302

--- simulations/vip-302/simulations.ts
+++ simulations/vip-302/simulations.ts
@@ -1,6 +1,6 @@
 import { forking, testVip } from "../../src/vip-framework";
-import vip300 from "../../vips/vip-300/bscmainnet";
+import vip302 from "../../vips/vip-302/bscmainnet";
 
 forking(38491593, () => {
-  testVip("VIP-300", vip300(), {});
+  testVip("VIP-302", vip302(), {});
 });
