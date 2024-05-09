4803e8fbfc6d667849dab399f4b02b0c337a6fad
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-09 18:20:44 +0530

    fix: remove export

--- multisig/proposals/ethereum/vip-027/index.ts
+++ multisig/proposals/ethereum/vip-027/index.ts
@@ -6,7 +6,7 @@ export const LST_POOL_COMPTROLLER = "0xF522cd0360EF8c2FF48B648d53EA1717Ec0F3Ac3"
 export const LST_POOL_VWEETH = "0xb4933AF59868986316Ed37fa865C829Eba2df0C7";
 export const VWEETH_BORROW_CAP = parseUnits("1500", 18);
 
-export const vip027 = () => {
+const vip027 = () => {
   return makeProposal([
     {
       target: LST_POOL_COMPTROLLER,
