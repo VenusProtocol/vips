29532c2a1237651dd54d41d5585bf727bf69cbba
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-16 15:54:13 +0530

    fix: set last reward block for frax and sfrax on mainnet

---
+++ multisig/simulations/ethereum/vip-028/index.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
+import { LAST_REWARD_BLOCK, REWARDS_DISTRIBUTOR_XVS, vFRAX, vip028, vsFRAX } from "../../../proposals/ethereum/vip-028";
+import REWARD_DISTRIBUTOR_ABI from "./abi/RewardsDistributor.json";
+
+forking(19881896, () => {
+  let rewardDistributor: Contract;
+
+  before(async () => {
+    rewardDistributor = await ethers.getContractAt(REWARD_DISTRIBUTOR_ABI, REWARDS_DISTRIBUTOR_XVS);
+  });
+
+  describe("Post-VIP behavior", async () => {
+    before(async () => {
+      await pretendExecutingVip(vip028());
+    });
+
+    it("check vFrax last reward block", async () => {
+      const supplyState = await rewardDistributor.rewardTokenSupplyState(vFRAX);
+      expect(supplyState.lastRewardingBlock).to.equal(LAST_REWARD_BLOCK);
+
+      const borrowState = await rewardDistributor.rewardTokenBorrowState(vFRAX);
+      expect(borrowState.lastRewardingBlock).to.equal(LAST_REWARD_BLOCK);
+    });
+
+    it("check vsFrax last reward block", async () => {
+      const supplyState = await rewardDistributor.rewardTokenSupplyState(vsFRAX);
+      expect(supplyState.lastRewardingBlock).to.equal(LAST_REWARD_BLOCK);
+
+      const borrowState = await rewardDistributor.rewardTokenBorrowState(vsFRAX);
+      expect(borrowState.lastRewardingBlock).to.equal(LAST_REWARD_BLOCK);
+    });
+  });
+});
