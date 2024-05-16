8b39b07cd25ab829001762830ab8217c061448d6
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-16 16:52:24 +0530

    fix: vip to set borrow cap to 0

---
+++ multisig/proposals/sepolia/vip-023/addendum.ts
@@ -0,0 +1,17 @@
+import { makeProposal } from "../../../../src/utils";
+
+export const vPTweETH = "0x3AF2bE7AbEF0f840b196D99d79F4B803a5dB14a1";
+export const COMPTROLLER = "0xd79CeB8EF8188E44b7Eb899094e8A3A4d7A1e236";
+
+export const vip023Addendum = () => {
+  return makeProposal([
+    // Configure Oracle
+    {
+      target: COMPTROLLER,
+      signature: "setMarketBorrowCaps(address[],uint256[])",
+      params: [[[vPTweETH], [0]]],
+    },
+  ]);
+};
+
+export default vip023Addendum;
