539fb3daa1e3aa09b3daad787fd559d23e8bf99e
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-05-27 15:49:40 +0200

    feat: set ID and description for vip 312

--- simulations/vip-308/bscmainnet.ts
+++
@@ -1,6 +0,0 @@
-import { forking, testVip } from "../../src/vip-framework";
-import vip308 from "../../vips/vip-308/bscmainnet";
-
-forking(38972300, () => {
-  testVip("VIP-308", vip308(), {});
-});
