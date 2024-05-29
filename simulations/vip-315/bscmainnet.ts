7daceda11845bbcb905c957088ffb40dbfbffacb
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-29 17:41:58 +0400

    fix: updare ir

--- simulations/vip-315/bscmainnet.ts
+++ simulations/vip-315/bscmainnet.ts
@@ -5,20 +5,34 @@ import { ethers } from "hardhat";
 
 import { calculateMappingStorageSlot, expectEvents, initMainnetUser } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import vip315 from "../../vips/vip-315/bscmainnet";
+import vip315, {vWBNB, vWBNB_IR} from "../../vips/vip-315/bscmainnet";
+import VTOKEN_ABI from "./abi/vToken.json";
 
-forking(39146407, async () => {
+forking(39147163, async () => {
   const provider = ethers.provider;
+  let vWBNBContract: Contract;
 
   before(async () => {
-    
+    vWBNBContract = new ethers.Contract(vWBNB, VTOKEN_ABI, provider);
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("check IR address", async () => {
+      const ir = await vWBNBContract.interestRateModel();
+      expect(ir).to.be.equal("0x6765202c3e6d3FdD05F0b26105d0C8DF59D3efaf");
+    })
   });
 
   testVip("VIP-315", vip315(), {
     callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [VTOKEN_ABI], ["NewMarketInterestRateModel"], [1]);
     },
   });
 
   describe("Post-VIP behavior", async () => {
+    it("check IR address", async () => {
+      const ir = await vWBNBContract.interestRateModel();
+      expect(ir).to.be.equal(vWBNB_IR);
+    })
   });
 });
--- vips/vip-315/bscmainnet.ts
+++ vips/vip-315/bscmainnet.ts
@@ -2,11 +2,8 @@ import { parseUnits } from "ethers/lib/utils";
 import { ProposalType } from "../../src/types";
 import { makeProposal } from "../../src/utils";
 
-export const vWBNB_IR = "0x6765202c3e6d3FdD05F0b26105d0C8DF59D3efaf";
-export const vWBNB_BASE_RATE_PER_YEAR = 0;
-export const vWBNB_MULTIPLIER_PER_YEAR = parseUnits("0.09", 18);
-export const vWBNB_JUMP_MULTIPLIER_PER_YEAR = parseUnits("3", 18);
-export const vWBNB_KINK = parseUnits("0.9", 18);
+export const vWBNB_IR = "0xdE41feaB3B17C05Ba596b11E2C8d9f3514B71d22";
+export const vWBNB = "0xe10E80B7FD3a29fE46E16C30CC8F4dd938B742e2";
 
 const vip315 = () => {
   const meta = {
@@ -21,9 +18,9 @@ const vip315 = () => {
   return makeProposal(
     [
       {
-        target: vWBNB_IR,
-        signature: "updateJumpRateModel(uint256,uint256,uint256,uint256)",
-        params: [vWBNB_BASE_RATE_PER_YEAR, vWBNB_MULTIPLIER_PER_YEAR, vWBNB_JUMP_MULTIPLIER_PER_YEAR, vWBNB_KINK],
+        target: vWBNB,
+        signature: "setInterestRateModel(address)",
+        params: [vWBNB_IR],
       },
     ],
     meta,
