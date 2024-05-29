b467f5712073335daa31eba50bffa37b577bd91d
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-29 18:32:15 +0400

    fix: add simulations

--- simulations/vip-315/bscmainnet.ts
+++ simulations/vip-315/bscmainnet.ts
@@ -1,31 +1,77 @@
 import { expect } from "chai";
-import { BigNumber, Contract, utils } from "ethers";
-import { parseEther, parseUnits } from "ethers/lib/utils";
+import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-import { calculateMappingStorageSlot, expectEvents, initMainnetUser } from "../../src/utils";
+import { expectEvents, setMaxStaleCoreAssets } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
-import vip315, {vWBNB, vWBNB_IR, vWBNB_RF} from "../../vips/vip-315/bscmainnet";
+import vip315, {
+  CF,
+  COMPTROLLER,
+  LT,
+  vBNBx,
+  vWBNB,
+  vWBNB_IR,
+  vWBNB_RF,
+  vankrBNB,
+  vslisBNB,
+  vstkBNB,
+} from "../../vips/vip-315/bscmainnet";
+import COMPTROLLER_ABI from "./abi/Comptroller.json";
 import VTOKEN_ABI from "./abi/vToken.json";
 
-forking(39147163, async () => {
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+const CHAINLINK = "0x1B2103441A0A108daD8848D8F5d790e4D402921F";
+const OLD_IR = "0x6765202c3e6d3FdD05F0b26105d0C8DF59D3efaf";
+
+forking(39147992, async () => {
   const provider = ethers.provider;
   let vWBNBContract: Contract;
+  let comptrollerContract: Contract;
 
   before(async () => {
     vWBNBContract = new ethers.Contract(vWBNB, VTOKEN_ABI, provider);
+    comptrollerContract = new ethers.Contract(COMPTROLLER, COMPTROLLER_ABI, await ethers.getSigner(NORMAL_TIMELOCK));
+
+    await setMaxStaleCoreAssets(CHAINLINK, NORMAL_TIMELOCK);
   });
 
   describe("Pre-VIP behavior", async () => {
     it("check IR address", async () => {
       const ir = await vWBNBContract.interestRateModel();
-      expect(ir).to.be.equal("0x6765202c3e6d3FdD05F0b26105d0C8DF59D3efaf");
-    })
+      expect(ir).to.be.equal(OLD_IR);
+    });
 
     it("check RF", async () => {
       const rf = await vWBNBContract.reserveFactorMantissa();
       expect(rf).to.be.equal(parseUnits("0.25", 18));
-    })
+    });
+
+    it("check CF and LT", async () => {
+      const cf = await comptrollerContract.markets(vslisBNB);
+      expect(cf.collateralFactorMantissa).to.be.equal(parseUnits("0.87", 18));
+
+      const lt = await comptrollerContract.markets(vslisBNB);
+      expect(lt.liquidationThresholdMantissa).to.be.equal(parseUnits("0.9", 18));
+
+      const cf2 = await comptrollerContract.markets(vstkBNB);
+      expect(cf2.collateralFactorMantissa).to.be.equal(parseUnits("0.87", 18));
+
+      const lt2 = await comptrollerContract.markets(vstkBNB);
+      expect(lt2.liquidationThresholdMantissa).to.be.equal(parseUnits("0.9", 18));
+
+      const cf3 = await comptrollerContract.markets(vBNBx);
+      expect(cf3.collateralFactorMantissa).to.be.equal(parseUnits("0.87", 18));
+
+      const lt3 = await comptrollerContract.markets(vBNBx);
+      expect(lt3.liquidationThresholdMantissa).to.be.equal(parseUnits("0.9", 18));
+
+      const cf4 = await comptrollerContract.markets(vankrBNB);
+      expect(cf4.collateralFactorMantissa).to.be.equal(parseUnits("0.87", 18));
+
+      const lt4 = await comptrollerContract.markets(vankrBNB);
+      expect(lt4.liquidationThresholdMantissa).to.be.equal(parseUnits("0.9", 18));
+    });
   });
 
   testVip("VIP-315", vip315(), {
@@ -38,11 +84,37 @@ forking(39147163, async () => {
     it("check IR address", async () => {
       const ir = await vWBNBContract.interestRateModel();
       expect(ir).to.be.equal(vWBNB_IR);
-    })
+    });
 
     it("check RF", async () => {
       const rf = await vWBNBContract.reserveFactorMantissa();
       expect(rf).to.be.equal(vWBNB_RF);
-    })
+    });
+
+    it("check CF and LT", async () => {
+      const cf = await comptrollerContract.markets(vslisBNB);
+      expect(cf.collateralFactorMantissa).to.be.equal(CF);
+
+      const lt = await comptrollerContract.markets(vslisBNB);
+      expect(lt.liquidationThresholdMantissa).to.be.equal(LT);
+
+      const cf2 = await comptrollerContract.markets(vstkBNB);
+      expect(cf2.collateralFactorMantissa).to.be.equal(CF);
+
+      const lt2 = await comptrollerContract.markets(vstkBNB);
+      expect(lt2.liquidationThresholdMantissa).to.be.equal(LT);
+
+      const cf3 = await comptrollerContract.markets(vBNBx);
+      expect(cf3.collateralFactorMantissa).to.be.equal(CF);
+
+      const lt3 = await comptrollerContract.markets(vBNBx);
+      expect(lt3.liquidationThresholdMantissa).to.be.equal(LT);
+
+      const cf4 = await comptrollerContract.markets(vankrBNB);
+      expect(cf4.collateralFactorMantissa).to.be.equal(CF);
+
+      const lt4 = await comptrollerContract.markets(vankrBNB);
+      expect(lt4.liquidationThresholdMantissa).to.be.equal(LT);
+    });
   });
 });
--- vips/vip-315/bscmainnet.ts
+++ vips/vip-315/bscmainnet.ts
@@ -1,10 +1,18 @@
 import { parseUnits } from "ethers/lib/utils";
+
 import { ProposalType } from "../../src/types";
 import { makeProposal } from "../../src/utils";
 
 export const vWBNB_IR = "0xdE41feaB3B17C05Ba596b11E2C8d9f3514B71d22";
 export const vWBNB = "0xe10E80B7FD3a29fE46E16C30CC8F4dd938B742e2";
 export const vWBNB_RF = parseUnits("0.5", 18);
+export const LT = parseUnits("0.93", 18);
+export const CF = parseUnits("0.9", 18);
+export const vslisBNB = "0xd3CC9d8f3689B83c91b7B59cAB4946B063EB894A";
+export const vstkBNB = "0xcc5D9e502574cda17215E70bC0B4546663785227";
+export const vBNBx = "0x5E21bF67a6af41c74C1773E4b473ca5ce8fd3791";
+export const vankrBNB = "0xBfe25459BA784e70E2D7a718Be99a1f3521cA17f";
+export const COMPTROLLER = "0xd933909A4a2b7A4638903028f44D1d38ce27c352";
 
 const vip315 = () => {
   const meta = {
@@ -26,8 +34,28 @@ const vip315 = () => {
       {
         target: vWBNB,
         signature: "setReserveFactor(uint256)",
-        params: [vWBNB_RF]
-      }
+        params: [vWBNB_RF],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "setCollateralFactor(address,uint256,uint256)",
+        params: [vslisBNB, CF, LT],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "setCollateralFactor(address,uint256,uint256)",
+        params: [vstkBNB, CF, LT],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "setCollateralFactor(address,uint256,uint256)",
+        params: [vBNBx, CF, LT],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "setCollateralFactor(address,uint256,uint256)",
+        params: [vankrBNB, CF, LT],
+      },
     ],
     meta,
     ProposalType.REGULAR,
