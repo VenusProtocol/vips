abf88d9da70ec78afec795555633829363af2d41
Debugger022 <debugger022@gmail.com>
2024-05-30 15:32:11 +0530

    feat: add vip for prime config on arbitrumone

---
+++ multisig/proposals/arbitrumone/vip-007/index.ts
@@ -0,0 +1,136 @@
+import { NETWORK_ADDRESSES } from "../../../../src/networkAddresses";
+import { makeProposal } from "../../../../src/utils";
+
+const { arbitrumone } = NETWORK_ADDRESSES;
+
+const ACM = "0xD9dD18EB0cf10CbA837677f28A8F9Bda4bc2b157";
+export const XVS_VAULT_PROXY = "0x8b79692AAB2822Be30a6382Eb04763A74752d5B4";
+export const PRIME_LIQUIDITY_PROVIDER = "0x86bf21dB200f29F21253080942Be8af61046Ec29";
+export const PRIME = "0xFE69720424C954A2da05648a0FAC84f9bf11Ef49";
+export const XVS = "0xc1Eb7689147C81aC840d4FF0D298489fc7986d52";
+
+export const POOL_REGISTRY = "0x382238f07Bc4Fe4aA99e561adE8A4164b5f815DA";
+const PRIME_POOL_ID = 0;
+
+const vip007 = () => {
+  return makeProposal([
+    {
+      target: PRIME,
+      signature: "initializeV2(address)",
+      params: [POOL_REGISTRY],
+    },
+    {
+      target: PRIME_LIQUIDITY_PROVIDER,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: PRIME,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        PRIME_LIQUIDITY_PROVIDER,
+        "setTokensDistributionSpeed(address[],uint256[])",
+        arbitrumone.NORMAL_TIMELOCK,
+      ],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        PRIME_LIQUIDITY_PROVIDER,
+        "setMaxTokensDistributionSpeed(address[],uint256[])",
+        arbitrumone.NORMAL_TIMELOCK,
+      ],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME_LIQUIDITY_PROVIDER, "setMaxLoopsLimit(uint256)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME_LIQUIDITY_PROVIDER, "pauseFundsTransfer()", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME_LIQUIDITY_PROVIDER, "resumeFundsTransfer()", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "updateAlpha(uint128,uint128)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "updateMultipliers(address,uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "setStakedAt(address[],uint256[])", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "addMarket(address,address,uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "setLimit(uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "setMaxLoopsLimit(uint256)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "issue(bool,address[])", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "burn(address)", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PRIME, "togglePause()", arbitrumone.NORMAL_TIMELOCK],
+    },
+    {
+      target: PRIME_LIQUIDITY_PROVIDER,
+      signature: "setPrimeToken(address)",
+      params: [PRIME],
+    },
+    {
+      target: XVS_VAULT_PROXY,
+      signature: "setPrimeToken(address,address,uint256)",
+      params: [PRIME, XVS, PRIME_POOL_ID],
+    },
+    {
+      target: PRIME,
+      signature: "setLimit(uint256,uint256)",
+      params: [
+        0, // irrevocable
+        0, // revocable
+      ],
+    },
+    {
+      target: PRIME_LIQUIDITY_PROVIDER,
+      signature: "pauseFundsTransfer()",
+      params: [],
+    },
+  ]);
+};
+
+export default vip007;
