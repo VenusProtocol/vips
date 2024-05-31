a32dea3a72095955b71e0e108aee1e4bf0563abd
Debugger022 <debugger022@gmail.com>
2024-05-31 15:04:30 +0530

    refactor: update reward speed

--- multisig/proposals/arbitrumone/vip-009/index.ts
+++ multisig/proposals/arbitrumone/vip-009/index.ts
@@ -4,7 +4,7 @@ import { makeProposal } from "../../../../src/utils";
 
 export const XVS_VAULT_PROXY = "0x8b79692AAB2822Be30a6382Eb04763A74752d5B4";
 export const XVS_STORE = "0x507D9923c954AAD8eC530ed8Dedb75bFc893Ec5e";
-export const REWARDS_SPEED = "31709791983764"; // to be decided
+export const REWARDS_SPEED = 0; // to be decided
 export const XVS = "0xc1Eb7689147C81aC840d4FF0D298489fc7986d52";
 export const XVS_AMOUNT = parseUnits("5000", 18);
 
