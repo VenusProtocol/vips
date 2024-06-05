58934f2ccd461b3037162ea99c66d3dad1f2d543
Narayan Prusty <arsenal.narayan@gmail.com>
2024-06-05 14:00:43 +0530

    fix: added borrow cap vip commands

--- simulations/vip-308/bsctestnet.ts
+++ simulations/vip-308/bsctestnet.ts
@@ -7,23 +7,28 @@ import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
 import { STABLECOIN_COMPTROLLER } from "../../vips/vip-185";
 import { NORMAL_TIMELOCK, UNITROLLER, vLUNA, vUST, vip308 } from "../../vips/vip-308/bsctestnet";
+import VTOKEN_ABI from "./abi/VBep20DelegateAbi.json";
 import ACM_ABI from "./abi/acm.json";
 import COMPTROLLER_FACET_ABI from "./abi/comptroller.json";
 import UPGRADABLE_BEACON_ABI from "./abi/upgradableBeacon.json";
 
+const USER = "0x6f057A858171e187124ddEDF034dAc63De5dE5dB";
 const POOL_STABLECOIN_COMPTROLLER = "0x10b57706AD2345e590c2eA4DC02faef0d9f5b08B";
 const vUSDT_POOL_STABLECOIN = "0x3338988d0beb4419Acb8fE624218754053362D06";
 const vUST_USER = "0xFEA1c651A47FE29dB9b1bf3cC1f224d8D9CFF68C";
 const vUSDT_USER = "0x9cc6F5f16498fCEEf4D00A350Bd8F8921D304Dc9";
+const vETH = "0x162D005F0Fff510E54958Cfc5CF32A3180A84aab";
 
-forking(40582718, () => {
+forking(40583996, () => {
   let comptroller: Contract;
   let stableCoinPoolComptroller: Contract;
+  let vETHContract: Contract;
 
   before(async () => {
     await impersonateAccount(UNITROLLER);
     await impersonateAccount(NORMAL_TIMELOCK);
     await impersonateAccount(vUST_USER);
+    await impersonateAccount(USER);
 
     comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_FACET_ABI, await ethers.getSigner(NORMAL_TIMELOCK));
     stableCoinPoolComptroller = new ethers.Contract(
@@ -31,6 +36,7 @@ forking(40582718, () => {
       COMPTROLLER_FACET_ABI,
       await ethers.getSigner(STABLECOIN_COMPTROLLER),
     );
+    vETHContract = new ethers.Contract(vETH, VTOKEN_ABI, await ethers.getSigner(USER));
   });
 
   describe("Pre-VIP", () => {
@@ -43,6 +49,11 @@ forking(40582718, () => {
     it("stablecoin pool market not unlisted", async () => {
       const markets = await stableCoinPoolComptroller.getAssetsIn(vUSDT_USER);
       expect(markets.includes(vUSDT_POOL_STABLECOIN)).to.be.true;
+    });
+
+    it("Verify borrow cap 0", async () => {
+      await comptroller._setMarketBorrowCaps([vETH], [0]);
+      await expect(vETHContract.borrow(10)).to.not.be.reverted;
     });
   });
 
@@ -68,6 +79,11 @@ forking(40582718, () => {
 
       const markets = await stableCoinPoolComptroller.getAssetsIn(vUSDT_USER);
       expect(markets.includes(vUSDT_POOL_STABLECOIN)).to.be.false;
+    });
+
+    it("Verify borrow cap 0", async () => {
+      await comptroller._setMarketBorrowCaps([vETH], [0]);
+      await expect(vETHContract.borrow(10)).to.be.revertedWith("market borrow cap is 0");
     });
   });
 });
