234e711bba4edc6578873e019ef1879fa020be44
Narayan Prusty <arsenal.narayan@gmail.com>
2024-06-05 13:26:18 +0530

    fix: upgrade vBNBAdmin in testnet

--- vips/vip-319/bsctestnet.ts
+++ vips/vip-319/bsctestnet.ts
@@ -1,16 +1,16 @@
 import { ProposalType } from "../../src/types";
 import { makeProposal } from "../../src/utils";
 
-export const vBNBAdmin_Implementation = "0xaA8D9558d8D45666552a72CECbdd0a746aeaCDc9";
-export const vBNBAdmin = "0x9A7890534d9d91d473F28cB97962d176e2B65f1d";
-export const ProxyAdmin = "0x6beb6D2695B67FEb73ad4f172E8E2975497187e4";
+export const vBNBAdmin_Implementation = "0x90c891bb2b1821ADE159ECa6FbA0418b2bD1b86D";
+export const vBNBAdmin = "0x04109575c1dbB4ac2e59e60c783800ea10441BBe";
+export const ProxyAdmin = "0x7877ffd62649b6a1557b55d4c20fcbab17344c91";
 
-export const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
-export const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
-export const CRITICAL_TIMELOCK = "0x213c446ec11e45b15a6E29C1C1b402B8897f606d";
-export const ACM = "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555";
+export const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+export const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
+export const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
+export const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
 
-const vip319 = () => {
+const vip320 = () => {
   const meta = {
     version: "v2",
     title: "VIP-319",
@@ -48,4 +48,4 @@ const vip319 = () => {
   );
 };
 
-export default vip319;
+export default vip320;
