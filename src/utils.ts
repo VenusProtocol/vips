fe22189e472a1cc1d71637e60b6a392d31f687bc
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-06-06 16:45:27 +0530

    fix: remove console

--- src/utils.ts
+++ src/utils.ts
@@ -93,7 +93,6 @@ export const initMainnetUser = async (user: string, balance: NumberLike) => {
 
 const getAdapterParam = (noOfCommands: number): string => {
   const requiredGas = calculateGasForAdapterParam(noOfCommands);
-  console.log(requiredGas, "llllllllllllllllllllllllllllllll");
   const adapterParam = ethers.utils.solidityPack(["uint16", "uint256"], [1, requiredGas]);
   return adapterParam;
 };
@@ -171,7 +170,6 @@ export const makeProposal = async (
       throw "Chain Id is not supported";
     }
   }
-  console.log(proposal);
   return proposal;
 };
 
