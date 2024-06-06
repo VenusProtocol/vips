5d5f147dba8d6db9794320eb710289ee63b8d7ab
Narayan Prusty <arsenal.narayan@gmail.com>
2024-06-06 17:20:25 +0530

    fix: bscmainnet vip - wip

--- vips/vip-308/bscmainnet.ts
+++ vips/vip-308/bscmainnet.ts
@@ -1,17 +1,17 @@
 import { ethers } from "hardhat";
 
-import { cutParams as params } from "../../simulations/vip-308/utils/cut-params-testnet.json";
+import { cutParams as params } from "../../simulations/vip-308/utils/cut-params-mainnet.json";
 import { ProposalType } from "../../src/types";
 import { makeProposal } from "../../src/utils";
 
-export const UNITROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
+export const UNITROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
 export const IL_ACM = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
-export const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
-export const FAST_TRACK_TIMELOCK = "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6";
-export const CRITICAL_TIMELOCK = "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D";
-export const vLUNA = "0x9C3015191d39cF1930F92EB7e7BCbd020bCA286a";
-export const vUST = "0xF206af85BC2761c4F876d27Bd474681CfB335EfA";
+export const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+export const FAST_TRACK_TIMELOCK = "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02";
+export const CRITICAL_TIMELOCK = "0x213c446ec11e45b15a6E29C1C1b402B8897f606d";
+export const vLUNA = "0xb91A659E88B51474767CD97EF3196A3e7cEDD2c8";
+export const vUST = "0x78366446547D062f45b4C0f320cDaa6d710D87bb";
 export const COMPTROLLER_IMPL = "0x3ce617fceb5e9ed622f73b483ac7c94053795197";
 export const COMPTROLLER_BEACON = "0xdddd7725c073105fb2abfcbdec16708fc4c24b74";
 export const cutParams = params;
