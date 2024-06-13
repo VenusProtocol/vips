86b5525820bb483fedc10cc40504a89a0605d800
Debugger022 <debugger022@gmail.com>
2024-06-13 12:14:08 +0530

    chore: move xvs bridge vip to current branch

---
+++ simulations/vip-322/bscmainnet.ts
@@ -0,0 +1,61 @@
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers } from "hardhat";
+
+import { expectEvents } from "../../src/utils";
+import { forking, testVip } from "../../src/vip-framework";
+import vip322, {
+  COMMUNITY_WALLET,
+  ETH,
+  ETH_AMOUNT_WALLET,
+  XVS,
+  XVS_AMOUNT,
+  XVS_AMOUNT_WALLET,
+  XVS_BRIDGE_SRC,
+} from "../../vips/vip-322/bscmainnet";
+import ERC20_ABI from "./abi/ERC20.json";
+import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTSrc.json";
+
+forking(39546351, () => {
+  let xvsBridge: Contract;
+  let xvs: Contract;
+  let eth: Contract;
+  let oldCirculatingSupply: BigNumber;
+  let oldXvsBalBridge: BigNumber;
+  let oldXvsBalWallet: BigNumber;
+  let oldEthBalWallet: BigNumber;
+
+  before(async () => {
+    xvsBridge = new ethers.Contract(XVS_BRIDGE_SRC, XVS_BRIDGE_ABI, ethers.provider);
+    xvs = new ethers.Contract(XVS, ERC20_ABI, ethers.provider);
+    eth = new ethers.Contract(ETH, ERC20_ABI, ethers.provider);
+
+    oldCirculatingSupply = await xvsBridge.circulatingSupply();
+    oldXvsBalBridge = await xvs.balanceOf(XVS_BRIDGE_SRC);
+    oldXvsBalWallet = await xvs.balanceOf(COMMUNITY_WALLET);
+    oldEthBalWallet = await eth.balanceOf(COMMUNITY_WALLET);
+  });
+
+  testVip("VIP-322 Send XVS to Dest Chain", vip322(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [XVS_BRIDGE_ABI], ["SendToChain"], [1]);
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("Should increase wallet xvs and eth balance", async () => {
+      expect(await xvs.balanceOf(COMMUNITY_WALLET)).to.equal(XVS_AMOUNT_WALLET.add(oldXvsBalWallet));
+      expect(await eth.balanceOf(COMMUNITY_WALLET)).to.equal(ETH_AMOUNT_WALLET.add(oldEthBalWallet));
+    });
+
+    it("Should decrease circulating supply", async () => {
+      const currCirculatingSupply = await xvsBridge.circulatingSupply();
+      expect(oldCirculatingSupply.sub(currCirculatingSupply)).equals(XVS_AMOUNT);
+    });
+
+    it("Should increase number of locked tokens on bridge", async () => {
+      const currXVSBal = await xvs.balanceOf(XVS_BRIDGE_SRC);
+      expect(currXVSBal.sub(oldXvsBalBridge)).equals(XVS_AMOUNT);
+    });
+  });
+});
---
+++ vips/vip-322/bscmainnet.ts
@@ -0,0 +1,81 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+
+export const XVS = "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63";
+export const XVS_BRIDGE_SRC = "0xf8F46791E3dB29a029Ec6c9d946226f3c613e854";
+export const ARBITRUM_ONE_VTREASURY = "0x8a662ceAC418daeF956Bc0e6B2dd417c80CDA631";
+
+const BSC_TREASURY = "0xF322942f644A996A617BD29c16bd7d231d9F35E9";
+const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+
+const BRIDGE_FEES = parseUnits("0.005", 18); // value bigger than real estimated fees(0.0005)
+export const XVS_AMOUNT = parseUnits("30000", 18);
+const RECEIVER_ADDRESS = ethers.utils.defaultAbiCoder.encode(["address"], [ARBITRUM_ONE_VTREASURY]);
+const ADAPTER_PARAMS = ethers.utils.solidityPack(["uint16", "uint256"], [1, 300000]);
+const DEST_CHAIN_ID = 110;
+
+export const COMMUNITY_WALLET = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
+export const ETH_AMOUNT_WALLET = parseUnits("5", 18);
+export const XVS_AMOUNT_WALLET = parseUnits("1500", 18);
+export const ETH = "0x2170Ed0880ac9A755fd29B2688956BD959F933F8";
+
+const vip322 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-322 Bootstrap XVS liquidity for the Arbitrum one rewards",
+    description: ``,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: COMPTROLLER,
+        signature: "_grantXVS(address,uint256)",
+        params: [NORMAL_TIMELOCK, XVS_AMOUNT],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "_grantXVS(address,uint256)",
+        params: [COMMUNITY_WALLET, XVS_AMOUNT_WALLET],
+      },
+      {
+        target: BSC_TREASURY,
+        signature: "withdrawTreasuryBNB(uint256,address)",
+        params: [BRIDGE_FEES, NORMAL_TIMELOCK],
+      },
+      {
+        target: BSC_TREASURY,
+        signature: "withdrawTreasuryBEP20(address,uint256,address)",
+        params: [ETH, ETH_AMOUNT_WALLET, COMMUNITY_WALLET],
+      },
+      {
+        target: XVS,
+        signature: "approve(address,uint256)",
+        params: [XVS_BRIDGE_SRC, XVS_AMOUNT],
+      },
+      {
+        target: XVS_BRIDGE_SRC,
+        signature: "sendFrom(address,uint16,bytes32,uint256,(address,address,bytes))",
+        params: [
+          NORMAL_TIMELOCK,
+          DEST_CHAIN_ID,
+          RECEIVER_ADDRESS,
+          XVS_AMOUNT,
+          [NORMAL_TIMELOCK, ethers.constants.AddressZero, ADAPTER_PARAMS],
+        ],
+        value: BRIDGE_FEES.toString(),
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip322;
