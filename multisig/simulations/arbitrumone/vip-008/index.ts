2945d2fb86a760a01af34ac92f6072f0d767a3bd
Debugger022 <debugger022@gmail.com>
2024-06-14 14:53:57 +0530

    fix: tests by minting xvs in simulations

--- multisig/simulations/arbitrumone/vip-008/index.ts
+++ multisig/simulations/arbitrumone/vip-008/index.ts
@@ -1,8 +1,9 @@
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { Contract, Signer } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
+import { initMainnetUser } from "../../../../src/utils";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
 import {
   RewardsDistributorConfig,
@@ -27,19 +28,27 @@ import vip008, {
 import XVS_VAULT_ABI from "./abi/XVSVaultProxy.json";
 import XVS_ABI from "./abi/xvs.json";
 
+const BRIDGE = "0x20cEa49B5F7a6DBD78cAE772CA5973eF360AA1e6";
+const TREAUSRY_AMOUNT = parseUnits("30000", 18);
+
 forking(221153914, () => {
   const provider = ethers.provider;
+  let bridgeSigner: Signer;
   let xvsVault: Contract;
   let xvs: Contract;
 
   describe("Pre-VIP behaviour", async () => {
     before(async () => {
+      bridgeSigner = await initMainnetUser(BRIDGE, parseUnits("1", 18));
+
       xvsVault = new ethers.Contract(XVS_VAULT_PROXY, XVS_VAULT_ABI, provider);
       xvs = new ethers.Contract(XVS, XVS_ABI, provider);
+
+      await xvs.connect(bridgeSigner).mint(VTREASURY, TREAUSRY_AMOUNT);
     });
 
     it("vTreasury should hold 30000 XVS", async () => {
-      expect(await xvs.balanceOf(VTREASURY)).to.be.equal(parseUnits("30000", 18));
+      expect(await xvs.balanceOf(VTREASURY)).to.be.equal(TREAUSRY_AMOUNT);
     });
   });
 
@@ -69,62 +78,56 @@ forking(221153914, () => {
     it("xvs store should have 4500 xvs", async () => {
       expect(await xvs.balanceOf(XVS_STORE)).to.be.equal(XVS_AMOUNT);
     });
-  });
-});
 
-forking(221153914, () => {
-  const rewardBasicConfig = {
-    pool: COMPTROLLER_CORE,
-    address: REWARD_DISTRIBUTOR_CORE_0,
-    token: XVS,
-  };
-  describe("Generic checks", async () => {
-    before(async () => {
-      await pretendExecutingVip(vip008());
+    const rewardBasicConfig = {
+      pool: COMPTROLLER_CORE,
+      address: REWARD_DISTRIBUTOR_CORE_0,
+      token: XVS,
+    };
+    describe("Generic checks", async () => {
+      checkRewardsDistributorPool(COMPTROLLER_CORE, 1);
+
+      const tokensRewardConfig: RewardsDistributorConfig[] = [
+        {
+          ...rewardBasicConfig,
+          vToken: VUSDT_CORE,
+          borrowSpeed: "196759259259258",
+          supplySpeed: "131172839506172",
+          totalRewardsToDistribute: parseUnits("2550", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VUSDC_CORE,
+          borrowSpeed: "196759259259258",
+          supplySpeed: "131172839506172",
+          totalRewardsToDistribute: parseUnits("2550", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VWBTC_CORE,
+          borrowSpeed: "196759259259258",
+          supplySpeed: "131172839506172",
+          totalRewardsToDistribute: parseUnits("2550", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VWETH_CORE,
+          borrowSpeed: "98379629629629",
+          supplySpeed: "65586419753086",
+          totalRewardsToDistribute: parseUnits("1275", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VARB_CORE,
+          borrowSpeed: "98379629629629",
+          supplySpeed: "65586419753086",
+          totalRewardsToDistribute: parseUnits("1275", 18),
+        },
+      ];
+
+      for (const config of tokensRewardConfig) {
+        checkRewardsDistributor("RewardsDistributor_Core_0_XVS", config);
+      }
     });
-
-    checkRewardsDistributorPool(COMPTROLLER_CORE, 1);
-
-    const tokensRewardConfig: RewardsDistributorConfig[] = [
-      {
-        ...rewardBasicConfig,
-        vToken: VUSDT_CORE,
-        borrowSpeed: "196759259259258",
-        supplySpeed: "131172839506172",
-        totalRewardsToDistribute: parseUnits("2550", 18),
-      },
-      {
-        ...rewardBasicConfig,
-        vToken: VUSDC_CORE,
-        borrowSpeed: "196759259259258",
-        supplySpeed: "131172839506172",
-        totalRewardsToDistribute: parseUnits("2550", 18),
-      },
-      {
-        ...rewardBasicConfig,
-        vToken: VWBTC_CORE,
-        borrowSpeed: "196759259259258",
-        supplySpeed: "131172839506172",
-        totalRewardsToDistribute: parseUnits("2550", 18),
-      },
-      {
-        ...rewardBasicConfig,
-        vToken: VWETH_CORE,
-        borrowSpeed: "98379629629629",
-        supplySpeed: "65586419753086",
-        totalRewardsToDistribute: parseUnits("1275", 18),
-      },
-      {
-        ...rewardBasicConfig,
-        vToken: VARB_CORE,
-        borrowSpeed: "98379629629629",
-        supplySpeed: "65586419753086",
-        totalRewardsToDistribute: parseUnits("1275", 18),
-      },
-    ];
-
-    for (const config of tokensRewardConfig) {
-      checkRewardsDistributor("RewardsDistributor_Core_0_XVS", config);
-    }
   });
 });
