b6738559f6e9d4da41b0c5c73f0eb8f1527fa4e1
Corey Rice <coreyarice@gmail.com>
2024-07-05 10:45:12 -0300

    test: add test checking beacon implementation

--- multisig/simulations/ethereum/vip-043/index.ts
+++ multisig/simulations/ethereum/vip-043/index.ts
@@ -6,18 +6,23 @@ import { ethers } from "hardhat";
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
 import vip043, {
   COLLATERAL_FACTOR,
+  COMPTROLLER_BEACON,
   LIQUIDATION_THRESHOLD,
   LST_COMPTROLLER,
+  NEW_COMPTROLLER_IMPLEMENTATION,
   weETH,
   wstETH,
 } from "../../../proposals/ethereum/vip-043";
 import COMPTROLLER_ABI from "./abi/ILComprollerAbi.json";
+import BEACON_ABI from "./abi/beacon.json";
 
 forking(20235713, () => {
   let comptroller: Contract;
+  let beacon: Contract;
 
   before(async () => {
     comptroller = await ethers.getContractAt(COMPTROLLER_ABI, LST_COMPTROLLER);
+    beacon = await ethers.getContractAt(BEACON_ABI, COMPTROLLER_BEACON);
   });
 
   describe("Pre-VIP behavior", () => {
@@ -37,6 +42,10 @@ forking(20235713, () => {
   describe("Post-VIP behavior", async () => {
     before(async () => {
       await pretendExecutingVip(vip043());
+    });
+
+    it("should set the new implementation on the beacon", async () => {
+      expect(await beacon.implementation()).to.equal(NEW_COMPTROLLER_IMPLEMENTATION);
     });
 
     it("new weETH collateral factor and liquidation threshold", async () => {
