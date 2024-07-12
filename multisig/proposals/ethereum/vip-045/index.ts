42cf64b41858b2c5069c526bf57793a506ec9701
Corey Rice <coreyarice@gmail.com>
2024-07-12 18:16:27 -0300

    fix: update reward speeds

--- multisig/proposals/ethereum/vip-045/index.ts
+++ multisig/proposals/ethereum/vip-045/index.ts
@@ -19,17 +19,17 @@ export const vUSDC = "0x17C07e0c232f2f80DfDbd7a95b942D893A4C5ACb";
 export const vUSDT = "0x8C3e3821259B82fFb32B2450A95d2dcbf161C24E";
 
 export const BLOCKS_PER_YEAR = BigNumber.from(2628000); // assuming a block is mined every 12 seconds
-export const BLOCKS_IN_30_DAYS = BLOCKS_PER_YEAR.mul(30).div(365);
+export const BLOCKS_IN_90_DAYS = BLOCKS_PER_YEAR.mul(90).div(365);
 
-export const WBTC_PER_90_DAYS_REWARD = parseUnits("0.2316", 8);
-export const WETH_PER_90_DAYS_REWARD = parseUnits("47.877", 18);
-export const USDC_PER_90_DAYS_REWARD = parseUnits("13995", 6);
-export const USDT_PER_90_DAYS_REWARD = parseUnits("13995", 6);
+export const WBTC_PER_90_DAYS_REWARD = parseUnits("0.25476", 8);
+export const WETH_PER_90_DAYS_REWARD = parseUnits("52.6647", 18);
+export const USDC_PER_90_DAYS_REWARD = parseUnits("15395", 6);
+export const USDT_PER_90_DAYS_REWARD = parseUnits("15395", 6);
 
-export const WBTC_PER_BLOCK_REWARD = WBTC_PER_90_DAYS_REWARD.div(BLOCKS_IN_30_DAYS);
-export const WETH_PER_BLOCK_REWARD = WETH_PER_90_DAYS_REWARD.div(BLOCKS_IN_30_DAYS);
-export const USDC_PER_BLOCK_REWARD = USDC_PER_90_DAYS_REWARD.div(BLOCKS_IN_30_DAYS);
-export const USDT_PER_BLOCK_REWARD = USDT_PER_90_DAYS_REWARD.div(BLOCKS_IN_30_DAYS);
+export const WBTC_PER_BLOCK_REWARD = WBTC_PER_90_DAYS_REWARD.div(BLOCKS_IN_90_DAYS);
+export const WETH_PER_BLOCK_REWARD = WETH_PER_90_DAYS_REWARD.div(BLOCKS_IN_90_DAYS);
+export const USDC_PER_BLOCK_REWARD = USDC_PER_90_DAYS_REWARD.div(BLOCKS_IN_90_DAYS);
+export const USDT_PER_BLOCK_REWARD = USDT_PER_90_DAYS_REWARD.div(BLOCKS_IN_90_DAYS);
 
 export const vip045 = () => {
   return makeProposal([
