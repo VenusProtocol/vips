8380b71f14db109eabb8fe7274e4529e580eb4dc
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-12 14:43:20 +0530

    fix: fixed reward speeds

--- multisig/proposals/ethereum/vip-045/index.ts
+++ multisig/proposals/ethereum/vip-045/index.ts
@@ -1,10 +1,12 @@
+import { BigNumber } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
+
 import { makeProposal } from "../../../../src/utils";
 
 export const PRIME_LIQUIDITY_PROVIDER = "0x8ba6aFfd0e7Bcd0028D1639225C84DdCf53D8872";
 export const PRIME = "0x14C4525f47A7f7C984474979c57a2Dccb8EACB39";
 export const CORE_COMPTROLLER = "0x687a01ecF6d3907658f7A7c714749fAC32336D1B";
-export const TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA"
+export const TREASURY = "0xFD9B071168bC27DBE16406eC3Aba050Ce8Eb22FA";
 
 export const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 export const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
@@ -13,8 +15,21 @@ export const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
 export const vWBTC = "0x8716554364f20BCA783cb2BAA744d39361fd1D8d";
 export const vWETH = "0xc82780Db1257C788F262FBbDA960B3706Dfdcaf2";
-export const vUSDC = "0x17C07e0c232f2f80DfDbd7a95b942D893A4C5ACb"
-export const vUSDT = "0x8C3e3821259B82fFb32B2450A95d2dcbf161C24E"
+export const vUSDC = "0x17C07e0c232f2f80DfDbd7a95b942D893A4C5ACb";
+export const vUSDT = "0x8C3e3821259B82fFb32B2450A95d2dcbf161C24E";
+
+export const BLOCKS_PER_YEAR = BigNumber.from(2628000); // assuming a block is mined every 12 seconds
+export const BLOCKS_IN_30_DAYS = BLOCKS_PER_YEAR.mul(30).div(365);
+
+export const WBTC_PER_MONTH_REWARD = parseUnits("0.0772", 8);
+export const WETH_PER_MONTH_REWARD = parseUnits("15.959", 18);
+export const USDC_PER_MONTH_REWARD = parseUnits("4665", 6);
+export const USDT_PER_MONTH_REWARD = parseUnits("4665", 6);
+
+export const WBTC_PER_BLOCK_REWARD = WBTC_PER_MONTH_REWARD.div(BLOCKS_IN_30_DAYS);
+export const WETH_PER_BLOCK_REWARD = WETH_PER_MONTH_REWARD.div(BLOCKS_IN_30_DAYS);
+export const USDC_PER_BLOCK_REWARD = USDC_PER_MONTH_REWARD.div(BLOCKS_IN_30_DAYS);
+export const USDT_PER_BLOCK_REWARD = USDT_PER_MONTH_REWARD.div(BLOCKS_IN_30_DAYS);
 
 export const vip045 = () => {
   return makeProposal([
@@ -23,28 +38,28 @@ export const vip045 = () => {
       signature: "setTokensDistributionSpeed(address[],uint256[])",
       params: [
         [WBTC, WETH, USDC, USDT],
-        ["126", "126", "126", "126"],
+        [WBTC_PER_BLOCK_REWARD, WETH_PER_BLOCK_REWARD, USDC_PER_BLOCK_REWARD, USDT_PER_BLOCK_REWARD],
       ],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [WETH, "2180000000000000000", PRIME_LIQUIDITY_PROVIDER],
+      params: [WETH, WETH_PER_MONTH_REWARD, PRIME_LIQUIDITY_PROVIDER],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [WBTC, "2180000000000000000", PRIME_LIQUIDITY_PROVIDER],
+      params: [WBTC, WBTC_PER_MONTH_REWARD, PRIME_LIQUIDITY_PROVIDER],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [USDC, "2180000000000000000", PRIME_LIQUIDITY_PROVIDER],
+      params: [USDC, USDC_PER_MONTH_REWARD, PRIME_LIQUIDITY_PROVIDER],
     },
     {
       target: TREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [USDT, "2180000000000000000", PRIME_LIQUIDITY_PROVIDER],
+      params: [USDT, USDT_PER_MONTH_REWARD, PRIME_LIQUIDITY_PROVIDER],
     },
     {
       target: PRIME_LIQUIDITY_PROVIDER,
--- multisig/simulations/ethereum/vip-045/index.ts
+++ multisig/simulations/ethereum/vip-045/index.ts
@@ -1,27 +1,37 @@
-import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
+
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
 import vip045, {
   PRIME,
   PRIME_LIQUIDITY_PROVIDER,
-  WBTC, 
-  WETH,
   USDC,
+  USDC_PER_BLOCK_REWARD,
+  USDC_PER_MONTH_REWARD,
   USDT,
+  USDT_PER_BLOCK_REWARD,
+  USDT_PER_MONTH_REWARD,
+  WBTC,
+  WBTC_PER_BLOCK_REWARD,
+  WBTC_PER_MONTH_REWARD,
+  WETH,
+  WETH_PER_BLOCK_REWARD,
+  WETH_PER_MONTH_REWARD,
 } from "../../../proposals/ethereum/vip-045";
-import { checkXVSVault } from "../../../../src/vip-framework/checks/checkXVSVault";
-
-import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
 import ERC20_ABI from "./abis/ERC20.json";
 import PRIME_ABI from "./abis/Prime.json";
+import PRIME_LIQUIDITY_PROVIDER_ABI from "./abis/PrimeLiquidityProvider.json";
 
 forking(20282042, () => {
   let prevBTCBBalance: any;
   let prevETHBalance: any;
+  let prevUSDCBalance: any;
+  let prevUSDTBalance: any;
   let btcbContract: Contract;
   let ethContract: Contract;
+  let usdcContract: Contract;
+  let usdtContract: Contract;
 
   describe("Pre-VIP behavior", () => {
     let primeLiquidityProvider: Contract;
@@ -31,9 +41,13 @@ forking(20282042, () => {
 
       btcbContract = await ethers.getContractAt(ERC20_ABI, WBTC);
       ethContract = await ethers.getContractAt(ERC20_ABI, WETH);
+      usdcContract = await ethers.getContractAt(ERC20_ABI, USDC);
+      usdtContract = await ethers.getContractAt(ERC20_ABI, USDT);
 
       prevBTCBBalance = await btcbContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
       prevETHBalance = await ethContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
+      prevUSDCBalance = await usdcContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
+      prevUSDTBalance = await usdtContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
     });
 
     it("speeds", async () => {
@@ -41,6 +55,12 @@ forking(20282042, () => {
       expect(speed).to.deep.equal(0);
 
       speed = await primeLiquidityProvider.tokenDistributionSpeeds(WBTC);
+      expect(speed).to.deep.equal(0);
+
+      speed = await primeLiquidityProvider.tokenDistributionSpeeds(USDC);
+      expect(speed).to.deep.equal(0);
+
+      speed = await primeLiquidityProvider.tokenDistributionSpeeds(USDT);
       expect(speed).to.deep.equal(0);
     });
 
@@ -63,10 +83,16 @@ forking(20282042, () => {
 
     it("speeds", async () => {
       let speed = await primeLiquidityProvider.tokenDistributionSpeeds(WETH);
-      expect(speed).to.deep.equal("126");
+      expect(speed).to.deep.equal(WETH_PER_BLOCK_REWARD);
 
       speed = await primeLiquidityProvider.tokenDistributionSpeeds(WBTC);
-      expect(speed).to.deep.equal("126");
+      expect(speed).to.deep.equal(WBTC_PER_BLOCK_REWARD);
+
+      speed = await primeLiquidityProvider.tokenDistributionSpeeds(USDC);
+      expect(speed).to.deep.equal(USDC_PER_BLOCK_REWARD);
+
+      speed = await primeLiquidityProvider.tokenDistributionSpeeds(USDT);
+      expect(speed).to.deep.equal(USDT_PER_BLOCK_REWARD);
     });
 
     it("paused", async () => {
@@ -80,13 +106,13 @@ forking(20282042, () => {
     it("check balance", async () => {
       const currentBTCBBalance = await btcbContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
       const currentETHBalance = await ethContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
+      const currentUSDCBalance = await usdcContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
+      const currentUSDTBalance = await usdtContract.balanceOf(PRIME_LIQUIDITY_PROVIDER);
 
-      expect(currentBTCBBalance.sub(prevBTCBBalance)).to.be.equal("2180000000000000000");
-      expect(currentETHBalance.sub(prevETHBalance)).to.be.equal("42230000000000000000");
-    });
-
-    describe("generic tests", async () => {
-      checkXVSVault();
+      expect(currentBTCBBalance.sub(prevBTCBBalance)).to.be.equal(WBTC_PER_MONTH_REWARD);
+      expect(currentETHBalance.sub(prevETHBalance)).to.be.equal(WETH_PER_MONTH_REWARD);
+      expect(currentUSDCBalance.sub(prevUSDCBalance)).to.be.equal(USDC_PER_MONTH_REWARD);
+      expect(currentUSDTBalance.sub(prevUSDTBalance)).to.be.equal(USDT_PER_MONTH_REWARD);
     });
   });
 });
