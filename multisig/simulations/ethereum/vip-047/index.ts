a9f4ffcfd57263d66543a12b94dc93cba5287147
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-07-16 21:48:24 +0200

    feat: set Prime contract on the Core and Curve comptrollers, on Ethereum

---
+++ multisig/simulations/ethereum/vip-047/index.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { forking, pretendExecutingVip } from "src/vip-framework";
+
+import vip047, { COMPTROLLER_CORE, COMPTROLLER_CURVE, PRIME } from "../../../proposals/ethereum/vip-047";
+import COMPTROLLER_ABI from "./abi/ILComptroller.json";
+
+forking(20321056, async () => {
+  const provider = ethers.provider;
+
+  describe("Post-VIP behavior", () => {
+    before(async () => {
+      await pretendExecutingVip(await vip047());
+    });
+
+    it("Comptroller Core should have correct Prime token address", async () => {
+      const comptrollerCore = new ethers.Contract(COMPTROLLER_CORE, COMPTROLLER_ABI, provider);
+      expect(await comptrollerCore.prime()).to.be.equal(PRIME);
+    });
+
+    it("Comptroller Curve should have correct Prime token address", async () => {
+      const comptrollerCurve = new ethers.Contract(COMPTROLLER_CURVE, COMPTROLLER_ABI, provider);
+      expect(await comptrollerCurve.prime()).to.be.equal(PRIME);
+    });
+  });
+});
