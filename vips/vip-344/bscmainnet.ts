32b6d5a51484ac737fde89caea18a64990fa6618
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-25 15:30:01 +0530

    fix: added empty vip

---
+++ vips/vip-344/bscmainnet.ts
@@ -0,0 +1,30 @@
+import { ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+
+const NORMAL_TIMELOCK = "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396";
+
+const vip344 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-344",
+    description: ``,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: NORMAL_TIMELOCK,
+        signature: "",
+        params: [],
+        value: "1",
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip344;
