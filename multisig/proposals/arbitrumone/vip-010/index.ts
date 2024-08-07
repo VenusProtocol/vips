2d209c222a0483b0895b56be0b7f3e17cd156a81
Narayan Prusty <arsenal.narayan@gmail.com>
2024-08-07 18:43:03 +0530

    fix: wip - arbitrum VIP

---
+++ multisig/proposals/arbitrumone/vip-010/index.ts
@@ -0,0 +1,45 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { makeProposal } from "src/utils";
+
+const { arbitrumone } = NETWORK_ADDRESSES;
+
+export const ARBITRUM_ONE_ACM = "0xD9dD18EB0cf10CbA837677f28A8F9Bda4bc2b157";
+
+const vip010 = () => {
+  return makeProposal([
+    // {
+    //   target: arbitrumone.XVS_VAULT_PROXY,
+    //   signature: "transferOwnership(address)",
+    //   params: [arbitrumone.NORMAL_TIMELOCK],
+    // },
+    // {
+    //   target: arbitrumone.XVS_VAULT_PROXY,
+    //   signature: "transferOwnership(address)",
+    //   params: [arbitrumone.NORMAL_TIMELOCK],
+    // },
+
+    // Revoke permissions
+    {
+      target: ARBITRUM_ONE_ACM,
+      signature: "revokeCallPermission(address,string,address)",
+      params: [arbitrumone.XVS_VAULT_PROXY, "add(address,uint256,address,uint256,uint256)", arbitrumone.GUARDIAN],
+    },
+    // {
+    //   target: ARBITRUM_ONE_ACM,
+    //   signature: "revokeCallPermission(address,string,address)",
+    //   params: [arbitrumone.XVS_VAULT_PROXY, "set(address,uint256,uint256)", arbitrumone.GUARDIAN],
+    // },
+    // {
+    //   target: ARBITRUM_ONE_ACM,
+    //   signature: "revokeCallPermission(address,string,address)",
+    //   params: [arbitrumone.XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", arbitrumone.GUARDIAN],
+    // },
+    // {
+    //   target: ARBITRUM_ONE_ACM,
+    //   signature: "revokeCallPermission(address,string,address)",
+    //   params: [arbitrumone.XVS_VAULT_PROXY, "setWithdrawalLockingPeriod(address,uint256,uint256)", arbitrumone.GUARDIAN],
+    // },
+  ]);
+};
+
+export default vip010;
---
+++ vips/vip-349/bscmainnet.ts
@@ -0,0 +1,76 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { arbitrumone, opbnbmainnet, ethereum } = NETWORK_ADDRESSES;
+
+export const ARBITRUM_ONE_ACM = "0xD9dD18EB0cf10CbA837677f28A8F9Bda4bc2b157";
+export const ETHEREUM_ACM = "0x230058da2D23eb8836EC5DB7037ef7250c56E25E";
+export const OPBNBMAINNET_ACM = "0xA60Deae5344F1152426cA440fb6552eA0e3005D6";
+
+const vip349 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-349 Accept ownership & Give permissions of XVS Vault to Normal Timelock",
+    description: `### Description`,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+  return makeProposal(
+    [
+      {
+        target: arbitrumone.XVS_VAULT_PROXY,
+        signature: "acceptOwnership()",
+        params: [],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: arbitrumone.XVS_VAULT_PROXY,
+        signature: "acceptOwnership()",
+        params: [],
+        dstChainId: LzChainId.arbitrumone,
+      },
+
+      // Normal Timelock Permissions
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "pause()", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "resume()", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "add(address,uint256,address,uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "set(address,uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [arbitrumone.XVS_VAULT_PROXY, "setWithdrawalLockingPeriod(address,uint256,uint256)", arbitrumone.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.arbitrumone,
+      },
+    ]
+  )
+};
+
+export default vip349;
