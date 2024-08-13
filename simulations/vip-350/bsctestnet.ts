51e6fbd0059b3a07ebe08c328d62f513c68a3546
Narayan Prusty <arsenal.narayan@gmail.com>
2024-08-13 13:36:35 +0530

    fix: added bsc vips

---
+++ simulations/vip-350/bsctestnet.ts
@@ -0,0 +1,17 @@
+import { expectEvents } from "../../src/utils";
+import { forking, testVip } from "../../src/vip-framework";
+import vip350 from "../../vips/vip-350/bsctestnet";
+import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
+
+forking(41938696, async () => {
+  testVip("vip350 XVS vault permission proposal", await vip350(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(
+        txResponse,
+        [OMNICHAIN_PROPOSAL_SENDER_ABI],
+        ["ExecuteRemoteProposal", "StorePayload"],
+        [2, 0],
+      );
+    },
+  });
+});
