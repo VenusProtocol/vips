b0431b192c1abdc8c3ad1b35a2ce3663f02dbb5e
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-08-13 10:09:25 +0200

    feat: add description for the VIP 350

--- vips/vip-350/vip-350/bscmainnet.ts
+++ vips/vip-350/vip-350/bscmainnet.ts
@@ -28,8 +28,32 @@ import {
 export const vip350 = () => {
   const meta = {
     version: "v2",
-    title: "Token converter: Incentive upgrade",
-    description: `VIP-350`,
+    title: "VIP-350 Incentives for Token Converters",
+    description: `#### Summary
+
+If passed, this VIP will perform the following actions:
+
+- set to 0.01% the incentive for every conversion pair in every Token Converter contract used by the Venus Protocol on BNB Chain, following the [Chaos Labs recommendations](https://community.venus.io/t/chaos-labs-token-converter/4521).
+- enable USDT conversions on every Token Converter, for any wallet, not only for other converters
+
+#### Description
+
+Token Converters are permissionless: anyone is able to perform a conversion on those contracts. Incentives on the Token Converters should create arbitrage opportunities, returning on each conversion slightly more than the expected amounts only considering the oracle prices of the tokens involved, or requiring fewer tokens than expected (depending on the type of conversion set by the caller). The community is expected to take advantage of these arbitrages and operate the Token Converter contracts fluently because they should be economically profitable.
+
+The distribution of funds to the RiskFundConverter has been zero since [VIP-332](https://app.venus.io/#/governance/proposal/332?chainId=56). Nowadays there is no converter interested in USDT receiving a fluent flow of tokens that could be used to convert the offer of the rest of the converters. So, it doesn’t make sense to maintain “only private” conversions of USDT in those converters. This VIP will open the USDT conversions in the following Token Converters on BNB Chain:
+
+- [USDTPrimeConverter](https://bscscan.com/address/0xD9f101AA67F3D72662609a2703387242452078C3)
+- [USDCPrimeConverter](https://bscscan.com/address/0xa758c9C215B6c4198F0a0e3FA46395Fa15Db691b)
+- [BTCBPrimeConverter](https://bscscan.com/address/0xE8CeAa79f082768f99266dFd208d665d2Dd18f53)
+- [ETHPrimeConverter](https://bscscan.com/address/0xca430B8A97Ea918fF634162acb0b731445B8195E)
+
+#### References
+
+- [VIP Simulation](https://github.com/VenusProtocol/vips/pull/339)
+- [Token Converters documentation](https://docs-v4.venus.io/whats-new/token-converter)
+- [Technical article about Token Converters](https://docs-v4.venus.io/technical-reference/reference-technical-articles/token-converters)
+- [Addresses of the deployed Token Converter contracts](https://docs-v4.venus.io/deployed-contracts/token-converters)
+- [Chaos Labs recommendation](https://community.venus.io/t/chaos-labs-token-converter/4521)`,
     forDescription: "Execute this proposal",
     againstDescription: "Do not execute this proposal",
     abstainDescription: "Indifferent to execution",
