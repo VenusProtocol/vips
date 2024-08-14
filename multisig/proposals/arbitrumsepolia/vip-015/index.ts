96dffd03958551f19b8017b93661647f5a75ad02
Debugger022 <debugger022@gmail.com>
2024-08-14 14:54:29 +0530

    refactor: liquid staked ETH vip for arbitrum sepolia

--- multisig/proposals/arbitrumsepolia/vip-015/index.ts
+++ multisig/proposals/arbitrumsepolia/vip-015/index.ts
@@ -4,36 +4,31 @@ import { makeProposal } from "src/utils";
 
 const { arbitrumsepolia } = NETWORK_ADDRESSES;
 
-export const COMPTROLLER_LIQUID_STAKED_ETH = "0x006D44b6f5927b3eD83bD0c1C36Fb1A3BaCaC208";
+export const COMPTROLLER_LIQUID_STAKED_ETH = "0x3D04F926b2a165BBa17FBfccCCB61513634fa5e4";
 
 export const Mock_wstETH = "0x4A9dc15aA6094eF2c7eb9d9390Ac1d71f9406fAE";
 export const Mock_weETH = "0x243141DBff86BbB0a082d790fdC21A6ff615Fa34";
 export const WETH = "0x980B62Da83eFf3D4576C647993b0c1D7faf17c73";
+
+export const VwstETH = "0x253515E19e8b888a4CA5a0a3363B712402ce4046";
+export const VweETH = "0x75f841b14305935D8D7E806f249D9FA52EF1550B";
+export const VWETH = "0xd7057250b439c0849377bB6C3263eb8f9cf49d98";
 
 const CHAINLINK_ETH_FEED = "0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165";
 const STALE_PERIOD_3M = 60 * 3; // 3 minutes (for pricefeeds with heartbeat of 2 mins)
 
 export const PSR = "0x09267d30798B59c581ce54E861A084C6FC298666";
 
+const COMPTROLLER_BEACON = "0x12Dcb8D9F1eE7Ad7410F5B36B07bcC7891ab4cEf";
+const NEW_COMPTROLLER_IMPLEMENTATION = "0x92e20Cdad0f06D557C4270Be724C9cf8A2E26Ac3";
+
 // IL configuration
 const vip015 = () => {
   return makeProposal([
-    { target: COMPTROLLER_LIQUID_STAKED_ETH, signature: "acceptOwnership()", params: [] },
     {
-      target: COMPTROLLER_LIQUID_STAKED_ETH,
-      signature: "setPriceOracle(address)",
-      params: [arbitrumsepolia.RESILIENT_ORACLE],
-    },
-    {
-      target: arbitrumsepolia.POOL_REGISTRY,
-      signature: "addPool(string,address,uint256,uint256,uint256)",
-      params: [
-        "Liquid Staked ETH",
-        COMPTROLLER_LIQUID_STAKED_ETH,
-        parseUnits("0.5", 18),
-        parseUnits("1.02", 18),
-        parseUnits("100", 18),
-      ],
+      target: COMPTROLLER_BEACON,
+      signature: "upgradeTo(address)",
+      params: [NEW_COMPTROLLER_IMPLEMENTATION],
     },
     {
       target: arbitrumsepolia.CHAINLINK_ORACLE,
@@ -60,28 +55,45 @@ const vip015 = () => {
       signature: "setDirectPrice(address,uint256)",
       params: [Mock_wstETH, "1100000000000000000"],
     },
+    { target: COMPTROLLER_LIQUID_STAKED_ETH, signature: "acceptOwnership()", params: [] },
+    {
+      target: COMPTROLLER_LIQUID_STAKED_ETH,
+      signature: "setPriceOracle(address)",
+      params: [arbitrumsepolia.RESILIENT_ORACLE],
+    },
+    {
+      target: arbitrumsepolia.POOL_REGISTRY,
+      signature: "addPool(string,address,uint256,uint256,uint256)",
+      params: [
+        "Liquid Staked ETH",
+        COMPTROLLER_LIQUID_STAKED_ETH,
+        parseUnits("0.5", 18),
+        parseUnits("1.02", 18),
+        parseUnits("100", 18),
+      ],
+    },
     {
       target: arbitrumsepolia.VTREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [Mock_wstETH, "2000000000000000000", arbitrumsepolia.NORMAL_TIMELOCK],
+      params: [Mock_wstETH, parseUnits("2", 18), arbitrumsepolia.GUARDIAN],
     },
     {
       target: Mock_wstETH,
       signature: "approve(address,uint256)",
-      params: [arbitrumsepolia.POOL_REGISTRY, 0],
+      params: ["0xf93Df3135e0D555185c0BC888073374cA551C5fE", 0],
     },
     {
       target: Mock_wstETH,
       signature: "approve(address,uint256)",
-      params: [arbitrumsepolia.POOL_REGISTRY, "2000000000000000000"],
+      params: ["0xf93Df3135e0D555185c0BC888073374cA551C5fE", parseUnits("2", 18)],
     },
     {
-      target: Mock_wstETH,
+      target: VwstETH,
       signature: "setReduceReservesBlockDelta(uint256)",
       params: ["86400"],
     },
     {
-      target: Mock_wstETH,
+      target: VwstETH,
       signature: "setProtocolShareReserve(address)",
       params: [PSR],
     },
@@ -90,13 +102,13 @@ const vip015 = () => {
       signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
       params: [
         [
-          Mock_wstETH,
-          "930000000000000000",
-          "950000000000000000",
-          "2000000000000000000",
+          VwstETH,
+          parseUnits("0.93", 18),
+          parseUnits("0.95", 18),
+          parseUnits("2", 18),
           arbitrumsepolia.VTREASURY,
-          "8000000000000000000000",
-          "800000000000000000000",
+          parseUnits("8000", 18),
+          parseUnits("800", 18),
         ],
       ],
     },
@@ -128,7 +140,7 @@ const vip015 = () => {
     {
       target: arbitrumsepolia.VTREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [Mock_weETH, "2000000000000000000", arbitrumsepolia.NORMAL_TIMELOCK],
+      params: [Mock_weETH, "2000000000000000000", arbitrumsepolia.GUARDIAN],
     },
     {
       target: Mock_weETH,
@@ -141,12 +153,12 @@ const vip015 = () => {
       params: [arbitrumsepolia.POOL_REGISTRY, "2000000000000000000"],
     },
     {
-      target: Mock_weETH,
+      target: VweETH,
       signature: "setReduceReservesBlockDelta(uint256)",
       params: ["86400"],
     },
     {
-      target: Mock_weETH,
+      target: VweETH,
       signature: "setProtocolShareReserve(address)",
       params: [PSR],
     },
@@ -155,38 +167,38 @@ const vip015 = () => {
       signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
       params: [
         [
-          Mock_weETH,
+          VweETH,
           "930000000000000000",
           "950000000000000000",
           "2000000000000000000",
           arbitrumsepolia.VTREASURY,
-          "8000000000000000000000",
-          "800000000000000000000",
+          "4600000000000000000000",
+          "2300000000000000000000",
         ],
       ],
     },
     {
       target: arbitrumsepolia.VTREASURY,
       signature: "withdrawTreasuryToken(address,uint256,address)",
-      params: [WETH, "2000000000000000000", arbitrumsepolia.NORMAL_TIMELOCK],
+      params: [WETH, "78042235579982593", arbitrumsepolia.GUARDIAN],
     },
     {
       target: WETH,
       signature: "approve(address,uint256)",
-      params: [arbitrumsepolia.POOL_REGISTRY, 0],
+      params: [arbitrumsepolia.POOL_REGISTRY, "0"],
     },
     {
       target: WETH,
       signature: "approve(address,uint256)",
-      params: [arbitrumsepolia.POOL_REGISTRY, "2000000000000000000"],
+      params: [arbitrumsepolia.POOL_REGISTRY, "78042235579982593"],
     },
     {
-      target: WETH,
+      target: VWETH,
       signature: "setReduceReservesBlockDelta(uint256)",
       params: ["86400"],
     },
     {
-      target: WETH,
+      target: VWETH,
       signature: "setProtocolShareReserve(address)",
       params: [PSR],
     },
@@ -195,10 +207,10 @@ const vip015 = () => {
       signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
       params: [
         [
-          WETH,
+          VWETH,
           "770000000000000000",
           "800000000000000000",
-          "2000000000000000000",
+          "78042235579982593",
           arbitrumsepolia.VTREASURY,
           "14000000000000000000000",
           "12500000000000000000000",
