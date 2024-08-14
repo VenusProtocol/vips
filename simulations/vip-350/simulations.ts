a7501bda2b3d155ed5df25f6f5f2e6e66584fc23
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-08-14 21:36:39 +0200

    feat: set description for VIP 352

--- simulations/vip-350/simulations.ts
+++
@@ -1,7 +0,0 @@
-import { forking, testVip } from "src/vip-framework";
-
-import vip350 from "../../vips/vip-350/bscmainnet";
-
-forking(41186665, async () => {
-  testVip("VIP-350", await vip350(), {});
-});
