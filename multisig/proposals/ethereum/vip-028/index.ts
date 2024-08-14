61258a533b5ac106e7b53e87a4717f379268826a
Narayan Prusty <arsenal.narayan@gmail.com>
2024-08-14 17:37:08 +0530

    fix: added ethereum multisig tx

---
+++ multisig/proposals/ethereum/vip-028/index.ts
@@ -0,0 +1,32 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { makeProposal } from "../../../../src/utils";
+
+export const LST_POOL_COMPTROLLER = "0xF522cd0360EF8c2FF48B648d53EA1717Ec0F3Ac3";
+export const LST_POOL_VWETH = "0xc82780Db1257C788F262FBbDA960B3706Dfdcaf2";
+export const NEW_IRM = "0x834078D691d431aAdC80197f7a61239F9F89547b";
+export const COLLATERAL_FACTOR = "0";
+export const RESERVE_FACTOR = parseUnits("0.25", 18);
+export const LIQUIDATION_THRESHOLD = parseUnits("0.93", 18);
+
+const vip028 = () => {
+  return makeProposal([
+    {
+      target: LST_POOL_VWETH,
+      signature: "setReserveFactor(uint256)",
+      params: [RESERVE_FACTOR],
+    },
+    {
+      target: LST_POOL_COMPTROLLER,
+      signature: "setCollateralFactor(address,uint256,uint256)",
+      params: [LST_POOL_VWETH, COLLATERAL_FACTOR, LIQUIDATION_THRESHOLD],
+    },
+    {
+      target: LST_POOL_VWETH,
+      signature: "setInterestRateModel(address)",
+      params: [NEW_IRM],
+    },
+  ]);
+};
+
+export default vip028;
