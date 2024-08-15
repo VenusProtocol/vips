2b187d93c67e082efa523f0394a9fde18efc1119
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-08-15 18:21:42 +0200

    feat: add description for VIP 353

--- simulations/vip-352/bscmainnet.ts
+++
@@ -1,6 +0,0 @@
-import { forking, testVip } from "../../src/vip-framework";
-import vip352 from "../../vips/vip-352/bscmainnet";
-
-forking(38491593, async () => {
-  testVip("VIP-352", await vip352(), {});
-});
