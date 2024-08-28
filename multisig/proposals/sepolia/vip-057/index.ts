0c9a4e240308a0f8070b7df7a26da287ac1ea6e3
Narayan Prusty <arsenal.narayan@gmail.com>
2024-08-28 17:26:12 +0400

    fix: changed vip number for sepolia

--- multisig/proposals/sepolia/vip-057/index.ts
+++ multisig/proposals/sepolia/vip-057/index.ts
@@ -7,7 +7,7 @@ const NEW_COMPTROLLER_IMPLEMENTATION = "0xDA3Cc2AC2971FE1EA14cE13B9F67d904d4819c
 const ACM = "0xbf705C00578d43B6147ab4eaE04DBBEd1ccCdc96";
 const GUARDIAN = "0x94fa6078b6b8a26F0B6EDFFBE6501B22A10470fB";
 
-export const vip030 = () => {
+export const vip057 = () => {
   return makeProposal([
     {
       target: COMPTROLLER_BEACON,
--- multisig/simulations/sepolia/vip-057/index.ts
+++ multisig/simulations/sepolia/vip-057/index.ts
@@ -5,7 +5,7 @@ import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 import { forking, pretendExecutingVip } from "../../../../src/vip-framework";
-import { vip030 } from "../../../proposals/sepolia/vip-030";
+import { vip057 } from "../../../proposals/sepolia/vip-057";
 import COMPTROLLER_FACET_ABI from "./abis/comptroller.json";
 
 const COMPTROLLER_STABLECOIN = "0x18eF8D2bee415b731C25662568dc1035001cEB2c";
@@ -13,7 +13,7 @@ const vUSDT_POOL_STABLECOIN = "0x93dff2053D4B08823d8B39F1dCdf8497f15200f4";
 const vUSDT_USER = "0xc444949e0054A23c44Fc45789738bdF64aed2391";
 const GUARDIAN = "0x94fa6078b6b8a26F0B6EDFFBE6501B22A10470fB";
 
-forking(6056812, () => {
+forking(6056812, async () => {
   let stableCoinPoolComptroller: Contract;
 
   before(async () => {
@@ -42,7 +42,7 @@ forking(6056812, () => {
 
   describe("Post-VIP behavior", async () => {
     before(async () => {
-      await pretendExecutingVip(vip030());
+      await pretendExecutingVip(await vip057());
     });
 
     it("stablecoin pool market unlisted", async () => {
