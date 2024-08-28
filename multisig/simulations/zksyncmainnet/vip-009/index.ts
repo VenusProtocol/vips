5229fb276850c88e0cde5c42437e604e8918fe22
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-08-28 19:09:16 +0530

    feat: configure PSR on zksync

---
+++ multisig/simulations/zksyncmainnet/vip-009/index.ts
@@ -0,0 +1,63 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { initMainnetUser } from "src/utils";
+import { forking, pretendExecutingVip } from "src/vip-framework/index";
+
+import vip009, { ACM, POOL_REGISTRY, PSR } from "../../../proposals/zksyncmainnet/vip-009";
+import ACM_ABI from "./abi/accessControlManager.json";
+import PSR_ABI from "./abi/protocolShareReserve.json";
+
+const { zksyncmainnet } = NETWORK_ADDRESSES;
+
+forking(42814864, async () => {
+  let protocolShareReserve: Contract;
+  let accessControlManager: Contract;
+  let psrSigner: SignerWithAddress;
+
+  before(async () => {
+    protocolShareReserve = await ethers.getContractAt(PSR_ABI, PSR);
+    accessControlManager = await ethers.getContractAt(ACM_ABI, ACM);
+
+    psrSigner = await initMainnetUser(PSR, ethers.utils.parseEther("1"));
+    await pretendExecutingVip(await vip009());
+  });
+
+  describe("Post tx checks", () => {
+    it("PSR owner should be multisig", async () => {
+      const owner = await protocolShareReserve.owner();
+      expect(owner).equals(zksyncmainnet.GUARDIAN);
+    });
+
+    it("PSR should have correct ACM reference", async () => {
+      const acm = await protocolShareReserve.accessControlManager();
+      expect(acm).equals(ACM);
+    });
+
+    it("PSR should have correct PoolRegistry reference", async () => {
+      expect(await protocolShareReserve.poolRegistry()).to.equal(POOL_REGISTRY);
+    });
+
+    it("Verify Multisig permissions for PSR", async () => {
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(zksyncmainnet.GUARDIAN, "addOrUpdateDistributionConfigs(DistributionConfig[])"),
+      ).to.be.true;
+
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(zksyncmainnet.GUARDIAN, "removeDistributionConfig(Schema,address)"),
+      ).to.be.true;
+    });
+
+    it("Validate PSR distribution config", async () => {
+      expect(await protocolShareReserve.totalDistributions()).to.equal(2);
+      expect(await protocolShareReserve.getPercentageDistribution(zksyncmainnet.VTREASURY, 0)).to.equal(10000);
+      expect(await protocolShareReserve.getPercentageDistribution(zksyncmainnet.VTREASURY, 1)).to.equal(10000);
+    });
+  });
+});
