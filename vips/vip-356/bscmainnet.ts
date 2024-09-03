aca9e6e27f9b2851b8d788d1c3e12e66f73104cf
Narayan Prusty <arsenal.narayan@gmail.com>
2024-09-03 18:32:47 +0400

    fix: use wildcard for reward distributors

--- vips/vip-356/bscmainnet.ts
+++ vips/vip-356/bscmainnet.ts
@@ -6,20 +6,6 @@ import { makeProposal } from "src/utils";
 const { arbitrumone, ethereum } = NETWORK_ADDRESSES;
 export const ARBITRUM_ONE_ACM = "0xD9dD18EB0cf10CbA837677f28A8F9Bda4bc2b157";
 export const ETHEREUM_ACM = "0x230058da2D23eb8836EC5DB7037ef7250c56E25E";
-export const OPBNBMAINNET_ACM = "0xA60Deae5344F1152426cA440fb6552eA0e3005D6";
-export const ARBITRUM_ONE_REWARD_DISTRIBUTORS = ["0x53b488baA4052094495b6De9E5505FE1Ee3EAc7a"];
-export const ETHEREUM_REWARD_DISTRIBUTORS = [
-  "0x134bfDEa7e68733921Bc6A87159FB0d68aBc6Cf8",
-  "0x76611EEA26aF8842281B56Bb742129E77133592F",
-  "0x886767B62C7ACD601672607373048FFD96Cf27B2",
-  "0x8473B767F68250F5309bae939337136a899E43F9",
-  "0x5f65A7b60b4F91229B8484F80bc2EEc52758EAf9",
-  "0x461dE281c453F447200D67C9Dd31b3046c8f49f8",
-  "0x7A91bEd36D96E4e644d3A181c287E0fcf9E9cc98",
-  "0xe72Aa7BaB160eaa2605964D2379AA56Cb4b9A1BB",
-  "0xDCB0CfA130496c749738Acbe2d6aA06C7C320f06",
-  "0x1e25CF968f12850003Db17E0Dba32108509C4359",
-];
 
 const vip356 = () => {
   const meta = {
@@ -33,14 +19,12 @@ const vip356 = () => {
   return makeProposal(
     [
       // Revoke Permissions
-      ...ARBITRUM_ONE_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_ONE_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setRewardTokenSpeeds(address[],uint256[],uint256[])", arbitrumone.GUARDIAN],
-          dstChainId: LzChainId.arbitrumone,
-        };
-      }),
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setRewardTokenSpeeds(address[],uint256[],uint256[])", arbitrumone.GUARDIAN],
+        dstChainId: LzChainId.arbitrumone,
+      },
       {
         target: ARBITRUM_ONE_ACM,
         signature: "revokeCallPermission(address,string,address)",
@@ -51,49 +35,41 @@ const vip356 = () => {
         ],
         dstChainId: LzChainId.arbitrumone,
       },
-      ...ARBITRUM_ONE_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_ONE_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setLastRewardingBlocks(address[],uint32[],uint32[])", arbitrumone.GUARDIAN],
-          dstChainId: LzChainId.arbitrumone,
-        };
-      }),
-      ...ARBITRUM_ONE_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_ONE_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [
-            rewardDistributor,
-            "setLastRewardingBlockTimestamps(address[],uint256[],uint256[])",
-            arbitrumone.GUARDIAN,
-          ],
-          dstChainId: LzChainId.arbitrumone,
-        };
-      }),
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setLastRewardingBlocks(address[],uint32[],uint32[])", arbitrumone.GUARDIAN],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: ARBITRUM_ONE_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [
+          ethers.constants.AddressZero,
+          "setLastRewardingBlockTimestamps(address[],uint256[],uint256[])",
+          arbitrumone.GUARDIAN,
+        ],
+        dstChainId: LzChainId.arbitrumone,
+      },
 
-      ...ETHEREUM_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ETHEREUM_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setRewardTokenSpeeds(address[],uint256[],uint256[])", ethereum.GUARDIAN],
-          dstChainId: LzChainId.ethereum,
-        };
-      }),
+      {
+        target: ETHEREUM_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setRewardTokenSpeeds(address[],uint256[],uint256[])", ethereum.GUARDIAN],
+        dstChainId: LzChainId.ethereum,
+      },
       {
         target: ETHEREUM_ACM,
         signature: "revokeCallPermission(address,string,address)",
         params: [ethers.constants.AddressZero, "setLastRewardingBlock(address[],uint32[],uint32[])", ethereum.GUARDIAN],
         dstChainId: LzChainId.ethereum,
       },
-      ...ETHEREUM_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ETHEREUM_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setLastRewardingBlocks(address[],uint32[],uint32[])", ethereum.GUARDIAN],
-          dstChainId: LzChainId.ethereum,
-        };
-      }),
+      {
+        target: ETHEREUM_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setLastRewardingBlocks(address[],uint32[],uint32[])", ethereum.GUARDIAN],
+        dstChainId: LzChainId.ethereum,
+      }
     ],
     meta,
     ProposalType.REGULAR,
--- vips/vip-356/bsctestnet.ts
+++ vips/vip-356/bsctestnet.ts
@@ -6,16 +6,6 @@ import { makeProposal } from "src/utils";
 const { arbitrumsepolia, sepolia } = NETWORK_ADDRESSES;
 export const ARBITRUM_SEPOLIA_ACM = "0xa36AD96441cB931D8dFEAAaC97D3FaB4B39E590F";
 export const SEPOLIA_ACM = "0xbf705C00578d43B6147ab4eaE04DBBEd1ccCdc96";
-export const ARBITRUM_SEPOLIA_REWARD_DISTRIBUTORS = ["0x6c65135d102e2Dfa1b0852351cF9b2cbc1788972"];
-export const SEPOLIA_REWARD_DISTRIBUTORS = [
-  "0xB60666395bEFeE02a28938b75ea620c7191cA77a",
-  "0x341f52BfecC10115087e46eB94AA06E384b8925E",
-  "0x67dA6435b35d43081c7c27685fAbb2662b7f1290",
-  "0xF6D57F8e37b1cb627470b5254fAb08dE07B49A0F",
-  "0x4597B9287fE0DF3c5513D66886706E0719bD270f",
-  "0xec594364d2B7eB3678f351Ac632cC71E718E0F89",
-  "0x92e8E3C202093A495e98C10f9fcaa5Abe288F74A",
-];
 
 const vip356 = () => {
   const meta = {
@@ -29,14 +19,12 @@ const vip356 = () => {
   return makeProposal(
     [
       // Revoke Permissions
-      ...ARBITRUM_SEPOLIA_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_SEPOLIA_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setRewardTokenSpeeds(address[],uint256[],uint256[])", arbitrumsepolia.GUARDIAN],
-          dstChainId: LzChainId.arbitrumsepolia,
-        };
-      }),
+      {
+        target: ARBITRUM_SEPOLIA_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setRewardTokenSpeeds(address[],uint256[],uint256[])", arbitrumsepolia.GUARDIAN],
+        dstChainId: LzChainId.arbitrumsepolia,
+      },
       {
         target: ARBITRUM_SEPOLIA_ACM,
         signature: "revokeCallPermission(address,string,address)",
@@ -47,34 +35,27 @@ const vip356 = () => {
         ],
         dstChainId: LzChainId.arbitrumsepolia,
       },
-      ...ARBITRUM_SEPOLIA_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_SEPOLIA_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setLastRewardingBlocks(address[],uint32[],uint32[])", arbitrumsepolia.GUARDIAN],
-          dstChainId: LzChainId.arbitrumsepolia,
-        };
-      }),
-      ...ARBITRUM_SEPOLIA_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: ARBITRUM_SEPOLIA_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [
-            rewardDistributor,
-            "setLastRewardingBlockTimestamps(address[],uint256[],uint256[])",
-            arbitrumsepolia.GUARDIAN,
-          ],
-        };
-      }),
-
-      ...SEPOLIA_REWARD_DISTRIBUTORS.map(rewardDistributor => {
-        return {
-          target: SEPOLIA_ACM,
-          signature: "revokeCallPermission(address,string,address)",
-          params: [rewardDistributor, "setRewardTokenSpeeds(address[],uint256[],uint256[])", sepolia.GUARDIAN],
-          dstChainId: LzChainId.sepolia,
-        };
-      }),
+      {
+        target: ARBITRUM_SEPOLIA_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setLastRewardingBlocks(address[],uint32[],uint32[])", arbitrumsepolia.GUARDIAN],
+        dstChainId: LzChainId.arbitrumsepolia,
+      },
+      {
+        target: ARBITRUM_SEPOLIA_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [
+          ethers.constants.AddressZero,
+          "setLastRewardingBlockTimestamps(address[],uint256[],uint256[])",
+          arbitrumsepolia.GUARDIAN,
+        ],
+      },
+      {
+        target: SEPOLIA_ACM,
+        signature: "revokeCallPermission(address,string,address)",
+        params: [ethers.constants.AddressZero, "setRewardTokenSpeeds(address[],uint256[],uint256[])", sepolia.GUARDIAN],
+        dstChainId: LzChainId.sepolia,
+      },
       {
         target: SEPOLIA_ACM,
         signature: "revokeCallPermission(address,string,address)",
