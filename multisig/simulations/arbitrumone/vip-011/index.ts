75b3c7a346b3867da48a6f039b28ec20ec87f529
Narayan Prusty <arsenal.narayan@gmail.com>
2024-09-04 14:47:14 +0400

    fix: fixed var names

--- multisig/simulations/arbitrumone/vip-011/index.ts
+++ multisig/simulations/arbitrumone/vip-011/index.ts
@@ -10,7 +10,7 @@ import { vip011 } from "../../../proposals/arbitrumone/vip-011";
 import COMPTROLLER_ABI from "./abis/comptroller.json";
 
 const COMPTROLLER_CORE = "0x317c1A5739F39046E20b08ac9BeEa3f10fD43326";
-const vUSDT_POOL_STABLECOIN = "0xB9F9117d4200dC296F9AcD1e8bE1937df834a2fD";
+const vUSDT_POOL_CORE = "0xB9F9117d4200dC296F9AcD1e8bE1937df834a2fD";
 const MULTISIG = "0x14e0E151b33f9802b3e75b621c1457afc44DcAA0";
 
 forking(249598943, async () => {
@@ -19,23 +19,23 @@ forking(249598943, async () => {
   before(async () => {
     await impersonateAccount(MULTISIG);
 
-    stableCoinPoolComptroller = new ethers.Contract(
+    corePoolComptroller = new ethers.Contract(
       COMPTROLLER_CORE,
-      COMPTROLLER_FACET_ABI,
+      COMPTROLLER_ABI,
       await ethers.getSigner(MULTISIG),
     );
 
     await setBalance(MULTISIG, parseUnits("1000", 18));
 
-    await stableCoinPoolComptroller.setActionsPaused([vUSDT_POOL_STABLECOIN], [0, 1, 2, 3, 4, 5, 6, 7, 8], true);
-    await stableCoinPoolComptroller.setCollateralFactor(vUSDT_POOL_STABLECOIN, 0, 0);
-    await stableCoinPoolComptroller.setMarketBorrowCaps([vUSDT_POOL_STABLECOIN], [0]);
-    await stableCoinPoolComptroller.setMarketSupplyCaps([vUSDT_POOL_STABLECOIN], [0]);
+    await corePoolComptroller.setActionsPaused([vUSDT_POOL_CORE], [0, 1, 2, 3, 4, 5, 6, 7, 8], true);
+    await corePoolComptroller.setCollateralFactor(vUSDT_POOL_CORE, 0, 0);
+    await corePoolComptroller.setMarketBorrowCaps([vUSDT_POOL_CORE], [0]);
+    await corePoolComptroller.setMarketSupplyCaps([vUSDT_POOL_CORE], [0]);
   });
 
   describe("Pre-VIP behavior", () => {
     it("unlist reverts", async () => {
-      await expect(stableCoinPoolComptroller.unlistMarket(vUSDT_POOL_STABLECOIN)).to.be.reverted;
+      await expect(corePoolComptroller.unlistMarket(vUSDT_POOL_CORE)).to.be.reverted;
     });
   });
 
@@ -45,7 +45,7 @@ forking(249598943, async () => {
     });
 
     it("unlist successful", async () => {
-      await expect(stableCoinPoolComptroller.unlistMarket(vUSDT_POOL_STABLECOIN)).to.be.not.reverted;
+      await expect(corePoolComptroller.unlistMarket(vUSDT_POOL_CORE)).to.be.not.reverted;
     });
 
     describe("generic tests", async () => {
--- multisig/simulations/ethereum/vip-057/index.ts
+++ multisig/simulations/ethereum/vip-057/index.ts
@@ -10,7 +10,7 @@ import { vip057 } from "../../../proposals/ethereum/vip-057";
 import COMPTROLLER_FACET_ABI from "./abis/comptroller.json";
 
 const COMPTROLLER_CORE_POOL = "0x687a01ecF6d3907658f7A7c714749fAC32336D1B";
-const vUSDT_POOL_STABLECOIN = "0x8C3e3821259B82fFb32B2450A95d2dcbf161C24E";
+const vUSDT_POOL_CORE = "0x8C3e3821259B82fFb32B2450A95d2dcbf161C24E";
 const vUSDT_USER = "0x1253F0bFf813370C601D29d7312759675bBB30B6";
 const GUARDIAN = "0x285960C5B22fD66A736C7136967A3eB15e93CC67";
 
@@ -28,16 +28,16 @@ forking(20664549, async () => {
 
     await setBalance(GUARDIAN, parseUnits("1000", 18));
 
-    await corePoolComptroller.setActionsPaused([vUSDT_POOL_STABLECOIN], [0, 1, 2, 3, 4, 5, 6, 7, 8], true);
-    await corePoolComptroller.setCollateralFactor(vUSDT_POOL_STABLECOIN, 0, 0);
-    await corePoolComptroller.setMarketBorrowCaps([vUSDT_POOL_STABLECOIN], [0]);
-    await corePoolComptroller.setMarketSupplyCaps([vUSDT_POOL_STABLECOIN], [0]);
+    await corePoolComptroller.setActionsPaused([vUSDT_POOL_CORE], [0, 1, 2, 3, 4, 5, 6, 7, 8], true);
+    await corePoolComptroller.setCollateralFactor(vUSDT_POOL_CORE, 0, 0);
+    await corePoolComptroller.setMarketBorrowCaps([vUSDT_POOL_CORE], [0]);
+    await corePoolComptroller.setMarketSupplyCaps([vUSDT_POOL_CORE], [0]);
   });
 
   describe("Pre-VIP behavior", () => {
     it("core pool market not unlisted", async () => {
       const markets = await corePoolComptroller.getAssetsIn(vUSDT_USER);
-      expect(markets.includes(vUSDT_POOL_STABLECOIN)).to.be.true;
+      expect(markets.includes(vUSDT_POOL_CORE)).to.be.true;
     });
   });
 
@@ -47,10 +47,10 @@ forking(20664549, async () => {
     });
 
     it("core pool market unlisted", async () => {
-      await corePoolComptroller.unlistMarket(vUSDT_POOL_STABLECOIN);
+      await corePoolComptroller.unlistMarket(vUSDT_POOL_CORE);
 
       const markets = await corePoolComptroller.getAssetsIn(vUSDT_USER);
-      expect(markets.includes(vUSDT_POOL_STABLECOIN)).to.be.false;
+      expect(markets.includes(vUSDT_POOL_CORE)).to.be.false;
     });
 
     describe("generic tests", async () => {
