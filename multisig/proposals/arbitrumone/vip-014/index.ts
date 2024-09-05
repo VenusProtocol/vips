267790db7929bcc125b736e07ed0fe6f95616bfb
Debugger022 <debugger022@gmail.com>
2024-09-06 01:05:18 +0530

    feat: set prime token for arbitrumone pools

--- multisig/proposals/arbitrumone/vip-014/index.ts
+++ multisig/proposals/arbitrumone/vip-014/index.ts
@@ -1,5 +1,8 @@
 import { makeProposal } from "src/utils";
 
+export const PRIME = "0xFE69720424C954A2da05648a0FAC84f9bf11Ef49";
+export const COMPTROLLER_CORE = "0x317c1A5739F39046E20b08ac9BeEa3f10fD43326";
+export const COMPTROLLER_LIQUID_STAKED_ETH = "0x52bAB1aF7Ff770551BD05b9FC2329a0Bf5E23F16";
 export const NATIVE_TOKEN_GATEWAY_LIQUID_STAKED_ETH_POOL = "0xD1e89806BAB8Cd7680DFc7425D1fA6d7D5F0C3FE";
 
 const vip014 = () => {
@@ -8,6 +11,16 @@ const vip014 = () => {
       target: NATIVE_TOKEN_GATEWAY_LIQUID_STAKED_ETH_POOL,
       signature: "acceptOwnership()",
       params: [],
+    },
+    {
+      target: COMPTROLLER_CORE,
+      signature: "setPrimeToken(address)",
+      params: [PRIME],
+    },
+    {
+      target: COMPTROLLER_LIQUID_STAKED_ETH,
+      signature: "setPrimeToken(address)",
+      params: [PRIME],
     },
   ]);
 };
