433ff87208e22e93acc603180db00b900e44bdc8
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-09-05 17:25:19 +0200

    feat: consider recommendation from Chaos labs for the borrow cap of FLOKI
    See: https://community.venus.io/t/chaos-labs-risk-parameter-update-08-29-24/4543

--- simulations/vip-360/bscmainnet.ts
+++ simulations/vip-360/bscmainnet.ts
@@ -5,15 +5,27 @@ import { ethers } from "hardhat";
 import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
-import vip360, { UNITROLLER, vDOGE, vDOGE_SUPPLY_CAP, vUNI, vUNI_SUPPLY_CAP } from "../../vips/vip-360/bscmainnet";
-import COMPTROLLER_ABI from "./abi/comptroller.json";
+import vip360, {
+  GAMEFI_COMPTROLLER,
+  UNITROLLER,
+  vDOGE,
+  vDOGE_SUPPLY_CAP,
+  vFLOKI,
+  vFLOKI_BORROW_CAP,
+  vUNI,
+  vUNI_SUPPLY_CAP,
+} from "../../vips/vip-360/bscmainnet";
+import CORE_COMPTROLLER_ABI from "./abi/comptroller.json";
+import IL_COMPTROLLER_ABI from "./abi/comptroller.json";
 
 forking(41983388, async () => {
   const provider = ethers.provider;
   let comptrollerContract: Contract;
+  let gameFiComptrollerContract: Contract;
 
   before(async () => {
-    comptrollerContract = new ethers.Contract(UNITROLLER, COMPTROLLER_ABI, provider);
+    comptrollerContract = new ethers.Contract(UNITROLLER, CORE_COMPTROLLER_ABI, provider);
+    gameFiComptrollerContract = new ethers.Contract(GAMEFI_COMPTROLLER, IL_COMPTROLLER_ABI, provider);
   });
 
   describe("Pre-VIP behavior", async () => {
@@ -23,22 +35,29 @@ forking(41983388, async () => {
 
       const DOGE_supplyCap = await comptrollerContract.supplyCaps(vDOGE);
       expect(DOGE_supplyCap).to.be.equal(parseUnits("80000000", 8));
+
+      const FLOKI_borrowCap = await gameFiComptrollerContract.borrowCaps(vFLOKI);
+      expect(FLOKI_borrowCap).to.be.equal(parseUnits("2000000000", 9));
     });
   });
 
   testVip("VIP-360", await vip360(), {
     callbackAfterExecution: async txResponse => {
-      await expectEvents(txResponse, [COMPTROLLER_ABI], ["NewSupplyCap"], [2]);
+      await expectEvents(txResponse, [CORE_COMPTROLLER_ABI], ["NewSupplyCap"], [2]);
+      await expectEvents(txResponse, [IL_COMPTROLLER_ABI], ["NewBorrowCap"], [1]);
     },
   });
 
   describe("Post-VIP behavior", async () => {
-    it("check supply cap", async () => {
+    it("check new caps", async () => {
       const UNI_supplyCap = await comptrollerContract.supplyCaps(vUNI);
       expect(UNI_supplyCap).to.be.equal(vUNI_SUPPLY_CAP);
 
       const DOGE_supplyCap = await comptrollerContract.supplyCaps(vDOGE);
       expect(DOGE_supplyCap).to.be.equal(vDOGE_SUPPLY_CAP);
+
+      const FLOKI_borrowCap = await gameFiComptrollerContract.borrowCaps(vFLOKI);
+      expect(FLOKI_borrowCap).to.be.equal(vFLOKI_BORROW_CAP);
     });
   });
 });
--- vips/vip-360/bscmainnet.ts
+++ vips/vip-360/bscmainnet.ts
@@ -4,9 +4,12 @@ import { makeProposal } from "src/utils";
 
 export const vUNI = "0x27FF564707786720C71A2e5c1490A63266683612";
 export const vDOGE = "0xec3422Ef92B2fb59e84c8B02Ba73F1fE84Ed8D71";
+export const vFLOKI = "0xc353B7a1E13dDba393B5E120D4169Da7185aA2cb";
 export const UNITROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+export const GAMEFI_COMPTROLLER = "0x1b43ea8622e76627B81665B1eCeBB4867566B963";
 export const vUNI_SUPPLY_CAP = parseUnits("900000", 18);
 export const vDOGE_SUPPLY_CAP = parseUnits("120000000", 8);
+export const vFLOKI_BORROW_CAP = parseUnits("4000000000", 9);
 
 const vip318 = () => {
   const meta = {
@@ -28,9 +31,14 @@ const vip318 = () => {
           [vUNI_SUPPLY_CAP, vDOGE_SUPPLY_CAP],
         ],
       },
+      {
+        target: GAMEFI_COMPTROLLER,
+        signature: "setMarketBorrowCaps(address[],uint256[])",
+        params: [[vFLOKI], [vFLOKI_BORROW_CAP]],
+      },
     ],
     meta,
-    ProposalType.REGULAR,
+    ProposalType.FAST_TRACK,
   );
 };
 
