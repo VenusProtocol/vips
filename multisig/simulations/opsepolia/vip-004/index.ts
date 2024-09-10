ef0fa556801111e4dd494752c1b7715d1bfdb531
Debugger022 <debugger022@gmail.com>
2024-09-10 20:19:06 +0530

    feat: add core pool vip for opsepolia

---
+++ multisig/simulations/opsepolia/vip-004/index.ts
@@ -0,0 +1,411 @@
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { BigNumberish, Signer } from "ethers";
+import { BigNumber, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, pretendExecutingVip } from "src/vip-framework";
+import { checkVToken } from "src/vip-framework/checks/checkVToken";
+import { checkInterestRate } from "src/vip-framework/checks/interestRateModel";
+
+import vip001 from "../../../proposals/opsepolia/vip-001";
+import vip004, {
+  COMPTROLLER_CORE,
+  MOCK_OP,
+  MOCK_USDC,
+  MOCK_USDT,
+  MOCK_WBTC,
+  PSR,
+  VOP_CORE,
+  VUSDC_CORE,
+  VUSDT_CORE,
+  VWBTC_CORE,
+  VWETH_CORE,
+  WETH,
+} from "../../../proposals/opsepolia/vip-004";
+import TOKEN_ABI from "./abi/WETH.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import ERC20_ABI from "./abi/erc20.json";
+import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
+import VTOKEN_ABI from "./abi/vToken.json";
+
+const { opsepolia } = NETWORK_ADDRESSES;
+
+const RESILIENT_ORACLE = opsepolia.RESILIENT_ORACLE;
+const GUARDIAN = opsepolia.GUARDIAN;
+const POOL_REGISTRY = opsepolia.POOL_REGISTRY;
+
+const BLOCKS_PER_YEAR = BigNumber.from("31536000"); // equal to seconds in a year as it is timebased deployment
+
+type VTokenSymbol = "vWBTC_Core" | "vWETH_Core" | "vUSDT_Core" | "vUSDC_Core" | "vOP_Core";
+
+const vTokens: { [key in VTokenSymbol]: string } = {
+  vWBTC_Core: VWBTC_CORE,
+  vWETH_Core: VWETH_CORE,
+  vUSDT_Core: VUSDT_CORE,
+  vUSDC_Core: VUSDC_CORE,
+  vOP_Core: VOP_CORE,
+};
+
+const tokens = {
+  WBTC: MOCK_WBTC,
+  WETH: WETH,
+  USDT: MOCK_USDT,
+  USDC: MOCK_USDC,
+  OP: MOCK_OP,
+};
+
+interface VTokenState {
+  name: string;
+  symbol: string;
+  decimals: number;
+  underlying: string;
+  exchangeRate: BigNumberish;
+  comptroller: string;
+}
+
+const vTokenState: { [key in VTokenSymbol]: VTokenState } = {
+  // Core Pool
+  vWBTC_Core: {
+    name: "Venus WBTC (Core)",
+    symbol: "vWBTC_Core",
+    decimals: 8,
+    underlying: tokens.WBTC,
+    exchangeRate: parseUnits("1", 18),
+    comptroller: COMPTROLLER_CORE,
+  },
+  vWETH_Core: {
+    name: "Venus WETH (Core)",
+    symbol: "vWETH_Core",
+    decimals: 8,
+    underlying: tokens.WETH,
+    exchangeRate: parseUnits("1", 28),
+    comptroller: COMPTROLLER_CORE,
+  },
+  vUSDT_Core: {
+    name: "Venus USDT (Core)",
+    symbol: "vUSDT_Core",
+    decimals: 8,
+    underlying: tokens.USDT,
+    exchangeRate: parseUnits("1", 16),
+    comptroller: COMPTROLLER_CORE,
+  },
+  vUSDC_Core: {
+    name: "Venus USDC (Core)",
+    symbol: "vUSDC_Core",
+    decimals: 8,
+    underlying: tokens.USDC,
+    exchangeRate: parseUnits("1", 16),
+    comptroller: COMPTROLLER_CORE,
+  },
+  vOP_Core: {
+    name: "Venus OP (Core)",
+    symbol: "vOP_Core",
+    decimals: 8,
+    underlying: tokens.OP,
+    exchangeRate: parseUnits("1", 28),
+    comptroller: COMPTROLLER_CORE,
+  },
+};
+
+interface RiskParameters {
+  borrowCap: string;
+  supplyCap: string;
+  collateralFactor: string;
+  liquidationThreshold: string;
+  reserveFactor: string;
+  initialSupply: string;
+  vTokenReceiver: string;
+}
+
+const riskParameters: { [key in VTokenSymbol]: RiskParameters } = {
+  // Core Pool
+  vWBTC_Core: {
+    borrowCap: "0.55",
+    supplyCap: "1",
+    collateralFactor: "0.70",
+    liquidationThreshold: "0.75",
+    reserveFactor: "0.2",
+    initialSupply: "0.03553143",
+    vTokenReceiver: opsepolia.VTREASURY,
+  },
+  vWETH_Core: {
+    borrowCap: "16",
+    supplyCap: "25",
+    collateralFactor: "0.7",
+    liquidationThreshold: "0.75",
+    reserveFactor: "0.2",
+    initialSupply: "0.61097887",
+    vTokenReceiver: opsepolia.VTREASURY,
+  },
+  vUSDC_Core: {
+    borrowCap: "130000",
+    supplyCap: "150000",
+    collateralFactor: "0.75",
+    liquidationThreshold: "0.77",
+    reserveFactor: "0.1",
+    initialSupply: "1800",
+    vTokenReceiver: opsepolia.VTREASURY,
+  },
+  vUSDT_Core: {
+    borrowCap: "130000",
+    supplyCap: "150000",
+    collateralFactor: "0.75",
+    liquidationThreshold: "0.77",
+    reserveFactor: "0.1",
+    initialSupply: "1800",
+    vTokenReceiver: opsepolia.VTREASURY,
+  },
+  vOP_Core: {
+    borrowCap: "16",
+    supplyCap: "25",
+    collateralFactor: "0.7",
+    liquidationThreshold: "0.75",
+    reserveFactor: "0.2",
+    initialSupply: "0.61097887",
+    vTokenReceiver: opsepolia.VTREASURY,
+  },
+};
+
+interface InterestRateModelSpec {
+  vTokens: VTokenSymbol[];
+  kink: string;
+  base: string;
+  multiplier: string;
+  jump: string;
+}
+
+const interestRateModels: InterestRateModelSpec[] = [
+  {
+    vTokens: ["vWBTC_Core", "vWETH_Core", "vOP_Core"],
+    kink: "0.45",
+    base: "0",
+    multiplier: "0.09",
+    jump: "3",
+  },
+  {
+    vTokens: ["vUSDC_Core", "vUSDT_Core"],
+    kink: "0.8",
+    base: "0",
+    multiplier: "0.075",
+    jump: "2.5",
+  },
+];
+
+const interestRateModelAddresses: { [key in VTokenSymbol]: string } = {
+  vWBTC_Core: "",
+  vWETH_Core: "",
+  vUSDT_Core: "",
+  vUSDC_Core: "",
+  vOP_Core: "",
+};
+
+forking(17082725, async () => {
+  let poolRegistry: Contract;
+
+  before(async () => {
+    poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, POOL_REGISTRY);
+  });
+
+  describe("Contracts setup", () => {
+    for (const [symbol, address] of Object.entries(vTokens) as [VTokenSymbol, string][]) {
+      checkVToken(address, vTokenState[symbol]);
+    }
+  });
+
+  describe("Post-Execution state", () => {
+    before(async () => {
+      const WETH_ACCOUNT = "0xff44346743550D9c973aaC5baA2ac819d75D0c18";
+
+      await impersonateAccount(WETH_ACCOUNT);
+      await setBalance(WETH_ACCOUNT, ethers.utils.parseEther("1"));
+
+      const WETHSigner: Signer = await ethers.getSigner(WETH_ACCOUNT);
+      const mockWSTToken = await ethers.getContractAt(TOKEN_ABI, WETH, WETHSigner);
+
+      await mockWSTToken.connect(WETHSigner).transfer(opsepolia.VTREASURY, ethers.utils.parseEther("1"));
+
+      // executing vip001 before vip004 to set the oracles for the mock tokens
+      await pretendExecutingVip(await vip001());
+      await pretendExecutingVip(await vip004());
+
+      for (const model of interestRateModels) {
+        for (const symbol of model.vTokens) {
+          const vToken = await ethers.getContractAt(VTOKEN_ABI, vTokens[symbol]);
+          interestRateModelAddresses[symbol] = await vToken.interestRateModel();
+        }
+      }
+    });
+
+    describe("PoolRegistry state", () => {
+      let registeredPools: { name: string; creator: string; comptroller: string }[];
+
+      before(async () => {
+        registeredPools = await poolRegistry.getAllPools();
+      });
+
+      it("should have 1 pool(Core Pool)", async () => {
+        expect(registeredPools).to.have.lengthOf(1);
+      });
+
+      it("should register Core pool in PoolRegistry", async () => {
+        const pool = registeredPools[0];
+        expect(pool.name).to.equal("Core");
+        expect(pool.creator).to.equal(GUARDIAN);
+        expect(pool.comptroller).to.equal(COMPTROLLER_CORE);
+      });
+
+      it("should register Core pool vTokens in Core pool Comptroller", async () => {
+        const comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER_CORE);
+        const poolVTokens = await comptroller.getAllMarkets();
+        expect(poolVTokens).to.have.lengthOf(5);
+        expect(poolVTokens).to.include(vTokens.vWBTC_Core);
+        expect(poolVTokens).to.include(vTokens.vWETH_Core);
+        expect(poolVTokens).to.include(vTokens.vUSDT_Core);
+        expect(poolVTokens).to.include(vTokens.vUSDC_Core);
+        expect(poolVTokens).to.include(vTokens.vOP_Core);
+      });
+
+      for (const [symbol, { underlying }] of Object.entries(vTokenState) as [VTokenSymbol, VTokenState][]) {
+        it(`should register ${symbol} in PoolRegistry`, async () => {
+          const registeredVToken = await poolRegistry.getVTokenForAsset(vTokenState[symbol].comptroller, underlying);
+          expect(registeredVToken).to.equal(vTokens[symbol]);
+        });
+      }
+    });
+
+    describe("Ownership", () => {
+      for (const [symbol, address] of Object.entries(vTokens) as [VTokenSymbol, string][]) {
+        it(`should transfer ownership of ${symbol} to GUARDIAN`, async () => {
+          const vToken = await ethers.getContractAt(VTOKEN_ABI, address);
+          expect(await vToken.owner()).to.equal(GUARDIAN);
+        });
+      }
+    });
+
+    describe("ProtocolShareReserve", () => {
+      for (const [symbol, address] of Object.entries(vTokens) as [VTokenSymbol, string][]) {
+        it(`should set PSR for ${symbol}`, async () => {
+          const vToken = await ethers.getContractAt(VTOKEN_ABI, address);
+          expect(await vToken.protocolShareReserve()).to.equal(PSR);
+        });
+      }
+    });
+
+    describe("Initial supply", () => {
+      for (const [symbol, params] of Object.entries(riskParameters) as [VTokenSymbol, RiskParameters][]) {
+        it(`should mint initial supply of ${symbol} to ${params.vTokenReceiver}`, async () => {
+          // Since we're distributing 1:1, decimals should be accounted for in the exchange rate
+          const expectedSupply = parseUnits(params.initialSupply, 8);
+          const vToken = await ethers.getContractAt(VTOKEN_ABI, vTokens[symbol]);
+          expect(await vToken.balanceOf(params.vTokenReceiver)).to.equal(expectedSupply);
+        });
+      }
+    });
+
+    describe("Risk parameters", () => {
+      for (const [symbol, params] of Object.entries(riskParameters) as [VTokenSymbol, RiskParameters][]) {
+        describe(`${symbol} risk parameters`, () => {
+          let vToken: Contract;
+          let comptroller: Contract;
+          let underlyingDecimals: number;
+
+          before(async () => {
+            vToken = await ethers.getContractAt(VTOKEN_ABI, vTokens[symbol]);
+            comptroller = await ethers.getContractAt(COMPTROLLER_ABI, vTokenState[symbol].comptroller);
+            const underlyingAddress = vTokenState[symbol].underlying;
+            const underlying = await ethers.getContractAt(ERC20_ABI, underlyingAddress);
+            underlyingDecimals = await underlying.decimals();
+          });
+
+          it(`should set ${symbol} reserve factor to ${params.reserveFactor}`, async () => {
+            expect(await vToken.reserveFactorMantissa()).to.equal(parseUnits(params.reserveFactor, 18));
+          });
+
+          it(`should set ${symbol} collateral factor to ${params.collateralFactor}`, async () => {
+            const market = await comptroller.markets(vTokens[symbol]);
+            expect(market.collateralFactorMantissa).to.equal(parseUnits(params.collateralFactor, 18));
+          });
+
+          it(`should set ${symbol} liquidation threshold to ${params.liquidationThreshold}`, async () => {
+            const market = await comptroller.markets(vTokens[symbol]);
+            expect(market.liquidationThresholdMantissa).to.equal(parseUnits(params.liquidationThreshold, 18));
+          });
+
+          it(`should set ${symbol} protocol seize share to 0.05`, async () => {
+            expect(await vToken.protocolSeizeShareMantissa()).to.equal(parseUnits("0.05", 18));
+          });
+
+          it(`should set ${symbol} supply cap to ${params.supplyCap}`, async () => {
+            expect(await comptroller.supplyCaps(vTokens[symbol])).to.equal(
+              parseUnits(params.supplyCap, underlyingDecimals),
+            );
+          });
+
+          it(`should set ${symbol} borrow cap to ${params.borrowCap}`, async () => {
+            expect(await comptroller.borrowCaps(vTokens[symbol])).to.equal(
+              parseUnits(params.borrowCap, underlyingDecimals),
+            );
+          });
+        });
+      }
+    });
+
+    describe("Pools configuration", () => {
+      const checkComptroller = (comptrollerAddress: string, comptrollerName: string) => {
+        describe(`${comptrollerName} Comptroller`, () => {
+          let comptroller: Contract;
+
+          before(async () => {
+            comptroller = await ethers.getContractAt(COMPTROLLER_ABI, comptrollerAddress);
+          });
+
+          it("should use the correct comptroller address", async () => {
+            expect(comptroller.address).to.equal(comptrollerAddress);
+          });
+
+          it("should have the correct price oracle", async () => {
+            expect(await comptroller.oracle()).to.equal(RESILIENT_ORACLE);
+          });
+
+          it("should have close factor = 0.5", async () => {
+            expect(await comptroller.closeFactorMantissa()).to.equal(parseUnits("0.5", 18));
+          });
+
+          it("should have liquidation incentive = 1.1", async () => {
+            expect(await comptroller.liquidationIncentiveMantissa()).to.equal(parseUnits("1.1", 18));
+          });
+
+          it("should have minLiquidatableCollateral = $100", async () => {
+            expect(await comptroller.minLiquidatableCollateral()).to.equal(parseUnits("100", 18));
+          });
+
+          it("should have owner = GUARDIAN", async () => {
+            expect(await comptroller.owner()).to.equal(GUARDIAN);
+          });
+        });
+      };
+
+      checkComptroller(COMPTROLLER_CORE, "Core");
+    });
+
+    it("Interest rates", async () => {
+      for (const model of interestRateModels) {
+        for (const symbol of model.vTokens) {
+          checkInterestRate(
+            interestRateModelAddresses[symbol],
+            symbol,
+            {
+              base: model.base,
+              multiplier: model.multiplier,
+              jump: model.jump,
+              kink: model.kink,
+            },
+            BLOCKS_PER_YEAR,
+          );
+        }
+      }
+    });
+  });
+});
