770a9becb5c2bf8ac382d27d9248ff927eedc988
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-09-19 17:57:20 +0530

    feat: add vip for rewards on zksync mainnet

---
+++ multisig/simulations/zksyncmainnet/vip-010/index.ts
@@ -0,0 +1,135 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { initMainnetUser } from "src/utils";
+import { forking, pretendExecutingVip } from "src/vip-framework";
+import {
+  RewardsDistributorConfig,
+  checkRewardsDistributor,
+  checkRewardsDistributorPool,
+} from "src/vip-framework/checks/rewardsDistributor";
+
+import vip010, {
+  COMPTROLLER_CORE,
+  RD_XVS_REWARD_AMOUNT,
+  REWARD_DISTRIBUTOR_CORE_0,
+  REWARD_TOKEN_SPEED,
+  STORE_XVS_REWARD_AMOUNT,
+  VUSDC_E_CORE,
+  VUSDT_CORE,
+  VWBTC_CORE,
+  VWETH_CORE,
+  VZK_CORE,
+  XVS,
+  XVS_STORE,
+  XVS_VAULT_PROXY,
+} from "../../../proposals/zksyncmainnet/vip-010";
+import XVS_VAULT_ABI from "./abi/XVSVaultProxy.json";
+import XVS_ABI from "./abi/xvs.json";
+
+const { zksyncmainnet } = NETWORK_ADDRESSES;
+
+const TREASURY_AMOUNT = parseUnits("31500", 18);
+const BRIDGE = "0x16a62B534e09A7534CD5847CFE5Bf6a4b0c1B116";
+
+forking(44579033, async () => {
+  const provider = ethers.provider;
+  let xvsVault: Contract;
+  let xvs: Contract;
+  let treasuryBalanceBefore: BigNumber;
+  let bridgeSigner: SignerWithAddress;
+
+  describe("Pre-VIP behaviour", async () => {
+    before(async () => {
+      bridgeSigner = await initMainnetUser(BRIDGE, parseUnits("1", 18));
+
+      xvsVault = new ethers.Contract(XVS_VAULT_PROXY, XVS_VAULT_ABI, provider);
+      xvs = new ethers.Contract(XVS, XVS_ABI, provider);
+
+      await xvs.connect(bridgeSigner).mint(zksyncmainnet.VTREASURY, TREASURY_AMOUNT);
+    });
+
+    it("vTreasury should hold atleast 31500 XVS", async () => {
+      expect(await xvs.balanceOf(zksyncmainnet.VTREASURY)).to.be.gte(TREASURY_AMOUNT);
+    });
+  });
+
+  describe("Post-VIP behavior", async () => {
+    before(async () => {
+      xvsVault = new ethers.Contract(XVS_VAULT_PROXY, XVS_VAULT_ABI, provider);
+      xvs = new ethers.Contract(XVS, XVS_ABI, provider);
+      treasuryBalanceBefore = await xvs.balanceOf(zksyncmainnet.VTREASURY);
+      await pretendExecutingVip(await vip010());
+    });
+
+    it("vTreasury balance should be updated", async () => {
+      expect(await xvs.balanceOf(zksyncmainnet.VTREASURY)).to.be.equal(treasuryBalanceBefore.sub(TREASURY_AMOUNT));
+    });
+
+    it("rewards distributor should have expected number of xvs tokens", async () => {
+      expect(await xvs.balanceOf(REWARD_DISTRIBUTOR_CORE_0)).to.be.equal(RD_XVS_REWARD_AMOUNT);
+    });
+
+    it("should have correct reward speed", async () => {
+      expect(await xvsVault.rewardTokenAmountsPerBlockOrSecond(XVS)).to.be.equal(REWARD_TOKEN_SPEED);
+    });
+
+    it("xvs store should have 6000 xvs", async () => {
+      expect(await xvs.balanceOf(XVS_STORE)).to.be.equal(STORE_XVS_REWARD_AMOUNT);
+    });
+
+    const rewardBasicConfig = {
+      pool: COMPTROLLER_CORE,
+      address: REWARD_DISTRIBUTOR_CORE_0,
+      token: XVS,
+    };
+    describe("Generic checks", async () => {
+      checkRewardsDistributorPool(COMPTROLLER_CORE, 1);
+
+      const tokensRewardConfig: RewardsDistributorConfig[] = [
+        {
+          ...rewardBasicConfig,
+          vToken: VUSDT_CORE,
+          supplySpeed: "277777777777777",
+          borrowSpeed: "69444444444444",
+          totalRewardsToDistribute: parseUnits("3600", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VUSDC_E_CORE,
+          supplySpeed: "555555555555555",
+          borrowSpeed: "138888888888888",
+          totalRewardsToDistribute: parseUnits("7200", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VWBTC_CORE,
+          supplySpeed: "370370370370370",
+          borrowSpeed: "92592592592592",
+          totalRewardsToDistribute: parseUnits("4800", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VWETH_CORE,
+          supplySpeed: "370370370370370",
+          borrowSpeed: "92592592592592",
+          totalRewardsToDistribute: parseUnits("4800", 18),
+        },
+        {
+          ...rewardBasicConfig,
+          vToken: VZK_CORE,
+          supplySpeed: "277777777777777",
+          borrowSpeed: "69444444444444",
+          totalRewardsToDistribute: parseUnits("3600", 18),
+        },
+      ];
+
+      for (const config of tokensRewardConfig) {
+        checkRewardsDistributor("RewardsDistributor_Core_0_XVS", config);
+      }
+    });
+  });
+});
---
+++ simulations/vip-368/bscmainnet.ts
@@ -0,0 +1,59 @@
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import { COMMUNITY_WALLET, ETH, XVS, XVS_AMOUNT, XVS_BRIDGE_SRC, vip368 } from "../../vips/vip-368/bscmainnet";
+import COMPTROLLER_ABI from "./abi/Comptroller.json";
+import ERC20_ABI from "./abi/ERC20.json";
+import VTREASURY_ABI from "./abi/VTreasury.json";
+import XVS_BRIDGE_ABI from "./abi/XVSProxyOFTSrc.json";
+
+forking(42389720, async () => {
+  let xvsBridge: Contract;
+  let xvs: Contract;
+  let oldCirculatingSupply: BigNumber;
+  let oldXVSBal: BigNumber;
+  let oldETHBalance: BigNumber;
+  let eth: Contract;
+
+  before(async () => {
+    eth = new ethers.Contract(ETH, ERC20_ABI, ethers.provider);
+    oldETHBalance = await eth.balanceOf(COMMUNITY_WALLET);
+
+    xvsBridge = new ethers.Contract(XVS_BRIDGE_SRC, XVS_BRIDGE_ABI, ethers.provider);
+    xvs = new ethers.Contract(XVS, ERC20_ABI, ethers.provider);
+    oldCirculatingSupply = await xvsBridge.circulatingSupply();
+    oldXVSBal = await xvs.balanceOf(XVS_BRIDGE_SRC);
+  });
+
+  testVip("VIP-368 Send XVS to Dest Chain(zksync mainnet)", await vip368(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(
+        txResponse,
+        [COMPTROLLER_ABI, XVS_BRIDGE_ABI, VTREASURY_ABI],
+        ["VenusGranted", "SendToChain", "WithdrawTreasuryBEP20"],
+        [1, 1, 1],
+      );
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("Should decrease circulating supply", async () => {
+      const currCirculatingSupply = await xvsBridge.circulatingSupply();
+      expect(oldCirculatingSupply.sub(currCirculatingSupply)).equals(XVS_AMOUNT);
+    });
+
+    it("Should increase number of locked tokens on bridge", async () => {
+      const currXVSBal = await xvs.balanceOf(XVS_BRIDGE_SRC);
+      expect(currXVSBal.sub(oldXVSBal)).equals(XVS_AMOUNT);
+    });
+
+    it("Verify that the community wallet has received the correct amount of ETH", async () => {
+      const balance = await eth.balanceOf(COMMUNITY_WALLET);
+      expect(balance).to.eq(oldETHBalance.add(parseUnits("5", 18)));
+    });
+  });
+});
