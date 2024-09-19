a7a30871ffafeececfa834f9a62bee4c785770f6
Debugger022 <debugger022@gmail.com>
2024-09-19 18:58:25 +0530

    feat: xvs bridge support for zksync to op mainnet

---
+++ multisig/proposals/zksyncmainnet/vip-010/index.ts
@@ -0,0 +1,49 @@
+import { parseUnits } from "ethers/lib/utils";
+import { LzChainId } from "src/types";
+import { makeProposal } from "src/utils";
+
+const XVS_BRIDGE_ADMIN = "0x2471043F05Cc41A6051dd6714DC967C7BfC8F902";
+
+export const MIN_DST_GAS = "300000";
+export const SINGLE_SEND_LIMIT = parseUnits("20000", 18);
+export const MAX_DAILY_SEND_LIMIT = parseUnits("100000", 18);
+export const SINGLE_RECEIVE_LIMIT = parseUnits("20400", 18);
+export const MAX_DAILY_RECEIVE_LIMIT = parseUnits("102000", 18);
+export const OP_MAINNET_TRUSTED_REMOTE = "0xbbe46baec851355c3fc4856914c47eb6cea0b8b4";
+
+const vip010 = () => {
+  return makeProposal([
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.opmainnet, OP_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setMinDstGas(uint16,uint16,uint256)",
+      params: [LzChainId.opmainnet, 0, MIN_DST_GAS],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setMaxDailyLimit(uint16,uint256)",
+      params: [LzChainId.opmainnet, MAX_DAILY_SEND_LIMIT],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setMaxSingleTransactionLimit(uint16,uint256)",
+      params: [LzChainId.opmainnet, SINGLE_SEND_LIMIT],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setMaxDailyReceiveLimit(uint16,uint256)",
+      params: [LzChainId.opmainnet, MAX_DAILY_RECEIVE_LIMIT],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN,
+      signature: "setMaxSingleReceiveTransactionLimit(uint16,uint256)",
+      params: [LzChainId.opmainnet, SINGLE_RECEIVE_LIMIT],
+    },
+  ]);
+};
+
+export default vip010;
