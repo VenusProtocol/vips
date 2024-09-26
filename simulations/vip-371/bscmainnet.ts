c1309e4693bbc47d7ea71512c0472edc702c6f90
Jesus Lanchas <jesus.lanchas@daureos.com>
2024-09-26 11:48:56 +0200

    feat: reduce MATIC caps, following Chaos labs recommendations

--- simulations/vip-371/bscmainnet.ts
+++ simulations/vip-371/bscmainnet.ts
@@ -7,9 +7,13 @@ import { forking, testVip } from "src/vip-framework";
 import {
   Actions,
   COLLATERAL_FACTOR,
+  CORE_COMPTROLLER,
   LIQUIDATION_THRESHOLD,
   LST_BNB_COMPTROLLER,
   RESERVE_FACTOR,
+  vMATIC,
+  vMATIC_BORROW_CAP,
+  vMATIC_SUPPLY_CAP,
   vip371,
   vstkBNB,
 } from "../../vips/vip-371/bscmainnet";
@@ -18,37 +22,46 @@ import VTOKEN_ABI from "./abi/VToken.json";
 
 forking(42389720, async () => {
   const provider = ethers.provider;
-  const comptroller = new ethers.Contract(LST_BNB_COMPTROLLER, COMPTROLLER_ABI, provider);
-  const vToken = new ethers.Contract(vstkBNB, VTOKEN_ABI, provider);
+  const lstBNBComptroller = new ethers.Contract(LST_BNB_COMPTROLLER, COMPTROLLER_ABI, provider);
+  const coreComptroller = new ethers.Contract(CORE_COMPTROLLER, COMPTROLLER_ABI, provider);
+  const vstkBNBContract = new ethers.Contract(vstkBNB, VTOKEN_ABI, provider);
 
   describe("Pre-VIP behavior", async () => {
-    it("check supply and borrow cap", async () => {
-      const supplyCap = await comptroller.supplyCaps(vstkBNB);
-      const borrowCap = await comptroller.borrowCaps(vstkBNB);
+    it("check supply and borrow cap for stkBNB", async () => {
+      const supplyCap = await lstBNBComptroller.supplyCaps(vstkBNB);
+      const borrowCap = await lstBNBComptroller.borrowCaps(vstkBNB);
 
       expect(supplyCap).to.eq(parseUnits("50", 18));
       expect(borrowCap).to.eq(0);
     });
 
     it("check reserve factor", async () => {
-      const reserveFactor = await vToken.reserveFactorMantissa();
+      const reserveFactor = await vstkBNBContract.reserveFactorMantissa();
       expect(reserveFactor).to.eq(parseUnits("0.25", 18));
     });
 
     it("check liquidation threshold", async () => {
-      const liquidationThreshold = (await comptroller.markets(vstkBNB)).liquidationThresholdMantissa;
+      const liquidationThreshold = (await lstBNBComptroller.markets(vstkBNB)).liquidationThresholdMantissa;
       expect(liquidationThreshold).to.eq(LIQUIDATION_THRESHOLD);
     });
 
     it("check collateral factor", async () => {
-      const collateralFactor = (await comptroller.markets(vstkBNB)).collateralFactorMantissa;
+      const collateralFactor = (await lstBNBComptroller.markets(vstkBNB)).collateralFactorMantissa;
       expect(collateralFactor).to.eq(parseUnits("0.9", 18));
     });
 
     it("actions unpaused", async () => {
-      expect(await comptroller.actionPaused(vstkBNB, Actions.BORROW)).to.be.false;
-      expect(await comptroller.actionPaused(vstkBNB, Actions.MINT)).to.be.false;
-      expect(await comptroller.actionPaused(vstkBNB, Actions.ENTER_MARKET)).to.be.false;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.BORROW)).to.be.false;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.MINT)).to.be.false;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.ENTER_MARKET)).to.be.false;
+    });
+
+    it("check supply and borrow cap for MATIC", async () => {
+      const supplyCap = await coreComptroller.supplyCaps(vMATIC);
+      const borrowCap = await coreComptroller.borrowCaps(vMATIC);
+
+      expect(supplyCap).to.eq(parseUnits("10000000", 18));
+      expect(borrowCap).to.eq(parseUnits("1000000", 18));
     });
   });
 
@@ -58,7 +71,7 @@ forking(42389720, async () => {
         txResponse,
         [COMPTROLLER_ABI],
         ["ActionPausedMarket", "NewCollateralFactor", "NewBorrowCap", "NewSupplyCap"],
-        [3, 1, 1, 1],
+        [3, 1, 2, 2],
       );
 
       await expectEvents(txResponse, [VTOKEN_ABI], ["NewReserveFactor"], [1]);
@@ -66,33 +79,41 @@ forking(42389720, async () => {
   });
 
   describe("Post-VIP behavior", async () => {
-    it("check supply and borrow cap", async () => {
-      const supplyCap = await comptroller.supplyCaps(vstkBNB);
-      const borrowCap = await comptroller.borrowCaps(vstkBNB);
+    it("check supply and borrow cap for stkBNB", async () => {
+      const supplyCap = await lstBNBComptroller.supplyCaps(vstkBNB);
+      const borrowCap = await lstBNBComptroller.borrowCaps(vstkBNB);
 
       expect(supplyCap).to.eq(0);
       expect(borrowCap).to.eq(0);
     });
 
     it("check reserve factor", async () => {
-      const reserveFactor = await vToken.reserveFactorMantissa();
+      const reserveFactor = await vstkBNBContract.reserveFactorMantissa();
       expect(reserveFactor).to.eq(RESERVE_FACTOR);
     });
 
     it("check liquidation threshold", async () => {
-      const liquidationThreshold = (await comptroller.markets(vstkBNB)).liquidationThresholdMantissa;
+      const liquidationThreshold = (await lstBNBComptroller.markets(vstkBNB)).liquidationThresholdMantissa;
       expect(liquidationThreshold).to.eq(LIQUIDATION_THRESHOLD);
     });
 
     it("check collateral factor", async () => {
-      const collateralFactor = (await comptroller.markets(vstkBNB)).collateralFactorMantissa;
+      const collateralFactor = (await lstBNBComptroller.markets(vstkBNB)).collateralFactorMantissa;
       expect(collateralFactor).to.eq(COLLATERAL_FACTOR);
     });
 
     it("actions paused", async () => {
-      expect(await comptroller.actionPaused(vstkBNB, Actions.BORROW)).to.be.true;
-      expect(await comptroller.actionPaused(vstkBNB, Actions.MINT)).to.be.true;
-      expect(await comptroller.actionPaused(vstkBNB, Actions.ENTER_MARKET)).to.be.true;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.BORROW)).to.be.true;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.MINT)).to.be.true;
+      expect(await lstBNBComptroller.actionPaused(vstkBNB, Actions.ENTER_MARKET)).to.be.true;
+    });
+
+    it("check supply and borrow cap for MATIC", async () => {
+      const supplyCap = await coreComptroller.supplyCaps(vMATIC);
+      const borrowCap = await coreComptroller.borrowCaps(vMATIC);
+
+      expect(supplyCap).to.eq(vMATIC_SUPPLY_CAP);
+      expect(borrowCap).to.eq(vMATIC_BORROW_CAP);
     });
   });
 });
--- vips/vip-371/bscmainnet.ts
+++ vips/vip-371/bscmainnet.ts
@@ -3,7 +3,9 @@ import { ProposalType } from "src/types";
 import { makeProposal } from "src/utils";
 
 export const LST_BNB_COMPTROLLER = "0xd933909A4a2b7A4638903028f44D1d38ce27c352";
+export const CORE_COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const vstkBNB = "0xcc5D9e502574cda17215E70bC0B4546663785227";
+export const vMATIC = "0x5c9476FcD6a4F9a3654139721c949c2233bBbBc8";
 export const Actions = {
   MINT: 0,
   BORROW: 2,
@@ -12,8 +14,10 @@ export const Actions = {
 export const RESERVE_FACTOR = parseUnits("1", 18);
 export const COLLATERAL_FACTOR = 0;
 export const LIQUIDATION_THRESHOLD = parseUnits("0.93", 18);
-export const SUPPLY_CAP = 0;
-export const BORROW_CAP = 0;
+export const vstkBNB_SUPPLY_CAP = 0;
+export const vstkBNB_BORROW_CAP = 0;
+export const vMATIC_SUPPLY_CAP = parseUnits("5500000", 18);
+export const vMATIC_BORROW_CAP = parseUnits("250000", 18);
 
 export const vip371 = () => {
   const meta = {
@@ -45,12 +49,22 @@ export const vip371 = () => {
       {
         target: LST_BNB_COMPTROLLER,
         signature: "setMarketBorrowCaps(address[],uint256[])",
-        params: [[vstkBNB], [BORROW_CAP]],
+        params: [[vstkBNB], [vstkBNB_BORROW_CAP]],
       },
       {
         target: LST_BNB_COMPTROLLER,
         signature: "setMarketSupplyCaps(address[],uint256[])",
-        params: [[vstkBNB], [SUPPLY_CAP]],
+        params: [[vstkBNB], [vstkBNB_SUPPLY_CAP]],
+      },
+      {
+        target: CORE_COMPTROLLER,
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [[vMATIC], [vMATIC_BORROW_CAP]],
+      },
+      {
+        target: CORE_COMPTROLLER,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[vMATIC], [vMATIC_SUPPLY_CAP]],
       },
     ],
     meta,
