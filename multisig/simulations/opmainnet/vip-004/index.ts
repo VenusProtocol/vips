2562c1b00f771e96a240d644eaf3db1c595b27c5
Debugger022 <debugger022@gmail.com>
2024-10-01 20:06:28 +0530

    feat: vip for psr and ntg on opmainnet

---
+++ multisig/simulations/opmainnet/vip-004/index.ts
@@ -0,0 +1,81 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { initMainnetUser } from "src/utils";
+import { forking, pretendExecutingVip } from "src/vip-framework/index";
+
+import vip004, { ACM, NATIVE_TOKEN_GATEWAY_CORE_POOL, PSR } from "../../../proposals/opmainnet/vip-004";
+import GATEWAY_ABI from "./abi/NativeTokenGateway.json";
+import ACM_ABI from "./abi/accessControlManager.json";
+import PSR_ABI from "./abi/protocolShareReserve.json";
+
+const { opmainnet } = NETWORK_ADDRESSES;
+
+const VWETH = "0x66d5AE25731Ce99D46770745385e662C8e0B4025";
+const WETH = "0x4200000000000000000000000000000000000006";
+
+forking(126092000, async () => {
+  let protocolShareReserve: Contract;
+  let accessControlManager: Contract;
+  let psrSigner: SignerWithAddress;
+
+  before(async () => {
+    protocolShareReserve = await ethers.getContractAt(PSR_ABI, PSR);
+    accessControlManager = await ethers.getContractAt(ACM_ABI, ACM);
+
+    psrSigner = await initMainnetUser(PSR, ethers.utils.parseEther("1"));
+    nativeTokenGateway = await ethers.getContractAt(GATEWAY_ABI, NATIVE_TOKEN_GATEWAY_CORE_POOL);
+    await pretendExecutingVip(await vip004());
+  });
+
+  describe("Post tx checks", () => {
+    it("PSR owner should be multisig", async () => {
+      const owner = await protocolShareReserve.owner();
+      expect(owner).equals(opmainnet.GUARDIAN);
+    });
+
+    it("PSR should have correct ACM reference", async () => {
+      const acm = await protocolShareReserve.accessControlManager();
+      expect(acm).equals(ACM);
+    });
+
+    it("PSR should have correct PoolRegistry reference", async () => {
+      expect(await protocolShareReserve.poolRegistry()).to.equal(opmainnet.POOL_REGISTRY);
+    });
+
+    it("Verify Multisig permissions for PSR", async () => {
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(opmainnet.GUARDIAN, "addOrUpdateDistributionConfigs(DistributionConfig[])"),
+      ).to.be.true;
+
+      expect(
+        await accessControlManager
+          .connect(psrSigner)
+          .isAllowedToCall(opmainnet.GUARDIAN, "removeDistributionConfig(Schema,address)"),
+      ).to.be.true;
+    });
+
+    it("Validate PSR distribution config", async () => {
+      expect(await protocolShareReserve.totalDistributions()).to.equal(2);
+      expect(await protocolShareReserve.getPercentageDistribution(opmainnet.VTREASURY, 0)).to.equal(10000);
+      expect(await protocolShareReserve.getPercentageDistribution(opmainnet.VTREASURY, 1)).to.equal(10000);
+    });
+  });
+
+  let nativeTokenGateway: Contract;
+
+  describe("Post tx checks", () => {
+    it("Should set owner to multisig", async () => {
+      expect(await nativeTokenGateway.owner()).equals(opmainnet.GUARDIAN);
+    });
+
+    it("Should have correct owner vWETH and wETH addresses", async () => {
+      expect(await nativeTokenGateway.vWNativeToken()).to.be.equal(VWETH);
+      expect(await nativeTokenGateway.wNativeToken()).to.be.equal(WETH);
+    });
+  });
+});
