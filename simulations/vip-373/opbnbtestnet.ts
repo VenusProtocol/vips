00e4b059381a897c534cff9575f557b53e25779c
web3rover <arsenal.narayan@gmail.com>
2024-10-09 16:11:02 +0400

    fix: check permissions

--- simulations/vip-373/opbnbtestnet.ts
+++ simulations/vip-373/opbnbtestnet.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { expectEvents } from "src/utils";
 import { forking, pretendExecutingVip, testForkedNetworkVipCommands } from "src/vip-framework";
 
@@ -8,6 +9,7 @@ import vip373, { DEFAULT_ADMIN_ROLE, OPBNBTESTNET_ACM_AGGREGATOR } from "../../v
 import ACM_COMMANDS_AGGREGATOR_ABI from "./abi/ACMCommandsAggregator.json";
 import ACCESS_CONTROL_MANAGER_ABI from "./abi/AccessControlManager.json";
 
+const { opbnbtestnet } = NETWORK_ADDRESSES;
 forking(41162234, async () => {
   before(async () => {
     await pretendExecutingVip(await vip021());
@@ -23,9 +25,25 @@ forking(41162234, async () => {
   });
 
   describe("Post-VIP behaviour", async () => {
+    const acm = new ethers.Contract(ACM, ACCESS_CONTROL_MANAGER_ABI, ethers.provider);
+
     it("check if DEFAULT_ROLE has been revoked for ACMAggregator", async () => {
-      const acm = new ethers.Contract(ACM, ACCESS_CONTROL_MANAGER_ABI, ethers.provider);
       expect(await acm.hasRole(DEFAULT_ADMIN_ROLE, OPBNBTESTNET_ACM_AGGREGATOR)).to.be.false;
+    });
+
+    it("check few permissions", async () => {
+      const role1 = ethers.utils.solidityPack(["address", "string"], [opbnbtestnet.RESILIENT_ORACLE, "pause()"]);
+
+      const roleHash = ethers.utils.keccak256(role1);
+      expect(await acm.hasRole(roleHash, opbnbtestnet.NORMAL_TIMELOCK)).to.be.true;
+
+      const role2 = ethers.utils.solidityPack(
+        ["address", "string"],
+        [opbnbtestnet.XVS_VAULT_PROXY, "set(address,uint256,uint256)"],
+      );
+
+      const roleHash2 = ethers.utils.keccak256(role2);
+      expect(await acm.hasRole(roleHash2, opbnbtestnet.NORMAL_TIMELOCK)).to.be.true;
     });
   });
 });
