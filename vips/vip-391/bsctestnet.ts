dcda279bd016236b5554ac527ef076d6d7e47fe3
web3rover <arsenal.narayan@gmail.com>
2024-10-30 16:57:28 +0400

    fix: wip - add sepolia vip

---
+++ vips/vip-391/bsctestnet.ts
@@ -0,0 +1,134 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { sepolia } = NETWORK_ADDRESSES;
+
+export const MOCK_ACCOUNTANT = "0xb4a5E8d4ba737fb27D6C5c43521fF1Da391c6CAE";
+export const ACCOUNTANT_ORACLE = "0xE621e50f74CfAdeeb059eEe1AdCd6e20cEe9E1a6";
+export const eBTC = "0xd48392CCf3fe028023D0783E570DFc71996859d7";
+const RATE = parseUnits("1", 8);
+const INITIAL_SUPPLY = parseUnits("0.14471345", 8);
+export const SUPPLY_CAP = parseUnits("25", 8);
+export const BORROW_CAP = parseUnits("12.5", 8);
+const CF = parseUnits("0.68", 18);
+const LT = parseUnits("0.72", 18);
+export const veBTC = "0x8E6241389e823111259413810b81a050bd45e5cE";
+export const CORE_COMPTROLLER = "0x7Aa39ab4BcA897F403425C9C6FDbd0f882Be0D70";
+
+export const USDT_PRIME_CONVERTER = "0x3716C24EA86A67cAf890d7C9e4C4505cDDC2F8A2";
+export const USDC_PRIME_CONVERTER = "0x511a559a699cBd665546a1F75908f7E9454Bfc67";
+export const WBTC_PRIME_CONVERTER = "0x8a3937F27921e859db3FDA05729CbCea8cfd82AE";
+export const WETH_PRIME_CONVERTER = "0x274a834eFFA8D5479502dD6e78925Bc04ae82B46";
+export const XVS_VAULT_CONVERTER = "0xc203bfA9dCB0B5fEC510Db644A494Ff7f4968ed2";
+export const BaseAssets = [
+  "0x8d412FD0bc5d826615065B931171Eed10F5AF266", // USDT USDTPrimeConverter BaseAsset
+  "0x772d68929655ce7234C8C94256526ddA66Ef641E", // USDC USDCPrimeConverter BaseAsset
+  "0x92A2928f5634BEa89A195e7BeCF0f0FEEDAB885b", // WBTC WBTCPrimeConverter BaseAsset
+  "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9", // WETH WETHPrimeConverter BaseAsset
+  "0x66ebd019E86e0af5f228a0439EBB33f045CBe63E", // XVS XVSPrimeConverter BaseAsset
+];
+const CONVERSION_INCENTIVE = parseUnits("0.0001", 18);
+
+const vip391 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-391",
+    description: `### Description`,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+  return makeProposal(
+    [
+      // Configure Oracle
+      {
+        target: MOCK_ACCOUNTANT,
+        signature: "setRate(uint256)",
+        params: [RATE],
+      },
+      {
+        target: sepolia.RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [eBTC, [ACCOUNTANT_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero], [true, false, false]],
+        ],
+      },
+
+      // Add Market
+      {
+        target: eBTC,
+        signature: "faucet(uint256)",
+        params: [INITIAL_SUPPLY],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: eBTC,
+        signature: "approve(address,uint256)",
+        params: [sepolia.POOL_REGISTRY, 0],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: eBTC,
+        signature: "approve(address,uint256)",
+        params: [sepolia.POOL_REGISTRY, INITIAL_SUPPLY],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: veBTC,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: ["7200"],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: sepolia.POOL_REGISTRY,
+        signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
+        params: [[veBTC, CF, LT, INITIAL_SUPPLY, sepolia.VTREASURY, SUPPLY_CAP, BORROW_CAP]],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: veBTC,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [parseUnits("0.01", 18)],
+        dstChainId: LzChainId.sepolia,
+      },
+
+      // Conversion config
+      {
+        target: USDT_PRIME_CONVERTER,
+        signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+        params: [BaseAssets[0], [eBTC], [[CONVERSION_INCENTIVE, 1]]],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: USDC_PRIME_CONVERTER,
+        signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+        params: [BaseAssets[1], [eBTC], [[CONVERSION_INCENTIVE, 1]]],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: WBTC_PRIME_CONVERTER,
+        signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+        params: [BaseAssets[2], [eBTC], [[CONVERSION_INCENTIVE, 1]]],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: WETH_PRIME_CONVERTER,
+        signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+        params: [BaseAssets[3], [eBTC], [[CONVERSION_INCENTIVE, 1]]],
+        dstChainId: LzChainId.sepolia,
+      },
+      {
+        target: XVS_VAULT_CONVERTER,
+        signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+        params: [BaseAssets[4], [eBTC], [[CONVERSION_INCENTIVE, 1]]],
+        dstChainId: LzChainId.sepolia,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+export default vip391;
