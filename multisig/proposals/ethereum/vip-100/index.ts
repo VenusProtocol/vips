e48befd07aee597ceb7b3052de924dd939286c2f
Kirill Kuvshinov <kkirka@gmail.com>
2024-11-08 10:45:20 +0300

    fixup! feat: add mainnet VIP for pufETH

--- multisig/proposals/ethereum/vip-100/index.ts
+++ multisig/proposals/ethereum/vip-100/index.ts
@@ -5,7 +5,7 @@ import { makeProposal } from "../../../../src/utils";
 
 const { VTREASURY, NORMAL_TIMELOCK } = NETWORK_ADDRESSES.ethereum;
 
-const PUFETH = "0xD9A442856C234a39a81a089C06451EBAa4306a72";
+export const PUFETH = "0xD9A442856C234a39a81a089C06451EBAa4306a72";
 const INITIAL_SUPPLY = parseUnits("5", 18);
 
 export const vip100 = () => {
---
+++ multisig/simulations/ethereum/vip-100/index.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, pretendExecutingVip } from "src/vip-framework";
+
+import vip100, { PUFETH } from "../../../proposals/ethereum/vip-100";
+import ERC20_ABI from "./abi/erc20.json";
+
+const { NORMAL_TIMELOCK, VTREASURY } = NETWORK_ADDRESSES.ethereum;
+
+forking(21130180, async () => {
+  const pufETH = new ethers.Contract(PUFETH, ERC20_ABI, ethers.provider);
+
+  describe("Post-VIP behavior", async () => {
+    before(async () => {
+      await pretendExecutingVip(await vip100());
+    });
+
+    it("transfers initial supply from treasury to normal timelock", async () => {
+      expect(await pufETH.balanceOf(NORMAL_TIMELOCK)).to.equal(parseUnits("5", 18));
+      expect(await pufETH.balanceOf(VTREASURY)).to.equal(0);
+    });
+  });
+});
