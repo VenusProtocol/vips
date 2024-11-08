6d1e5c27e94e99e4751cec6767f1c66adbef884d
web3rover <arsenal.narayan@gmail.com>
2024-11-08 16:47:13 +0400

    fix: deploy oracles

--- multisig/proposals/basesepolia/vip-000/index.ts
+++ multisig/proposals/basesepolia/vip-000/index.ts
@@ -1,11 +1,71 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { makeProposal } from "src/utils";
 
+const { basesepolia } = NETWORK_ADDRESSES;
+
 export const TREASURY = "0x07e880DaA6572829cE8ABaaf0f5323A4eFC417A6";
+export const ACM = "0x724138223D8F76b519fdE715f60124E7Ce51e051";
+export const BOUND_VALIDATOR = "0xC76284488E57554A457A75a8b166fB2ADAB430dB";
 
 const vip000 = () => {
   return makeProposal([
     {
       target: TREASURY,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.RESILIENT_ORACLE, "pause()", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.RESILIENT_ORACLE, "unpause()", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.RESILIENT_ORACLE, "setOracle(address,address,uint8)", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.RESILIENT_ORACLE, "enableOracle(address,uint8,bool)", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.RESILIENT_ORACLE, "setTokenConfig(TokenConfig)", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.CHAINLINK_ORACLE, "setTokenConfig(TokenConfig)", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.CHAINLINK_ORACLE, "setDirectPrice(address,uint256)", basesepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [BOUND_VALIDATOR, "setValidateConfig(ValidateConfig)", basesepolia.GUARDIAN],
+    },
+    {
+      target: basesepolia.RESILIENT_ORACLE,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: basesepolia.CHAINLINK_ORACLE,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: BOUND_VALIDATOR,
       signature: "acceptOwnership()",
       params: [],
     },
--- multisig/simulations/basesepolia/vip-000/index.ts
+++ multisig/simulations/basesepolia/vip-000/index.ts
@@ -4,20 +4,39 @@ import { ethers } from "hardhat";
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { forking, pretendExecutingVip } from "src/vip-framework/index";
 
-import vip000, { TREASURY } from "../../../proposals/basesepolia/vip-000";
+import vip000, { BOUND_VALIDATOR, TREASURY } from "../../../proposals/basesepolia/vip-000";
+import BOUND_VALIDATOR_ABI from "./abi/boundValidator.json";
+import CHAINLINK_ORACLE_ABI from "./abi/chainlinkOracle.json";
+import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
 import TREASURY_ABI from "./abi/treasury.json";
 
-forking(16735962, async () => {
+forking(17650710, async () => {
   let treasury: Contract;
+  let resilientOracle: Contract;
+  let chainlinkOracle: Contract;
+  let boundValidator: Contract;
 
   before(async () => {
     treasury = await ethers.getContractAt(TREASURY_ABI, TREASURY);
+    resilientOracle = await ethers.getContractAt(RESILIENT_ORACLE_ABI, NETWORK_ADDRESSES.basesepolia.RESILIENT_ORACLE);
+    chainlinkOracle = await ethers.getContractAt(CHAINLINK_ORACLE_ABI, NETWORK_ADDRESSES.basesepolia.CHAINLINK_ORACLE);
+    boundValidator = await ethers.getContractAt(BOUND_VALIDATOR_ABI, BOUND_VALIDATOR);
+
     await pretendExecutingVip(await vip000());
   });
 
   describe("Post tx checks", () => {
     it("Should set owner to multisig", async () => {
-      const owner = await treasury.owner();
+      let owner = await treasury.owner();
+      expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
+
+      owner = await resilientOracle.owner();
+      expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
+
+      owner = await chainlinkOracle.owner();
+      expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
+
+      owner = await boundValidator.owner();
       expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
     });
   });
