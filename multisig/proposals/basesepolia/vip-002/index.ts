8c329656d1515707f04c6978fc4277ac61619f25
web3rover <arsenal.narayan@gmail.com>
2024-11-14 12:00:34 +0400

    fix: added vip and simulation

---
+++ multisig/proposals/basesepolia/vip-002/index.ts
@@ -0,0 +1,76 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { makeProposal } from "src/utils";
+
+const { basesepolia } = NETWORK_ADDRESSES;
+
+export const XVS_STORE = "0x0ff9Ec1118f1E332CF4DE2A685110316af60F9fD";
+export const ACM = "0x724138223D8F76b519fdE715f60124E7Ce51e051";
+
+const vip002 = () => {
+  return makeProposal([
+    {
+      target: basesepolia.XVS_VAULT_PROXY,
+      signature: "_acceptAdmin()",
+      params: [],
+    },
+
+    {
+      target: XVS_STORE,
+      signature: "acceptAdmin()",
+      params: [],
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.XVS_VAULT_PROXY, "pause()", basesepolia.GUARDIAN],
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.XVS_VAULT_PROXY, "resume()", basesepolia.GUARDIAN],
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.XVS_VAULT_PROXY, "add(address,uint256,address,uint256,uint256)", basesepolia.GUARDIAN],
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.XVS_VAULT_PROXY, "set(address,uint256,uint256)", basesepolia.GUARDIAN],
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [basesepolia.XVS_VAULT_PROXY, "setRewardAmountPerBlockOrSecond(address,uint256)", basesepolia.GUARDIAN], // func name changed from setRewardAmountPerBlock to setRewardAmountPerBlockOrSecond
+    },
+
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [
+        basesepolia.XVS_VAULT_PROXY,
+        "setWithdrawalLockingPeriod(address,uint256,uint256)",
+        basesepolia.GUARDIAN,
+      ],
+    },
+
+    {
+      target: basesepolia.XVS_VAULT_PROXY,
+      signature: "add(address,uint256,address,uint256,uint256)",
+      params: [basesepolia.XVS, 100, basesepolia.XVS, "0", 604800],
+    },
+    {
+      target: basesepolia.XVS_VAULT_PROXY,
+      signature: "pause()",
+      params: [],
+    },
+  ]);
+};
+
+export default vip002;
---
+++ multisig/simulations/basesepolia/vip-002/index.ts
@@ -0,0 +1,76 @@
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { parseEther, parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { initMainnetUser } from "src/utils";
+import { checkXVSVault } from "src/vip-framework/checks/checkXVSVault";
+import { forking, pretendExecutingVip } from "src/vip-framework/index";
+
+import vip002, { ACM, XVS_STORE } from "../../../proposals/basesepolia/vip-002";
+import ACM_ABI from "./abi/acm.json";
+import XVS_ABI from "./abi/xvs.json";
+import XVS_STORE_ABI from "./abi/xvsstore.json";
+import XVS_VAULT_ABI from "./abi/xvsvault.json";
+
+const { basesepolia } = NETWORK_ADDRESSES;
+
+const XVS_BRIDGE = "0x8d9eC252984Ed6524ABE5EB19f02bDAdC9a868f1";
+
+forking(17901277, async () => {
+  let xvsVault: Contract;
+  let xvsStore: Contract;
+  let xvsMinter: SignerWithAddress;
+
+  before(async () => {
+    xvsVault = await ethers.getContractAt(XVS_VAULT_ABI, basesepolia.XVS_VAULT_PROXY);
+    xvsStore = await ethers.getContractAt(XVS_STORE_ABI, XVS_STORE);
+
+    await pretendExecutingVip(await vip002());
+  });
+
+  describe("Post tx checks", () => {
+    describe("Generic checks", async () => {
+      before(async () => {
+        const acm: Contract = await ethers.getContractAt(ACM_ABI, ACM);
+        const xvs: Contract = await ethers.getContractAt(XVS_ABI, basesepolia.XVS);
+        xvsMinter = await initMainnetUser(XVS_BRIDGE, ethers.utils.parseEther("1"));
+        const admin = await initMainnetUser(basesepolia.GUARDIAN, ethers.utils.parseEther("1"));
+        const xvsHolder = await initMainnetUser(basesepolia.GENERIC_TEST_USER_ACCOUNT, ethers.utils.parseEther("1"));
+        await xvsVault.connect(admin).setRewardAmountPerBlockOrSecond(basesepolia.XVS, "61805555555555555");
+        await xvsVault.connect(admin).resume();
+        // Giving call permissions to call the functions as xvs bridge vip is not executed now.
+        await acm.connect(admin).giveCallPermission(basesepolia.XVS, "mint(address,uint256)", XVS_BRIDGE);
+        await acm.connect(admin).giveCallPermission(basesepolia.XVS, "setMintCap(address,uint256)", admin.address);
+        await xvs.connect(admin).setMintCap(XVS_BRIDGE, parseUnits("100", 18));
+        await xvs.connect(xvsMinter).mint(xvsHolder.address, parseEther("10"));
+
+        await xvs.connect(xvsHolder).transfer(XVS_STORE, ethers.utils.parseEther("1"));
+        await mine(604800);
+      });
+      checkXVSVault();
+    });
+
+    it("Should set xvs vault owner to multisig", async () => {
+      const owner = await xvsVault.admin();
+      expect(owner).equals(basesepolia.GUARDIAN);
+    });
+
+    it("Should set xvs store owner to multisig", async () => {
+      const owner = await xvsStore.admin();
+      expect(owner).equals(basesepolia.GUARDIAN);
+    });
+
+    it("Should set correct xvs store address", async () => {
+      const xvsStore = await xvsVault.xvsStore();
+      expect(xvsStore).equals(XVS_STORE);
+    });
+
+    it("Should set correct reward token address", async () => {
+      const isActive = await xvsStore.rewardTokens(basesepolia.XVS);
+      expect(isActive).equals(true);
+    });
+  });
+});
