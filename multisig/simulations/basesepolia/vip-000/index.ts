0522c4c34e41e607d5c4d86710f5dc97a0af5d7b
web3rover <arsenal.narayan@gmail.com>
2024-11-15 13:24:08 +0400

    fix: added redstone oracle

--- multisig/simulations/basesepolia/vip-000/index.ts
+++ multisig/simulations/basesepolia/vip-000/index.ts
@@ -7,19 +7,22 @@ import { forking, pretendExecutingVip } from "src/vip-framework/index";
 import vip000, { BOUND_VALIDATOR, TREASURY } from "../../../proposals/basesepolia/vip-000";
 import BOUND_VALIDATOR_ABI from "./abi/boundValidator.json";
 import CHAINLINK_ORACLE_ABI from "./abi/chainlinkOracle.json";
+import REDSTONE_ORACLE_ABI from "./abi/chainlinkOracle.json";
 import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
 import TREASURY_ABI from "./abi/treasury.json";
 
-forking(17650710, async () => {
+forking(17947005, async () => {
   let treasury: Contract;
   let resilientOracle: Contract;
   let chainlinkOracle: Contract;
+  let redstoneOracle: Contract;
   let boundValidator: Contract;
 
   before(async () => {
     treasury = await ethers.getContractAt(TREASURY_ABI, TREASURY);
     resilientOracle = await ethers.getContractAt(RESILIENT_ORACLE_ABI, NETWORK_ADDRESSES.basesepolia.RESILIENT_ORACLE);
     chainlinkOracle = await ethers.getContractAt(CHAINLINK_ORACLE_ABI, NETWORK_ADDRESSES.basesepolia.CHAINLINK_ORACLE);
+    redstoneOracle = await ethers.getContractAt(REDSTONE_ORACLE_ABI, NETWORK_ADDRESSES.basesepolia.REDSTONE_ORACLE);
     boundValidator = await ethers.getContractAt(BOUND_VALIDATOR_ABI, BOUND_VALIDATOR);
 
     await pretendExecutingVip(await vip000());
@@ -37,6 +40,9 @@ forking(17650710, async () => {
       expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
 
       owner = await boundValidator.owner();
+      expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
+
+      owner = await redstoneOracle.owner();
       expect(owner).equals(NETWORK_ADDRESSES.basesepolia.GUARDIAN);
     });
   });
