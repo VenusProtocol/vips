4d64e265c00e24c584afa5c76916d9e2ddd24524
Narayan Prusty <arsenal.narayan@gmail.com>
2024-12-13 21:51:46 +0530

    fix: fixed opmainnet and zksyncmainnet simulations

--- simulations/vip-500/zksyncmainnet.ts
+++ simulations/vip-500/zksyncmainnet.ts
@@ -1,5 +1,6 @@
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { LzChainId } from "src/types";
+import { setRedstonePrice } from "src/utils";
 import { forking } from "src/vip-framework";
 
 import vip500, { BASE_MAINNET_TRUSTED_REMOTE, MIN_DST_GAS, remoteBridgeEntries } from "../../vips/vip-500/bscmainnet";
@@ -7,6 +8,19 @@ import { RemoteBridgeEntry } from "../../vips/vip-500/types";
 import { checkXVSBridge } from "./checkXVSBridge";
 
 forking(51152831, async () => {
+  before(async () => {
+    const ONE_YEAR = 31536000;
+    const XVS_REDSTONE_FEED = "0xca4793Eeb7a837E30884279b3D557970E444EBDe";
+    await setRedstonePrice(
+      NETWORK_ADDRESSES.zksyncmainnet.REDSTONE_ORACLE,
+      NETWORK_ADDRESSES.zksyncmainnet.XVS,
+      XVS_REDSTONE_FEED,
+      NETWORK_ADDRESSES.zksyncmainnet.NORMAL_TIMELOCK,
+      ONE_YEAR,
+      { tokenDecimals: 18 },
+    );
+  });
+
   await checkXVSBridge(
     LzChainId.basemainnet,
     NETWORK_ADDRESSES.zksyncmainnet,
