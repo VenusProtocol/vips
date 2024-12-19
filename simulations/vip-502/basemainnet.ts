56bafc5f1cc9f92600f008baead1b4b2713b6bc2
web3rover <arsenal.narayan@gmail.com>
2024-12-19 16:36:22 +0530

    fix: addec check to withdraw from treasury

--- simulations/vip-502/basemainnet.ts
+++ simulations/vip-502/basemainnet.ts
@@ -1,3 +1,4 @@
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
@@ -14,7 +15,6 @@ import vip502, {
   BOUND_VALIDATOR,
   COMPTROLLERS,
   VTOKENS,
-  XVS_BRIDGE,
   XVS_BRIDGE_ADMIN,
   XVS_STORE,
 } from "../../vips/vip-502/bscmainnet";
@@ -29,12 +29,11 @@ import BOUND_VALIDATOR_ABI from "../vip-502/abi/boundValidator.json";
 import CHAINLINK_ORACLE_ABI from "../vip-502/abi/chainlinkOracle.json";
 import RESILLIENT_ORACLE_ABI from "../vip-502/abi/resilientOracle.json";
 import TREASURY_ABI from "../vip-502/abi/treasury.json";
-import XVS_BRIDGE_ABI from "../vip-502/abi/xvsBridge.json";
 import XVS_BRIDGE_ADMIN_ABI from "../vip-502/abi/xvsBridgeAdmin.json";
 
 const { basemainnet } = NETWORK_ADDRESSES;
 
-forking(23864228, async () => {
+forking(23908020, async () => {
   const provider = ethers.provider;
   let prime: Contract;
   let plp: Contract;
@@ -45,10 +44,13 @@ forking(23864228, async () => {
   let resilientOracle: Contract;
   let boundValidator: Contract;
   let xvsBridgeAdmin: Contract;
-  let xvsBridge: Contract;
   let treasury: Contract;
 
   before(async () => {
+    await impersonateAccount(basemainnet.NORMAL_TIMELOCK);
+    await setBalance(basemainnet.NORMAL_TIMELOCK, ethers.utils.parseEther("1"));
+    await setBalance(basemainnet.VTREASURY, ethers.utils.parseEther("100"));
+
     prime = new ethers.Contract(PRIME, PRIME_ABI, provider);
     plp = new ethers.Contract(PLP, PRIME_LIQUIDITY_PROVIDER_ABI, provider);
     chainLinkOracle = new ethers.Contract(basemainnet.CHAINLINK_ORACLE, CHAINLINK_ORACLE_ABI, provider);
@@ -56,14 +58,25 @@ forking(23864228, async () => {
     resilientOracle = new ethers.Contract(basemainnet.RESILIENT_ORACLE, RESILLIENT_ORACLE_ABI, provider);
     boundValidator = new ethers.Contract(BOUND_VALIDATOR, BOUND_VALIDATOR_ABI, provider);
     xvsBridgeAdmin = await ethers.getContractAt(XVS_BRIDGE_ADMIN_ABI, XVS_BRIDGE_ADMIN);
-    xvsBridge = await ethers.getContractAt(XVS_BRIDGE_ABI, XVS_BRIDGE);
-    treasury = await ethers.getContractAt(TREASURY_ABI, basemainnet.VTREASURY);
+    treasury = await ethers.getContractAt(
+      TREASURY_ABI,
+      basemainnet.VTREASURY,
+      await ethers.getSigner(basemainnet.NORMAL_TIMELOCK),
+    );
 
     await pretendExecutingVip(await vip003());
     await pretendExecutingVip(await vip004());
     await pretendExecutingVip(await vip005());
     await pretendExecutingVip(await vip006());
     await pretendExecutingVip(await vip007());
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("cannot transfer tokens from the treasury", async () => {
+      await expect(
+        treasury.withdrawTreasuryNative(ethers.utils.parseEther("100"), basemainnet.NORMAL_TIMELOCK),
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
   });
 
   testForkedNetworkVipCommands("vip502", await vip502());
@@ -109,6 +122,12 @@ forking(23864228, async () => {
     });
     it("Normal Timelock should be the owner of the Vtreasury", async () => {
       expect(await treasury.owner()).equals(basemainnet.NORMAL_TIMELOCK);
+    });
+    it("can transfer tokens from the treasury", async () => {
+      const prevBalance = await provider.getBalance(basemainnet.NORMAL_TIMELOCK);
+      await treasury.withdrawTreasuryNative(ethers.utils.parseEther("100"), basemainnet.NORMAL_TIMELOCK);
+      const newBalance = await provider.getBalance(basemainnet.NORMAL_TIMELOCK);
+      expect(newBalance.sub(prevBalance)).to.be.closeTo(ethers.utils.parseEther("100"), ethers.utils.parseEther("0.1"));
     });
   });
 });
