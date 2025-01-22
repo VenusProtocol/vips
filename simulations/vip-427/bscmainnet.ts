236ae76637eaa22b194bea7056f19f19c924b114
web3rover <arsenal.narayan@gmail.com>
2025-01-22 16:59:51 +0530

    feat: replicate fallback price use scenario

--- simulations/vip-427/bscmainnet.ts
+++ simulations/vip-427/bscmainnet.ts
@@ -1,35 +1,35 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
-import {
-  expectEvents,
-  setMaxStalePeriod,
-  setMaxStalePeriodInBinanceOracle,
-  setMaxStalePeriodInChainlinkOracle,
-} from "src/utils";
+import { expectEvents, setMaxStalePeriodInBinanceOracle, setMaxStalePeriodInChainlinkOracle } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
 import { BNB, BTCB, vip427 } from "../../vips/vip-427/bscmainnet";
 import BINANCE_ORACLE_ABI from "./abi/BinanceOracle.json";
-import ERC20_ABI from "./abi/ERC20.json";
 import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
 
 const { bscmainnet } = NETWORK_ADDRESSES;
 const CHAINLINK_BTC_FEED = "0x264990fbd0A4796A3E3d8E37C4d5F87a3aCa5Ebf";
+const CHAINLINK_BNB_FEED = "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE";
+const REDSTONE_BTC_FEED = "0xa51738d1937FFc553d5070f43300B385AA2D9F55";
+const REDSTONE_BNB_FEED = "0xa51738d1937FFc553d5070f43300B385AA2D9F55";
 
 forking(45959640, async () => {
   const provider = ethers.provider;
   const resilientOracle = new ethers.Contract(bscmainnet.RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
   const binanceOracle = new ethers.Contract(bscmainnet.BINANCE_ORACLE, BINANCE_ORACLE_ABI, provider);
-  const bnb = new ethers.Contract(BNB, ERC20_ABI, provider);
+  const redstoneOracle = new ethers.Contract(bscmainnet.REDSTONE_ORACLE, RESILIENT_ORACLE_ABI, provider);
 
   describe("Pre-VIP behavior", async () => {
-    it("get price from resilient oracle", async () => {
+    it("get price from main oracle", async () => {
       const btcPrice = await resilientOracle.getPrice(BTCB);
       const bnbPrice = await resilientOracle.getPrice(BNB);
 
-      expect(btcPrice).to.eq("103795629920000000000000");
-      expect(bnbPrice).to.eq("688818807240000000000");
+      const btcPriceRedstone = await redstoneOracle.getPrice(BTCB);
+      const bnbPriceRedstone = await redstoneOracle.getPrice(BNB);
+
+      expect(btcPrice).to.eq(btcPriceRedstone);
+      expect(bnbPrice).to.eq(bnbPriceRedstone);
     });
 
     it("get price from binance oracle", async () => {
@@ -47,7 +47,7 @@ forking(45959640, async () => {
   });
 
   describe("Post-VIP behavior", async () => {
-    it("get price from resilient oracle", async () => {
+    it("use fallback price when main oracle fails", async () => {
       await setMaxStalePeriodInChainlinkOracle(
         bscmainnet.CHAINLINK_ORACLE,
         BTCB,
@@ -55,7 +55,29 @@ forking(45959640, async () => {
         bscmainnet.NORMAL_TIMELOCK,
       );
 
-      await setMaxStalePeriod(resilientOracle, bnb);
+      await setMaxStalePeriodInChainlinkOracle(
+        bscmainnet.CHAINLINK_ORACLE,
+        BNB,
+        CHAINLINK_BNB_FEED,
+        bscmainnet.NORMAL_TIMELOCK,
+      );
+
+      await setMaxStalePeriodInChainlinkOracle(
+        bscmainnet.REDSTONE_ORACLE,
+        BTCB,
+        REDSTONE_BTC_FEED,
+        bscmainnet.NORMAL_TIMELOCK,
+        1,
+      );
+
+      await setMaxStalePeriodInChainlinkOracle(
+        bscmainnet.REDSTONE_ORACLE,
+        BNB,
+        REDSTONE_BNB_FEED,
+        bscmainnet.NORMAL_TIMELOCK,
+        1,
+      );
+
       await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "BTC");
       await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "BNB");
 
@@ -63,7 +85,7 @@ forking(45959640, async () => {
       const bnbPrice = await resilientOracle.getPrice(BNB);
 
       expect(btcPrice).to.eq("103778122138560000000000");
-      expect(bnbPrice).to.eq("688818807240000000000");
+      expect(bnbPrice).to.eq("688760890800000000000");
     });
 
     it("get price from binance oracle", async () => {
