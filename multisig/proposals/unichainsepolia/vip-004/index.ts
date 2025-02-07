1a76a067b28c6740dc1f275912da80bf1c13e826
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-02-07 14:36:20 +0100

    feat: configuration of PSR on unichain sepolia

--- multisig/proposals/unichainsepolia/vip-004/index.ts
+++ multisig/proposals/unichainsepolia/vip-004/index.ts
@@ -1,9 +1,44 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { makeProposal } from "src/utils";
 
+const { unichainsepolia } = NETWORK_ADDRESSES;
+
+export const ACM = "0x854C064EA6b503A97980F481FA3B7279012fdeDd";
+export const PSR = "0xcCcFc9B37A5575ae270352CC85D55C3C52a646C0";
 export const NATIVE_TOKEN_GATEWAY_CORE_POOL = "0x148C41b07A5c1f289CFB57C2F40d5EEF8ab30DB1";
 
 const vip004 = () => {
   return makeProposal([
+    {
+      target: PSR,
+      signature: "acceptOwnership()",
+      params: [],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PSR, "addOrUpdateDistributionConfigs(DistributionConfig[])", unichainsepolia.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [PSR, "removeDistributionConfig(Schema,address)", unichainsepolia.GUARDIAN],
+    },
+    {
+      target: PSR,
+      signature: "addOrUpdateDistributionConfigs((uint8,uint16,address)[])",
+      params: [
+        [
+          [0, 10000, unichainsepolia.VTREASURY],
+          [1, 10000, unichainsepolia.VTREASURY],
+        ],
+      ],
+    },
+    {
+      target: PSR,
+      signature: "setPoolRegistry(address)",
+      params: [unichainsepolia.POOL_REGISTRY],
+    },
     {
       target: NATIVE_TOKEN_GATEWAY_CORE_POOL,
       signature: "acceptOwnership()",
--- multisig/simulations/unichainsepolia/vip-004/index.ts
+++ multisig/simulations/unichainsepolia/vip-004/index.ts
@@ -1,20 +1,33 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { initMainnetUser } from "src/utils";
 import { forking, pretendExecutingVip } from "src/vip-framework";
 
-import vip004, { NATIVE_TOKEN_GATEWAY_CORE_POOL } from "../../../proposals/unichainsepolia/vip-004";
+import vip004, { ACM, NATIVE_TOKEN_GATEWAY_CORE_POOL, PSR } from "../../../proposals/unichainsepolia/vip-004";
+import ACM_ABI from "./abi/AccessControlManager.json";
 import GATEWAY_ABI from "./abi/NativeTokenGateway.json";
+import PSR_ABI from "./abi/ProtocolShareReserve.json";
+
+const { unichainsepolia } = NETWORK_ADDRESSES;
 
 const VWETH = "0x3dEAcBe87e4B6333140a46aBFD12215f4130B132";
 const WETH = "0x4200000000000000000000000000000000000006";
 
 forking(4724011, async () => {
   let nativeTokenGateway: Contract;
+  let protocolShareReserve: Contract;
+  let accessControlManager: Contract;
+  let psrSigner: SignerWithAddress;
 
   before(async () => {
     nativeTokenGateway = await ethers.getContractAt(GATEWAY_ABI, NATIVE_TOKEN_GATEWAY_CORE_POOL);
+    protocolShareReserve = await ethers.getContractAt(PSR_ABI, PSR);
+    accessControlManager = await ethers.getContractAt(ACM_ABI, ACM);
+
+    psrSigner = await initMainnetUser(PSR, ethers.utils.parseEther("1"));
     await pretendExecutingVip(await vip004());
   });
 
@@ -26,6 +39,41 @@ forking(4724011, async () => {
     it("Should have correct vWETH and wETH addresses", async () => {
       expect(await nativeTokenGateway.vWNativeToken()).to.be.equal(VWETH);
       expect(await nativeTokenGateway.wNativeToken()).to.be.equal(WETH);
+    });
+    describe("PSR configuration", () => {
+      it("PSR owner should be multisig", async () => {
+        const owner = await protocolShareReserve.owner();
+        expect(owner).equals(unichainsepolia.GUARDIAN);
+      });
+
+      it("PSR should have correct ACM reference", async () => {
+        const acm = await protocolShareReserve.accessControlManager();
+        expect(acm).equals(ACM);
+      });
+
+      it("PSR should have correct PoolRegistry reference", async () => {
+        expect(await protocolShareReserve.poolRegistry()).to.equal(unichainsepolia.POOL_REGISTRY);
+      });
+
+      it("Verify Multisig permissions for PSR", async () => {
+        expect(
+          await accessControlManager
+            .connect(psrSigner)
+            .isAllowedToCall(unichainsepolia.GUARDIAN, "addOrUpdateDistributionConfigs(DistributionConfig[])"),
+        ).to.be.true;
+
+        expect(
+          await accessControlManager
+            .connect(psrSigner)
+            .isAllowedToCall(unichainsepolia.GUARDIAN, "removeDistributionConfig(Schema,address)"),
+        ).to.be.true;
+      });
+
+      it("Validate PSR distribution config", async () => {
+        expect(await protocolShareReserve.totalDistributions()).to.equal(2);
+        expect(await protocolShareReserve.getPercentageDistribution(unichainsepolia.VTREASURY, 0)).to.equal(10000);
+        expect(await protocolShareReserve.getPercentageDistribution(unichainsepolia.VTREASURY, 1)).to.equal(10000);
+      });
     });
   });
 });
