cc191e75e29c076f613ede69fbbb9ce9fb984c29
GitGuru7 <aditya@venus.io>
2025-02-10 16:59:58 +0530

    feat: add xvs bridge vip on unichain

---
+++ multisig/proposals/unichainmainnet/vip-003/index.ts
@@ -0,0 +1,220 @@
+import { parseUnits } from "ethers/lib/utils";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { unichainmainnet } = NETWORK_ADDRESSES;
+
+const ACM = "0x1f12014c497a9d905155eB9BfDD9FaC6885e61d0";
+
+export const XVS_BRIDGE_ADMIN_PROXY = "0x2EAaa880f97C9B63d37b39b0b316022d93d43604";
+export const XVS = "0x81908BBaad3f6fC74093540Ab2E9B749BB62aA0d";
+export const XVS_BRIDGE_DEST = "0x9c95f8aa28fFEB7ECdC0c407B9F632419c5daAF8";
+
+export const OPBNB_MAINNET_TRUSTED_REMOTE = "0x100d331c1b5dcd41eacb1eced0e83dcebf3498b2";
+export const ETHEREUM_TRUSTED_REMOTE = "0x888e317606b4c590bbad88653863e8b345702633";
+export const BNB_MAINNET_TRUSTED_REMOTE = "0xf8f46791e3db29a029ec6c9d946226f3c613e854";
+export const ARBITRUM_ONE_TRUSTED_REMOTE = "0x20cea49b5f7a6dbd78cae772ca5973ef360aa1e6";
+export const ZKSYNC_MAINNET_TRUSTED_REMOTE = "0x16a62b534e09a7534cd5847cfe5bf6a4b0c1b116";
+export const OP_MAINNET_TRUSTED_REMOTE = "0xbbe46baec851355c3fc4856914c47eb6cea0b8b4";
+export const BASE_MAINNET_TRUSTED_REMOTE = "0x3dd92fb51a5d381ae78e023dfb5dd1d45d2426cd";
+
+export const XVS_MINT_LIMIT = parseUnits("500000", 18);
+
+const vip003 = () => {
+  return makeProposal([
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setSendVersion(uint16)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setReceiveVersion(uint16)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "forceResumeReceive(uint16,bytes)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setOracle(address)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxSingleTransactionLimit(uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxDailyLimit(uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxSingleReceiveTransactionLimit(uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setMaxDailyReceiveLimit(uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "pause()", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "unpause()", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "removeTrustedRemote(uint16)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "dropFailedMessage(uint16,bytes,uint64)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setPrecrime(address)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setMinDstGas(uint16,uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setPayloadSizeLimit(uint16,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setWhitelist(address,bool)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setConfig(uint16,uint16,uint256,bytes)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "sweepToken(address,address,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "updateSendAndCallEnabled(bool)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "mint(address,uint256)", XVS_BRIDGE_DEST],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "burn(address,uint256)", XVS_BRIDGE_DEST],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "setTrustedRemoteAddress(uint16,bytes)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS_BRIDGE_ADMIN_PROXY, "transferBridgeOwnership(address)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "migrateMinterTokens(address,address)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "setMintCap(address,uint256)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "updateBlacklist(address,bool)", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "pause()", unichainmainnet.GUARDIAN],
+    },
+    {
+      target: ACM,
+      signature: "giveCallPermission(address,string,address)",
+      params: [XVS, "unpause()", unichainmainnet.GUARDIAN],
+    },
+    { target: XVS_BRIDGE_ADMIN_PROXY, signature: "acceptOwnership()", params: [] },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.bscmainnet, BNB_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.opbnbmainnet, OPBNB_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.ethereum, ETHEREUM_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.arbitrumone, ARBITRUM_ONE_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.zksyncmainnet, ZKSYNC_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.opmainnet, OP_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.basemainnet, BASE_MAINNET_TRUSTED_REMOTE],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setWhitelist(address,bool)",
+      params: [unichainmainnet.VTREASURY, true],
+    },
+    {
+      target: XVS_BRIDGE_ADMIN_PROXY,
+      signature: "setWhitelist(address,bool)",
+      params: [unichainmainnet.GUARDIAN, true],
+    },
+    {
+      target: XVS,
+      signature: "setMintCap(address,uint256)",
+      params: [XVS_BRIDGE_DEST, XVS_MINT_LIMIT],
+    },
+  ]);
+};
+
+export default vip003;
---
+++ vips/vip-455/bscmainnet.ts
@@ -0,0 +1,131 @@
+import { parseUnits } from "ethers/lib/utils";
+
+import { LzChainId, ProposalType } from "../../src/types";
+import { makeProposal } from "../../src/utils";
+import { RemoteBridgeCommand, RemoteBridgeEntry } from "./types";
+
+export const UNICHAIN_MAINNET_TRUSTED_REMOTE = "0x9c95f8aa28fFEB7ECdC0c407B9F632419c5daAF8";
+
+export const MIN_DST_GAS = "300000";
+
+export const remoteBridgeEntries: RemoteBridgeEntry[] = [
+  {
+    bridgeAdmin: "0x70d644877b7b73800E9073BCFCE981eAaB6Dbc21",
+    proxyOFT: "0xf8F46791E3dB29a029Ec6c9d946226f3c613e854",
+    dstChainId: undefined,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0x9C6C95632A8FB3A74f2fB4B7FfC50B003c992b96",
+    proxyOFT: "0x888E317606b4c590BBAD88653863e8B345702633",
+    dstChainId: LzChainId.ethereum,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0x52fcE05aDbf6103d71ed2BA8Be7A317282731831",
+    proxyOFT: "0x100D331C1B5Dcd41eACB1eCeD0e83DCEbf3498B2",
+    dstChainId: LzChainId.opbnbmainnet,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0xf5d81C6F7DAA3F97A6265C8441f92eFda22Ad784",
+    proxyOFT: "0x20cEa49B5F7a6DBD78cAE772CA5973eF360AA1e6",
+    dstChainId: LzChainId.arbitrumone,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0x2471043F05Cc41A6051dd6714DC967C7BfC8F902",
+    proxyOFT: "0x16a62B534e09A7534CD5847CFE5Bf6a4b0c1B116",
+    dstChainId: LzChainId.zksyncmainnet,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0x3c307DF1Bf3198a2417d9CA86806B307D147Ddf7",
+    proxyOFT: "0xbBe46bAec851355c3FC4856914c47eB6Cea0B8B4",
+    dstChainId: LzChainId.opmainnet,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+  {
+    bridgeAdmin: "0x6303FEcee7161bF959d65df4Afb9e1ba5701f78e",
+    proxyOFT: "0x3dd92fb51a5d381ae78e023dfb5dd1d45d2426cd",
+    dstChainId: LzChainId.basemainnet,
+    maxDailyLimit: parseUnits("0", 18),
+    maxSingleTransactionLimit: parseUnits("0", 18),
+    maxDailyReceiveLimit: parseUnits("0", 18),
+    maxSingleReceiveTransactionLimit: parseUnits("0", 18),
+  },
+];
+
+function getRemoteBridgeCommands(remoteBridgeEntry: RemoteBridgeEntry): RemoteBridgeCommand[] {
+  return [
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setTrustedRemoteAddress(uint16,bytes)",
+      params: [LzChainId.unichainmainnet, UNICHAIN_MAINNET_TRUSTED_REMOTE],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setMinDstGas(uint16,uint16,uint256)",
+      params: [LzChainId.unichainmainnet, 0, MIN_DST_GAS],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setMaxDailyLimit(uint16,uint256)",
+      params: [LzChainId.unichainmainnet, remoteBridgeEntry.maxDailyLimit],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setMaxSingleTransactionLimit(uint16,uint256)",
+      params: [LzChainId.unichainmainnet, remoteBridgeEntry.maxSingleTransactionLimit],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setMaxDailyReceiveLimit(uint16,uint256)",
+      params: [LzChainId.unichainmainnet, remoteBridgeEntry.maxDailyReceiveLimit],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+    {
+      target: remoteBridgeEntry.bridgeAdmin,
+      signature: "setMaxSingleReceiveTransactionLimit(uint16,uint256)",
+      params: [LzChainId.unichainmainnet, remoteBridgeEntry.maxSingleReceiveTransactionLimit],
+      dstChainId: remoteBridgeEntry.dstChainId,
+    },
+  ];
+}
+
+const vip455 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-455 ",
+    description: `#### Summary`,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(remoteBridgeEntries.flatMap(getRemoteBridgeCommands), meta, ProposalType.REGULAR);
+};
+
+export default vip455;
