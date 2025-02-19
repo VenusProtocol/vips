a9c56686634c66bc0768d479cfecf0e64285bdaf
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-02-19 15:46:49 +0100

    test: update the simulation block after receiving the expected funds

--- multisig/simulations/unichainmainnet/vip-007/index.ts
+++ multisig/simulations/unichainmainnet/vip-007/index.ts
@@ -3,7 +3,6 @@ import { Contract } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
-import { initMainnetUser } from "src/utils";
 import {
   RewardsDistributorConfig,
   checkRewardsDistributor,
@@ -27,10 +26,7 @@ import XVS_VAULT_ABI from "./abi/xvsvault.json";
 
 const { unichainmainnet } = NETWORK_ADDRESSES;
 
-const XVS_PROXY_OFT_DEST = "0x9c95f8aa28fFEB7ECdC0c407B9F632419c5daAF8";
-const TREASURY_AMOUNT = parseUnits("19500", 18);
-
-forking(8711530, async () => {
+forking(9227729, async () => {
   let xvs: Contract;
   let xvsVault: Contract;
   describe("Post VIP checks", async () => {
@@ -38,10 +34,6 @@ forking(8711530, async () => {
       xvs = new ethers.Contract(XVS, XVS_ABI, ethers.provider);
 
       xvsVault = new ethers.Contract(unichainmainnet.XVS_VAULT_PROXY, XVS_VAULT_ABI, ethers.provider);
-
-      const impersonateBridge = await initMainnetUser(XVS_PROXY_OFT_DEST, ethers.utils.parseEther("2"));
-
-      await xvs.connect(impersonateBridge).mint(unichainmainnet.VTREASURY, TREASURY_AMOUNT);
 
       await pretendExecutingVip(await vip007());
     });
