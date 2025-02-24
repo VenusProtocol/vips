b1d9ea90eba794645fe9dfafdda10f5ac2ee3e9a
Debugger022 <debugger022@gmail.com>
2025-02-24 12:05:27 +0530

    fix: import

--- simulations/vip-454/bscmainnet.ts
+++ simulations/vip-454/bscmainnet.ts
@@ -1,7 +1,7 @@
 import { expectEvents } from "../../src/utils";
 import { forking, testVip } from "../../src/vip-framework";
 import vip454Base from "../../vips/vip-454/bscmainnet";
-import vip454Zksync from "../../vips/vip-454/bscmainnetZksync";
+import vip454Zksync from "../../vips/vip-454/bscmainnet";
 import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
 
 forking(46847530, async () => {
