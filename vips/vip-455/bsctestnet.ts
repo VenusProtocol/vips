89082aebe0295b9bc23b5b85719de81b53cdeb68
web3rover <arsenal.narayan@gmail.com>
2025-02-25 13:30:22 +0400

    fix: wip - simulation

---
+++ vips/vip-455/bsctestnet.ts
@@ -0,0 +1,111 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { berachainbartio } = NETWORK_ADDRESSES;
+
+export const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+export const ACM_AGGREGATOR = "0x1ba10ca9a744131aD8428D719767816A693c3b71";
+export const ACM = "0xEf368e4c1f9ACC9241E66CD67531FEB195fF7536";
+export const PSR = "0xE4dD1B52c3D9d93d42B44cB77D769A9F73225012";
+export const PRIME = "0x3AAEd911374A60856a205cEf545F5Af49969aAa7";
+export const PRIME_LIQUIDITY_PROVIDER = "0x4039Ba7b3837FA9C2Ae95e59573f5CBfB4691c40";
+export const COMPTROLLER_CORE = "0x854Ba54c41bE5e54408EDF432e28A195Bcd3E88d";
+
+const PRIME_POOL_ID = 0;
+
+const vip453 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-4539",
+    description: ``,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: ACM,
+        signature: "grantRole(bytes32,address)",
+        params: [DEFAULT_ADMIN_ROLE, ACM_AGGREGATOR],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: ACM_AGGREGATOR,
+        signature: "executeGrantPermissions(uint256)",
+        params: [8],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: ACM,
+        signature: "revokeRole(bytes32,address)",
+        params: [DEFAULT_ADMIN_ROLE, ACM_AGGREGATOR],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: PRIME,
+        signature: "initializeV2(address)",
+        params: [berachainbartio.POOL_REGISTRY],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: PRIME_LIQUIDITY_PROVIDER,
+        signature: "acceptOwnership()",
+        params: [],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: PRIME,
+        signature: "acceptOwnership()",
+        params: [],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: PRIME_LIQUIDITY_PROVIDER,
+        signature: "setPrimeToken(address)",
+        params: [PRIME],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: berachainbartio.XVS_VAULT_PROXY,
+        signature: "setPrimeToken(address,address,uint256)",
+        params: [PRIME, berachainbartio.XVS, PRIME_POOL_ID],
+      },
+      {
+        target: PRIME,
+        signature: "setLimit(uint256,uint256)",
+        params: [
+          0, // irrevocable
+          0, // revocable
+        ],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: PRIME_LIQUIDITY_PROVIDER,
+        signature: "pauseFundsTransfer()",
+        params: [],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: berachainbartio.XVS_VAULT_PROXY,
+        signature: "resume()",
+        params: [],
+        dstChainId: LzChainId.berachainbartio,
+      },
+      {
+        target: COMPTROLLER_CORE,
+        signature: "setPrimeToken(address)",
+        params: [PRIME],
+        dstChainId: LzChainId.berachainbartio,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip453;
---
+++ vips/vip-455/permissions.ts
@@ -0,0 +1,90 @@
+import ACM_COMMANDS_AGGREATOR_ABI from "@venusprotocol/governance-contracts/artifacts/contracts/Utils/ACMCommandsAggregator.sol/ACMCommandsAggregator.json";
+import { ACMCommandsAggregator } from "@venusprotocol/governance-contracts/typechain/contracts/Utils/ACMCommandsAggregator";
+import { ethers } from "hardhat";
+import hre from "hardhat";
+import {
+  AccountType,
+  getPrimeLiquidityProviderPermissions,
+  getPrimePermissions,
+} from "src/permissions";
+
+interface Permissions {
+  [key: string]: string[][];
+}
+
+const BERACHAINBARTIO_GUARDIAN = "0xdf3b635d2b535f906BB02abb22AED71346E36a00";
+const PRIME = "0x3AAEd911374A60856a205cEf545F5Af49969aAa7";
+const PLP = "0x4039Ba7b3837FA9C2Ae95e59573f5CBfB4691c40";
+
+const grantPermissions: Permissions = {
+  berachainbartio: [
+    ...getPrimePermissions(PRIME),
+    ...getPrimeLiquidityProviderPermissions(PLP)
+  ],
+};
+
+const acmCommandsAggreator: any = {
+  berachainbartio: "0x1ba10ca9a744131aD8428D719767816A693c3b71",
+};
+
+const accounts: any = {
+  berachainbartio: {
+    NormalTimelock: "0x8699D418D8bae5CFdc566E4fce897B08bd9B03B0",
+    FastTrackTimelock: "0x723b7CB226d86bd89638ec77936463453a46C656",
+    CriticalTimelock: "0x920eeE8A5581e80Ca9C47CbF11B7A6cDB30204BD",
+    Guardian: BERACHAINBARTIO_GUARDIAN,
+  },
+};
+
+function splitPermissions(
+  array: ACMCommandsAggregator.PermissionStruct[],
+  chunkSize: number = 200,
+): ACMCommandsAggregator.PermissionStruct[][] {
+  const result: ACMCommandsAggregator.PermissionStruct[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+
+  return result;
+}
+
+async function main() {
+  const acmCommandsAggregator = await ethers.getContractAt(
+    ACM_COMMANDS_AGGREATOR_ABI.abi,
+    acmCommandsAggreator[hre.network.name],
+  );
+  const networkGrantPermissions = grantPermissions[hre.network.name];
+
+  for (const permission of networkGrantPermissions) {
+    if (Object.values(AccountType).includes(permission[2] as AccountType)) {
+      permission[2] = accounts[hre.network.name][permission[2]];
+    }
+  }
+
+  const _grantPermissions: ACMCommandsAggregator.PermissionStruct[] = networkGrantPermissions.map(permission => ({
+    contractAddress: permission[0],
+    functionSig: permission[1],
+    account: permission[2],
+  }));
+
+  const grantChunks = splitPermissions(_grantPermissions);
+  const grantIndexes: string[] = [];
+
+  for (const chunk of grantChunks) {
+    const tx = await acmCommandsAggregator.addGrantPermissions(chunk);
+    const receipt = await tx.wait();
+    const events = receipt.events?.filter((event: any) => event.event === "GrantPermissionsAdded");
+    grantIndexes.push(events?.[0].args?.index.toString());
+  }
+
+  console.log("Grant Permissions added with indexes: ", grantIndexes.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
