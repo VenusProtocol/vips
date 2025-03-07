3ffa04f25446b3eb69ed338abfe5affad69c8bda
Corey Rice <coreyarice@gmail.com>
2025-03-07 15:54:06 -0300

    feat: add crosschain risk config configuration for testnet

--- simulations/vip-xxx/bsctestnet.ts
+++ simulations/vip-xxx/bsctestnet.ts
@@ -1,32 +1,28 @@
-
-import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers"; import { expect } from "chai";
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
-import { parseUnits } from "ethers/lib/utils";
 
-import vipxxx, { COMPTROLLER, MARKET_CAP_RISK_STEWARD, ACCESS_CONTROL_MANAGER, ANY_TARGET_CONTRACT } from "../../vips/vip-xxx/bsctestnet";
+import vipxxx, {
+  ACCESS_CONTROL_MANAGER,
+  ANY_TARGET_CONTRACT,
+  COMPTROLLER,
+  MARKET_CAP_RISK_STEWARD,
+} from "../../vips/vip-xxx/bsctestnet";
 import ACCESS_CONTROL_MANAGER_ABI from "./abi/AccessControlManager.json";
-import VENUS_RISK_STEWARD_RECEIVER_ABI from "./abi/VenusRiskStewardReceiver.json";
 import COMPTROLLER_ABI from "./abi/Comproller.json";
 import ISOLATED_POOL_COMPTROLLER_ABI from "./abi/IsolatedPoolComptroller.json";
-
+import VENUS_RISK_STEWARD_RECEIVER_ABI from "./abi/VenusRiskStewardReceiver.json";
 
 forking(48650752, async () => {
   const provider = ethers.provider;
-  const acm = new ethers.Contract(
-    ACCESS_CONTROL_MANAGER,
-    ACCESS_CONTROL_MANAGER_ABI,
-    provider,
-  );
-  const comptroller = new ethers.Contract(
-    COMPTROLLER,
-    COMPTROLLER_ABI,
-    provider,
-  );
+  const acm = new ethers.Contract(ACCESS_CONTROL_MANAGER, ACCESS_CONTROL_MANAGER_ABI, provider);
+  const comptroller = new ethers.Contract(COMPTROLLER, COMPTROLLER_ABI, provider);
 
   const isolatedPoolComptroller = new ethers.Contract(
-    '0x1F4f0989C51f12DAcacD4025018176711f3Bf289',
+    "0x1F4f0989C51f12DAcacD4025018176711f3Bf289",
     ISOLATED_POOL_COMPTROLLER_ABI,
     provider,
   );
@@ -76,20 +72,27 @@ forking(48650752, async () => {
     it("Market Cap Risk Steward should be able to set supply and borrow caps on markets", async () => {
       await impersonateAccount(MARKET_CAP_RISK_STEWARD);
       await setBalance(MARKET_CAP_RISK_STEWARD, parseUnits("1000000", 18));
-      await expect(comptroller.connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
-        ._setMarketSupplyCaps(['0xeDaC03D29ff74b5fDc0CC936F6288312e1459BC6'], ['150000000000000000000000']))
-        .to.emit(comptroller, "NewSupplyCap");
-      await expect(comptroller.connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
-        ._setMarketBorrowCaps(['0xeDaC03D29ff74b5fDc0CC936F6288312e1459BC6'], ['55000000000000000000000']))
-        .to.emit(comptroller, "NewBorrowCap");
+      await expect(
+        comptroller
+          .connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
+          ._setMarketSupplyCaps(["0xeDaC03D29ff74b5fDc0CC936F6288312e1459BC6"], ["150000000000000000000000"]),
+      ).to.emit(comptroller, "NewSupplyCap");
+      await expect(
+        comptroller
+          .connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
+          ._setMarketBorrowCaps(["0xeDaC03D29ff74b5fDc0CC936F6288312e1459BC6"], ["55000000000000000000000"]),
+      ).to.emit(comptroller, "NewBorrowCap");
 
-
-      await expect(isolatedPoolComptroller.connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
-        .setMarketSupplyCaps(['0xef470AbC365F88e4582D8027172a392C473A5B53'], ['150000000000000000000000']))
-        .to.emit(isolatedPoolComptroller, "NewSupplyCap");
-      await expect(isolatedPoolComptroller.connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
-        .setMarketBorrowCaps(['0xef470AbC365F88e4582D8027172a392C473A5B53'], ['55000000000000000000000']))
-        .to.emit(isolatedPoolComptroller, "NewBorrowCap");
+      await expect(
+        isolatedPoolComptroller
+          .connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
+          .setMarketSupplyCaps(["0xef470AbC365F88e4582D8027172a392C473A5B53"], ["150000000000000000000000"]),
+      ).to.emit(isolatedPoolComptroller, "NewSupplyCap");
+      await expect(
+        isolatedPoolComptroller
+          .connect(await ethers.getSigner(MARKET_CAP_RISK_STEWARD))
+          .setMarketBorrowCaps(["0xef470AbC365F88e4582D8027172a392C473A5B53"], ["55000000000000000000000"]),
+      ).to.emit(isolatedPoolComptroller, "NewBorrowCap");
     });
   });
 });
--- vips/vip-xxx/bsctestnet.ts
+++ vips/vip-xxx/bsctestnet.ts
@@ -6,7 +6,7 @@ export const MARKET_CAP_RISK_STEWARD = "0x9b40390771cAeEa69DE55EEd176aeDC72d70cA
 export const ACCESS_CONTROL_MANAGER = "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA";
 export const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
 export const COMPTROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
-export const ANY_TARGET_CONTRACT = "0x0000000000000000000000000000000000000000"; 
+export const ANY_TARGET_CONTRACT = "0x0000000000000000000000000000000000000000";
 
 const vip457 = () => {
   const meta = {
@@ -29,13 +29,13 @@ Configure Risk Steward
       },
       {
         target: RISK_STEWARD_RECEIVER,
-          signature: "setRiskParameterConfig(string,address,uint256)",
-          params: ["supplyCap", MARKET_CAP_RISK_STEWARD, 86401],
+        signature: "setRiskParameterConfig(string,address,uint256)",
+        params: ["supplyCap", MARKET_CAP_RISK_STEWARD, 86401],
       },
       {
-        target:  RISK_STEWARD_RECEIVER,
-          signature: "setRiskParameterConfig(string,address,uint256)",
-          params: ["borrowCap", MARKET_CAP_RISK_STEWARD, 86401],
+        target: RISK_STEWARD_RECEIVER,
+        signature: "setRiskParameterConfig(string,address,uint256)",
+        params: ["borrowCap", MARKET_CAP_RISK_STEWARD, 86401],
       },
       {
         target: ACCESS_CONTROL_MANAGER,
@@ -57,10 +57,9 @@ Configure Risk Steward
         signature: "giveCallPermission(address,string,address)",
         params: [COMPTROLLER, "_setMarketSupplyCaps(address[],uint256[])", MARKET_CAP_RISK_STEWARD],
       },
-      
     ],
     meta,
-    ProposalType.REGULAR
+    ProposalType.REGULAR,
   );
 };
 
