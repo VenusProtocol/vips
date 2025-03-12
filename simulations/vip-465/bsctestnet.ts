7a29a5a43f6f75f3f37512f782f01cdb15a5fc42
web3rover <arsenal.narayan@gmail.com>
2025-03-12 15:47:11 +0400

    feat: added clisBNB market

---
+++ simulations/vip-465/bsctestnet.ts
@@ -0,0 +1,146 @@
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { formatUnits, parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testVip } from "src/vip-framework";
+import ERC20_ABI from "src/vip-framework/abi/erc20.json";
+import { checkIsolatedPoolsComptrollers } from "src/vip-framework/checks/checkIsolatedPoolsComptrollers";
+import { checkRiskParameters } from "src/vip-framework/checks/checkRiskParameters";
+import { checkVToken } from "src/vip-framework/checks/checkVToken";
+import { checkInterestRate } from "src/vip-framework/checks/interestRateModel";
+
+import vip465, {
+  COMPTROLLER_LIQUID_STAKED_BNB_POOL,
+  market,
+  token,
+  vBNBx_LiquidStakedBNB,
+  vWBNB_LiquidStakedBNB,
+  vankrBNB_LiquidStakedBNB,
+  vslisBNB_LiquidStakedBNB,
+  vstkBNB_LiquidStakedBNB,
+} from "../../vips/vip-465/bsctestnet";
+import POOL_REGISTRY_ABI from "./abi/PoolRegistry.json";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import VTOKEN_ABI from "./abi/vToken.json";
+
+const { POOL_REGISTRY, NORMAL_TIMELOCK, RESILIENT_ORACLE } = NETWORK_ADDRESSES["bsctestnet"];
+const BLOCKS_PER_YEAR = BigNumber.from("10512000");
+
+forking(49018130, async () => {
+  const provider = ethers.provider;
+  const oracle = new ethers.Contract(RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
+  const poolRegistry = new ethers.Contract(POOL_REGISTRY, POOL_REGISTRY_ABI, provider);
+  const comptroller = new ethers.Contract(COMPTROLLER_LIQUID_STAKED_BNB_POOL, COMPTROLLER_ABI, provider);
+  const vTokenContract = new ethers.Contract(market.vToken.address, VTOKEN_ABI, provider);
+
+  describe("vTokens deployment", () => {
+    it(`should deploy market`, async () => {
+      await checkVToken(market.vToken.address, market.vToken);
+    });
+  });
+
+  testVip("vip465", await vip465());
+
+  describe("Post-VIP behavior", () => {
+    describe("Oracle configuration", async () => {
+      it(`has the correct ${token.symbol} price`, async () => {
+        const price = await oracle.getPrice(token.address);
+        expect(price).to.be.eq(parseUnits("2234.756423853548610779", 18));
+      });
+    });
+
+    describe("PoolRegistry state", () => {
+      it(`should add ${market.vToken.symbol} to the Comptroller`, async () => {
+        const poolVTokens = await comptroller.getAllMarkets();
+        expect(poolVTokens).to.contain(market.vToken.address);
+      });
+
+      it(`should register ${market.vToken.symbol} in PoolRegistry`, async () => {
+        const registeredVToken = await poolRegistry.getVTokenForAsset(
+          COMPTROLLER_LIQUID_STAKED_BNB_POOL,
+          market.vToken.underlying.address,
+        );
+        expect(registeredVToken).to.equal(market.vToken.address);
+      });
+    });
+
+    describe("Risk parameters", () => {
+      checkRiskParameters(market.vToken.address, market.vToken, market.riskParameters);
+
+      it("should pause borrowing", async () => {
+        expect(await comptroller.actionPaused(market.vToken.address, 2)).to.equal(true);
+      });
+
+      it(`should have a protocol seize share ${market.riskParameters.protocolSeizeShare}`, async () => {
+        expect(await vTokenContract.protocolSeizeShareMantissa()).to.equal(market.riskParameters.protocolSeizeShare);
+      });
+    });
+
+    describe("Ownership and initial supply", () => {
+      const { vToken: vTokenSpec, initialSupply } = market;
+      const underlyingSymbol = token.symbol;
+
+      describe(`${vTokenSpec.symbol}`, () => {
+        it(`should have owner = normal timelock`, async () => {
+          expect(await vTokenContract.owner()).to.equal(NORMAL_TIMELOCK);
+        });
+
+        // Initial exchange rate should account for decimal transformations such that
+        // the string representation is the same (i.e. 1 vToken == 1 underlying)
+        const multiplier = 10 ** (vTokenSpec.underlying.decimals - vTokenSpec.decimals);
+        const vTokenSupply = initialSupply.amount.div(multiplier);
+        const underlyingSupplyString = formatUnits(initialSupply.amount, vTokenSpec.underlying.decimals);
+        const vTokenSupplyString = formatUnits(vTokenSupply, vTokenSpec.decimals);
+
+        it(`should have initial supply = ${vTokenSupplyString} ${vTokenSpec.symbol}`, async () => {
+          expect(await vTokenContract.balanceOf(initialSupply.vTokenReceiver)).to.equal(vTokenSupply);
+        });
+
+        it(`should have balance of underlying = ${underlyingSupplyString} ${underlyingSymbol}`, async () => {
+          const underlying = new ethers.Contract(vTokenSpec.underlying.address, ERC20_ABI, provider);
+          expect(await underlying.balanceOf(vTokenSpec.address)).to.equal(initialSupply.amount);
+        });
+      });
+    });
+
+    describe("Interest rates", () => {
+      checkInterestRate(
+        market.interestRateModel.address,
+        market.vToken.symbol,
+        market.interestRateModel,
+        BLOCKS_PER_YEAR,
+      );
+    });
+
+    checkIsolatedPoolsComptrollers();
+
+    it("check liquidation incentive", async () => {
+      const liquidationIncentiveMantissa = await comptroller.liquidationIncentiveMantissa();
+      expect(liquidationIncentiveMantissa).to.equal(parseUnits("1.025", 18));
+    });
+
+    it("check protocol seize share incentive", async () => {
+      let vtoken = new ethers.Contract(vankrBNB_LiquidStakedBNB, VTOKEN_ABI, provider);
+      let protocolSeizeShare = await vtoken.protocolSeizeShareMantissa();
+      expect(protocolSeizeShare).to.equal(parseUnits("0.0125", 18));
+
+      vtoken = new ethers.Contract(vBNBx_LiquidStakedBNB, VTOKEN_ABI, provider);
+      protocolSeizeShare = await vtoken.protocolSeizeShareMantissa();
+      expect(protocolSeizeShare).to.equal(parseUnits("0.0125", 18));
+
+      vtoken = new ethers.Contract(vWBNB_LiquidStakedBNB, VTOKEN_ABI, provider);
+      protocolSeizeShare = await vtoken.protocolSeizeShareMantissa();
+      expect(protocolSeizeShare).to.equal(parseUnits("0.0125", 18));
+
+      vtoken = new ethers.Contract(vstkBNB_LiquidStakedBNB, VTOKEN_ABI, provider);
+      protocolSeizeShare = await vtoken.protocolSeizeShareMantissa();
+      expect(protocolSeizeShare).to.equal(parseUnits("0.0125", 18));
+
+      vtoken = new ethers.Contract(vslisBNB_LiquidStakedBNB, VTOKEN_ABI, provider);
+      protocolSeizeShare = await vtoken.protocolSeizeShareMantissa();
+      expect(protocolSeizeShare).to.equal(parseUnits("0.0125", 18));
+    });
+  });
+});
---
+++ vips/vip-465/bsctestnet.ts
@@ -0,0 +1,208 @@
+import { BigNumber } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { POOL_REGISTRY, VTREASURY, RESILIENT_ORACLE } = NETWORK_ADDRESSES["bsctestnet"];
+
+export const COMPTROLLER_LIQUID_STAKED_BNB_POOL = "0x596B11acAACF03217287939f88d63b51d3771704";
+export const MOCK_PENDLE_PT_ORACLE = "0xa37A9127C302fEc17d456a6E1a5643a18a1779aD";
+export const PT_CLISBNB_PENDLE_ORACLE = "0x791E59Ac0495F69C983c17330C2F6154682E3c70";
+
+export const vankrBNB_LiquidStakedBNB = "0x57a664Dd7f1dE19545fEE9c86C949e3BF43d6D47";
+export const vBNBx_LiquidStakedBNB = "0x644A149853E5507AdF3e682218b8AC86cdD62951";
+export const vslisBNB_LiquidStakedBNB = "0xeffE7874C345aE877c1D893cd5160DDD359b24dA";
+export const vstkBNB_LiquidStakedBNB = "0x75aa42c832a8911B77219DbeBABBB40040d16987";
+export const vWBNB_LiquidStakedBNB = "0x231dED0Dfc99634e52EE1a1329586bc970d773b3";
+
+type Token = {
+  address: string;
+  decimals: number;
+  symbol: string;
+};
+
+export const token = {
+  address: "0x14AECeEc177085fd09EA07348B4E1F7Fcc030fA1",
+  decimals: 18,
+  symbol: "PT-clisBNB-24APR2025",
+};
+
+type Market = {
+  vToken: {
+    address: string;
+    name: string;
+    symbol: string;
+    underlying: Token;
+    decimals: number;
+    exchangeRate: BigNumber;
+    comptroller: string;
+  };
+  riskParameters: {
+    collateralFactor: BigNumber;
+    liquidationThreshold: BigNumber;
+    supplyCap: BigNumber;
+    borrowCap: BigNumber;
+    reserveFactor: BigNumber;
+    protocolSeizeShare: BigNumber;
+  };
+  initialSupply: {
+    amount: BigNumber;
+    vTokenReceiver: string;
+  };
+  interestRateModel: {
+    address: string;
+    base: string;
+    multiplier: string;
+    jump: string;
+    kink: string;
+  };
+};
+
+export const market: Market = {
+  vToken: {
+    address: "0x7C4890D673985CE22A4D38761473f190e434c956",
+    name: "Venus PT-clisBNB-24APR2025 (Liquid Staked BNB)",
+    symbol: "vPT-clisBNB-24APR2025_LiquidStakedBNB",
+    underlying: token,
+    decimals: 8,
+    exchangeRate: parseUnits("1", 28),
+    comptroller: COMPTROLLER_LIQUID_STAKED_BNB_POOL,
+  },
+  riskParameters: {
+    collateralFactor: parseUnits("0.8", 18),
+    liquidationThreshold: parseUnits("0.85", 18),
+    supplyCap: parseUnits("2000", 18),
+    borrowCap: parseUnits("0", 18),
+    reserveFactor: parseUnits("0.1", 18),
+    protocolSeizeShare: parseUnits("0.0125", 18),
+  },
+  initialSupply: {
+    amount: parseUnits("10", 18),
+    vTokenReceiver: VTREASURY,
+  },
+  interestRateModel: {
+    address: "0x2b37a63AFB834B6C47C319cDC5694bD104c86454",
+    base: "0",
+    multiplier: "0.035",
+    jump: "0.8",
+    kink: "0.8",
+  },
+};
+
+const vip465 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-465",
+    description: ``,
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: MOCK_PENDLE_PT_ORACLE,
+        signature: "setPtToSyRate(address,uint32,uint256)",
+        params: ["0x0000000000000000000000000000000000000002", 900, "956271978187548724"],
+      },
+      {
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [
+            token.address,
+            [PT_CLISBNB_PENDLE_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+            [true, false, false],
+          ],
+        ],
+      },
+      {
+        target: token.address,
+        signature: "faucet(uint256)",
+        params: [market.initialSupply.amount],
+      },
+      {
+        target: market.vToken.address,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: ["28800"],
+      },
+      {
+        target: market.vToken.address,
+        signature: "setReserveFactor(uint256)",
+        params: [market.riskParameters.reserveFactor],
+      },
+      {
+        target: market.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [POOL_REGISTRY, market.initialSupply.amount],
+      },
+      {
+        target: POOL_REGISTRY,
+        signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
+        params: [
+          [
+            market.vToken.address,
+            market.riskParameters.collateralFactor,
+            market.riskParameters.liquidationThreshold,
+            market.initialSupply.amount,
+            market.initialSupply.vTokenReceiver,
+            market.riskParameters.supplyCap,
+            market.riskParameters.borrowCap,
+          ],
+        ],
+      },
+      {
+        target: market.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [POOL_REGISTRY, 0],
+      },
+      {
+        target: COMPTROLLER_LIQUID_STAKED_BNB_POOL,
+        signature: "setActionsPaused(address[],uint8[],bool)",
+        params: [[market.vToken.address], [2], true],
+      },
+      {
+        target: market.vToken.address,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: vankrBNB_LiquidStakedBNB,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: vBNBx_LiquidStakedBNB,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: vslisBNB_LiquidStakedBNB,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: vstkBNB_LiquidStakedBNB,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: vWBNB_LiquidStakedBNB,
+        signature: "setProtocolSeizeShare(uint256)",
+        params: [market.riskParameters.protocolSeizeShare],
+      },
+      {
+        target: COMPTROLLER_LIQUID_STAKED_BNB_POOL,
+        signature: "setLiquidationIncentive(uint256)",
+        params: ["1025000000000000000"],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip465;
