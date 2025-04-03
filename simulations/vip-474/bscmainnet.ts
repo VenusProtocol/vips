d6061f58eb48c2fc34460caf1b22f575894ff510
Debugger022 <debugger022@gmail.com>
2025-04-03 15:43:52 +0530

    fix: lint issues

--- simulations/vip-474/bscmainnet.ts
+++ simulations/vip-474/bscmainnet.ts
@@ -5,46 +5,45 @@ import { ethers } from "hardhat";
 import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
+import {
+  BSCMAINNET_USDT,
+  RISK_FUND,
+  USDT_TOKENS_AMOUNT,
+  VANGUARD_TREASURY,
+  vip474,
+} from "../../vips/vip-474/bscmainnet";
 import RISK_FUND_ABI from "./abi/riskFund.json";
 import USDT_ABI from "./abi/usdt.json";
 
-import { 
-    RISK_FUND,
-    BSCMAINNET_USDT,  
-    VANGUARD_TREASURY,
-    USDT_TOKENS_AMOUNT,
-    vip474
-} from "../../vips/vip-474/bscmainnet";
-
-
 forking(48003323, async () => {
-    const provider = ethers.provider;
-    let usdt: Contract;
-    let treasuryBalanceBefore: BigNumber;
-    let treasuryBalanceAfter: BigNumber;
-    let riskFundBalanceBefore: BigNumber;
-    let riskFundBalanceAfter: BigNumber;
-  
-    before(async () => {
-        usdt = new ethers.Contract(BSCMAINNET_USDT, USDT_ABI, provider);
-        treasuryBalanceBefore = await usdt.balanceOf(VANGUARD_TREASURY);
-        riskFundBalanceBefore = await usdt.balanceOf(RISK_FUND);
+  const provider = ethers.provider;
+  let usdt: Contract;
+  let treasuryBalanceBefore: BigNumber;
+  let treasuryBalanceAfter: BigNumber;
+  let riskFundBalanceBefore: BigNumber;
+  let riskFundBalanceAfter: BigNumber;
+
+  before(async () => {
+    usdt = new ethers.Contract(BSCMAINNET_USDT, USDT_ABI, provider);
+    treasuryBalanceBefore = await usdt.balanceOf(VANGUARD_TREASURY);
+    riskFundBalanceBefore = await usdt.balanceOf(RISK_FUND);
+  });
+
+  testVip("VIP-474", await vip474(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(txResponse, [RISK_FUND_ABI], ["SweepTokenFromPool"], [1]);
+    },
+  });
+
+  describe("Post-VIP behavior", () => {
+    it("balance of treasury after sweeping tokens", async () => {
+      treasuryBalanceAfter = await usdt.balanceOf(VANGUARD_TREASURY);
+      expect(treasuryBalanceAfter).equals(treasuryBalanceBefore.add(USDT_TOKENS_AMOUNT));
     });
-  
-    testVip("VIP-474", await vip474(), {
-      callbackAfterExecution: async (txResponse: TransactionResponse) => {
-        await expectEvents(txResponse, [RISK_FUND_ABI], ["SweepTokenFromPool"], [1]);
-      },
-    });
-  
-    describe("Post-VIP behavior", () => {
-      it("balance of treasury after sweeping tokens", async () => {
-        treasuryBalanceAfter = await usdt.balanceOf(VANGUARD_TREASURY);
-        expect(treasuryBalanceAfter).equals(treasuryBalanceBefore.add(USDT_TOKENS_AMOUNT));
-      });
-      it("balance of risk fund after sweeping tokens", async () => {
-        riskFundBalanceAfter = await usdt.balanceOf(RISK_FUND);
-        expect(riskFundBalanceAfter).equals(riskFundBalanceBefore.sub(USDT_TOKENS_AMOUNT));
-      });
+
+    it("balance of risk fund after sweeping tokens", async () => {
+      riskFundBalanceAfter = await usdt.balanceOf(RISK_FUND);
+      expect(riskFundBalanceAfter).equals(riskFundBalanceBefore.sub(USDT_TOKENS_AMOUNT));
     });
   });
+});
--- vips/vip-474/bscmainnet.ts
+++ vips/vip-474/bscmainnet.ts
@@ -6,7 +6,7 @@ export const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 export const BSCMAINNET_USDT = "0x55d398326f99059ff775485246999027b3197955";
 export const BSCMAINNET_CORE_COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const VANGUARD_TREASURY = "0xf645a387180F5F74b968305dF81d54EB328d21ca";
-export const USDT_TOKENS_AMOUNT = parseUnits("200000",18);
+export const USDT_TOKENS_AMOUNT = parseUnits("200000", 18);
 
 export const vip474 = () => {
   const meta = {
@@ -23,8 +23,8 @@ export const vip474 = () => {
       {
         target: RISK_FUND,
         signature: "sweepTokenFromPool(address,address,address,uint256)",
-        params: [ BSCMAINNET_USDT, BSCMAINNET_CORE_COMPTROLLER, VANGUARD_TREASURY, USDT_TOKENS_AMOUNT],
-      }
+        params: [BSCMAINNET_USDT, BSCMAINNET_CORE_COMPTROLLER, VANGUARD_TREASURY, USDT_TOKENS_AMOUNT],
+      },
     ],
     meta,
     ProposalType.REGULAR,
