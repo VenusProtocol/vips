5ed42efdce6639b6959b88a5367475d3a20c6854
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-04-09 14:02:52 +0200

    feat: update the amount of USDT to be withdraw from the Risk Fund

--- vips/vip-474/bscmainnet.ts
+++ vips/vip-474/bscmainnet.ts
@@ -6,13 +6,13 @@ export const RISK_FUND = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 export const BSCMAINNET_USDT = "0x55d398326f99059ff775485246999027b3197955";
 export const BSCMAINNET_CORE_COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const VANGUARD_TREASURY = "0xf645a387180F5F74b968305dF81d54EB328d21ca";
-export const USDT_TOKENS_AMOUNT = parseUnits("200000", 18);
+export const USDT_TOKENS_AMOUNT = parseUnits("400000", 18);
 
 export const vip474 = () => {
   const meta = {
     version: "v2",
     title: "VIP-474",
-    description: "Withdraw 200,000 USDT from the Risk Fund contract on BNB Chain, to the Vanguard Treasury contract",
+    description: "Withdraw 400,000 USDT from the Risk Fund contract on BNB Chain, to the Vanguard Treasury contract",
     forDescription: "Execute this proposal",
     againstDescription: "Do not execute this proposal",
     abstainDescription: "Indifferent to execution",
