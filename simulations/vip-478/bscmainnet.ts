511ff9c2e35167e8530cad8d7482e61d2bf76359
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-04-11 20:14:48 +0200

    feat: unpause TUSD

--- simulations/vip-478/bscmainnet.ts
+++ simulations/vip-478/bscmainnet.ts
@@ -4,7 +4,8 @@ import { ethers } from "hardhat";
 import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
-import { Actions, COMPTROLLER, vFDUSD, vip478 } from "../../vips/vip-478/bscmainnet";
+import { Actions, COMPTROLLER, vFDUSD, vTUSD, vip478 } from "../../vips/vip-478/bscmainnet";
+import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
 import SETTER_FACET_ABI from "./abi/SetterFacet.json";
 
 forking(48263595, async () => {
@@ -15,7 +16,7 @@ forking(48263595, async () => {
   });
 
   describe("Pre-VIP behavior", async () => {
-    it("unpuased actions", async () => {
+    it("unpuased actions for FDUSD", async () => {
       let paused = await comptroller.actionPaused(vFDUSD, Actions.BORROW);
       expect(paused).to.be.true;
 
@@ -25,16 +26,33 @@ forking(48263595, async () => {
       paused = await comptroller.actionPaused(vFDUSD, Actions.ENTER_MARKET);
       expect(paused).to.be.true;
     });
+
+    it("unpuased actions for TUSD", async () => {
+      let paused = await comptroller.actionPaused(vTUSD, Actions.BORROW);
+      expect(paused).to.be.true;
+
+      paused = await comptroller.actionPaused(vTUSD, Actions.MINT);
+      expect(paused).to.be.true;
+
+      paused = await comptroller.actionPaused(vTUSD, Actions.ENTER_MARKET);
+      expect(paused).to.be.true;
+    });
   });
 
   testVip("VIP-478", await vip478(), {
     callbackAfterExecution: async txResponse => {
-      await expectEvents(txResponse, [SETTER_FACET_ABI], ["ActionPausedMarket"], [3]);
+      await expectEvents(txResponse, [SETTER_FACET_ABI], ["ActionPausedMarket"], [6]);
+      await expectEvents(
+        txResponse,
+        [OMNICHAIN_PROPOSAL_SENDER_ABI],
+        ["ExecuteRemoteProposal", "StorePayload"],
+        [1, 0],
+      );
     },
   });
 
   describe("Post-VIP behavior", async () => {
-    it("unpuased actions", async () => {
+    it("unpuased actions for FDUSD", async () => {
       let paused = await comptroller.actionPaused(vFDUSD, Actions.BORROW);
       expect(paused).to.be.false;
 
@@ -42,6 +60,17 @@ forking(48263595, async () => {
       expect(paused).to.be.false;
 
       paused = await comptroller.actionPaused(vFDUSD, Actions.ENTER_MARKET);
+      expect(paused).to.be.false;
+    });
+
+    it("unpuased actions for TUSD", async () => {
+      let paused = await comptroller.actionPaused(vTUSD, Actions.BORROW);
+      expect(paused).to.be.false;
+
+      paused = await comptroller.actionPaused(vTUSD, Actions.MINT);
+      expect(paused).to.be.false;
+
+      paused = await comptroller.actionPaused(vTUSD, Actions.ENTER_MARKET);
       expect(paused).to.be.false;
     });
   });
--- vips/vip-478/bscmainnet.ts
+++ vips/vip-478/bscmainnet.ts
@@ -1,8 +1,12 @@
-import { ProposalType } from "src/types";
+import { LzChainId, ProposalType } from "src/types";
 import { makeProposal } from "src/utils";
 
 export const COMPTROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
 export const vFDUSD = "0xC4eF4229FEc74Ccfe17B2bdeF7715fAC740BA0ba";
+export const vTUSD = "0xBf762cd5991cA1DCdDaC9ae5C638F5B5Dc3Bee6E";
+
+export const ETH_COMPTROLLER = "0x687a01ecF6d3907658f7A7c714749fAC32336D1B";
+export const ETH_vTUSD = "0x13eB80FDBe5C5f4a7039728E258A6f05fb3B912b";
 
 export const Actions = {
   MINT: 0,
@@ -25,7 +29,13 @@ export const vip478 = () => {
       {
         target: COMPTROLLER,
         signature: "_setActionsPaused(address[],uint8[],bool)",
-        params: [[vFDUSD], [Actions.MINT, Actions.BORROW, Actions.ENTER_MARKET], false],
+        params: [[vFDUSD, vTUSD], [Actions.MINT, Actions.BORROW, Actions.ENTER_MARKET], false],
+      },
+      {
+        target: ETH_COMPTROLLER,
+        signature: "setActionsPaused(address[],uint8[],bool)",
+        params: [[ETH_vTUSD], [Actions.MINT, Actions.BORROW, Actions.ENTER_MARKET], false],
+        dstChainId: LzChainId.ethereum,
       },
     ],
     meta,
