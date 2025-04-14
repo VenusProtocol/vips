70e30348865fae5b7d65a0c8cefa48a34c1573bd
GitGuru7 <aditya@venus.io>
2025-04-14 22:38:06 +0530

    feat: add opbnb mainnet vip

---
+++ simulations/vip-475/opbnbmainnet.ts
@@ -0,0 +1,97 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testForkedNetworkVipCommands } from "src/vip-framework";
+
+import {
+  OPBNBMAINNET_NEW_VTOKEN_IMPLEMENTATION,
+  OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION,
+  OPBNBMAINNET_VTOKEN_BEACON,
+  OPBNBMAINNET_XVS_VAULT_PROXY,
+  vip475,
+} from "../../vips/vip-475/bscmainnet";
+import XVS_VAULT_ABI from "./abi/XVSVault.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import PROXY_ABI from "./abi/manualProxy.json";
+import POOL_REGISTRY_ABI from "./abi/poolRegistry.json";
+import VTOKEN_ABI from "./abi/vtoken.json";
+import VTOKEN_BEACON_ABI from "./abi/vtokenBeacon.json";
+
+forking(52895873, async () => {
+  let xvsVault: Contract;
+  let xvsVaultProxy: Contract;
+  let vtokenBeacon: Contract;
+  let poolRegistry: Contract;
+
+  before(async () => {
+    xvsVault = await ethers.getContractAt(XVS_VAULT_ABI, OPBNBMAINNET_XVS_VAULT_PROXY);
+    xvsVaultProxy = await ethers.getContractAt(PROXY_ABI, OPBNBMAINNET_XVS_VAULT_PROXY);
+    vtokenBeacon = await ethers.getContractAt(VTOKEN_BEACON_ABI, OPBNBMAINNET_VTOKEN_BEACON);
+    poolRegistry = await ethers.getContractAt(POOL_REGISTRY_ABI, NETWORK_ADDRESSES.opbnbmainnet.POOL_REGISTRY);
+  });
+
+  describe("Pre-VIP behaviour", async () => {
+    describe("Old Implementations & old block rate", () => {
+      describe("XVS Vault", () => {
+        it("XVSVAULT should not point to new impl", async () => {
+          expect(await xvsVaultProxy.implementation()).not.equals(OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION);
+        });
+        it("XVSVAULT should have block rate of 31536000", async () => {
+          expect(await xvsVault.blocksOrSecondsPerYear()).equals(31536000);
+        });
+      });
+
+      describe("VToken", () => {
+        it("VToken beacon should not point to new impl", async () => {
+          expect(await vtokenBeacon.implementation()).not.equals(OPBNBMAINNET_NEW_VTOKEN_IMPLEMENTATION);
+        });
+        it("All Vtokens should have old block rate in IL", async () => {
+          const registeredPools = await poolRegistry.getAllPools();
+          for (const pool of registeredPools) {
+            const comptrollerAddress = pool.comptroller;
+            const comptroller = await ethers.getContractAt(COMPTROLLER_ABI, comptrollerAddress);
+            const poolVTokens = await comptroller.getAllMarkets();
+            for (const vtokenAddress of poolVTokens) {
+              const vtoken = await ethers.getContractAt(VTOKEN_ABI, vtokenAddress);
+              expect(await vtoken.blocksOrSecondsPerYear()).equals(31536000);
+            }
+          }
+        });
+      });
+    });
+  });
+
+  testForkedNetworkVipCommands("VIP-475", await vip475(), {});
+
+  describe("Post-VIP behavior", async () => {
+    describe("Should point to new impl and have updated block rate", () => {
+      describe("XVS Vault", async () => {
+        it("XVSVAULT should point to new impl", async () => {
+          expect(await xvsVaultProxy.implementation()).equals(OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION);
+        });
+        it("XVSVAULT should have block rate of 63072000", async () => {
+          expect(await xvsVault.blocksOrSecondsPerYear()).equals(63072000);
+        });
+      });
+
+      describe("VToken", () => {
+        it("VToken beacon should point to new impl", async () => {
+          expect(await vtokenBeacon.implementation()).equals(OPBNBMAINNET_NEW_VTOKEN_IMPLEMENTATION);
+        });
+        it("All Vtokens should have new block rate in IL", async () => {
+          const registeredPools = await poolRegistry.getAllPools();
+          for (const pool of registeredPools) {
+            const comptrollerAddress = pool.comptroller;
+            const comptroller = await ethers.getContractAt(COMPTROLLER_ABI, comptrollerAddress);
+            const poolVTokens = await comptroller.getAllMarkets();
+            for (const vtokenAddress of poolVTokens) {
+              const vtoken = await ethers.getContractAt(VTOKEN_ABI, vtokenAddress);
+              expect(await vtoken.blocksOrSecondsPerYear()).equals(63072000);
+            }
+          }
+        });
+      });
+    });
+  });
+});
---
+++ vips/vip-475/bscmainnet.ts
@@ -0,0 +1,67 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+export const OPBNBMAINNET_VTOKEN_BEACON = "0xfeD1d3a13597c5aBc893Af41ED5cb17e64c847c7";
+export const OPBNBMAINNET_NEW_VTOKEN_IMPLEMENTATION = "0x8Fa6460437F6Ba76808C93108F2134c5D6394D94";
+export const OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION = "0x16c4d000d27aFc73492fFeF5daE48B4Fa5e11847";
+export const OPBNBMAINNET_XVS_VAULT_PROXY = "0x7dc969122450749A8B0777c0e324522d67737988";
+export const OPBNBMAINNET_XVS = "0x3E2e61F1c075881F3fB8dd568043d8c221fd5c61";
+export const OPBNBMAINNET_ACM = "0xA60Deae5344F1152426cA440fb6552eA0e3005D6";
+const OPBNBMAINNET_NEW_BLOCK_RATE = 63072000;
+
+export const vip475 = () => {
+  const meta = {
+    version: "v2",
+    title:
+      "Reduce the distribution speeds and upgrade implementations considering the update of the blockrate on BNB and OPBNB Chain",
+    description: ``,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: OPBNBMAINNET_VTOKEN_BEACON,
+        signature: "upgradeTo(address)",
+        params: [OPBNBMAINNET_NEW_VTOKEN_IMPLEMENTATION],
+        dstChainId: LzChainId.opbnbmainnet,
+      },
+      {
+        target: OPBNBMAINNET_XVS_VAULT_PROXY,
+        signature: "_setPendingImplementation(address)",
+        params: [OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION],
+        dstChainId: LzChainId.opbnbmainnet,
+      },
+      {
+        target: OPBNBMAINNET_NEW_XVS_VAULT_IMPLEMENTATION,
+        signature: "_become(address)",
+        params: [OPBNBMAINNET_XVS_VAULT_PROXY],
+        dstChainId: LzChainId.opbnbmainnet,
+      },
+      {
+        target: OPBNBMAINNET_ACM,
+        signature: "giveCallPermission(address,string,address)",
+        params: [
+          OPBNBMAINNET_XVS_VAULT_PROXY,
+          "setBlocksPerYear(uint256)",
+          NETWORK_ADDRESSES.opbnbmainnet.NORMAL_TIMELOCK,
+        ],
+        dstChainId: LzChainId.opbnbmainnet,
+      },
+      // set new block rate in xvs vault
+      {
+        target: OPBNBMAINNET_XVS_VAULT_PROXY,
+        signature: "setBlocksPerYear(uint256)",
+        params: [OPBNBMAINNET_NEW_BLOCK_RATE],
+        dstChainId: LzChainId.opbnbmainnet,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip475;
