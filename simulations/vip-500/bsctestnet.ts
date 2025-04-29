06178e39e61f5aa402f133fd58c633f9902da980
Debugger022 <debugger022@gmail.com>
2025-04-29 16:16:13 +0530

    [VEN-3195]: add vip and simulation for bsctestnet

---
+++ simulations/vip-500/bsctestnet.ts
@@ -0,0 +1,126 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+import { checkVToken } from "src/vip-framework/checks/checkVToken";
+import { checkInterestRate } from "src/vip-framework/checks/interestRateModel";
+
+import {
+  PROTOCOL_SHARE_RESERVE,
+  REDUCE_RESERVES_BLOCK_DELTA,
+  USD1,
+  VUSD1,
+  marketSpec,
+  vip500,
+} from "../../vips/vip-500/bsctestnet";
+import VUSD1_ABI from "./abi/VBep20_ABI.json";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import USD1_ABI from "./abi/mockToken.json";
+import PRICE_ORACLE_ABI from "./abi/resilientOracle.json";
+
+const NORMAL_TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
+const RATE_MODEL = "0xaE81cA4E6eA7f7E72165dc68dBa2A0B1465E3B1f";
+const INITIAL_VTOKENS = parseUnits("4988.03272766", 8);
+
+const Actions = {
+  MINT: 0,
+  BORROW: 2,
+  ENTER_MARKET: 7,
+};
+
+forking(50999945, async () => {
+  let comptroller: Contract;
+  let usd1: Contract;
+  let vusd1: Contract;
+  let oracle: Contract;
+  const provider = ethers.provider;
+
+  before(async () => {
+    comptroller = new ethers.Contract(marketSpec.vToken.comptroller, COMPTROLLER_ABI, provider);
+    usd1 = new ethers.Contract(USD1, USD1_ABI, provider);
+    vusd1 = new ethers.Contract(VUSD1, VUSD1_ABI, provider);
+    oracle = new ethers.Contract(await comptroller.oracle(), PRICE_ORACLE_ABI, provider);
+  });
+
+  testVip("VIP-500-testnet Add USD1 Market", await vip500(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(
+        txResponse,
+        [COMPTROLLER_ABI, VUSD1_ABI],
+        [
+          "MarketListed",
+          "NewSupplyCap",
+          "NewBorrowCap",
+          "NewReserveFactor",
+          "NewProtocolShareReserve",
+          "NewReduceReservesBlockDelta",
+        ],
+        [1, 1, 1, 1, 1, 1],
+      );
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("adds a new USD1 market and set collateral factor to 75%", async () => {
+      const market = await comptroller.markets(VUSD1);
+      expect(market.isListed).to.equal(true);
+      expect(market.collateralFactorMantissa).to.equal(marketSpec.riskParameters.collateralFactor);
+    });
+    it("reserves factor equals 20%", async () => {
+      const reserveFactor = await vusd1.reserveFactorMantissa();
+      expect(reserveFactor).to.equal(marketSpec.riskParameters.reserveFactor);
+    });
+    it("sets protocol share reserve", async () => {
+      expect(await vusd1.protocolShareReserve()).to.equal(PROTOCOL_SHARE_RESERVE);
+    });
+    it("sets Reduce Reserves Block Delta to 28800", async () => {
+      expect(await vusd1.reduceReservesBlockDelta()).to.equal(REDUCE_RESERVES_BLOCK_DELTA);
+    });
+    it("sets the supply cap", async () => {
+      const newCap = await comptroller.supplyCaps(VUSD1);
+      expect(newCap).to.equal(marketSpec.riskParameters.supplyCap);
+    });
+    it("sets the borrow cap", async () => {
+      const newCap = await comptroller.borrowCaps(VUSD1);
+      expect(newCap).to.equal(marketSpec.riskParameters.borrowCap);
+    });
+    it("does not leave usd1 balance on the address of the timelock", async () => {
+      const timelockBalance = await usd1.balanceOf(NORMAL_TIMELOCK);
+      expect(timelockBalance).to.equal(0);
+    });
+    it("does not leave vusd1 balance on the address of the timelock", async () => {
+      const timelockBalance = await vusd1.balanceOf(NORMAL_TIMELOCK);
+      expect(timelockBalance).to.equal(0);
+    });
+    it("moves INITIAL_VTOKENS vusd1 to VENUS_TREASURY", async () => {
+      const vTokenReceiverBalance = await vusd1.balanceOf(marketSpec.initialSupply.vTokenReceiver);
+      expect(vTokenReceiverBalance).to.equal(INITIAL_VTOKENS);
+    });
+    it("sets the admin to normal timelock", async () => {
+      expect(await vusd1.admin()).to.equal(NORMAL_TIMELOCK);
+    });
+    it("get correct price from oracle ", async () => {
+      const price = await oracle.getUnderlyingPrice(VUSD1);
+      expect(price).to.equal(parseUnits("1", 18));
+    });
+    it("enter market paused", async () => {
+      const borrowPaused = await comptroller.actionPaused(VUSD1, Actions.ENTER_MARKET);
+      expect(borrowPaused).to.equal(true);
+    });
+
+    await checkInterestRate(RATE_MODEL, "USD1", { base: "0", kink: "0.8", multiplier: "0.1", jump: "2.5" });
+    await checkVToken(VUSD1, {
+      name: "Venus USD1",
+      symbol: "vUSD1",
+      decimals: 8,
+      underlying: USD1,
+      // In exchangeRateStoredInternal()
+      // If there are no tokens minted: exchangeRate = initialExchangeRate
+      // Otherwise: exchangeRate = (totalCash + totalBorrows - totalReserves) / totalSupply
+      exchangeRate: parseUnits("1.0000000000014954307010931156", 28),
+      comptroller: marketSpec.vToken.comptroller,
+    });
+  });
+});
---
+++ vips/vip-500/bsctestnet.ts
@@ -0,0 +1,153 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { RESILIENT_ORACLE, REDSTONE_ORACLE, UNITROLLER, ACCESS_CONTROL_MANAGER, VTREASURY } =
+  NETWORK_ADDRESSES.bsctestnet;
+export const PROTOCOL_SHARE_RESERVE = "0x25c7c7D6Bf710949fD7f03364E9BA19a1b3c10E3";
+export const USD1 = "0x70d57B23347dB44AED9AB61A9c589A7877a53B4D";
+export const VUSD1 = "0x18F85F4517201528921c6452fA8bC5276a07ca3C";
+export const REDUCE_RESERVES_BLOCK_DELTA = "28800";
+
+export const marketSpec = {
+  vToken: {
+    address: VUSD1,
+    name: "Venus USD1",
+    symbol: "vUSD1",
+    underlying: {
+      address: USD1,
+      decimals: 18,
+      symbol: "USD1",
+    },
+    decimals: 8,
+    exchangeRate: parseUnits("1", 28),
+    comptroller: UNITROLLER,
+    isLegacyPool: true,
+  },
+  interestRateModel: {
+    model: "jump",
+    baseRatePerYear: "0",
+    multiplierPerYear: "0.1",
+    jumpMultiplierPerYear: "2.5",
+    kink: "0.8",
+  },
+  initialSupply: {
+    amount: parseUnits("4988.032727667459257279", 18),
+    vTokenReceiver: VTREASURY,
+  },
+  riskParameters: {
+    supplyCap: parseUnits("16000000", 18),
+    borrowCap: parseUnits("14400000", 18),
+    collateralFactor: parseUnits("0", 18),
+    reserveFactor: parseUnits("0.2", 18),
+  },
+};
+
+export const vip500 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-3195 [BNB Chain] Add support for USD1 on Venus Core Pool",
+    description: "",
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      // Configure Oracle
+      {
+        target: REDSTONE_ORACLE,
+        signature: "setDirectPrice(address,uint256)",
+        params: [marketSpec.vToken.underlying.address, parseUnits("1", 18)],
+      },
+
+      {
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3]))",
+        params: [
+          [
+            marketSpec.vToken.underlying.address,
+            [REDSTONE_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+            [true, false, false],
+          ],
+        ],
+      },
+
+      // Add Market
+      {
+        target: marketSpec.vToken.comptroller,
+        signature: "_supportMarket(address)",
+        params: [marketSpec.vToken.address],
+      },
+
+      {
+        target: marketSpec.vToken.comptroller,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[marketSpec.vToken.address], [marketSpec.riskParameters.supplyCap]],
+      },
+
+      {
+        target: marketSpec.vToken.comptroller,
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [[marketSpec.vToken.address], [marketSpec.riskParameters.borrowCap]],
+      },
+
+      {
+        target: marketSpec.vToken.address,
+        signature: "setAccessControlManager(address)",
+        params: [ACCESS_CONTROL_MANAGER],
+      },
+      {
+        target: marketSpec.vToken.address,
+        signature: "setProtocolShareReserve(address)",
+        params: [PROTOCOL_SHARE_RESERVE],
+      },
+
+      {
+        target: marketSpec.vToken.address,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: [REDUCE_RESERVES_BLOCK_DELTA],
+      },
+      {
+        target: marketSpec.vToken.address,
+        signature: "_setReserveFactor(uint256)",
+        params: [marketSpec.riskParameters.reserveFactor],
+      },
+      {
+        target: marketSpec.vToken.underlying.address,
+        signature: "faucet(uint256)",
+        params: [marketSpec.initialSupply.amount],
+      },
+
+      {
+        target: marketSpec.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [marketSpec.vToken.address, marketSpec.initialSupply.amount],
+      },
+      {
+        target: marketSpec.vToken.address,
+        signature: "mintBehalf(address,uint256)",
+        params: [marketSpec.initialSupply.vTokenReceiver, marketSpec.initialSupply.amount],
+      },
+
+      {
+        target: marketSpec.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [marketSpec.vToken.address, 0],
+      },
+
+      {
+        target: marketSpec.vToken.comptroller,
+        signature: "_setActionsPaused(address[],uint8[],bool)",
+        params: [[marketSpec.vToken.address], [7], true],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip500;
