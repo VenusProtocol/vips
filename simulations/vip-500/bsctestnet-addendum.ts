f2a641ca13ee80f8491afd4be641b3f20c13fc4b
Debugger022 <debugger022@gmail.com>
2025-05-06 19:15:34 +0530

    fix: lint

--- simulations/vip-500/bsctestnet-addendum.ts
+++ simulations/vip-500/bsctestnet-addendum.ts
@@ -3,39 +3,34 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { forking, testVip } from "src/vip-framework";
 
-import {
-    VUSD1,
-    UNITROLLER,
-    vip500Addendum,
-} from "../../vips/vip-500/bsctestnet-addendum";
+import { UNITROLLER, VUSD1, vip500Addendum } from "../../vips/vip-500/bsctestnet-addendum";
 import COMPTROLLER_ABI from "./abi/comptroller.json";
 
-
 const Actions = {
-    ENTER_MARKET: 7
+  ENTER_MARKET: 7,
 };
 
 forking(51415815, async () => {
-    let comptroller: Contract
-    const provider = ethers.provider;
+  let comptroller: Contract;
+  const provider = ethers.provider;
 
-    before(async () => {
-        comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_ABI, provider);
+  before(async () => {
+    comptroller = new ethers.Contract(UNITROLLER, COMPTROLLER_ABI, provider);
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("enter market paused", async () => {
+      const borrowPaused = await comptroller.actionPaused(VUSD1, Actions.ENTER_MARKET);
+      expect(borrowPaused).to.equal(true);
     });
+  });
 
-    describe("Pre-VIP behavior", async () => {
-        it("enter market paused", async () => {
-            const borrowPaused = await comptroller.actionPaused(VUSD1, Actions.ENTER_MARKET);
-            expect(borrowPaused).to.equal(true);
-        });
+  testVip("VIP-500-testnet-Addendum: Add USD1 Market", await vip500Addendum(), {});
+
+  describe("Post-VIP behavior", async () => {
+    it("enter market not paused", async () => {
+      const borrowPaused = await comptroller.actionPaused(VUSD1, Actions.ENTER_MARKET);
+      expect(borrowPaused).to.equal(false);
     });
-
-    testVip("VIP-500-testnet-Addendum: Add USD1 Market", await vip500Addendum(), {});
-
-    describe("Post-VIP behavior", async () => {
-        it("enter market not paused", async () => {
-            const borrowPaused = await comptroller.actionPaused(VUSD1, Actions.ENTER_MARKET);
-            expect(borrowPaused).to.equal(false);
-        });
-    });
+  });
 });
