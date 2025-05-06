1d3ed196525e65320d32da596430c34086ff1b39
web3rover <arsenal.narayan@gmail.com>
2025-05-06 17:49:01 +0400

    fix: added unichain mainnet simulation

---
+++ simulations/vip-492/unichainmainnet.ts
@@ -0,0 +1,74 @@
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testForkedNetworkVipCommands } from "src/vip-framework";
+
+import vip491, { REDSTONE_ORACLE_UNICHAIN, RESILIENT_ORACLE_UNICHAIN } from "../../vips/vip-492/bscmainnet";
+import REDSTONE_ORACLE_ABI from "./abi/RedstoneOracle.json";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+import ERC20_ABI from "./abi/ERC20.json";
+import { setMaxStalePeriod, setMaxStalePeriodInChainlinkOracle, setRedstonePrice } from "src/utils";
+
+const { unichainmainnet } = NETWORK_ADDRESSES;
+
+forking(15789111, async () => {
+  const provider = ethers.provider;
+
+  await impersonateAccount(unichainmainnet.NORMAL_TIMELOCK);
+  await setBalance(unichainmainnet.NORMAL_TIMELOCK, ethers.utils.parseEther("1000000"));
+  const signer = await ethers.getSigner(unichainmainnet.NORMAL_TIMELOCK);
+
+  const resilientOracle = new ethers.Contract(RESILIENT_ORACLE_UNICHAIN, RESILIENT_ORACLE_ABI, provider);
+  const redstoneOracle = new ethers.Contract(REDSTONE_ORACLE_UNICHAIN, REDSTONE_ORACLE_ABI, signer);
+
+  describe("Pre-VIP behaviour", async () => {
+    it("check USDC price", async () => {
+      expect(await resilientOracle.getPrice("0x078D782b760474a361dDA0AF3839290b0EF57AD6")).to.equal(
+        parseUnits("0.99998588", 30),
+      );
+    });
+
+    it("check WETH price", async () => {
+      expect(await resilientOracle.getPrice("0x4200000000000000000000000000000000000006")).to.equal(
+        parseUnits("1766.67625532", 18),
+      );
+    });
+
+    it("check UNI price", async () => {
+      expect(await resilientOracle.getPrice("0x8f187aA05619a017077f5308904739877ce9eA21")).to.equal(
+        parseUnits("4.8735125", 18),
+      );
+    });
+  });
+
+  testForkedNetworkVipCommands("vip491", await vip491());
+
+  describe("Post-VIP behaviour", async () => {
+    it("check USDC price", async () => {
+      const token = new ethers.Contract("0x078D782b760474a361dDA0AF3839290b0EF57AD6", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token);
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("0.99998588", 30),
+      );
+    });
+
+    it("check WETH price", async () => {
+      const token = new ethers.Contract("0x4200000000000000000000000000000000000006", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token);
+
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("1766.67625532", 18),
+      );
+    });
+
+    it("check UNI price", async () => {
+      const token = new ethers.Contract("0x8f187aA05619a017077f5308904739877ce9eA21", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token);
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("4.8735125", 18),
+      );
+    });
+  });
+});
