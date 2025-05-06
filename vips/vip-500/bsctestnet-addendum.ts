72bc6e0e801555dc2ed06191b920f88ae08263d0
Debugger022 <debugger022@gmail.com>
2025-05-06 19:11:16 +0530

    feat: unpause enter market for bsctestnet

---
+++ vips/vip-500/bsctestnet-addendum.ts
@@ -0,0 +1,32 @@
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+export const { UNITROLLER } = NETWORK_ADDRESSES.bsctestnet;
+
+export const VUSD1 = "0x519e61D2CDA04184FB086bbD2322C1bfEa0917Cf";
+
+export const vip500Addendum = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-500 [BNB Chain] Unpause enter market action for USD1 on Venus Core Pool",
+    description: "",
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: UNITROLLER,
+        signature: "_setActionsPaused(address[],uint8[],bool)",
+        params: [[VUSD1], [7], false],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip500Addendum;
