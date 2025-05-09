294096f2e051bffde2c15be4620b5b5613b8ce49
web3rover <arsenal.narayan@gmail.com>
2025-05-09 14:08:41 +0400

    feat: added testnet vip

---
+++ simulations/vip-493/arbitrumsepolia.ts
@@ -0,0 +1,147 @@
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testForkedNetworkVipCommands } from "src/vip-framework";
+
+import vip491, { RESILIENT_ORACLE_ARBITRUM_SEPOLIA } from "../../vips/vip-493/bsctestnet";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+import { setMaxStalePeriod } from "src/utils";
+import ERC20_ABI from "./abi/ERC20.json";
+
+const { arbitrumsepolia } = NETWORK_ADDRESSES;
+
+forking(151083977, async () => {
+  const provider = ethers.provider;
+
+  await impersonateAccount(arbitrumsepolia.NORMAL_TIMELOCK);
+  await setBalance(arbitrumsepolia.NORMAL_TIMELOCK, ethers.utils.parseEther("1000000"));
+  const resilientOracle = new ethers.Contract(RESILIENT_ORACLE_ARBITRUM_SEPOLIA, RESILIENT_ORACLE_ABI, provider);
+
+  describe("Pre-VIP behaviour", async () => {
+    it("check ARB price", async () => {
+      expect(await resilientOracle.getPrice("0x4371bb358aB5cC192E481543417D2F67b8781731")).to.equal(
+        parseUnits("0.382117", 18),
+      );
+    });
+
+    it("check gmBTC price", async () => {
+      expect(await resilientOracle.getPrice("0xbd3AAd064295dcA0f45fab4C6A5adFb0D23a19D2")).to.equal(
+        parseUnits("2.32639502", 18),
+      );
+    });
+
+    it("check gmETH price", async () => {
+      expect(await resilientOracle.getPrice("0x0012875a7395a293Adfc9b5cDC2Cfa352C4cDcD3")).to.equal(
+        parseUnits("1.75254694", 18),
+      );
+    });
+
+    it("check USDC price", async () => {
+      expect(await resilientOracle.getPrice("0x86f096B1D970990091319835faF3Ee011708eAe8")).to.equal(
+        parseUnits("1.00003147", 30),
+      );
+    });
+
+    it("check USDT price", async () => {
+      expect(await resilientOracle.getPrice("0xf3118a17863996B9F2A073c9A66Faaa664355cf8")).to.equal(
+        parseUnits("0.99995", 30),
+      );
+    });
+
+    it("check WBTC price", async () => {
+      // set to 180 seconds but not yet updated from 1 hour
+      const token = new ethers.Contract("0xFb8d93FD3Cf18386a5564bb5619cD1FdB130dF7D", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token)
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("103594.52", 28),
+      );
+    });
+
+    it("check WETH price", async () => {
+      // set to 180 seconds but not yet updated from 1 hour
+      const token = new ethers.Contract("0x980B62Da83eFf3D4576C647993b0c1D7faf17c73", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token)
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("2382.70878478", 18),
+      );
+    });
+
+    it("check weETH price", async () => {
+      expect(await resilientOracle.getPrice("0x243141DBff86BbB0a082d790fdC21A6ff615Fa34")).to.equal(
+        parseUnits("2620.979663258", 18),
+      );
+    });
+
+    it("check wstETH price", async () => {
+      expect(await resilientOracle.getPrice("0x4A9dc15aA6094eF2c7eb9d9390Ac1d71f9406fAE")).to.equal(
+        parseUnits("2620.979663258", 18),
+      );
+    });
+  });
+
+  testForkedNetworkVipCommands("vip491", await vip491());
+
+  describe("Post-VIP behaviour", async () => {
+    it("check ARB price", async () => {
+      expect(await resilientOracle.getPrice("0x4371bb358aB5cC192E481543417D2F67b8781731")).to.equal(
+        parseUnits("0.382117", 18),
+      );
+    });
+
+    it("check gmBTC price", async () => {
+      expect(await resilientOracle.getPrice("0xbd3AAd064295dcA0f45fab4C6A5adFb0D23a19D2")).to.equal(
+        parseUnits("2.32639502", 18),
+      );
+    });
+
+    it("check gmETH price", async () => {
+      expect(await resilientOracle.getPrice("0x0012875a7395a293Adfc9b5cDC2Cfa352C4cDcD3")).to.equal(
+        parseUnits("1.75254694", 18),
+      );
+    });
+
+    it("check USDC price", async () => {
+      expect(await resilientOracle.getPrice("0x86f096B1D970990091319835faF3Ee011708eAe8")).to.equal(
+        parseUnits("1.00003147", 30),
+      );
+    });
+
+    it("check USDT price", async () => {
+      expect(await resilientOracle.getPrice("0xf3118a17863996B9F2A073c9A66Faaa664355cf8")).to.equal(
+        parseUnits("0.99995", 30),
+      );
+    });
+
+    it("check WBTC price", async () => {
+      // set to 180 seconds but not yet updated from 1 hour
+      const token = new ethers.Contract("0xFb8d93FD3Cf18386a5564bb5619cD1FdB130dF7D", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token)
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("103594.52", 28),
+      );
+    });
+
+    it("check WETH price", async () => {
+      // set to 180 seconds but not yet updated from 1 hour
+      const token = new ethers.Contract("0x980B62Da83eFf3D4576C647993b0c1D7faf17c73", ERC20_ABI, provider);
+      await setMaxStalePeriod(resilientOracle, token)
+      expect(await resilientOracle.getPrice(token.address)).to.equal(
+        parseUnits("2382.70878478", 18),
+      );
+    });
+
+    it("check weETH price", async () => {
+      expect(await resilientOracle.getPrice("0x243141DBff86BbB0a082d790fdC21A6ff615Fa34")).to.equal(
+        parseUnits("2620.979663258", 18),
+      );
+    });
+
+    it("check wstETH price", async () => {
+      expect(await resilientOracle.getPrice("0x4A9dc15aA6094eF2c7eb9d9390Ac1d71f9406fAE")).to.equal(
+        parseUnits("2620.979663258", 18),
+      );
+    });
+  });
+});
---
+++ simulations/vip-493/zksyncsepolia.ts
@@ -0,0 +1,135 @@
+import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testForkedNetworkVipCommands } from "src/vip-framework";
+
+import vip491, { RESILIENT_ORACLE_ZKSYNC_SEPOLIA } from "../../vips/vip-493/bsctestnet";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+import { setMaxStalePeriod } from "src/utils";
+import ERC20_ABI from "./abi/ERC20.json";
+
+const { zksyncsepolia } = NETWORK_ADDRESSES;
+
+forking(151083977, async () => {
+  const provider = ethers.provider;
+
+  await impersonateAccount(zksyncsepolia.NORMAL_TIMELOCK);
+  await setBalance(zksyncsepolia.NORMAL_TIMELOCK, ethers.utils.parseEther("1000000"));
+  const resilientOracle = new ethers.Contract(RESILIENT_ORACLE_ZKSYNC_SEPOLIA, RESILIENT_ORACLE_ABI, provider);
+
+  describe("Pre-VIP behaviour", async () => {
+    it("check USDC price", async () => {
+      expect(await resilientOracle.getPrice("0x512F8b4a3c466a30e8c9BAC9c64638dd710968c2")).to.equal(
+        parseUnits("1.00007109", 30),
+      );
+    });
+
+    it("check USDC.e price", async () => {
+      expect(await resilientOracle.getPrice("0xF98780C8a0843829f98e624d83C3FfDDf43BE984")).to.equal(
+        parseUnits("1.00007109", 30),
+      );
+    });
+
+    it("check USDT price", async () => {
+      expect(await resilientOracle.getPrice("0x9Bf62C9C6AaB7AB8e01271f0d7A401306579709B")).to.equal(
+        parseUnits("1.00011", 30),
+      );
+    });
+
+    it("check WBTC price", async () => {
+      expect(await resilientOracle.getPrice("0xeF891B3FA37FfD83Ce8cC7b682E4CADBD8fFc6F0")).to.equal(
+        parseUnits("102900.2031", 28),
+      );
+    });
+
+    it("check WETH price", async () => {
+      expect(await resilientOracle.getPrice("0x53F7e72C7ac55b44c7cd73cC13D4EF4b121678e6")).to.equal(
+        parseUnits("2340.22575314", 18),
+      );
+    });
+
+    it("check wstETH price", async () => {
+      expect(await resilientOracle.getPrice("0x8507bb4F4f0915D05432011E384850B65a7FCcD1")).to.equal(
+        parseUnits("2574.248328454", 18),
+      );
+    });
+
+    it("check wUSDM price", async () => {
+      expect(await resilientOracle.getPrice("0x0b3C8fB109f144f6296bF4Ac52F191181bEa003a")).to.equal(
+        parseUnits("1.1", 18),
+      );
+    });
+
+    it("check ZK price", async () => {
+      expect(await resilientOracle.getPrice("0x8A2E9048F5d658E88D6eD89DdD1F3B5cA0250B9F")).to.equal(
+        parseUnits("0.2", 18),
+      );
+    });
+
+    it("check ZKETH price", async () => {
+      expect(await resilientOracle.getPrice("0x13231E8B60BE0900fB3a3E9dc52C2b39FA4794df")).to.equal(
+        parseUnits("2351.9268819057", 18),
+      );
+    });
+  });
+
+  testForkedNetworkVipCommands("vip491", await vip491());
+
+  describe("Post-VIP behaviour", async () => {
+    it("check USDC price", async () => {
+      expect(await resilientOracle.getPrice("0x512F8b4a3c466a30e8c9BAC9c64638dd710968c2")).to.equal(
+        parseUnits("1.00007109", 30),
+      );
+    });
+
+    it("check USDC.e price", async () => {
+      expect(await resilientOracle.getPrice("0xF98780C8a0843829f98e624d83C3FfDDf43BE984")).to.equal(
+        parseUnits("1.00007109", 30),
+      );
+    });
+
+    it("check USDT price", async () => {
+      expect(await resilientOracle.getPrice("0x9Bf62C9C6AaB7AB8e01271f0d7A401306579709B")).to.equal(
+        parseUnits("1.00011", 30),
+      );
+    });
+
+    it("check WBTC price", async () => {
+      expect(await resilientOracle.getPrice("0xeF891B3FA37FfD83Ce8cC7b682E4CADBD8fFc6F0")).to.equal(
+        parseUnits("102900.2031", 28),
+      );
+    });
+
+    it("check WETH price", async () => {
+      expect(await resilientOracle.getPrice("0x53F7e72C7ac55b44c7cd73cC13D4EF4b121678e6")).to.equal(
+        parseUnits("2340.22575314", 18),
+      );
+    });
+
+    it("check wstETH price", async () => {
+      expect(await resilientOracle.getPrice("0x8507bb4F4f0915D05432011E384850B65a7FCcD1")).to.equal(
+        parseUnits("2574.248328454", 18),
+      );
+    });
+
+    it("check wUSDM price", async () => {
+      expect(await resilientOracle.getPrice("0x0b3C8fB109f144f6296bF4Ac52F191181bEa003a")).to.equal(
+        parseUnits("1.1", 18),
+      );
+    });
+
+    it("check ZK price", async () => {
+      expect(await resilientOracle.getPrice("0x8A2E9048F5d658E88D6eD89DdD1F3B5cA0250B9F")).to.equal(
+        parseUnits("0.2", 18),
+      );
+    });
+
+    it("check ZKETH price", async () => {
+      expect(await resilientOracle.getPrice("0x13231E8B60BE0900fB3a3E9dc52C2b39FA4794df")).to.equal(
+        parseUnits("2351.9268819057", 18),
+      );
+    });
+  });
+});
