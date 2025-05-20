70d5c10f6040074571c965fde253587de022383e
Debugger022 <debugger022@gmail.com>
2025-05-20 19:39:43 +0530

    feat: add simulation of ERC4626Factory for opbnbtestnet

---
+++ simulations/vip-500/opbnbtestnet.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { expectEvents } from "src/utils";
+import { forking, testForkedNetworkVipCommands } from "src/vip-framework";
+
+import vip500, { ERC4626_FACTORY_OPBNB } from "../../vips/vip-500/bsctestnet";
+import ERC4626FACTORY_ABI from "./abi/ERC4626Factory.json";
+
+const { opbnbtestnet } = NETWORK_ADDRESSES;
+const ACM = "0x049f77F7046266d27C3bC96376f53C17Ef09c986";
+const DEPLOYER = "0x4c65A0342C8E632209147345c973A18f6A8c1979";
+const BLOCK_NUMBER = 65016997;
+
+forking(BLOCK_NUMBER, async () => {
+  const provider = ethers.provider;
+  let erc4626Factory: Contract;
+
+  before(async () => {
+    erc4626Factory = new ethers.Contract(ERC4626_FACTORY_OPBNB, ERC4626FACTORY_ABI, provider);
+  });
+
+  describe("Pre-VIP behaviour", async () => {
+    it("ERC4626Factory owner should be the Deployer", async () => {
+      expect(await erc4626Factory.owner()).to.be.equals(DEPLOYER);
+    });
+
+    it("ERC4626Factory pending owner should be Normal Timelock", async () => {
+      expect(await erc4626Factory.pendingOwner()).to.be.equals(opbnbtestnet.NORMAL_TIMELOCK);
+    });
+
+    it("ERC4626Factory should have correct ACM", async () => {
+      expect(await erc4626Factory.accessControlManager()).to.be.equals(ACM);
+    });
+  });
+
+  testForkedNetworkVipCommands("Accept ownerships for ERC4626Factory", await vip500(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [ERC4626FACTORY_ABI], ["OwnershipTransferred"], [1]);
+    },
+  });
+
+  describe("Post-VIP behaviour", async () => {
+    it("ERC4626Factory ownership transferred to Normal Timelock", async () => {
+      expect(await erc4626Factory.owner()).to.be.equals(opbnbtestnet.NORMAL_TIMELOCK);
+    });
+
+    it("ERC4626Factory pending owner should be zero address", async () => {
+      expect(await erc4626Factory.pendingOwner()).to.be.equals(ethers.constants.AddressZero);
+    });
+  });
+});
