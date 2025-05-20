ab5747baf7436682c4f5d06fc85f53cfbbd22c6b
web3rover <arsenal.narayan@gmail.com>
2025-05-20 16:41:44 +0400

    fix: fixed lint

--- simulations/vip-499/bsctestnet.ts
+++ simulations/vip-499/bsctestnet.ts
@@ -7,7 +7,12 @@ import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
 forking(51177545, async () => {
   testVip("VIP-499", await vip499(), {
     callbackAfterExecution: async txResponse => {
-      await expectEvents(txResponse, [OMNICHAIN_PROPOSAL_SENDER_ABI], ["ExecuteRemoteProposal", "StorePayload"], [2, 0]);
+      await expectEvents(
+        txResponse,
+        [OMNICHAIN_PROPOSAL_SENDER_ABI],
+        ["ExecuteRemoteProposal", "StorePayload"],
+        [2, 0],
+      );
     },
   });
 });
