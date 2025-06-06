ff81265f5b1bb3340328d17df259b520ead78859
Debugger022 <debugger022@gmail.com>
2025-06-06 14:29:51 +0530

    feat: add WBTC and USDTO to core pool on unichain mainnet

---
+++ simulations/vip-512/unichainmainnet.ts
@@ -0,0 +1,228 @@
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { setRedstonePrice } from "src/utils";
+import { NORMAL_TIMELOCK, forking, testForkedNetworkVipCommands } from "src/vip-framework";
+import { checkIsolatedPoolsComptrollers } from "src/vip-framework/checks/checkIsolatedPoolsComptrollers";
+import { checkVToken } from "src/vip-framework/checks/checkVToken";
+import { checkInterestRate, checkTwoKinksInterestRateIL } from "src/vip-framework/checks/interestRateModel";
+
+import vip512, {
+  COMPTROLLER_CORE,
+  USDTO,
+  USDTOMarket,
+  USDTO_REDSTONE_FEED,
+  WBTC,
+  WBTCMarket,
+  WBTC_REDSTONE_FEED,
+} from "../../vips/vip-512/bscmainnet";
+import COMPTROLLER_ABI from "./abi/comptroller.json";
+import PRICE_ORACLE_ABI from "./abi/resilientOracle.json";
+import VTOKEN_ABI from "./abi/vToken.json";
+
+const { unichainmainnet } = NETWORK_ADDRESSES;
+
+const VWBTC_IRM = "0xf781f2D923f37F4dD10c3541D8A1Be04571e7966";
+const VUSDTO_IRM = "0x3AD4F23275F8d3eda8C210e9FDB875769BBa1987";
+const PSR = "0x0A93fBcd7B53CE6D335cAB6784927082AD75B242";
+const ONE_YEAR = 31536000; // 1 year in seconds
+const BLOCKS_PER_YEAR = BigNumber.from("31536000"); // equal to seconds in a year as it is timebased deployment
+
+forking(18443512, async () => {
+  let comptroller: Contract;
+  let oracle: Contract;
+
+  before(async () => {
+    await setRedstonePrice(
+      unichainmainnet.REDSTONE_ORACLE,
+      WBTC.address,
+      WBTC_REDSTONE_FEED,
+      unichainmainnet.NORMAL_TIMELOCK,
+      ONE_YEAR,
+      { tokenDecimals: 8 },
+    );
+
+    await setRedstonePrice(
+      unichainmainnet.REDSTONE_ORACLE,
+      USDTO.address,
+      USDTO_REDSTONE_FEED,
+      unichainmainnet.NORMAL_TIMELOCK,
+      ONE_YEAR,
+      { tokenDecimals: 6 },
+    );
+  });
+
+  describe("Contracts setup", () => {
+    checkVToken(WBTCMarket.vToken.address, WBTCMarket.vToken);
+    checkVToken(USDTOMarket.vToken.address, USDTOMarket.vToken);
+  });
+
+  testForkedNetworkVipCommands("add WBTC and USDTO market", await vip512());
+
+  describe("Post-Execution state", () => {
+    let vWBTC: Contract;
+    let vUSDTO: Contract;
+
+    before(async () => {
+      vWBTC = await ethers.getContractAt(VTOKEN_ABI, WBTCMarket.vToken.address);
+      vUSDTO = await ethers.getContractAt(VTOKEN_ABI, USDTOMarket.vToken.address);
+      comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER_CORE);
+      oracle = await ethers.getContractAt(PRICE_ORACLE_ABI, await comptroller.oracle());
+    });
+
+    describe("PoolRegistry state", () => {
+      it("should register WBTC vToken in Core pool Comptroller", async () => {
+        const poolVTokens = await comptroller.getAllMarkets();
+        expect(poolVTokens).to.include(WBTCMarket.vToken.address);
+      });
+
+      it("should register USDTO vToken in Core pool Comptroller", async () => {
+        const poolVTokens = await comptroller.getAllMarkets();
+        expect(poolVTokens).to.include(USDTOMarket.vToken.address);
+      });
+    });
+
+    describe("Ownership", () => {
+      it(`should transfer ownership of ${WBTCMarket.vToken.address} to NORMAL_TIMELOCK`, async () => {
+        expect(await vWBTC.owner()).to.equal(NORMAL_TIMELOCK);
+      });
+
+      it(`should transfer ownership of ${USDTOMarket.vToken.address} to NORMAL_TIMELOCK`, async () => {
+        expect(await vUSDTO.owner()).to.equal(NORMAL_TIMELOCK);
+      });
+    });
+
+    describe("ProtocolShareReserve", () => {
+      it(`should set PSR for ${WBTCMarket.vToken.address}`, async () => {
+        expect(await vWBTC.protocolShareReserve()).to.equal(PSR);
+      });
+
+      it(`should set PSR for ${USDTOMarket.vToken.address}`, async () => {
+        expect(await vUSDTO.protocolShareReserve()).to.equal(PSR);
+      });
+    });
+
+    describe("Risk parameters", () => {
+      let vToken: Contract;
+      let comptroller: Contract;
+
+      describe("risk parameters for WBTC", () => {
+        before(async () => {
+          vToken = await ethers.getContractAt(VTOKEN_ABI, WBTCMarket.vToken.address);
+          comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER_CORE);
+        });
+
+        it(`should set reserve factor to 0.2`, async () => {
+          expect(await vToken.reserveFactorMantissa()).to.equal(WBTCMarket.riskParameters.reserveFactor);
+        });
+
+        it(`should set collateral factor to 0.77`, async () => {
+          const market = await comptroller.markets(WBTCMarket.vToken.address);
+          expect(market.collateralFactorMantissa).to.equal(WBTCMarket.riskParameters.collateralFactor);
+        });
+
+        it(`should set liquidation threshold to 0.8`, async () => {
+          const market = await comptroller.markets(WBTCMarket.vToken.address);
+          expect(market.liquidationThresholdMantissa).to.equal(WBTCMarket.riskParameters.liquidationThreshold);
+        });
+
+        it(`should set protocol seize share to 0.05`, async () => {
+          expect(await vToken.protocolSeizeShareMantissa()).to.equal(parseUnits("0.05", 18));
+        });
+
+        it(`should set supply cap to 400`, async () => {
+          expect(await comptroller.supplyCaps(WBTCMarket.vToken.address)).to.equal(WBTCMarket.riskParameters.supplyCap);
+        });
+
+        it(`should set borrow cap to 40`, async () => {
+          expect(await comptroller.borrowCaps(WBTCMarket.vToken.address)).to.equal(WBTCMarket.riskParameters.borrowCap);
+        });
+      });
+
+      describe("risk parameters for USDTO", () => {
+        before(async () => {
+          vToken = await ethers.getContractAt(VTOKEN_ABI, USDTOMarket.vToken.address);
+          comptroller = await ethers.getContractAt(COMPTROLLER_ABI, COMPTROLLER_CORE);
+        });
+
+        it(`should set reserve factor to 0.1`, async () => {
+          expect(await vToken.reserveFactorMantissa()).to.equal(USDTOMarket.riskParameters.reserveFactor);
+        });
+
+        it(`should set collateral factor to 0.77`, async () => {
+          const market = await comptroller.markets(USDTOMarket.vToken.address);
+          expect(market.collateralFactorMantissa).to.equal(USDTOMarket.riskParameters.collateralFactor);
+        });
+
+        it(`should set liquidation threshold to 0.8`, async () => {
+          const market = await comptroller.markets(USDTOMarket.vToken.address);
+          expect(market.liquidationThresholdMantissa).to.equal(USDTOMarket.riskParameters.liquidationThreshold);
+        });
+
+        it(`should set protocol seize share to 0.05`, async () => {
+          expect(await vToken.protocolSeizeShareMantissa()).to.equal(parseUnits("0.05", 18));
+        });
+
+        it(`should set supply cap to 50000000`, async () => {
+          expect(await comptroller.supplyCaps(USDTOMarket.vToken.address)).to.equal(
+            USDTOMarket.riskParameters.supplyCap,
+          );
+        });
+
+        it(`should set borrow cap to 45000000`, async () => {
+          expect(await comptroller.borrowCaps(USDTOMarket.vToken.address)).to.equal(
+            USDTOMarket.riskParameters.borrowCap,
+          );
+        });
+      });
+    });
+
+    describe("Prices", () => {
+      it("get correct price from oracle for WBTC", async () => {
+        const price = await oracle.getUnderlyingPrice(WBTCMarket.vToken.address);
+        expect(price).to.equal(parseUnits("1028534353071000", 18));
+      });
+
+      it("get correct price from oracle for USDTO", async () => {
+        const price = await oracle.getUnderlyingPrice(USDTOMarket.vToken.address);
+        expect(price).to.equal(parseUnits("1000357360000", 18));
+      });
+    });
+
+    it("Interest rates for WBTC", async () => {
+      checkInterestRate(
+        VWBTC_IRM,
+        "vWBTC",
+        {
+          base: "0",
+          multiplier: "0.09",
+          jump: "2",
+          kink: "0.45",
+        },
+        BLOCKS_PER_YEAR,
+      );
+    });
+
+    it("Interest rates for USDTO", async () => {
+      checkTwoKinksInterestRateIL(
+        VUSDTO_IRM,
+        "vUSDTO",
+        {
+          base: "0",
+          multiplier: "0.1",
+          kink1: "0.8",
+          multiplier2: "0.7",
+          base2: "0",
+          kink2: "0.9",
+          jump: "0.8",
+        },
+        BLOCKS_PER_YEAR,
+      );
+    });
+    it("check isolated pools", async () => {
+      checkIsolatedPoolsComptrollers();
+    });
+  });
+});
---
+++ vips/vip-512/bscmainnet.ts
@@ -0,0 +1,265 @@
+import { BigNumber } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { unichainmainnet } = NETWORK_ADDRESSES;
+
+export const COMPTROLLER_CORE = "0xe22af1e6b78318e1Fe1053Edbd7209b8Fc62c4Fe";
+export const WBTC_REDSTONE_FEED = "0xc44be6D00307c3565FDf753e852Fc003036cBc13";
+export const USDTO_REDSTONE_FEED = "0x58fa68A373956285dDfb340EDf755246f8DfCA16";
+export const WBTC_MAX_STALE_PERIOD = 7 * 60 * 60; // 7 hours in seconds
+export const USDTO_MAX_STALE_PERIOD = 4 * 60 * 60; // 4 hours in seconds
+
+type Token = {
+  address: string;
+  decimals: number;
+  symbol: string;
+};
+
+export const WBTC: Token = {
+  address: "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c",
+  decimals: 8,
+  symbol: "WBTC",
+};
+
+export const USDTO: Token = {
+  address: "0x9151434b16b9763660705744891fA906F660EcC5",
+  decimals: 6,
+  symbol: "USD₮0",
+};
+
+type Market = {
+  vToken: {
+    address: string;
+    name: string;
+    symbol: string;
+    underlying: Token;
+    decimals: number;
+    exchangeRate: BigNumber;
+    comptroller: string;
+  };
+  riskParameters: {
+    collateralFactor: BigNumber;
+    liquidationThreshold: BigNumber;
+    supplyCap: BigNumber;
+    borrowCap: BigNumber;
+    reserveFactor: BigNumber;
+    protocolSeizeShare: BigNumber;
+  };
+  initialSupply: {
+    amount: BigNumber;
+    vTokensToBurn: BigNumber;
+  };
+};
+
+export const WBTCMarket: Market = {
+  vToken: {
+    address: "0x68e2A6F7257FAc2F5a557b9E83E1fE6D5B408CE5",
+    name: "Venus WBTC (Core)",
+    symbol: "vWBTC_Core",
+    underlying: WBTC,
+    decimals: 8,
+    exchangeRate: parseUnits("1", 18),
+    comptroller: COMPTROLLER_CORE,
+  },
+  riskParameters: {
+    collateralFactor: parseUnits("0.77", 18),
+    liquidationThreshold: parseUnits("0.8", 18),
+    supplyCap: parseUnits("400", 8),
+    borrowCap: parseUnits("40", 8),
+    reserveFactor: parseUnits("0.2", 18),
+    protocolSeizeShare: parseUnits("0.05", 18),
+  },
+  initialSupply: {
+    amount: parseUnits("0.00100364", 8),
+    vTokensToBurn: parseUnits("0.00100364", 8), // 100%
+  },
+};
+
+export const USDTOMarket: Market = {
+  vToken: {
+    address: "0xDa7Ce7Ba016d266645712e2e4Ebc6cC75eA8E4CD",
+    name: "Venus USD₮0 (Core)",
+    symbol: "vUSD₮0_Core",
+    underlying: USDTO,
+    decimals: 8,
+    exchangeRate: parseUnits("1", 16),
+    comptroller: COMPTROLLER_CORE,
+  },
+  riskParameters: {
+    collateralFactor: parseUnits("0.77", 18),
+    liquidationThreshold: parseUnits("0.8", 18),
+    supplyCap: parseUnits("50000000", 6),
+    borrowCap: parseUnits("45000000", 6),
+    reserveFactor: parseUnits("0.1", 18),
+    protocolSeizeShare: parseUnits("0.05", 18),
+  },
+  initialSupply: {
+    amount: parseUnits("102.84258", 6),
+    vTokensToBurn: parseUnits("102.84258", 6), // 100%
+  },
+};
+
+export const vip512 = async () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-512 [Unichain] add WBTC and USDTO markets to the Core pool",
+    description: "",
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      // oracle config
+      {
+        target: unichainmainnet.REDSTONE_ORACLE,
+        signature: "setTokenConfigs((address,address,uint256)[])",
+        params: [
+          [
+            [WBTC.address, WBTC_REDSTONE_FEED, WBTC_MAX_STALE_PERIOD],
+            [USDTO.address, USDTO_REDSTONE_FEED, USDTO_MAX_STALE_PERIOD],
+          ],
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: unichainmainnet.RESILIENT_ORACLE,
+        signature: "setTokenConfigs((address,address[3],bool[3],bool)[])",
+        params: [
+          [
+            // WBTC config
+            [
+              WBTC.address,
+              [unichainmainnet.REDSTONE_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            // USDTO config
+            [
+              USDTO.address,
+              [unichainmainnet.REDSTONE_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+          ],
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+
+      // <--- WBTC Market --->
+      // Market configurations
+      {
+        target: WBTCMarket.vToken.address,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: ["86400"],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: unichainmainnet.VTREASURY,
+        signature: "withdrawTreasuryToken(address,uint256,address)",
+        params: [
+          WBTCMarket.vToken.underlying.address,
+          WBTCMarket.initialSupply.amount,
+          unichainmainnet.NORMAL_TIMELOCK,
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: WBTC.address,
+        signature: "approve(address,uint256)",
+        params: [unichainmainnet.POOL_REGISTRY, 0],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: WBTC.address,
+        signature: "approve(address,uint256)",
+        params: [unichainmainnet.POOL_REGISTRY, WBTCMarket.initialSupply.amount],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: unichainmainnet.POOL_REGISTRY,
+        signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
+        params: [
+          [
+            WBTCMarket.vToken.address,
+            WBTCMarket.riskParameters.collateralFactor, // CF
+            WBTCMarket.riskParameters.liquidationThreshold, // LT
+            WBTCMarket.initialSupply.amount, // initial supply
+            unichainmainnet.NORMAL_TIMELOCK, // vToken receiver
+            WBTCMarket.riskParameters.supplyCap, // supply cap
+            WBTCMarket.riskParameters.borrowCap, // borrow cap
+          ],
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: WBTCMarket.vToken.address,
+        signature: "transfer(address,uint256)",
+        params: [ethers.constants.AddressZero, WBTCMarket.initialSupply.vTokensToBurn],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+
+      // <--- USDTO Market --->
+      // Market configurations
+      {
+        target: USDTOMarket.vToken.address,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: ["86400"],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: unichainmainnet.VTREASURY,
+        signature: "withdrawTreasuryToken(address,uint256,address)",
+        params: [
+          USDTOMarket.vToken.underlying.address,
+          USDTOMarket.initialSupply.amount,
+          unichainmainnet.NORMAL_TIMELOCK,
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: USDTO.address,
+        signature: "approve(address,uint256)",
+        params: [unichainmainnet.POOL_REGISTRY, 0],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: USDTO.address,
+        signature: "approve(address,uint256)",
+        params: [unichainmainnet.POOL_REGISTRY, USDTOMarket.initialSupply.amount],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: unichainmainnet.POOL_REGISTRY,
+        signature: "addMarket((address,uint256,uint256,uint256,address,uint256,uint256))",
+        params: [
+          [
+            USDTOMarket.vToken.address,
+            USDTOMarket.riskParameters.collateralFactor, // CF
+            USDTOMarket.riskParameters.liquidationThreshold, // LT
+            USDTOMarket.initialSupply.amount, // initial supply
+            unichainmainnet.NORMAL_TIMELOCK, // vToken receiver
+            USDTOMarket.riskParameters.supplyCap, // supply cap
+            USDTOMarket.riskParameters.borrowCap, // borrow cap
+          ],
+        ],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: USDTOMarket.vToken.address,
+        signature: "transfer(address,uint256)",
+        params: [ethers.constants.AddressZero, USDTOMarket.initialSupply.vTokensToBurn],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip512;
