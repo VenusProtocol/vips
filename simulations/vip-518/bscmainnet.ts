632df8c2b3a3a32108fbe6b9e7dc5d72777b8fa2
Debugger022 <debugger022@gmail.com>
2025-06-13 15:50:43 +0530

    feat: add vip and simulation for bscmainnet

--- simulations/vip-518/bscmainnet.ts
+++ simulations/vip-518/bscmainnet.ts
@@ -1,7 +1,8 @@
 import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
-import { expectEvents } from "src/utils";
+import { expectEvents, setMaxStalePeriod } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
 import vip518, {
@@ -10,27 +11,108 @@ import vip518, {
   BOUND_VALIDATOR_IMPLEMENTATION,
   CHAINLINK_ORACLE_IMPLEMENTATION,
   DEFAULT_PROXY_ADMIN,
+  PTsUSDE_26JUN2025,
   REDSTONE_ORACLE_IMPLEMENTATION,
   RESILIENT_ORACLE_IMPLEMENTATION,
+  SUSDE,
+  XSOLVBTC,
 } from "../../vips/vip-518/bscmainnet";
 import ACM_ABI from "./abi/ACM.json";
+import ERC20_ABI from "./abi/ERC20.json";
 import PROXY_ABI from "./abi/Proxy.json";
 import PROXY_ADMIN_ABI from "./abi/ProxyAdmin.json";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+import VTOKEN_ABI from "./abi/VToken.json";
 
 const { bscmainnet } = NETWORK_ADDRESSES;
 
-forking(51270995, async () => {
+const prices = [
+  {
+    symbol: "vslisBNB_LiquidStakedBNB",
+    address: "0xd3CC9d8f3689B83c91b7B59cAB4946B063EB894A",
+    expectedPrice: parseUnits("1.12284689", 18),
+    postVIP: async function (resilientOracle: any, address: string) {
+      const vtoken = new ethers.Contract(address, VTOKEN_ABI, ethers.provider);
+      const underlying = await vtoken.underlying();
+      console.log("Underlying token address:", underlying);
+      const token = new ethers.Contract(underlying, ERC20_ABI, ethers.provider);
+      await setMaxStalePeriod(resilientOracle, token);
+    },
+  },
+  {
+    symbol: "vBNBx_LiquidStakedBNB",
+    address: "0x5E21bF67a6af41c74C1773E4b473ca5ce8fd3791",
+    expectedPrice: parseUnits("0.74698", 18),
+    postVIP: async function (resilientOracle: any, address: string) {
+      const vtoken = new ethers.Contract(address, VTOKEN_ABI, ethers.provider);
+      const underlying = await vtoken.underlying();
+      console.log("Underlying token address:", underlying);
+      const token = new ethers.Contract(underlying, ERC20_ABI, ethers.provider);
+      await setMaxStalePeriod(resilientOracle, token);
+    },
+  },
+  {
+    symbol: "vasBNB_LiquidStakedBNB",
+    address: "0x4A50a0a1c832190362e1491D5bB464b1bc2Bd288",
+    expectedPrice: parseUnits("0.99960456", 18),
+    postVIP: async function (resilientOracle: any, address: string) {
+      const vtoken = new ethers.Contract(address, VTOKEN_ABI, ethers.provider);
+      const underlying = await vtoken.underlying();
+      const token = new ethers.Contract(underlying, ERC20_ABI, ethers.provider);
+      await setMaxStalePeriod(resilientOracle, token);
+    },
+  },
+  {
+    symbol: "vankrBNB_LiquidStakedBNB",
+    address: "0xBfe25459BA784e70E2D7a718Be99a1f3521cA17f",
+    expectedPrice: parseUnits("0.99995684", 18),
+    postVIP: async function (resilientOracle: any, address: string) {
+      const vtoken = new ethers.Contract(address, VTOKEN_ABI, ethers.provider);
+      const underlying = await vtoken.underlying();
+      const token = new ethers.Contract(underlying, ERC20_ABI, ethers.provider);
+      await setMaxStalePeriod(resilientOracle, token);
+    },
+  },
+  {
+    symbol: "xSolvBTC",
+    address: XSOLVBTC,
+    expectedPrice: parseUnits("2844.085421477404790808", 18),
+  },
+  {
+    symbol: "sUSDe",
+    address: SUSDE,
+    expectedPrice: parseUnits("3204.385384786432008563", 18),
+  },
+  {
+    symbol: "PT-sUSDE-26JUN2025",
+    address: PTsUSDE_26JUN2025,
+    expectedPrice: parseUnits("3204.385384786432008563", 18),
+  },
+];
+
+forking(51372289, async () => {
   const provider = ethers.provider;
+  const resilientOracle = new ethers.Contract(bscmainnet.RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
   const proxyAdmin = new ethers.Contract(DEFAULT_PROXY_ADMIN, PROXY_ADMIN_ABI, provider);
 
   testVip("VIP-518", await vip518(), {
     callbackAfterExecution: async txResponse => {
       await expectEvents(txResponse, [PROXY_ABI], ["Upgraded"], [5]);
+      await expectEvents(txResponse, [RESILIENT_ORACLE_ABI], ["TokenConfigAdded"], [7]);
       await expectEvents(txResponse, [ACM_ABI], ["RoleGranted"], [9]);
     },
   });
 
   describe("Post-VIP behaviour", async () => {
+    for (const price of prices) {
+      it(`check ${price.symbol} price`, async () => {
+        if (price.postVIP) {
+          await price.postVIP(resilientOracle, price.address);
+        }
+        expect(await resilientOracle.getUnderlyingPrice(price.address)).to.equal(price.expectedPrice);
+      });
+    }
+
     describe("New implementations", () => {
       it("Resilient oracle", async () => {
         expect(await proxyAdmin.getProxyImplementation(bscmainnet.RESILIENT_ORACLE)).to.equal(
--- vips/vip-518/bscmainnet.ts
+++ vips/vip-518/bscmainnet.ts
@@ -12,6 +12,21 @@ export const CHAINLINK_ORACLE_IMPLEMENTATION = "0x219cFfEFB1afA9F34695C7fACD9B98
 export const BINANCE_ORACLE_IMPLEMENTATION = "0x201C72986d391A5a8E1713ac5a42CEAf90556a1b";
 export const REDSTONE_ORACLE_IMPLEMENTATION = "0x452FeCfa5dd59243EeC214577345d21F7D8AC5Bf";
 export const BOUND_VALIDATOR_IMPLEMENTATION = "0xbE4176749a74320641e24102B2Af2Ca37FAF2DF1";
+export const PTsUSDe_ORACLE = "0xC407403fa78Bce509821D776b6Be7f91cC04474f";
+export const PTsUSDE_26JUN2025 = "0xDD809435ba6c9d6903730f923038801781cA66ce";
+export const SUSDE_ONEJUMP_CHAINLINK_ORACLE = "0xA67F01322AF8EBa444D788Ee398775b446de51a0";
+export const SUSDE_ONEJUMP_REDSTONE_ORACLE = "0x2B2895104f958E1EC042E6Ba5cbfeCbAD3C5beDb";
+export const SUSDE = "0x211Cc4DD073734dA055fbF44a2b4667d5E5fE5d2";
+export const XSOLVBTC_ONEJUMP_REDSTONE_ORACLE = "0xf5534f78Df9b610B19A63956d498d00CFaD8B9D3";
+export const XSOLVBTC = "0x1346b618dC92810EC74163e4c27004c921D446a5";
+export const AnkrBNB_ORACLE = "0x4512e9579734f7B8730f0a05Cd0D92DC33EB2675";
+export const AnkrBNB = "0x52f24a5e03aee338da5fd9df68d2b6fae1178827";
+export const AsBNB_ORACLE = "0x652B90D1d45a7cD5BE82c5Fb61a4A00bA126dde5";
+export const AsBNB = "0x77734e70b6E88b4d82fE632a168EDf6e700912b6";
+export const BNBx_ORACLE = "0xC2E2b6f9CdE2BFA5Ba5fda2Dd113CAcD781bdb31";
+export const BNBx = "0x1bdd3cf7f79cfb8edbb955f20ad99211551ba275";
+export const SlisBNB_ORACLE = "0xDDE6446E66c786afF4cd3D183a908bCDa57DF9c1";
+export const SlisBNB = "0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B";
 
 export const vip518 = () => {
   const meta = {
@@ -94,6 +109,56 @@ export const vip518 = () => {
         target: bscmainnet.ACCESS_CONTROL_MANAGER,
         signature: "giveCallPermission(address,string,address)",
         params: [ethers.constants.AddressZero, "setSnapshotGap(uint256)", bscmainnet.FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: bscmainnet.RESILIENT_ORACLE,
+        signature: "setTokenConfigs((address,address[3],bool[3],bool)[])",
+        params: [
+          [
+            [
+              XSOLVBTC,
+              [XSOLVBTC_ONEJUMP_REDSTONE_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            [
+              PTsUSDE_26JUN2025,
+              [PTsUSDe_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            [
+              AnkrBNB,
+              [AnkrBNB_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            [
+              AsBNB,
+              [AsBNB_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            [
+              SUSDE,
+              [SUSDE_ONEJUMP_REDSTONE_ORACLE, SUSDE_ONEJUMP_CHAINLINK_ORACLE, SUSDE_ONEJUMP_CHAINLINK_ORACLE],
+              [true, true, true],
+              false,
+            ],
+            [
+              BNBx,
+              [BNBx_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+            [
+              SlisBNB,
+              [SlisBNB_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+              [true, false, false],
+              false,
+            ],
+          ],
+        ],
       },
     ],
     meta,
