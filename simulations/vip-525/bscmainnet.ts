342500dd86f4bd114b4b0cbf9182d56eadad095e
Debugger022 <debugger022@gmail.com>
2025-06-26 16:41:48 +0530

    feat: add vip and simulations to pause/unlist PT markets

---
+++ simulations/vip-525/bscmainnet.ts
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import vip525, { Actions, COMPTROLLER_Core, VToken_vPT_sUSDE_26JUN2025 } from "../../vips/vip-525/bscmainnet";
+import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
+import COMPTROLLER_ABI from "./abi/SetterFacet.json";
+
+const provider = ethers.provider;
+
+forking(52121155, async () => {
+  let comptroller: Contract;
+
+  before(async () => {
+    comptroller = new ethers.Contract(COMPTROLLER_Core, COMPTROLLER_ABI, provider);
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("Check mint action is not paused", async () => {
+      const isPaused = await comptroller.actionPaused(VToken_vPT_sUSDE_26JUN2025, Actions.MINT); // Mint action
+      expect(isPaused).to.be.false;
+    });
+
+    it("Check enter market action is not paused", async () => {
+      const isPaused = await comptroller.actionPaused(VToken_vPT_sUSDE_26JUN2025, Actions.ENTER_MARKET); // Enter market action
+      expect(isPaused).to.be.false;
+    });
+  });
+
+  testVip("VIP-525 bscmainnet", await vip525(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [COMPTROLLER_ABI], ["ActionPausedMarket"], [2]);
+      await expectEvents(
+        txResponse,
+        [OMNICHAIN_PROPOSAL_SENDER_ABI],
+        ["ExecuteRemoteProposal", "StorePayload"],
+        [1, 0],
+      );
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("Mint action should be paused", async () => {
+      const isPaused = await comptroller.actionPaused(VToken_vPT_sUSDE_26JUN2025, Actions.MINT);
+      expect(isPaused).to.be.true;
+    });
+
+    it("Enter market action should be paused", async () => {
+      const isPaused = await comptroller.actionPaused(VToken_vPT_sUSDE_26JUN2025, Actions.ENTER_MARKET); // Enter market action
+      expect(isPaused).to.be.true;
+    });
+  });
+});
