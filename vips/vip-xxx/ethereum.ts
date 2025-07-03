eca0f66a248b930c278eb53ab237efe4da7ca505
Filip Malachowicz <filip.m@venus.io>
2025-07-03 13:51:39 +0200

    fix: add proposal type

--- vips/vip-xxx/ethereum.ts
+++ vips/vip-xxx/ethereum.ts
@@ -1,5 +1,5 @@
 import { NETWORK_ADDRESSES } from "src/networkAddresses";
-import { LzChainId } from "src/types";
+import { LzChainId, ProposalType } from "src/types";
 import { makeProposal } from "src/utils";
 
 const { ethereum } = NETWORK_ADDRESSES;
@@ -22,114 +22,118 @@ Configure Remote Risk Stewards
     abstainDescription: "Indifferent to execution",
   };
 
-  return makeProposal([
-    // Permissions
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "setRiskParameterConfig(string,address,uint256)", ethereum.NORMAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [ANY_TARGET_CONTRACT, "setMarketSupplyCaps(address[],uint256[])", MARKET_CAP_RISK_STEWARD],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [ANY_TARGET_CONTRACT, "setMarketBorrowCaps(address[],uint256[])", MARKET_CAP_RISK_STEWARD],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.NORMAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.CRITICAL_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.NORMAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.CRITICAL_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.NORMAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.CRITICAL_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.FAST_TRACK_TIMELOCK],
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [MARKET_CAP_RISK_STEWARD, "processUpdate(RiskParameterUpdate)", RISK_STEWARD_RECEIVER],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.NORMAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.CRITICAL_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
-    {
-      target: ethereum.ACCESS_CONTROL_MANAGER,
-      signature: "giveCallPermission(address,string,address)",
-      params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.FAST_TRACK_TIMELOCK],
-      dstChainId: LzChainId.ethereum,
-    },
+  return makeProposal(
+    [
+      // Permissions
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "setRiskParameterConfig(string,address,uint256)", ethereum.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [ANY_TARGET_CONTRACT, "setMarketSupplyCaps(address[],uint256[])", MARKET_CAP_RISK_STEWARD],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [ANY_TARGET_CONTRACT, "setMarketBorrowCaps(address[],uint256[])", MARKET_CAP_RISK_STEWARD],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.CRITICAL_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "toggleConfigActive(string)", ethereum.FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.CRITICAL_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "pause()", ethereum.FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.CRITICAL_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [RISK_STEWARD_RECEIVER, "unpause()", ethereum.FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [MARKET_CAP_RISK_STEWARD, "processUpdate(RiskParameterUpdate)", RISK_STEWARD_RECEIVER],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.NORMAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.CRITICAL_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [MARKET_CAP_RISK_STEWARD, "setMaxIncreaseBps(uint256)", ethereum.FAST_TRACK_TIMELOCK],
+        dstChainId: LzChainId.ethereum,
+      },
 
-    // Set risk parameter configurations
-    {
-      target: RISK_STEWARD_RECEIVER,
-      signature: "setRiskParameterConfig(string,address,uint256)",
-      params: ["supplyCap", MARKET_CAP_RISK_STEWARD, MORE_THAN_1_DAY],
-      dstChainId: LzChainId.ethereum,
-    },
+      // Set risk parameter configurations
+      {
+        target: RISK_STEWARD_RECEIVER,
+        signature: "setRiskParameterConfig(string,address,uint256)",
+        params: ["supplyCap", MARKET_CAP_RISK_STEWARD, MORE_THAN_1_DAY],
+        dstChainId: LzChainId.ethereum,
+      },
 
-    {
-      target: RISK_STEWARD_RECEIVER,
-      signature: "setRiskParameterConfig(string,address,uint256)",
-      params: ["borrowCap", MARKET_CAP_RISK_STEWARD, MORE_THAN_1_DAY],
-      dstChainId: LzChainId.ethereum,
-    },
-  ]);
+      {
+        target: RISK_STEWARD_RECEIVER,
+        signature: "setRiskParameterConfig(string,address,uint256)",
+        params: ["borrowCap", MARKET_CAP_RISK_STEWARD, MORE_THAN_1_DAY],
+        dstChainId: LzChainId.ethereum,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
 };
 
 export default vip599;
