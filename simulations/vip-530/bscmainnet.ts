08517184b9360e3eea43c3644f98bc5b90f4becc
web3rover <arsenal.narayan@gmail.com>
2025-07-15 14:57:22 +0530

    feat: added matic feed and new caps

--- simulations/vip-530/bscmainnet.ts
+++ simulations/vip-530/bscmainnet.ts
@@ -1,12 +1,41 @@
 import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
-import vip530 from "../../vips/vip-530/bscmainnet";
+import vip530, { BORROW_CAP, MATIC, SUPPLY_CAP, vMATIC } from "../../vips/vip-530/bscmainnet";
+import COMPTROLLER_ABI from "./abi/CoreComptroller.json";
 import OMNICHAIN_PROPOSAL_SENDER_ABI from "./abi/OmnichainProposalSender.json";
+import RESILIENT_ORACLE_ABI from "./abi/ResilientOracle.json";
+
+const { bscmainnet } = NETWORK_ADDRESSES;
 
 forking(53622111, async () => {
-  testVip("vip-530", await vip530(), {
+  const comptroller = new ethers.Contract(bscmainnet.UNITROLLER, COMPTROLLER_ABI, ethers.provider);
+
+  const resilientOracle = new ethers.Contract(bscmainnet.RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, ethers.provider);
+
+  describe("Pre-VIP behaviour", async () => {
+    it("check supply cap", async () => {
+      const supplyCap = await comptroller.supplyCaps(vMATIC);
+      expect(supplyCap).to.equals(parseUnits("5500000", 18));
+    });
+
+    it("check borrow cap", async () => {
+      const borrowCap = await comptroller.borrowCaps(vMATIC);
+      expect(borrowCap).to.equals(parseUnits("250000", 18));
+    });
+
+    it("check MATIC price", async () => {
+      const maticPrice = await resilientOracle.getPrice(MATIC);
+      expect(maticPrice).to.equals(parseUnits("0.2273", 18));
+    });
+  });
+
+  testVip("vip-530", await vip530(365 * 24 * 60 * 60), {
     callbackAfterExecution: async (txResponse: TransactionResponse) => {
       await expectEvents(
         txResponse,
@@ -15,5 +44,22 @@ forking(53622111, async () => {
         [1, 0],
       );
     },
+  });
+
+  describe("Post-VIP behaviour", async () => {
+    it("check supply cap", async () => {
+      const supplyCap = await comptroller.supplyCaps(vMATIC);
+      expect(supplyCap).to.equals(SUPPLY_CAP);
+    });
+
+    it("check borrow cap", async () => {
+      const borrowCap = await comptroller.borrowCaps(vMATIC);
+      expect(borrowCap).to.equals(BORROW_CAP);
+    });
+
+    it("check MATIC price", async () => {
+      const maticPrice = await resilientOracle.getPrice(MATIC);
+      expect(maticPrice).to.equals(parseUnits("0.22702797", 18));
+    });
   });
 });
--- vips/vip-530/bscmainnet.ts
+++ vips/vip-530/bscmainnet.ts
@@ -3,11 +3,18 @@ import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { LzChainId, ProposalMeta, ProposalType } from "src/types";
 import { makeProposal } from "src/utils";
 
-const { zksyncmainnet } = NETWORK_ADDRESSES;
+const { zksyncmainnet, bscmainnet } = NETWORK_ADDRESSES;
 export const USDM = "0x7715c206A14Ac93Cb1A6c0316A6E5f8aD7c9Dc31";
 export const PRICE = parseUnits("1", 18);
 
-export const vip530 = async () => {
+export const vMATIC = "0x5c9476FcD6a4F9a3654139721c949c2233bBbBc8";
+export const MATIC = "0xCC42724C6683B7E57334c4E856f4c9965ED682bD";
+export const MATIC_FEED = "0x081195B56674bb87b2B92F6D58F7c5f449aCE19d";
+export const MAX_STALE_PERIOD = 900; // 15 minutes
+export const SUPPLY_CAP = parseUnits("2500000", 18);
+export const BORROW_CAP = parseUnits("100000", 18);
+
+export const vip530 = async (maxStalePeriod?: number) => {
   const meta: ProposalMeta = {
     version: "v2",
     title: "VIP-530",
@@ -24,6 +31,21 @@ export const vip530 = async () => {
         signature: "setDirectPrice(address,uint256)",
         params: [USDM, PRICE],
         dstChainId: LzChainId.zksyncmainnet,
+      },
+      {
+        target: bscmainnet.CHAINLINK_ORACLE,
+        signature: "setTokenConfig((address,address,uint256))",
+        params: [[MATIC, MATIC_FEED, maxStalePeriod || MAX_STALE_PERIOD]],
+      },
+      {
+        target: bscmainnet.UNITROLLER,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[vMATIC], [SUPPLY_CAP]],
+      },
+      {
+        target: bscmainnet.UNITROLLER,
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [[vMATIC], [BORROW_CAP]],
       },
     ],
     meta,
