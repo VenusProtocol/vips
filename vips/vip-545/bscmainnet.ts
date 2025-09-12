e9d8bce4ce239e03b7edbc154c3c66a314f78486
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-09-12 15:59:20 +0200

    feat: add description for VIP 543

--- vips/vip-545/bscmainnet.ts
+++ vips/vip-545/bscmainnet.ts
@@ -62,8 +62,71 @@ const vTokensRemaining = vTokensMinted.sub(WBNBMarketSpec.initialSupply.vTokensT
 export const vip545 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-545 [BNB Chain] Add WBNB markets to the Core pool",
-    description: "VIP-545 [BNB Chain] Add WBNB markets to the Core pool",
+    title: "VIP-545 [BNB Chain] New WBNB market in the Core pool",
+    description: `#### Summary
+
+If passed, this VIP will add a [WBNB](https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c) market to the [Core pool on BNB Chain](https://bscscan.com/address/0xfD36E2c2a6789Db23113685031d7F16329158384). It will also update the oracle configuration for the WBNB token, using the same oracles as the BNB asset.
+
+#### Description
+
+The [smart contract of the current BNB market in the Core pool](https://bscscan.com/address/0xA07c5b74C9B40447a954e1466938b865b6BBea36) is not upgradeable, which prevents the development of new features and negatively impacts BNB users. The new market will overcome this limitation and support the addition of new features in the future, as with the other markets.
+
+Users will be able to interact with the new market using WBNB tokens or native BNB (using the [Native Token Gateway feature](https://docs-v4.venus.io/technical-reference/reference-technical-articles/native-token-gateway) under the hood).
+
+**Risk parameters**
+
+The risk parameters for the new market are the same as those configured in the [current BNB market in the Core pool](https://bscscan.com/address/0xA07c5b74C9B40447a954e1466938b865b6BBea36), that is:
+
+- Borrow cap: 2,008,000 WBNB
+- Supply cap: 2,672,000 WBNB
+- Collateral factor: 80%
+- Reserve factor: 30%
+
+Interest rate curve parameters::
+
+- base 1 (yearly): 0%
+- multiplier 1 (yearly): 4.5%
+- kink 1: 70%
+- base 2 (yearly): 0%
+- multiplier 2 (yearly): 140%
+- kink 2: 80%
+- jump multiplier (yearly): 300%
+
+Bootstrap liquidity: 1 WBNB, provided by the [Venus Treasury](https://bscscan.com/address/0xf322942f644a996a617bd29c16bd7d231d9f35e9).
+
+Underlying token: [WBNB](https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c)
+
+**Oracles configuration**
+
+The [ResilientOracle](https://docs-v4.venus.io/risk/resilient-price-oracle) deployed to [BNB Chain](https://bscscan.com/address/0x6592b5DE802159F3E74B2486b091D11a8256ab8A) is used for WBNB, with the following configuration (same as BNB):
+
+- MAIN oracle - [RedStone oracle](https://bscscan.com/address/0x8455EFA4D7Ff63b8BFD96AdD889483Ea7d39B70a)
+- PIVOT oracle - [Chainlink oracle](https://bscscan.com/address/0x1B2103441A0A108daD8848D8F5d790e4D402921F)
+- FALLBACK oracle - [Binance oracle](https://bscscan.com/address/0x594810b741d136f1960141C0d8Fb4a91bE78A820)
+
+#### Security and additional considerations
+
+We applied the following security procedures for this VIP:
+
+- **Audit**. Certik has audited the updated Native Token Gateway contract, compatible with the BNB Chain Core pool.
+- **VIP execution simulation**: in a simulation environment, validating the new market is properly added to the Core pool on BNB Chain, with the right parameters and the expected bootstrap liquidity
+- **Deployment on testnet**: the same market has been deployed to BNB testnet, and used in the Venus Protocol testnet deployment
+
+#### Deployed contracts
+
+BNB Chain
+
+- vWBNB: [0x6bCa74586218dB34cdB402295796b79663d816e9](https://bscscan.com/address/0x6bCa74586218dB34cdB402295796b79663d816e9)
+
+BNB Chain testnet
+
+- vWBNB: [0xd9E77847ec815E56ae2B9E69596C69b6972b0B1C](https://testnet.bscscan.com/address/0xd9E77847ec815E56ae2B9E69596C69b6972b0B1C)
+
+#### References
+
+- [VIP simulation](https://github.com/VenusProtocol/vips/pull/611)
+- [Audit report from Certik (September 11th, 2025)](https://github.com/VenusProtocol/venus-periphery/blob/fb401caa0cf03eab28a3ac0a0e94b7028f6126bc/audits/152_nativeTokenGateway_certik_20250911.pdf)
+- [Documentation](https://docs-v4.venus.io/)`,
     forDescription: "I agree that Venus Protocol should proceed with this proposal",
     againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
     abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
