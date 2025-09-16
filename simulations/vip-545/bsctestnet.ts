5005b7dea13b123cb3ee92a5cbc0697926ded1a0
GitGuru7 <aditya@venus.io>
2025-09-16 17:39:49 +0530

    feat: add stablecoins emode pool on testnet

---
+++ simulations/vip-545/bsctestnet.ts
@@ -0,0 +1,64 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import { POOL_SPECS, UNITROLLER, vip545 } from "../../vips/vip-545/bsctestnet";
+import COMPTROLLER_ABI from "./abi/Comptroller.json";
+
+forking(65570708, async () => {
+  let comptroller: Contract;
+
+  before(async () => {
+    comptroller = await ethers.getContractAt(COMPTROLLER_ABI, UNITROLLER);
+  });
+
+  describe("Pre-VIP state", async () => {
+    it("check the new poolId does not exist", async () => {
+      expect(await comptroller.lastPoolId()).to.be.lessThan(POOL_SPECS.id);
+    });
+  });
+
+  testVip("VIP-545", await vip545(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(
+        txResponse,
+        [COMPTROLLER_ABI],
+        [
+          "PoolCreated",
+          "NewCollateralFactor",
+          "NewLiquidationThreshold",
+          "NewLiquidationIncentive",
+          "PoolMarketInitialized",
+        ],
+        [1, 2, 2, 2, 2],
+      );
+    },
+  });
+
+  describe("Post-VIP state", async () => {
+    it("should update lastPoolId to the new pool", async () => {
+      expect(await comptroller.lastPoolId()).to.equals(POOL_SPECS.id);
+    });
+
+    it("should set the newly created pool as active with correct label", async () => {
+      const newPool = await comptroller.pools(POOL_SPECS.id);
+      expect(newPool.label).to.equals(POOL_SPECS.label);
+      expect(newPool.isActive).to.equals(true);
+    });
+
+    it("should set the correct risk parameters to all pool markets", async () => {
+      for (const market of POOL_SPECS.marketsConfig) {
+        const marketData = await comptroller.poolMarkets(POOL_SPECS.id, market.address);
+        expect(marketData.marketPoolId).to.be.equal(POOL_SPECS.id);
+        expect(marketData.isListed).to.be.equal(true);
+        expect(marketData.collateralFactorMantissa).to.be.equal(market.collateralFactor);
+        expect(marketData.liquidationThresholdMantissa).to.be.equal(market.liquidationThreshold);
+        expect(marketData.liquidationIncentiveMantissa).to.be.equal(market.liquidationIncentive);
+        expect(marketData.isBorrowAllowed).to.be.equal(market.borrowAllowed);
+      }
+    });
+  });
+});
---
+++ vips/vip-545/bsctestnet.ts
@@ -0,0 +1,80 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+export const UNITROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
+export const vUSDE = "0x86f8DfB7CA84455174EE9C3edd94867b51Da46BD";
+export const vsUSDE = "0x8c8A1a0b6e1cb8058037F7bF24de6b79Aca5B7B0";
+
+export const POOL_SPECS = {
+  label: "Stablecoins",
+  id: 1,
+  markets: [vUSDE, vsUSDE],
+  marketsConfig: [
+    {
+      address: vUSDE,
+      collateralFactor: parseUnits("0.75", 18),
+      liquidationThreshold: parseUnits("0.75", 18),
+      liquidationIncentive: parseUnits("1.04", 18),
+      borrowAllowed: true,
+    },
+    {
+      address: vsUSDE,
+      collateralFactor: parseUnits("0.7875", 18), // +5%
+      liquidationThreshold: parseUnits("0.825", 18), // +10%
+      liquidationIncentive: parseUnits("1.04", 18),
+      borrowAllowed: false,
+    },
+  ],
+};
+
+export const vip545 = () => {
+  const meta = {
+    version: "v2",
+    title: "USDe Stable Emode Pool",
+    description: `USDe Stable Emode Pool`,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: UNITROLLER,
+        signature: "createPool(string)",
+        params: [POOL_SPECS.label],
+      },
+      {
+        target: UNITROLLER,
+        signature: "addPoolMarkets(uint96[],address[])",
+        params: [Array(POOL_SPECS.markets.length).fill(POOL_SPECS.id), POOL_SPECS.markets],
+      },
+      ...POOL_SPECS.marketsConfig.map(market => {
+        return {
+          target: UNITROLLER,
+          signature: "setCollateralFactor(uint96,address,uint256,uint256)",
+          params: [POOL_SPECS.id, market.address, market.collateralFactor, market.liquidationThreshold],
+        };
+      }),
+      ...POOL_SPECS.marketsConfig.map(market => {
+        return {
+          target: UNITROLLER,
+          signature: "setLiquidationIncentive(uint96,address,uint256)",
+          params: [POOL_SPECS.id, market.address, market.liquidationIncentive],
+        };
+      }),
+      ...POOL_SPECS.marketsConfig.map(market => {
+        return {
+          target: UNITROLLER,
+          signature: "setIsBorrowAllowed(uint96,address,bool)",
+          params: [POOL_SPECS.id, market.address, market.borrowAllowed],
+        };
+      }),
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip545;
