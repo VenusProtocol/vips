e1511f85e728bed89e9d163a1e5b4da19526de0d
GitGuru7 <aditya@venus.io>
2025-09-17 15:07:26 +0530

    feat: add remove-stablecoins-emode vip

---
+++ simulations/vip-545/bsctestnet-remove-stablecoins.ts
@@ -0,0 +1,62 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import { POOL_SPECS, UNITROLLER, vip545 } from "../../vips/vip-545/bsctestnet-remove-stablecoins";
+import { vip545 as addEmode } from "../../vips/vip-545/bsctestnet-stablecoins";
+import COMPTROLLER_ABI from "./abi/Comptroller.json";
+
+forking(65570708, async () => {
+  let comptroller: Contract;
+
+  before(async () => {
+    comptroller = await ethers.getContractAt(COMPTROLLER_ABI, UNITROLLER);
+  });
+
+  // This can be removed once the stablecoin VIP has been executed
+  testVip("add-stablecoins-emdoe-VIP", await addEmode(), {});
+
+  describe("Pre-VIP state", async () => {
+    it("check the current pool status to be active", async () => {
+      const pool = await comptroller.pools(POOL_SPECS.id);
+      expect(pool.label).to.equals(POOL_SPECS.label);
+      expect(pool.isActive).to.equals(true);
+    });
+    it("should include all expected markets in the pool", async () => {
+      const markets = await comptroller.getPoolVTokens(POOL_SPECS.id);
+      expect(markets.length).to.equals(POOL_SPECS.markets.length);
+    });
+  });
+
+  testVip("VIP-545", await vip545(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(txResponse, [COMPTROLLER_ABI], ["PoolActiveStatusUpdated", "PoolMarketRemoved"], [1, 2]);
+    },
+  });
+
+  describe("Post-VIP state", async () => {
+    it("should update the pool status to inactive", async () => {
+      const pool = await comptroller.pools(POOL_SPECS.id);
+      expect(pool.label).to.equals(POOL_SPECS.label);
+      expect(pool.isActive).to.equals(false);
+    });
+
+    it("should remove all the markets from the pool", async () => {
+      const markets = await comptroller.getPoolVTokens(POOL_SPECS.id);
+      expect(markets.length).to.equal(0);
+    });
+
+    it("should reset pool market data", async () => {
+      for (const market of POOL_SPECS.markets) {
+        const markets = await comptroller.poolMarkets(POOL_SPECS.id, market);
+        expect(markets.isListed).to.equal(false);
+        expect(markets.collateralFactorMantissa).to.equal(0);
+        expect(markets.liquidationThresholdMantissa).to.equal(0);
+        expect(markets.liquidationIncentiveMantissa).to.equal(0);
+      }
+    });
+  });
+});
