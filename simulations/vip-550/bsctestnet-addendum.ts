a4daa37886728be5b95a6c61bdcdca7421f4c2c9
GitGuru7 <aditya@venus.io>
2025-09-18 19:03:24 +0530

    fix: update addendum testnet vip

--- simulations/vip-550/bsctestnet-addendum.ts
+++ simulations/vip-550/bsctestnet-addendum.ts
@@ -7,69 +7,114 @@ import { expectEvents } from "src/utils";
 import { forking, testVip } from "src/vip-framework";
 
 import {
-  CUT_PARAMS,
-  NEW_SETTER_FACET,
-  OLD_SETTER_FACET,
+  NEW_DIAMOND,
+  NEW_VAI_CONTROLLER,
+  OLD_DIAMOND,
+  OLD_VAI_CONTROLLER,
   UNITROLLER,
+  VAI_UNITROLLER,
   vip550,
 } from "../../vips/vip-550/bsctestnet-addendum";
 import ACM_ABI from "./abi/AccessControlManager.json";
 import DIAMOND_ABI from "./abi/Diamond.json";
+import VAI_UNITROLLR_ABI from "./abi/VAIUnitroller.json";
+import { cutParams as params } from "./utils/bsctestnet-addendum-cut-params.json";
 
 const bsctestnet = NETWORK_ADDRESSES.bsctestnet;
 type CutParam = [string, number, string[]];
-const cutParams = CUT_PARAMS as unknown as CutParam[];
+const cutParams = params as unknown as CutParam[];
 
-forking(65695214, async () => {
+const OLD_SETTER_FACET = "0xF1844c6d56314a10C28175db638B51b4Ee14C402";
+const OLD_REWARD_FACET = "0x2B1b7FA16FE9B9ED5571663396bC16EBC079193B";
+const OLD_MARKET_FACET = "0x92B9CE322B0A4a3701fd3dC609740c7Df80f479D";
+const OLD_POLICY_FACET = "0xBDd1F07F4eF1748657FDA0d29CF4D7361120c187";
+
+const NEW_SETTER_FACET = "0x3CCC9fC2fDA021ADb9C9FB0493C1a4a9357f4064";
+const NEW_REWARD_FACET = "0xDD150De13849fB0776B466114b95770714c8Cc9d";
+const NEW_MARKET_FACET = "0xD3D5f6c68677051e6855Fa38dca0cD6D56ED0c4f";
+const NEW_POLICY_FACET = "0x11968dab15a9e59Cf9721b1F5c68418383CC1320";
+
+forking(65806620, async () => {
   let unitroller: Contract;
+  let vaiUnitroller: Contract;
   let accessControlManager: Contract;
 
   before(async () => {
     unitroller = await ethers.getContractAt(DIAMOND_ABI, UNITROLLER);
+    vaiUnitroller = await ethers.getContractAt(VAI_UNITROLLR_ABI, VAI_UNITROLLER);
     accessControlManager = await ethers.getContractAt(ACM_ABI, bsctestnet.ACCESS_CONTROL_MANAGER);
   });
 
   describe("Pre-VIP state", async () => {
-    it("unitroller should have old setterFacet", async () => {
-      expect(await unitroller.facetAddresses()).to.include(OLD_SETTER_FACET);
+    it("unitroller should have old implementation", async () => {
+      expect((await unitroller.comptrollerImplementation()).toLowerCase()).to.equal(OLD_DIAMOND.toLowerCase());
+    });
+    it("VAI Unitroller should point to old VAI Controller", async () => {
+      expect(await vaiUnitroller.vaiControllerImplementation()).to.equal(OLD_VAI_CONTROLLER);
+    });
+    it("unitroller should have old Facets", async () => {
+      expect(await unitroller.facetAddresses()).to.include(OLD_SETTER_FACET, OLD_REWARD_FACET);
+      expect(await unitroller.facetAddresses()).to.include(OLD_MARKET_FACET, OLD_POLICY_FACET);
     });
   });
 
   testVip("VIP-550", await vip550(), {
     callbackAfterExecution: async (txResponse: TransactionResponse) => {
-      await expectEvents(txResponse, [DIAMOND_ABI, ACM_ABI], ["DiamondCut", "PermissionGranted"], [1, 3]);
+      await expectEvents(txResponse, [DIAMOND_ABI, ACM_ABI], ["DiamondCut", "PermissionGranted"], [1, 6]);
     },
   });
 
   describe("Post-VIP state", async () => {
-    it("unitroller should contain the new setterFacet address", async () => {
-      expect(await unitroller.facetAddresses()).to.include(NEW_SETTER_FACET);
+    it("Unitroller should point to new implementation", async () => {
+      expect(await unitroller.comptrollerImplementation()).equals(NEW_DIAMOND);
     });
 
-    it("setter facet function selectors should be replaced with new facet", async () => {
-      const functionSelectors = [...cutParams[0][2], ...cutParams[1][2]];
+    it("VAI Controller should point to new impl", async () => {
+      expect(await vaiUnitroller.vaiControllerImplementation()).to.equal(NEW_VAI_CONTROLLER);
+    });
+
+    it("unitroller should contain the new Facet addresses", async () => {
+      expect(await unitroller.facetAddresses()).to.include(NEW_SETTER_FACET, NEW_REWARD_FACET);
+      expect(await unitroller.facetAddresses()).to.include(NEW_MARKET_FACET, NEW_POLICY_FACET);
+
+      expect(await unitroller.facetAddresses()).to.not.include(OLD_SETTER_FACET, OLD_REWARD_FACET);
+      expect(await unitroller.facetAddresses()).to.not.include(OLD_MARKET_FACET, OLD_POLICY_FACET);
+    });
+
+    it("market facet function selectors should be replaced with new facet address", async () => {
+      const functionSelectors = cutParams[0][2];
+      expect(await unitroller.facetFunctionSelectors(NEW_MARKET_FACET)).to.deep.equal(functionSelectors);
+      expect(await unitroller.facetFunctionSelectors(OLD_MARKET_FACET)).to.deep.equal([]);
+    });
+
+    it("policy facet function selectors should be replaced with new facet address", async () => {
+      const functionSelectors = cutParams[1][2];
+      expect(await unitroller.facetFunctionSelectors(NEW_POLICY_FACET)).to.deep.equal(functionSelectors);
+      expect(await unitroller.facetFunctionSelectors(OLD_POLICY_FACET)).to.deep.equal([]);
+    });
+
+    it("reward facet function selectors should be replaced with new facet address", async () => {
+      const functionSelectors = cutParams[2][2];
+      expect(await unitroller.facetFunctionSelectors(NEW_REWARD_FACET)).to.deep.equal(functionSelectors);
+      expect(await unitroller.facetFunctionSelectors(OLD_REWARD_FACET)).to.deep.equal([]);
+    });
+
+    it("setter facet function selectors should be replaced with new facet address", async () => {
+      const functionSelectors = [...cutParams[3][2], ...cutParams[4][2]];
       expect(await unitroller.facetFunctionSelectors(NEW_SETTER_FACET)).to.deep.equal(functionSelectors);
       expect(await unitroller.facetFunctionSelectors(OLD_SETTER_FACET)).to.deep.equal([]);
     });
 
     it("Check new permission", async () => {
-      expect(
-        await accessControlManager.hasPermission(bsctestnet.NORMAL_TIMELOCK, UNITROLLER, "setPoolLabel(uint96,string)"),
-      ).to.equal(true);
-      expect(
-        await accessControlManager.hasPermission(
-          bsctestnet.FAST_TRACK_TIMELOCK,
-          UNITROLLER,
-          "setPoolLabel(uint96,string)",
-        ),
-      ).to.equal(true);
-      expect(
-        await accessControlManager.hasPermission(
-          bsctestnet.CRITICAL_TIMELOCK,
-          UNITROLLER,
-          "setPoolLabel(uint96,string)",
-        ),
-      ).to.equal(true);
+      for (const method of ["setPoolLabel(uint96,string)", "setAllowCorePoolFallback(uint96,bool)"]) {
+        expect(await accessControlManager.hasPermission(bsctestnet.NORMAL_TIMELOCK, UNITROLLER, method)).to.equal(true);
+        expect(await accessControlManager.hasPermission(bsctestnet.FAST_TRACK_TIMELOCK, UNITROLLER, method)).to.equal(
+          true,
+        );
+        expect(await accessControlManager.hasPermission(bsctestnet.CRITICAL_TIMELOCK, UNITROLLER, method)).to.equal(
+          true,
+        );
+      }
     });
   });
 });
--- vips/vip-550/bsctestnet-addendum.ts
+++ vips/vip-550/bsctestnet-addendum.ts
@@ -2,55 +2,17 @@ import { NETWORK_ADDRESSES } from "src/networkAddresses";
 import { ProposalType } from "src/types";
 import { makeProposal } from "src/utils";
 
+import { cutParams as params } from "../../simulations/vip-550/utils/bsctestnet-addendum-cut-params.json";
+
 const bsctestnet = NETWORK_ADDRESSES.bsctestnet;
 export const UNITROLLER = "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D";
-export const OLD_SETTER_FACET = "0xF1844c6d56314a10C28175db638B51b4Ee14C402";
-export const NEW_SETTER_FACET = "0xc352898B455eC325fD1d31f265b53Aa2409B9F68";
-const FacetCutAction = { Add: 0, Replace: 1, Remove: 2 };
+export const VAI_UNITROLLER = "0xf70C3C6b749BbAb89C081737334E74C9aFD4BE16";
 
-export const CUT_PARAMS = [
-  [
-    NEW_SETTER_FACET,
-    FacetCutAction.Replace,
-    [
-      "0xf519fc30",
-      "0x2b5d790c",
-      "0x317b0b77",
-      "0x9bf34cbb",
-      "0x522c656b",
-      "0x17db2163",
-      "0xbb857450",
-      "0x607ef6c1",
-      "0x51a485e4",
-      "0x5f5af1aa",
-      "0x55ee1fe1",
-      "0x9460c8b5",
-      "0x2a6a6065",
-      "0xd24febad",
-      "0x9cfdd9e6",
-      "0x2ec04124",
-      "0x4e0853db",
-      "0x6662c7c9",
-      "0x919a3736",
-      "0x4ef233fc",
-      "0x24aaa220",
-      "0x12348e96",
-      "0x5cc4fdeb",
-      "0x9159b177",
-      "0x8b3113f6",
-      "0xa89766dd",
-      "0x35439240",
-      "0x9bd8f6e8",
-      "0x186db48f",
-      "0xd136af44",
-      "0xfd51a3ad",
-      "0x4964f48c",
-      "0x530e784f",
-      "0xc32094c7",
-    ],
-  ],
-  [NEW_SETTER_FACET, FacetCutAction.Add, ["0xb88d846b"]],
-];
+export const NEW_DIAMOND = "0xCe314cA8be79435FB0E4ffc102DAcA172B676a47";
+export const NEW_VAI_CONTROLLER = "0xA8122Fe0F9db39E266DE7A5BF953Cd72a87fe345";
+
+export const OLD_DIAMOND = "0x11Aa7fF5990E0A341eCeEeE9ddFdF8cE570DD5FD";
+export const OLD_VAI_CONTROLLER = "0x1eA874d53Dad10711Ff69C145bb59d2DFCCD7322";
 
 export const vip550 = () => {
   const meta = {
@@ -66,8 +28,28 @@ export const vip550 = () => {
     [
       {
         target: UNITROLLER,
+        signature: "_setPendingImplementation(address)",
+        params: [NEW_DIAMOND],
+      },
+      {
+        target: NEW_DIAMOND,
+        signature: "_become(address)",
+        params: [UNITROLLER],
+      },
+      {
+        target: UNITROLLER,
         signature: "diamondCut((address,uint8,bytes4[])[])",
-        params: [CUT_PARAMS],
+        params: [params],
+      },
+      {
+        target: VAI_UNITROLLER,
+        signature: "_setPendingImplementation(address)",
+        params: [NEW_VAI_CONTROLLER],
+      },
+      {
+        target: NEW_VAI_CONTROLLER,
+        signature: "_become(address)",
+        params: [VAI_UNITROLLER],
       },
       {
         target: bsctestnet.ACCESS_CONTROL_MANAGER,
@@ -83,6 +65,21 @@ export const vip550 = () => {
         target: bsctestnet.ACCESS_CONTROL_MANAGER,
         signature: "giveCallPermission(address,string,address)",
         params: [UNITROLLER, "setPoolLabel(uint96,string)", bsctestnet.CRITICAL_TIMELOCK],
+      },
+      {
+        target: bsctestnet.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [UNITROLLER, "setAllowCorePoolFallback(uint96,bool)", bsctestnet.NORMAL_TIMELOCK],
+      },
+      {
+        target: bsctestnet.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [UNITROLLER, "setAllowCorePoolFallback(uint96,bool)", bsctestnet.FAST_TRACK_TIMELOCK],
+      },
+      {
+        target: bsctestnet.ACCESS_CONTROL_MANAGER,
+        signature: "giveCallPermission(address,string,address)",
+        params: [UNITROLLER, "setAllowCorePoolFallback(uint96,bool)", bsctestnet.CRITICAL_TIMELOCK],
       },
     ],
     meta,
