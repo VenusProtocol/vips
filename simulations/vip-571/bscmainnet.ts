95467622fd2f9c1c616cd9fa2cc4168ddc7a868a
web3rover <arsenal.narayan@gmail.com>
2025-11-24 16:37:46 +0530

    fix: wip - vip and simulation

---
+++ simulations/vip-571/bscmainnet.ts
@@ -0,0 +1,373 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { expectEvents, setMaxStalePeriod, setMaxStalePeriodInBinanceOracle } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import vip571, { 
+  AAVE,
+  ADA,
+  asBNB,
+  BCH,
+  CAKE,
+  DAI,
+  DOGE,
+  DOT, 
+  FDUSD,
+  FIL,
+  LINK,
+  lisUSD,
+  LTC,
+  SOL,
+  SolvBTC,
+  THE,
+  TUSD,
+  TWT,
+  UNI,
+  VAI,
+  WBETH,
+  XRP,
+  XVS
+ } from "../../vips/vip-571/bscmainnet";
+import ERC20_ABI from "./abi/ERC20.json";
+import BINANCE_ORACLE_ABI from "./abi/binanceOracle.json";
+import BOUND_VALIDATOR_ABI from "./abi/boundValidator.json";
+import RESILIENT_ORACLE_ABI from "./abi/resilientOracle.json";
+
+const provider = ethers.provider;
+
+const { bscmainnet } = NETWORK_ADDRESSES;
+
+forking(69306243, async () => {
+  let resilientOracle: Contract;
+
+  before(async () => {
+    resilientOracle = new ethers.Contract(bscmainnet.RESILIENT_ORACLE, RESILIENT_ORACLE_ABI, provider);
+  });
+
+  describe("Pre-VIP behavior", async () => {
+    it("check AAVE price", async () => {
+      const price = await resilientOracle.getPrice(AAVE);
+      expect(price).to.be.equal(parseUnits("170.32621913", 18));
+    });
+
+    it("check ADA price", async () => {
+      const price = await resilientOracle.getPrice(ADA);
+      expect(price).to.be.equal(parseUnits("0.40761056", 18));
+    });
+
+    it("check asBNB price", async () => {
+      const price = await resilientOracle.getPrice(asBNB);
+      expect(price).to.be.equal(parseUnits("895.652717622043784833", 18));
+    });
+
+    it("check BCH price", async () => {
+      const price = await resilientOracle.getPrice(BCH);
+      expect(price).to.be.equal(parseUnits("543.200461790000000000", 18));
+    });
+
+    it("check CAKE price", async () => {
+      const price = await resilientOracle.getPrice(CAKE);
+      expect(price).to.be.equal(parseUnits("2.324840000000000000", 18));
+    });
+
+    it("check DAI price", async () => {
+      const price = await resilientOracle.getPrice(DAI);
+      expect(price).to.be.equal(parseUnits("0.99967909", 18));
+    });
+
+    it("check DOGE price", async () => {
+      const price = await resilientOracle.getPrice(DOGE);
+      expect(price).to.be.equal(parseUnits("1454520000", 18));
+    });
+
+    it("check DOT price", async () => {
+      const price = await resilientOracle.getPrice(DOT);
+      expect(price).to.be.equal(parseUnits("2.259085520000000000", 18));
+    });
+
+    it("check FDUSD price", async () => {
+      const price = await resilientOracle.getPrice(FDUSD);
+      expect(price).to.be.equal(parseUnits("0.996830", 18));
+    });
+
+    it("check FIL price", async () => {
+      const price = await resilientOracle.getPrice(FIL);
+      expect(price).to.be.equal(parseUnits("1.61386171", 18));
+    });
+
+    it("check LINK price", async () => {
+      const price = await resilientOracle.getPrice(LINK);
+      expect(price).to.be.equal(parseUnits("12.434677", 18));
+    });
+
+    it("check lisUSD price", async () => {
+      const price = await resilientOracle.getPrice(lisUSD);
+      expect(price).to.be.equal(parseUnits("0.99916213", 18));
+    });
+
+    it("check LTC price", async () => {
+      const price = await resilientOracle.getPrice(LTC);
+      expect(price).to.be.equal(parseUnits("83.031787930000000000", 18));
+    });
+
+    it("check SOL price", async () => {
+      const price = await resilientOracle.getPrice(SOL);
+      expect(price).to.be.equal(parseUnits("129.056394760000000000", 18));
+    });
+
+    it("check SolvBTC price", async () => {
+      const price = await resilientOracle.getPrice(SolvBTC);
+      expect(price).to.be.equal(parseUnits("85930.196134040000000000", 18));
+    });
+
+    it("check THE price", async () => {
+      const price = await resilientOracle.getPrice(THE);
+      expect(price).to.be.equal(parseUnits("0.147200000000000000", 18));
+    });
+
+    it("check TUSD price", async () => {
+      const price = await resilientOracle.getPrice(TUSD);
+      expect(price).to.be.equal(parseUnits("0.99598455", 18));
+    });
+
+    it("check TWT price", async () => {
+      const price = await resilientOracle.getPrice(TWT);
+      expect(price).to.be.equal(parseUnits("1.051994960000000000", 18));
+    });
+
+    it("check UNI price", async () => {
+      const price = await resilientOracle.getPrice(UNI);
+      expect(price).to.be.equal(parseUnits("6.1390708", 18));
+    });
+
+    it("check VAI price", async () => {
+      const price = await resilientOracle.getPrice(VAI);
+      expect(price).to.be.equal(parseUnits("0.999048", 18));
+    });
+
+    it("check WBETH price", async () => {
+      const price = await resilientOracle.getPrice(WBETH);  
+      expect(price).to.be.equal(parseUnits("3038.578498421588886094", 18));
+    });
+
+    it("check XRP price", async () => {
+      const price = await resilientOracle.getPrice(XRP);
+      expect(price).to.be.equal(parseUnits("2.058733000000000000", 18));
+    });
+
+    it("check XVS price", async () => {
+      const price = await resilientOracle.getPrice(XVS);
+      expect(price).to.be.equal(parseUnits("4.148370200000000000", 18));
+    });
+
+  });
+
+  testVip("VIP-571 bscmainnet", await vip571(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(
+        txResponse,
+        [RESILIENT_ORACLE_ABI, BINANCE_ORACLE_ABI, BOUND_VALIDATOR_ABI],
+        ["TokenConfigAdded", "MaxStalePeriodAdded", "ValidateConfigAdded"],
+        [23, 23, 23],
+      );
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    before(async () => {
+      const aave = await new ethers.Contract(AAVE, ERC20_ABI, provider);
+      const ada = await new ethers.Contract(ADA, ERC20_ABI, provider);
+      const asbnb = await new ethers.Contract(asBNB, ERC20_ABI, provider);
+      const bch = await new ethers.Contract(BCH, ERC20_ABI, provider);
+      const cake = await new ethers.Contract(CAKE, ERC20_ABI, provider);
+      const dai = await new ethers.Contract(DAI, ERC20_ABI, provider);
+      const doge = await new ethers.Contract(DOGE, ERC20_ABI, provider);
+      const dot = await new ethers.Contract(DOT, ERC20_ABI, provider);
+      const fdusd = await new ethers.Contract(FDUSD, ERC20_ABI, provider);
+      const fil = await new ethers.Contract(FIL, ERC20_ABI, provider);
+      const link = await new ethers.Contract(LINK, ERC20_ABI, provider);
+      const lisusd = await new ethers.Contract(lisUSD, ERC20_ABI, provider);
+      const ltc = await new ethers.Contract(LTC, ERC20_ABI, provider);
+      const sol = await new ethers.Contract(SOL, ERC20_ABI, provider);
+      const solvbtc = await new ethers.Contract(SolvBTC, ERC20_ABI, provider);
+      const the = await new ethers.Contract(THE, ERC20_ABI, provider);
+      const tusd = await new ethers.Contract(TUSD, ERC20_ABI, provider);
+      const twt = await new ethers.Contract(TWT, ERC20_ABI, provider);
+      const uni = await new ethers.Contract(UNI, ERC20_ABI, provider);
+      const vai = await new ethers.Contract(VAI, ERC20_ABI, provider);
+      const wbeth = await new ethers.Contract(WBETH, ERC20_ABI, provider);
+      const xrp = await new ethers.Contract(XRP, ERC20_ABI, provider);
+      const xvs = await new ethers.Contract(XVS, ERC20_ABI, provider);
+
+
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "AAVE");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "ADA");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "asBNB");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "BCH");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "CAKE");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "DAI");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "DOGE");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "DOT");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "FDUSD");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "FIL");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "LINK");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "lisUSD");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "LTC");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "SOL");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "SolvBTC");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "THE");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "TUSD");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "TWT");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "UNI");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "VAI");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "WBETH");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "XRP");
+      await setMaxStalePeriodInBinanceOracle(bscmainnet.BINANCE_ORACLE, "XVS");
+
+
+
+      await setMaxStalePeriod(resilientOracle, aave);
+      await setMaxStalePeriod(resilientOracle, ada);
+      await setMaxStalePeriod(resilientOracle, asbnb);
+      await setMaxStalePeriod(resilientOracle, bch);
+      await setMaxStalePeriod(resilientOracle, cake);
+      await setMaxStalePeriod(resilientOracle, dai);
+      await setMaxStalePeriod(resilientOracle, doge);
+      await setMaxStalePeriod(resilientOracle, dot);
+      await setMaxStalePeriod(resilientOracle, fdusd);
+      await setMaxStalePeriod(resilientOracle, fil);
+      await setMaxStalePeriod(resilientOracle, link);
+      await setMaxStalePeriod(resilientOracle, lisusd);
+      await setMaxStalePeriod(resilientOracle, ltc);
+      await setMaxStalePeriod(resilientOracle, sol);
+      await setMaxStalePeriod(resilientOracle, solvbtc);
+      await setMaxStalePeriod(resilientOracle, the);
+      await setMaxStalePeriod(resilientOracle, tusd);
+      await setMaxStalePeriod(resilientOracle, twt);
+      await setMaxStalePeriod(resilientOracle, uni);
+      await setMaxStalePeriod(resilientOracle, vai);
+      await setMaxStalePeriod(resilientOracle, wbeth);
+      await setMaxStalePeriod(resilientOracle, xrp);
+      await setMaxStalePeriod(resilientOracle, xvs);
+
+    });
+
+    it("check AAVE price", async () => {
+      const price = await resilientOracle.getPrice(AAVE);
+      expect(price).to.be.equal(parseUnits("170.32621913", 18));
+    });
+
+    it("check ADA price", async () => {
+      const price = await resilientOracle.getPrice(ADA);
+      expect(price).to.be.equal(parseUnits("0.40761056", 18));
+    });
+
+    it("check asBNB price", async () => {
+      const price = await resilientOracle.getPrice(asBNB);
+      expect(price).to.be.equal(parseUnits("895.652717622043784833", 18));
+    });
+
+    it("check BCH price", async () => {
+      const price = await resilientOracle.getPrice(BCH);
+      expect(price).to.be.equal(parseUnits("543.200461790000000000", 18));
+    });
+
+    it("check CAKE price", async () => {
+      const price = await resilientOracle.getPrice(CAKE);
+      expect(price).to.be.equal(parseUnits("2.324840000000000000", 18));
+    });
+
+    it("check DAI price", async () => {
+      const price = await resilientOracle.getPrice(DAI);
+      expect(price).to.be.equal(parseUnits("0.99967909", 18));
+    });
+
+    it("check DOGE price", async () => {
+      const price = await resilientOracle.getPrice(DOGE);
+      expect(price).to.be.equal(parseUnits("1454520000", 18));
+    });
+
+    it("check DOT price", async () => {
+      const price = await resilientOracle.getPrice(DOT);
+      expect(price).to.be.equal(parseUnits("2.259085520000000000", 18));
+    });
+
+    it("check FDUSD price", async () => {
+      const price = await resilientOracle.getPrice(FDUSD);
+      expect(price).to.be.equal(parseUnits("0.996830", 18));
+    });
+
+    it("check FIL price", async () => {
+      const price = await resilientOracle.getPrice(FIL);
+      expect(price).to.be.equal(parseUnits("1.61386171", 18));
+    }); 
+
+    it("check LINK price", async () => {
+      const price = await resilientOracle.getPrice(LINK);
+      expect(price).to.be.equal(parseUnits("12.434677", 18));
+    });
+
+    it("check lisUSD price", async () => {
+      const price = await resilientOracle.getPrice(lisUSD);
+      expect(price).to.be.equal(parseUnits("0.99916213", 18));
+    });
+
+    it("check LTC price", async () => {
+      const price = await resilientOracle.getPrice(LTC);
+      expect(price).to.be.equal(parseUnits("83.031787930000000000", 18));
+    });
+
+    it("check SOL price", async () => {
+      const price = await resilientOracle.getPrice(SOL);
+      expect(price).to.be.equal(parseUnits("129.056394760000000000", 18));
+    });
+
+    it("check SolvBTC price", async () => {
+      const price = await resilientOracle.getPrice(SolvBTC);
+      expect(price).to.be.equal(parseUnits("85923.46994603", 18));
+    });
+
+    it("check THE price", async () => {
+      const price = await resilientOracle.getPrice(THE);
+      expect(price).to.be.equal(parseUnits("0.147200000000000000", 18));
+    });
+
+    it("check TUSD price", async () => {
+      const price = await resilientOracle.getPrice(TUSD);
+      expect(price).to.be.equal(parseUnits("0.99598455", 18));
+    });
+
+    it("check TWT price", async () => {
+      const price = await resilientOracle.getPrice(TWT);
+      expect(price).to.be.equal(parseUnits("1.051994960000000000", 18));
+    });
+
+    it("check UNI price", async () => {
+      const price = await resilientOracle.getPrice(UNI);
+      expect(price).to.be.equal(parseUnits("6.1390708", 18));
+    });
+
+    it("check VAI price", async () => {
+      const price = await resilientOracle.getPrice(VAI);
+      expect(price).to.be.equal(parseUnits("0.999048", 18));
+    });
+
+    it("check WBETH price", async () => {
+      const price = await resilientOracle.getPrice(WBETH);  
+      expect(price).to.be.equal(parseUnits("3038.578498421588886094", 18));
+    });
+
+    it("check XRP price", async () => {
+      const price = await resilientOracle.getPrice(XRP);
+      expect(price).to.be.equal(parseUnits("2.058733000000000000", 18));
+    });
+
+    it("check XVS price", async () => {
+      const price = await resilientOracle.getPrice(XVS);
+      expect(price).to.be.equal(parseUnits("4.148370200000000000", 18));
+    });
+  });
+});
---
+++ vips/vip-571/bscmainnet.ts
@@ -0,0 +1,352 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { bscmainnet } = NETWORK_ADDRESSES;
+
+export const AAVE = "0xfb6115445Bff7b52FeB98650C87f44907E58f802";
+export const ADA = "0x3EE2200Efb3400fAbB9AacF31297cBdD1d435D47";
+export const asBNB = "0x77734e70b6E88b4d82fE632a168EDf6e700912b6";
+export const BCH = "0x8fF795a6F4D97E7887C79beA79aba5cc76444aDf";
+export const CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
+export const DAI = "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3";
+export const DOGE = "0xbA2aE424d960c26247Dd6c32edC70B295c744C43";
+export const DOT = "0x7083609fCE4d1d8Dc0C979AAb8c869Ea2C873402";
+export const FDUSD = "0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409";
+export const FIL = "0x0D8Ce2A99Bb6e3B7Db580eD848240e4a0F9aE153";
+export const LINK = "0xF8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD";
+export const lisUSD = "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5";
+export const LTC = "0x4338665CBB7B2485A8855A139b75D5e34AB0DB94";
+export const SOL = "0x570A5D26f7765Ecb712C0924E4De545B89fD43dF";
+export const SolvBTC = "0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7";
+export const THE = "0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11";
+export const TUSD = "0x40af3827F39D0EAcBF4A168f8D4ee67c121D11c9";
+export const TWT = "0x4B0F1812e5Df2A09796481Ff14017e6005508003";
+export const UNI = "0xBf5140A22578168FD562DCcF235E5D43A02ce9B1";
+export const VAI = "0x4BD17003473389A42DAF6a0a729f6Fdb328BbBd7";
+export const WBETH = "0xa2e3356610840701bdf5611a53974510ae27e2e1";
+export const XRP = "0x1D2F0da169ceB9fC7B3144628dB156f3F6c60dBE";
+export const XVS = "0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63";
+
+export const BOUND_VALIDATOR = "0x6E332fF0bB52475304494E4AE5063c1051c7d735";
+
+export const HEARTBEAT_25_HOURS = 25 * 60 * 60;
+export const HEARTBEAT_20_MINUTES = 20 * 60;
+export const HEARTBEAT_13_HOURS = 13 * 60 * 60;
+
+export const PRICE_LOWER_BOUND = parseUnits("0.95", 18);
+export const PRICE_UPPER_BOUND = parseUnits("1.05", 18);
+
+export const vip571 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-571",
+    description: ``,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["AAVE", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["ADA", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["asBNB", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["BCH", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["CAKE", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["DAI", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["DOGE", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["DOT", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["FDUSD", HEARTBEAT_20_MINUTES],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["FIL", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["LINK", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["lisUSD", HEARTBEAT_20_MINUTES],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["LTC", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["SOL", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["SolvBTC", HEARTBEAT_13_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["THE", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["TUSD", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["TWT", HEARTBEAT_20_MINUTES],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["UNI", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["VAI", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["WBETH", HEARTBEAT_20_MINUTES],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["XRP", HEARTBEAT_25_HOURS],
+      },
+      {
+        target: bscmainnet.BINANCE_ORACLE,
+        signature: "setMaxStalePeriod(string,uint256)",
+        params: ["XVS", HEARTBEAT_20_MINUTES],
+      },
+      {
+        target: bscmainnet.RESILIENT_ORACLE,
+        signature: "setTokenConfigs((address,address[3],bool[3],bool)[])",
+        params: [
+          [
+            [
+              AAVE,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              ADA,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              asBNB,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              BCH,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              CAKE,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              DAI,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              DOGE,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              DOT,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              FDUSD,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              FIL,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              LINK,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              lisUSD,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              LTC,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              SOL,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              SolvBTC,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              THE,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              TUSD,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              TWT,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              UNI,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              VAI,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              WBETH,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              XRP,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+            [
+              XVS,
+              [bscmainnet.CHAINLINK_ORACLE, bscmainnet.BINANCE_ORACLE, ethers.constants.AddressZero],
+              [true, true, false],
+              false,
+            ],
+          ],
+        ],
+      },
+      {
+        target: BOUND_VALIDATOR,
+        signature: "setValidateConfigs((address,uint256,uint256)[])",
+        params: [
+          [
+            [AAVE, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [ADA, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [asBNB, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [BCH, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [CAKE, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [DAI, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [DOGE, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [DOT, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [FDUSD, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [FIL, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [LINK, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [lisUSD, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [LTC, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [SOL, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [SolvBTC, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [THE, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [TUSD, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [TWT, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [UNI, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [VAI, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [WBETH, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [XRP, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+            [XVS, PRICE_UPPER_BOUND, PRICE_LOWER_BOUND],
+          ],
+        ],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip571;
