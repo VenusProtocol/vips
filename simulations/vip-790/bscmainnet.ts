f2ed28fe7b114dffa043a161eabe64ded1b8dede
GitGuru7 <aditya@venus.io>
2026-01-07 20:12:10 +0530

    feat: add vip and simulations to adjust the prime reward and xvs vault reward

---
+++ simulations/vip-790/bscmainnet.ts
@@ -0,0 +1,79 @@
+import { TransactionResponse } from "@ethersproject/providers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import vip790, {
+  BSC_SPEED,
+  BSC_XVS_AMOUNT,
+  BSC_XVS_STORE,
+  BSC_XVS_VAULT_TREASURY,
+  NEW_PRIME_SPEED_FOR_USDC_USDT,
+  PRIME_LIQUIDITY_PROVIDER,
+  UNICHAIN_XVS_BRIDGE_AMOUNT,
+  USDC,
+  USDT,
+  ZKSYNC_XVS_BRIDGE_AMOUNT,
+} from "../../vips/vip-790/bscmainnet";
+import CORE_COMPTROLLER_ABI from "./abi/CoreComptroller.json";
+import PRIME_LIQUIDITY_PROVIDER_ABI from "./abi/PrimeLiquidityProvider.json";
+import XVS_ABI from "./abi/XVS.json";
+import XVS_VAULT_ABI from "./abi/XVSVault.json";
+
+const { bscmainnet } = NETWORK_ADDRESSES;
+
+forking(74385484, async () => {
+  let xvs: Contract;
+  let comptrollerPreviousXVSBalance: Contract;
+  let xvsVaultTreasuryPreviousXVSBalance: Contract;
+  let xvsStorePreviousXVSBalance: Contract;
+  let xvsVault: Contract;
+  let primeLiquidityProvider: Contract;
+
+  before(async () => {
+    xvs = await ethers.getContractAt(XVS_ABI, bscmainnet.XVS);
+    comptrollerPreviousXVSBalance = await xvs.balanceOf(bscmainnet.UNITROLLER);
+    xvsVaultTreasuryPreviousXVSBalance = await xvs.balanceOf(BSC_XVS_VAULT_TREASURY);
+    xvsStorePreviousXVSBalance = await xvs.balanceOf(BSC_XVS_STORE);
+    xvsVault = await ethers.getContractAt(XVS_VAULT_ABI, bscmainnet.XVS_VAULT_PROXY);
+    primeLiquidityProvider = await ethers.getContractAt(PRIME_LIQUIDITY_PROVIDER_ABI, PRIME_LIQUIDITY_PROVIDER);
+  });
+
+  testVip("vip-790", await vip790(), {
+    callbackAfterExecution: async (txResponse: TransactionResponse) => {
+      await expectEvents(txResponse, [CORE_COMPTROLLER_ABI], ["VenusGranted"], [1]);
+    },
+  });
+
+  describe("Post-VIP behavior", async () => {
+    it("should update Prime rewards distribution speed for USDC and USDT", async () => {
+      const usdcSpeed = await primeLiquidityProvider.tokenDistributionSpeeds(USDC);
+      const usdtSpeed = await primeLiquidityProvider.tokenDistributionSpeeds(USDT);
+      expect(usdcSpeed).to.equal(NEW_PRIME_SPEED_FOR_USDC_USDT);
+      expect(usdtSpeed).to.equal(NEW_PRIME_SPEED_FOR_USDC_USDT);
+    });
+
+    it("should transfer XVS from the Comptroller", async () => {
+      const comptrollerXVSBalanceAfter = await xvs.balanceOf(bscmainnet.UNITROLLER);
+      expect(comptrollerXVSBalanceAfter).to.equal(
+        comptrollerPreviousXVSBalance.sub(ZKSYNC_XVS_BRIDGE_AMOUNT.add(UNICHAIN_XVS_BRIDGE_AMOUNT)),
+      );
+    });
+
+    it("should transfer XVS from the XVS Vault Treasury to the XVS Store", async () => {
+      const xvsVaultTreasuryXVSBalanceAfter = await xvs.balanceOf(BSC_XVS_VAULT_TREASURY);
+      expect(xvsVaultTreasuryXVSBalanceAfter).to.equal(xvsVaultTreasuryPreviousXVSBalance.sub(BSC_XVS_AMOUNT));
+
+      const xvsStoreXVSBalanceAfter = await xvs.balanceOf(BSC_XVS_STORE);
+      expect(xvsStoreXVSBalanceAfter).to.equal(xvsStorePreviousXVSBalance.add(BSC_XVS_AMOUNT));
+    });
+
+    it("should update the XVS distribution speed", async () => {
+      const speed = await xvsVault.rewardTokenAmountsPerBlockOrSecond(bscmainnet.XVS);
+      expect(speed).to.equal(BSC_SPEED);
+    });
+  });
+});
---
+++ vips/vip-790/bscmainnet.ts
@@ -0,0 +1,156 @@
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { LzChainId, ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const { bscmainnet, ethereum, unichainmainnet, zksyncmainnet, arbitrumone } = NETWORK_ADDRESSES;
+
+export const PSR = "0xCa01D5A9A248a830E9D93231e791B1afFed7c446";
+export const USDT_PRIME_CONVERTER = "0xD9f101AA67F3D72662609a2703387242452078C3";
+export const USDC_PRIME_CONVERTER = "0xa758c9C215B6c4198F0a0e3FA46395Fa15Db691b";
+export const PRIME_LIQUIDITY_PROVIDER = "0x23c4F844ffDdC6161174eB32c770D4D8C07833F2";
+
+export const USDC = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
+export const USDT = "0x55d398326f99059fF775485246999027B3197955";
+
+export const NEW_PRIME_SPEED_FOR_USDC_USDT = parseUnits("0.005422374429223744", 18);
+
+// TODO: XVS Buyback and Funds Allocation Values TO BE UPDATED
+export const ADAPTER_PARAMS = ethers.utils.solidityPack(["uint16", "uint256"], [1, 300000]);
+export const BRIDGE_FEES_BSC = parseUnits("0.5", 18);
+
+export const ZKSYNC_XVS_BRIDGE_AMOUNT = parseUnits("2", 18);
+export const ZKSYNC_XVS_STORE = "0x84266F552756cBed893b1FFA85248cD99501e3ce";
+export const ZKSYNC_SPEED = parseUnits("0.00011632", 18); // 10.05 XVS/day
+
+export const UNICHAIN_XVS_BRIDGE_AMOUNT = parseUnits("2", 18);
+export const UNICHAIN_XVS_STORE = "0x0ee4b35c2cEAb19856Bf35505F81608d12B2a7Bb";
+export const UNICHAIN_SPEED = parseUnits("0.00003588", 18); // 3.1 XVS/day
+
+export const BSC_XVS_BRIDGE = "0xf8F46791E3dB29a029Ec6c9d946226f3c613e854";
+export const BSC_XVS_VAULT_TREASURY = "0x269ff7818DB317f60E386D2be0B259e1a324a40a";
+export const BSC_XVS_STORE = "0x1e25CF968f12850003Db17E0Dba32108509C4359";
+export const BSC_XVS_AMOUNT = parseUnits("2", 18);
+export const BSC_SPEED = parseUnits("0.01213542", 18); // 1,398 XVS/day
+
+export const ETH_XVS_VAULT_TREASURY = "0xaE39C38AF957338b3cEE2b3E5d825ea88df02EfE";
+export const ETH_XVS_STORE = "0x1Db646E1Ab05571AF99e47e8F909801e5C99d37B";
+export const ETH_XVS_AMOUNT = parseUnits("2", 18);
+export const ETH_SPEED = parseUnits("0.00378889", 18); // 27.28 XVS/day
+
+export const ARB_XVS_VAULT_TREASURY = "0xb076D4f15c08D7A7B89466327Ba71bc7e1311b58";
+export const ARB_XVS_STORE = "0x507D9923c954AAD8eC530ed8Dedb75bFc893Ec5e";
+export const ARB_XVS_AMOUNT = parseUnits("2", 18);
+export const ARB_SPEED = parseUnits("0.00002593", 18); // 2.24 XVS/day
+
+export const vip790 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP790 PRIME and XVS REWARDS ALLOCATION",
+    description: `VIP790 PRIME and XVS REWARDS ALLOCATION`,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      // Adjust Prime Rewards Distribution Speed
+      {
+        target: PRIME_LIQUIDITY_PROVIDER,
+        signature: "setTokensDistributionSpeed(address[],uint256[])",
+        params: [
+          [USDC, USDT],
+          [NEW_PRIME_SPEED_FOR_USDC_USDT, NEW_PRIME_SPEED_FOR_USDC_USDT],
+        ],
+      },
+
+      // XVS Buyback and Funds Allocation
+      {
+        target: bscmainnet.UNITROLLER,
+        signature: "_grantXVS(address,uint256)",
+        params: [bscmainnet.NORMAL_TIMELOCK, ZKSYNC_XVS_BRIDGE_AMOUNT.add(UNICHAIN_XVS_BRIDGE_AMOUNT)],
+      },
+      {
+        target: bscmainnet.XVS,
+        signature: "approve(address,uint256)",
+        params: [BSC_XVS_BRIDGE, ZKSYNC_XVS_BRIDGE_AMOUNT.add(UNICHAIN_XVS_BRIDGE_AMOUNT)],
+      },
+      {
+        target: BSC_XVS_BRIDGE,
+        signature: "sendFrom(address,uint16,bytes32,uint256,(address,address,bytes))",
+        params: [
+          bscmainnet.NORMAL_TIMELOCK,
+          LzChainId.zksyncmainnet,
+          ethers.utils.defaultAbiCoder.encode(["address"], [ZKSYNC_XVS_STORE]),
+          ZKSYNC_XVS_BRIDGE_AMOUNT,
+          [bscmainnet.NORMAL_TIMELOCK, ethers.constants.AddressZero, ADAPTER_PARAMS],
+        ],
+        value: BRIDGE_FEES_BSC.toString(),
+      },
+      {
+        target: BSC_XVS_BRIDGE,
+        signature: "sendFrom(address,uint16,bytes32,uint256,(address,address,bytes))",
+        params: [
+          bscmainnet.NORMAL_TIMELOCK,
+          LzChainId.unichainmainnet,
+          ethers.utils.defaultAbiCoder.encode(["address"], [UNICHAIN_XVS_STORE]),
+          UNICHAIN_XVS_BRIDGE_AMOUNT,
+          [bscmainnet.NORMAL_TIMELOCK, ethers.constants.AddressZero, ADAPTER_PARAMS],
+        ],
+        value: BRIDGE_FEES_BSC.toString(),
+      },
+      {
+        target: zksyncmainnet.XVS_VAULT_PROXY,
+        signature: "setRewardAmountPerBlockOrSecond(address,uint256)",
+        params: [zksyncmainnet.XVS, ZKSYNC_SPEED],
+        dstChainId: LzChainId.zksyncmainnet,
+      },
+      {
+        target: unichainmainnet.XVS_VAULT_PROXY,
+        signature: "setRewardAmountPerBlockOrSecond(address,uint256)",
+        params: [unichainmainnet.XVS, UNICHAIN_SPEED],
+        dstChainId: LzChainId.unichainmainnet,
+      },
+      {
+        target: BSC_XVS_VAULT_TREASURY,
+        signature: "fundXVSVault(uint256)",
+        params: [BSC_XVS_AMOUNT],
+      },
+      {
+        target: bscmainnet.XVS_VAULT_PROXY,
+        signature: "setRewardAmountPerBlockOrSecond(address,uint256)",
+        params: [bscmainnet.XVS, BSC_SPEED],
+      },
+      {
+        target: ETH_XVS_VAULT_TREASURY,
+        signature: "fundXVSVault(uint256)",
+        params: [ETH_XVS_AMOUNT],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ethereum.XVS_VAULT_PROXY,
+        signature: "setRewardAmountPerBlockOrSecond(address,uint256)",
+        params: [ethereum.XVS, ETH_SPEED],
+        dstChainId: LzChainId.ethereum,
+      },
+      {
+        target: ARB_XVS_VAULT_TREASURY,
+        signature: "fundXVSVault(uint256)",
+        params: [ARB_XVS_AMOUNT],
+        dstChainId: LzChainId.arbitrumone,
+      },
+      {
+        target: arbitrumone.XVS_VAULT_PROXY,
+        signature: "setRewardAmountPerBlockOrSecond(address,uint256)",
+        params: [arbitrumone.XVS, ARB_SPEED],
+        dstChainId: LzChainId.arbitrumone,
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip790;
