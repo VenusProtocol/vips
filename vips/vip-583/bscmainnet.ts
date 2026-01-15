06b11ff5b447cf66be7f9406b190b828dbaa04e9
fred-venus <fred@venus.io>
2026-01-15 17:17:47 +0800

    fix: added more tests to verify the fix

--- vips/vip-583/bscmainnet.ts
+++ vips/vip-583/bscmainnet.ts
@@ -5,7 +5,7 @@ export const DEFAULT_PROXY_ADMIN = "0x6beb6D2695B67FEb73ad4f172E8E2975497187e4";
 export const RISK_FUND_V2_PROXY = "0xdF31a28D68A2AB381D42b380649Ead7ae2A76E42";
 export const RISK_FUND_V2_NEW_IMPLEMENTATION = "0x60e322C3418EcAEA5E6859551c299c968adc9816";
 
-export const vip780 = () => {
+export const vip583 = () => {
   const meta = {
     version: "v2",
     title: "VIP-780 Upgrade RiskFundV2 Implementation",
@@ -28,4 +28,4 @@ export const vip780 = () => {
   );
 };
 
-export default vip780;
+export default vip583;
--- vips/vip-583/bsctestnet.ts
+++ vips/vip-583/bsctestnet.ts
@@ -5,10 +5,10 @@ export const DEFAULT_PROXY_ADMIN = "0x7877ffd62649b6a1557b55d4c20fcbab17344c91";
 export const RISK_FUND_V2_PROXY = "0x487CeF72dacABD7E12e633bb3B63815a386f7012";
 export const RISK_FUND_V2_NEW_IMPLEMENTATION = "0x36b236c62AB32430016EC0EaE8e40ebe29d95869";
 
-export const vip780 = () => {
+export const vip583 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-780 Upgrade RiskFundV2 Implementation",
+    title: "VIP-583 Upgrade RiskFundV2 Implementation",
     description: ``,
     forDescription: "Execute this proposal",
     againstDescription: "Do not execute this proposal",
@@ -28,4 +28,4 @@ export const vip780 = () => {
   );
 };
 
-export default vip780;
+export default vip583;
