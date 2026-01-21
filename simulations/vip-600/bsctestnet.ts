9a409f45b79c05d604e96f4f533e3c5117ad911f
Debugger022 <debugger022@gmail.com>
2026-01-21 16:55:55 +0530

    feat: add vip and simulation for bsctestnet

---
+++ simulations/vip-600/bsctestnet.ts
@@ -0,0 +1,46 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { forking, testVip } from "src/vip-framework";
+
+import { SWAP_HELPER, SWAP_ROUTER, UNITROLLER, vip600 } from "../../vips/vip-600/bsctestnet";
+import SWAP_ROUTER_ABI from "./abi/SwapRouter.json";
+
+const { bsctestnet } = NETWORK_ADDRESSES;
+
+forking(85710269, async () => {
+  let swapRouter: Contract;
+
+  before(async () => {
+    swapRouter = await ethers.getContractAt(SWAP_ROUTER_ABI, SWAP_ROUTER);
+  });
+
+  describe("Pre-VIP behavior", () => {
+    it("SwapRouter should have NORMAL_TIMELOCK as pending owner", async () => {
+      expect(await swapRouter.pendingOwner()).to.equal(bsctestnet.NORMAL_TIMELOCK);
+    });
+
+    describe("SwapRouter configuration", () => {
+      it("should have correct COMPTROLLER", async () => {
+        expect(await swapRouter.COMPTROLLER()).to.equal(UNITROLLER);
+      });
+
+      it("should have correct swapHelper", async () => {
+        expect(await swapRouter.SWAP_HELPER()).to.equal(SWAP_HELPER);
+      });
+    });
+  });
+
+  testVip("VIP-600", await vip600(), {});
+
+  describe("Post-VIP behavior", () => {
+    it("SwapRouter should have NORMAL_TIMELOCK as owner", async () => {
+      expect(await swapRouter.owner()).to.equal(bsctestnet.NORMAL_TIMELOCK);
+    });
+
+    it("SwapRouter should have zero address as pending owner", async () => {
+      expect(await swapRouter.pendingOwner()).to.equal(ethers.constants.AddressZero);
+    });
+  });
+});
