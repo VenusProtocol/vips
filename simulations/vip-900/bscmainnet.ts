21958fd3f1d53dac153aa9e82f4abd2adfe21c8c
Debugger022 <debugger022@gmail.com>
2026-02-10 19:28:49 +0530

    feat: add VIP-900 to whitelist Chainlink OEV Searcher and transfer WBNBBurnConverter ownership

---
+++ simulations/vip-900/bscmainnet.ts
@@ -0,0 +1,50 @@
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+import { expectEvents } from "src/utils";
+import { forking, testVip } from "src/vip-framework";
+
+import vip900, {
+  CHAINLINK_OEV_SEARCHER,
+  GUARDIAN_2,
+  UNITROLLER,
+  WBNB_BURN_CONVERTER,
+} from "../../vips/vip-900/bscmainnet";
+import COMPTROLLER_ABI from "./abi/FlashLoanFacet.json";
+
+const SINGLE_TOKEN_CONVERTER_ABI = [
+  "function owner() view returns (address)",
+  "function pendingOwner() view returns (address)",
+];
+
+forking(80413671, async () => {
+  let comptroller: Contract;
+  let wbnbBurnConverter: Contract;
+
+  before(async () => {
+    comptroller = await ethers.getContractAt(COMPTROLLER_ABI, UNITROLLER);
+    wbnbBurnConverter = await ethers.getContractAt(SINGLE_TOKEN_CONVERTER_ABI, WBNB_BURN_CONVERTER);
+  });
+
+  describe("Pre-VIP behavior", () => {
+    it("Chainlink OEV Searcher should not be whitelisted for flash loans", async () => {
+      expect(await comptroller.authorizedFlashLoan(CHAINLINK_OEV_SEARCHER)).to.equal(false);
+    });
+  });
+
+  testVip("VIP-900", await vip900(), {
+    callbackAfterExecution: async txResponse => {
+      await expectEvents(txResponse, [COMPTROLLER_ABI], ["IsAccountFlashLoanWhitelisted"], [1]);
+    },
+  });
+
+  describe("Post-VIP behavior", () => {
+    it("Chainlink OEV Searcher should be whitelisted for flash loans", async () => {
+      expect(await comptroller.authorizedFlashLoan(CHAINLINK_OEV_SEARCHER)).to.equal(true);
+    });
+
+    it("Guardian 2 should be set as pending owner of WBNBBurnConverter", async () => {
+      expect(await wbnbBurnConverter.pendingOwner()).to.equal(GUARDIAN_2);
+    });
+  });
+});
---
+++ vips/vip-900/bscmainnet.ts
@@ -0,0 +1,48 @@
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+export const UNITROLLER = "0xfD36E2c2a6789Db23113685031d7F16329158384";
+export const WBNB_BURN_CONVERTER = "0x9eF79830e626C8ccA7e46DCEd1F90e51E7cFCeBE";
+export const CHAINLINK_OEV_SEARCHER = "0x82aC8c837E31D47c6508CBfD4c1627F04dDe5298";
+export const GUARDIAN_2 = "0x1C2CAc6ec528c20800B2fe734820D87b581eAA6B";
+
+export const vip900 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-900 Add Chainlink OEV Searcher to Flashloan Whitelist and Transfer WBNBBurnConverter Ownership",
+    description: `#### Summary
+
+This VIP performs two actions:
+
+1. **Add Chainlink OEV Searcher to Flashloan Whitelist**: As part of our ongoing cooperation with Chainlink to enable OEV (Oracle Extractable Value), Chainlink has requested to whitelist their OEV Searcher address (0x82aC8c837E31D47c6508CBfD4c1627F04dDe5298) to use flashloans for performing liquidations.
+
+2. **Transfer WBNBBurnConverter Ownership to Guardian 2**: Following the cessation of the BNB burn program (VIP-585), we need to transfer ownership of the WBNBBurnConverter contract back to Guardian 2 to withdraw the remaining funds from the converter.
+
+#### Actions
+
+- Whitelist Chainlink OEV Searcher (0x82aC8c837E31D47c6508CBfD4c1627F04dDe5298) for flashloans on the Core Pool
+- Transfer ownership of WBNBBurnConverter to Guardian 2 (0x1C2CAc6ec528c20800B2fe734820D87b581eAA6B)`,
+    forDescription: "Execute this proposal",
+    againstDescription: "Do not execute this proposal",
+    abstainDescription: "Indifferent to execution",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: UNITROLLER,
+        signature: "setWhiteListFlashLoanAccount(address,bool)",
+        params: [CHAINLINK_OEV_SEARCHER, true],
+      },
+      {
+        target: WBNB_BURN_CONVERTER,
+        signature: "transferOwnership(address)",
+        params: [GUARDIAN_2],
+      },
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip900;
