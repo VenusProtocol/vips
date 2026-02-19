cca199181e4210134c53dac711551f95aa735f0f
GitGuru7 <aditya@venus.io>
2026-02-19 19:00:51 +0530

    feat: add vip and simulations to configure XAUM market in bsctestnet

---
+++ vips/vip-615/bsctestnet.ts
@@ -0,0 +1,241 @@
+import { BigNumber, BigNumberish } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { NETWORK_ADDRESSES } from "src/networkAddresses";
+import { ProposalType } from "src/types";
+import { makeProposal } from "src/utils";
+
+const bsctestnet = NETWORK_ADDRESSES.bsctestnet;
+export const { RESILIENT_ORACLE, CHAINLINK_ORACLE } = NETWORK_ADDRESSES.bsctestnet;
+
+export const PROTOCOL_SHARE_RESERVE = "0x25c7c7D6Bf710949fD7f03364E9BA19a1b3c10E3";
+export const XAUM = "0xd2e2575c24302C82d38A9079c85722e0D0C0D2cD";
+export const vXAUM = "0xc93CBF6CA7F3124737F2f4daDa8dBBC7be56d125";
+export const RATE_MODEL = "0x08a2F4387BcAe9776243dF1f5BFfb6Ea2bE9c7dD";
+export const REDUCE_RESERVES_BLOCK_DELTA = "28800";
+
+// Converters
+const ETH = "0x98f7A83361F7Ac8765CcEBAB1425da6b341958a7";
+const USDT = "0xA11c8D9DC9b66E209Ef60F0C8D969D3CD988782c";
+const USDC = "0x16227D60f7a0e586C66B005219dfc887D13C9531";
+const BTCB = "0xA808e341e8e723DC6BA0Bb5204Bafc2330d7B8e4";
+const XVS = "0xB9e0E753630434d7863528cc73CB7AC638a7c8ff";
+const WBNB = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";
+const WBNB_BURN_CONVERTER = "0x42DBA48e7cCeB030eC73AaAe29d4A3F0cD4facba";
+const RISK_FUND_CONVERTER = "0x32Fbf7bBbd79355B86741E3181ef8c1D9bD309Bb";
+const USDT_PRIME_CONVERTER = "0xf1FA230D25fC5D6CAfe87C5A6F9e1B17Bc6F194E";
+const USDC_PRIME_CONVERTER = "0x2ecEdE6989d8646c992344fF6C97c72a3f811A13";
+const BTCB_PRIME_CONVERTER = "0x989A1993C023a45DA141928921C0dE8fD123b7d1";
+const ETH_PRIME_CONVERTER = "0xf358650A007aa12ecC8dac08CF8929Be7f72A4D9";
+const XVS_VAULT_CONVERTER = "0x258f49254C758a0E37DAb148ADDAEA851F4b02a2";
+export const CONVERSION_INCENTIVE = 1e14;
+
+export const converterBaseAssets = {
+  [RISK_FUND_CONVERTER]: USDT,
+  [USDT_PRIME_CONVERTER]: USDT,
+  [USDC_PRIME_CONVERTER]: USDC,
+  [BTCB_PRIME_CONVERTER]: BTCB,
+  [ETH_PRIME_CONVERTER]: ETH,
+  [XVS_VAULT_CONVERTER]: XVS,
+  [WBNB_BURN_CONVERTER]: WBNB,
+};
+
+export const marketSpecs = {
+  vToken: {
+    address: vXAUM,
+    name: "Venus Matrixdock Gold",
+    symbol: "vXAUM",
+    underlying: {
+      address: XAUM,
+      decimals: 18,
+      symbol: "XAUM",
+    },
+    decimals: 8,
+    exchangeRate: parseUnits("1", 28),
+    comptroller: bsctestnet.UNITROLLER,
+    isLegacyPool: true,
+  },
+  interestRateModel: {
+    model: "jump",
+    baseRatePerYear: "0",
+    multiplierPerYear: "0.02",
+    jumpMultiplierPerYear: "0.04",
+    kink: "0.8",
+  },
+  riskParameters: {
+    collateralFactor: parseUnits("0.65", 18),
+    liquidationThreshold: parseUnits("0.65", 18),
+    liquidationIncentive: parseUnits("1.1", 18),
+    reserveFactor: parseUnits("0.2", 18),
+    supplyCap: parseUnits("200", 18),
+    borrowCap: parseUnits("0", 18),
+  },
+  initialSupply: {
+    amount: parseUnits("0.025", 18),
+    vTokenReceiver: bsctestnet.VTREASURY,
+    vTokensToBurn: parseUnits("0.02", 8),
+  },
+};
+
+export const convertAmountToVTokens = (amount: BigNumber, exchangeRate: BigNumber) => {
+  const EXP_SCALE = parseUnits("1", 18);
+  return amount.mul(EXP_SCALE).div(exchangeRate);
+};
+
+const vTokensMinted = convertAmountToVTokens(marketSpecs.initialSupply.amount, marketSpecs.vToken.exchangeRate);
+export const vTokensRemaining = vTokensMinted.sub(marketSpecs.initialSupply.vTokensToBurn);
+
+const configureConverters = (fromAssets: string[], incentive: BigNumberish = CONVERSION_INCENTIVE) => {
+  enum ConversionAccessibility {
+    NONE = 0,
+    ALL = 1,
+    ONLY_FOR_CONVERTERS = 2,
+    ONLY_FOR_USERS = 3,
+  }
+
+  return Object.entries(converterBaseAssets).map(([converter, baseAsset]: [string, string]) => {
+    const conversionConfigs = fromAssets.map(() => [incentive, ConversionAccessibility.ALL]);
+    return {
+      target: converter,
+      signature: "setConversionConfigs(address,address[],(uint256,uint8)[])",
+      params: [baseAsset, fromAssets, conversionConfigs],
+    };
+  });
+};
+
+export const vip615 = () => {
+  const meta = {
+    version: "v2",
+    title: "VIP-615 [BNB Chain] Add XAUM (Matrixdock Gold) market to the Core pool",
+    description: "VIP-615 [BNB Chain] Add XAUM (Matrixdock Gold) market to the Core pool",
+    forDescription: "I agree that Venus Protocol should proceed with this proposal",
+    againstDescription: "I do not think that Venus Protocol should proceed with this proposal",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds or not",
+  };
+
+  return makeProposal(
+    [
+      {
+        target: CHAINLINK_ORACLE,
+        signature: "setDirectPrice(address,uint256)",
+        params: [marketSpecs.vToken.underlying.address, parseUnits("1", 18)],
+      },
+      {
+        target: RESILIENT_ORACLE,
+        signature: "setTokenConfig((address,address[3],bool[3],bool))",
+        params: [
+          [
+            marketSpecs.vToken.underlying.address,
+            [CHAINLINK_ORACLE, ethers.constants.AddressZero, ethers.constants.AddressZero],
+            [true, false, false],
+            false,
+          ],
+        ],
+      },
+
+      // Add Market
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "_supportMarket(address)",
+        params: [marketSpecs.vToken.address],
+      },
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[marketSpecs.vToken.address], [marketSpecs.riskParameters.supplyCap]],
+      },
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [[marketSpecs.vToken.address], [marketSpecs.riskParameters.borrowCap]],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "setAccessControlManager(address)",
+        params: [bsctestnet.ACCESS_CONTROL_MANAGER],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "setProtocolShareReserve(address)",
+        params: [PROTOCOL_SHARE_RESERVE],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "setReduceReservesBlockDelta(uint256)",
+        params: [REDUCE_RESERVES_BLOCK_DELTA],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "_setReserveFactor(uint256)",
+        params: [marketSpecs.riskParameters.reserveFactor],
+      },
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "setCollateralFactor(address,uint256,uint256)",
+        params: [
+          marketSpecs.vToken.address,
+          marketSpecs.riskParameters.collateralFactor,
+          marketSpecs.riskParameters.liquidationThreshold,
+        ],
+      },
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "setLiquidationIncentive(address,uint256)",
+        params: [marketSpecs.vToken.address, marketSpecs.riskParameters.liquidationIncentive],
+      },
+
+      // initial liquidity
+      {
+        target: XAUM,
+        signature: "faucet(uint256)",
+        params: [marketSpecs.initialSupply.amount],
+      },
+      {
+        target: marketSpecs.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [marketSpecs.vToken.address, marketSpecs.initialSupply.amount],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "mint(uint256)",
+        params: [marketSpecs.initialSupply.amount],
+      },
+      {
+        target: marketSpecs.vToken.underlying.address,
+        signature: "approve(address,uint256)",
+        params: [marketSpecs.vToken.address, 0],
+      },
+      // Burn some vTokens to prevents exchange rate manipulation at market launch.
+      {
+        target: marketSpecs.vToken.address,
+        signature: "transfer(address,uint256)",
+        params: [ethers.constants.AddressZero, marketSpecs.initialSupply.vTokensToBurn],
+      },
+      // Transfer leftover vTokens to receiver
+      {
+        target: marketSpecs.vToken.address,
+        signature: "transfer(address,uint256)",
+        params: [marketSpecs.initialSupply.vTokenReceiver, vTokensRemaining],
+      },
+
+      // Pause Borrow actions for vXAUM market
+      {
+        target: marketSpecs.vToken.comptroller,
+        signature: "_setActionsPaused(address[],uint8[],bool)",
+        params: [[marketSpecs.vToken.address], [2], true],
+      },
+      {
+        target: marketSpecs.vToken.address,
+        signature: "setFlashLoanEnabled(bool)",
+        params: [true],
+      },
+
+      // Configure converters
+      ...configureConverters([marketSpecs.vToken.underlying.address]),
+    ],
+    meta,
+    ProposalType.REGULAR,
+  );
+};
+
+export default vip615;
